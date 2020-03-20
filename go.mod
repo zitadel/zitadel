f6801200a96301e6f7e857c847e603061939b548
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-03-20 17:13:53 +0100

    feat: add admin proto

--- go.mod
+++ go.mod
@@ -4,9 +4,8 @@ go 1.14
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/caos/citadel v0.37.8 // indirect
 	github.com/caos/logging v0.0.0-20191210002624-b3260f690a6a
-	github.com/caos/utils v0.0.0-20200305060859-ac2fa70f313e // indirect
 	github.com/ghodss/yaml v1.0.0
 	golang.org/x/sys v0.0.0-20200317113312-5766fd39f98d // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
---
+++ pkg/admin/api/grpc/generate.go
@@ -0,0 +1,7 @@
+package grpc
+
+//go:generate protoc -I$GOPATH/src -I../proto -I${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate --go_out=plugins=grpc:$GOPATH/src ../proto/org.proto
+//go:generate protoc -I$GOPATH/src -I../proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate -I${GOPATH}/src/github.com/caos/citadel/utils/protoc/protoc-gen-authoption --go_out=plugins=grpc:${GOPATH}/src --grpc-gateway_out=logtostderr=true:${GOPATH}/src --swagger_out=logtostderr=true:. --authoption_out=. ../proto/admin.proto
+//go:generate mockgen -package api -destination ./mock/admin.proto.mock.go github.com/caos/citadel/admin/api/grpc AdminServiceClient
+
+//go:generate ../../../console/etc/generate-grpc.sh
