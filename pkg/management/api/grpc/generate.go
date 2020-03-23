e55b137455afbe861a591b37004e2afc41493426
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-03-23 16:56:02 +0100

    fix: generate proto files

--- pkg/management/api/grpc/generate.go
+++ pkg/management/api/grpc/generate.go
@@ -1,6 +1,6 @@
 package grpc
 
 //go:generate protoc -I$GOPATH/src -I../proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate -I${GOPATH}/src/github.com/caos/zitadel/internal/protoc/protoc-gen-authoption --go_out=plugins=grpc:$GOPATH/src --grpc-gateway_out=logtostderr=true:$GOPATH/src --swagger_out=logtostderr=true:. --authoption_out=. ../proto/management.proto
-//go:generate mockgen -package api -destination ./mock/management.proto.mock.go github.com/caos/zitadel/management/api/grpc ManagementServiceClient
+//go:generate mockgen -package api -destination ./mock/management.proto.mock.go github.com/caos/zitadel/pkg/management/api/grpc ManagementServiceClient
 
 //go:generate ../../../console/etc/generate-grpc.sh
