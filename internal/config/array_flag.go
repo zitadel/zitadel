eb1d22c6a3fc2b424aa8d0e7636698477aa4a58a
Livio Amstutz <livio.a@gmail.com>
2020-03-27 13:44:50 +0100

    clarify array_flag.go

--- internal/config/array_flag.go
+++ internal/config/array_flag.go
@@ -1,7 +1,14 @@
 package config
 
-import "strings"
+import (
+	"flag"
+	"strings"
+)
 
+var _ flag.Value = (*ArrayFlags)(nil)
+
+//ArrayFlags implements the flag/Value interface
+//allowing to set multiple string flags with the same name
 type ArrayFlags []string
 
 func (i *ArrayFlags) String() string {
