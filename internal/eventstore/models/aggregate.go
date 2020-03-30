a5e037c01f7ef8076146a15136725da66a3c0d2c
adlerhurst <silvan.reusser@gmail.com>
2020-03-30 13:26:49 +0200

    version

--- internal/eventstore/models/aggregate.go
+++ internal/eventstore/models/aggregate.go
@@ -1,17 +1,27 @@
 package models
 
-import "github.com/caos/eventstore-lib/pkg/models"
+import (
+	"regexp"
+
+	"github.com/caos/eventstore-lib/pkg/models"
+	"github.com/caos/zitadel/internal/errors"
+)
 
 var _ models.Aggregate = (*Aggregate)(nil)
+
+var versionRegexp = regexp.MustCompile(`^v[0-9]+(\.[0-9]+){0,2}$`)
+
+type Version string
 
 type Aggregate struct {
 	id             string
 	typ            string
 	events         []*Event
 	latestSequence uint64
+	version        Version
 }
 
-func NewAggregate(id, typ string, latestSequence uint64, events ...*Event) *Aggregate {
+func NewAggregate(id, typ, version string, latestSequence uint64, events ...*Event) *Aggregate {
 	return &Aggregate{id: id, typ: typ, events: events, latestSequence: latestSequence}
 }
 
@@ -34,4 +44,24 @@ func (a *Aggregate) Events() models.Events {
 
 func (a *Aggregate) LatestSequence() uint64 {
 	return a.latestSequence
+}
+
+func (a *Aggregate) Validate() error {
+	if a.id == "" {
+		return errors.ThrowPreconditionFailed(nil, "MODEL-FSjKV", "id not set")
+	}
+	if a.typ == "" {
+		return errors.ThrowPreconditionFailed(nil, "MODEL-aj4t2", "type not set")
+	}
+	if len(a.events) < 1 {
+		return errors.ThrowPreconditionFailed(nil, "MODEL-PupjX", "no events set")
+	}
+	return a.version.Validate()
+}
+
+func (v Version) Validate() error {
+	if !versionRegexp.MatchString(string(v)) {
+		return errors.ThrowPreconditionFailed(nil, "MODEL-luDuS", "version is not semver")
+	}
+	return nil
 }
