07b957d0969daafc630624d6059f46901644e3ef
adlerhurst <silvan.reusser@gmail.com>
2020-03-31 02:14:52 +0200

    gnueg

--- internal/eventstore/app_test.go
+++ internal/eventstore/app_test.go
@@ -4,17 +4,16 @@ import (
 	"context"
 	"testing"
 
-	"github.com/caos/zitadel/internal/eventstore/models"
-	"github.com/caos/zitadel/internal/eventstore/repository"
+	"github.com/caos/zitadel/internal/eventstore/internal/repository"
 )
 
-func Test_app_CreateEvents(t *testing.T) {
+func Test_app_PushAggregates(t *testing.T) {
 	type fields struct {
 		repo repository.Repository
 	}
 	type args struct {
 		ctx        context.Context
-		aggregates []*models.Aggregate
+		aggregates []*Aggregate
 	}
 	tests := []struct {
 		name    string
@@ -22,14 +21,14 @@ func Test_app_CreateEvents(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		//TODO: test
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			es := &app{
 				repo: tt.fields.repo,
 			}
-			if err := es.CreateEvents(tt.args.ctx, tt.args.aggregates...); (err != nil) != tt.wantErr {
+			if _, err := es.PushAggregates(tt.args.ctx, tt.args.aggregates...); (err != nil) != tt.wantErr {
 				t.Errorf("app.CreateEvents() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
--- internal/eventstore/repository/mock/repository.go
+++
@@ -1,6 +0,0 @@
-package mock
-
-func NewMock(t *testing.T) *MockRepository {
-	m := NewMockRepository(gomock.NewController(t))
-	return nil
-}
