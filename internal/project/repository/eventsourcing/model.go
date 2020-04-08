15c03a4333d855905a8b4d68ff2fa0e002cd1e56
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-04-08 12:16:47 +0200

    fix: merge master

--- internal/project/repository/eventsourcing/model.go
+++ internal/project/repository/eventsourcing/model.go
@@ -14,11 +14,27 @@ const (
 
 type Project struct {
 	es_models.ObjectRoot
-	Name  string `json:"name,omitempty"`
-	State int32  `json:"-"`
+	Name    string           `json:"name,omitempty"`
+	State   int32            `json:"-"`
+	Members []*ProjectMember `json:"-"`
+}
+
+type ProjectMember struct {
+	es_models.ObjectRoot
+	UserID string   `json:"userId,omitempty"`
+	Roles  []string `json:"roles,omitempty"`
+}
+
+func (p *Project) Changes(changed *Project) map[string]interface{} {
+	changes := make(map[string]interface{}, 1)
+	if changed.Name != "" && p.Name != changed.Name {
+		changes["name"] = changed.Name
+	}
+	return changes
 }
 
 func ProjectFromModel(project *model.Project) *Project {
+	members := ProjectMembersFromModel(project.Members)
 	return &Project{
 		ObjectRoot: es_models.ObjectRoot{
 			ID:           project.ObjectRoot.ID,
@@ -26,12 +42,14 @@ func ProjectFromModel(project *model.Project) *Project {
 			ChangeDate:   project.ChangeDate,
 			CreationDate: project.CreationDate,
 		},
-		Name:  project.Name,
-		State: model.ProjectStateToInt(project.State),
+		Name:    project.Name,
+		State:   model.ProjectStateToInt(project.State),
+		Members: members,
 	}
 }
 
 func ProjectToModel(project *Project) *model.Project {
+	members := ProjectMembersToModel(project.Members)
 	return &model.Project{
 		ObjectRoot: es_models.ObjectRoot{
 			ID:           project.ID,
@@ -39,8 +57,51 @@ func ProjectToModel(project *Project) *model.Project {
 			CreationDate: project.CreationDate,
 			Sequence:     project.Sequence,
 		},
-		Name:  project.Name,
-		State: model.ProjectStateFromInt(project.State),
+		Name:    project.Name,
+		State:   model.ProjectStateFromInt(project.State),
+		Members: members,
+	}
+}
+
+func ProjectMembersToModel(members []*ProjectMember) []*model.ProjectMember {
+	convertedMembers := make([]*model.ProjectMember, len(members))
+	for i, m := range members {
+		convertedMembers[i] = ProjectMemberToModel(m)
+	}
+	return convertedMembers
+}
+
+func ProjectMembersFromModel(members []*model.ProjectMember) []*ProjectMember {
+	convertedMembers := make([]*ProjectMember, len(members))
+	for i, m := range members {
+		convertedMembers[i] = ProjectMemberFromModel(m)
+	}
+	return convertedMembers
+}
+
+func ProjectMemberFromModel(member *model.ProjectMember) *ProjectMember {
+	return &ProjectMember{
+		ObjectRoot: es_models.ObjectRoot{
+			ID:           member.ObjectRoot.ID,
+			Sequence:     member.Sequence,
+			ChangeDate:   member.ChangeDate,
+			CreationDate: member.CreationDate,
+		},
+		UserID: member.UserID,
+		Roles:  member.Roles,
+	}
+}
+
+func ProjectMemberToModel(member *ProjectMember) *model.ProjectMember {
+	return &model.ProjectMember{
+		ObjectRoot: es_models.ObjectRoot{
+			ID:           member.ID,
+			ChangeDate:   member.ChangeDate,
+			CreationDate: member.CreationDate,
+			Sequence:     member.Sequence,
+		},
+		UserID: member.UserID,
+		Roles:  member.Roles,
 	}
 }
 
@@ -50,14 +111,6 @@ func ProjectFromEvents(project *Project, events ...*es_models.Event) (*Project, 
 	}
 
 	return project, project.AppendEvents(events...)
-}
-
-func (p *Project) Changes(changed *Project) map[string]interface{} {
-	changes := make(map[string]interface{}, 1)
-	if changed.Name != "" && p.Name != changed.Name {
-		changes["name"] = changed.Name
-	}
-	return changes
 }
 
 func (p *Project) AppendEvents(events ...*es_models.Event) error {
