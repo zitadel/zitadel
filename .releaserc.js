03b8cc88423687ede8e0552bf693387810f900dd
Florian Forster <florian@caos.ch>
2020-04-15 21:13:22 +0200

    test

--- .releaserc.js
+++ .releaserc.js
@@ -1,19 +1,11 @@
 module.exports = {
     branches: ["master", "docker-semrel"],
-    verifyConditions: [
-        ["semantic-release-docker", {
-            "registryUrl": "docker.pkg.github.com",
-            }
-        ]
-    ],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/github",
-        ["semantic-release-docker", {
-            "registryUrl": "docker.pkg.github.com",
-            "name": "caos/zitadel/zitadel"
-            }
-        ],
+        ["@semantic-release/exec", {
+            "publishCmd": "echo '::set-env name=CAOS_NEXT_VERSION::${nextRelease.version}'"
+            }],
     ]
 };
