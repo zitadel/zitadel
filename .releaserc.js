e875fa4735f2a48f25a84e917be7a09c633851fa
Florian Forster <florian@caos.ch>
2020-04-15 18:23:35 +0200

    chore: debug docker semrel

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ["master"],
+    branches: ["master", "docker-semrel"],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
