93946da8f7f2f35aaa0d2e56a71690d184add33e
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-04-21 16:55:04 +0200

    fix: caos err id

--- internal/errors/caos_error_test.go
+++ internal/errors/caos_error_test.go
@@ -10,10 +10,10 @@ import (
 
 func TestErrorMethod(t *testing.T) {
 	err := caos_errs.ThrowError(nil, "id", "msg")
-	expected := "AggregateID=id Message=msg"
+	expected := "ID=id Message=msg"
 	assert.Equal(t, expected, err.Error())
 
 	err = caos_errs.ThrowError(err, "subID", "subMsg")
-	subExptected := "AggregateID=subID Message=subMsg Parent=(AggregateID=id Message=msg)"
+	subExptected := "ID=subID Message=subMsg Parent=(ID=id Message=msg)"
 	assert.Equal(t, subExptected, err.Error())
 }
