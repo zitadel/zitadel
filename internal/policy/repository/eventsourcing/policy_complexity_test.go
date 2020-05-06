38e215bb4e4e70b4393057bda16cd56f0c77b724
Michael Wäger <michael@caos.ch>
2020-05-06 15:29:21 +0200

    Init tests

---
+++ internal/policy/repository/eventsourcing/policy_complexity_test.go
@@ -0,0 +1,57 @@
+package eventsourcing
+
+import (
+	"testing"
+
+	caos_errs "github.com/caos/zitadel/internal/errors"
+)
+
+func TestGetPasswordComplexityPolicyQuery(t *testing.T) {
+	type args struct {
+		recourceOwner string
+		sequence      uint64
+	}
+	type res struct {
+		filterLen int
+		wantErr   bool
+		errFunc   func(err error) bool
+	}
+	tests := []struct {
+		name string
+		args args
+		res  res
+	}{
+		{
+			name: "Get password complexity policy query ok",
+			args: args{
+				recourceOwner: "org",
+				sequence:      0,
+			},
+			res: res{
+				filterLen: 3,
+			},
+		},
+		{
+			name: "Get password complexity policy query, no org",
+			args: args{
+				sequence: 1,
+			},
+			res: res{
+				filterLen: 3,
+				wantErr:   true,
+				errFunc:   caos_errs.IsPreconditionFailed,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			query := PasswordComplexityPolicyQuery(tt.args.recourceOwner, tt.args.sequence)
+			if !tt.res.wantErr && query == nil {
+				t.Errorf("query should not be nil")
+			}
+			if !tt.res.wantErr && len(query.Filters) != tt.res.filterLen {
+				t.Errorf("got wrong filter len: expected: %v, actual: %v ", tt.res.filterLen, len(query.Filters))
+			}
+		})
+	}
+}
