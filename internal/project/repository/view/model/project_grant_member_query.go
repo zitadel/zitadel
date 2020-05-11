561794ae02c6b800ecb9ed3c7774663c06ab8452
Fabi <38692350+fgerschwiler@users.noreply.github.com>
2020-05-11 11:51:32 +0200

    Update internal/project/repository/view/model/project_grant_member_query.go
    Co-authored-by: Silvan <silvan.reusser@gmail.com>

--- internal/project/repository/view/model/project_grant_member_query.go
+++ internal/project/repository/view/model/project_grant_member_query.go
@@ -30,9 +30,9 @@ func (req ProjectGrantMemberSearchRequest) GetAsc() bool {
 }
 
 func (req ProjectGrantMemberSearchRequest) GetQueries() []view.SearchQuery {
-	result := make([]view.SearchQuery, 0)
-	for _, q := range req.Queries {
-		result = append(result, ProjectGrantMemberSearchQuery{Key: q.Key, Value: q.Value, Method: q.Method})
+	result := make([]view.SearchQuery, len(req.Queries))
+	for i, q := range req.Queries {
+		result[i] = ProjectGrantMemberSearchQuery{Key: q.Key, Value: q.Value, Method: q.Method}
 	}
 	return result
 }
