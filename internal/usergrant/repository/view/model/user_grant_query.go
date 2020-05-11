7e83dce10db9d069b89ba50d3b392acc9638d804
Fabi <38692350+fgerschwiler@users.noreply.github.com>
2020-05-11 16:22:02 +0200

    Update internal/usergrant/repository/view/model/user_grant_query.go
    Co-authored-by: Livio Amstutz <livio.a@gmail.com>

--- internal/usergrant/repository/view/model/user_grant_query.go
+++ internal/usergrant/repository/view/model/user_grant_query.go
@@ -30,9 +30,9 @@ func (req UserGrantSearchRequest) GetAsc() bool {
 }
 
 func (req UserGrantSearchRequest) GetQueries() []view.SearchQuery {
-	result := make([]view.SearchQuery, 0)
-	for _, q := range req.Queries {
-		result = append(result, UserGrantSearchQuery{Key: q.Key, Value: q.Value, Method: q.Method})
+	result := make([]view.SearchQuery, len(req.Queries))
+	for i, q := range req.Queries {
+		result[i] = UserGrantSearchQuery{Key: q.Key, Value: q.Value, Method: q.Method}
 	}
 	return result
 }
