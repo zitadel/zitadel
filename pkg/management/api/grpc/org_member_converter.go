79d4923190d30856dd826a6f29721437a699f6f2
adlerhurst <silvan.reusser@gmail.com>
2020-05-12 17:43:43 +0200

    return member on add and change

--- pkg/management/api/grpc/org_member_converter.go
+++ pkg/management/api/grpc/org_member_converter.go
@@ -1,6 +1,10 @@
 package grpc
 
-import org_model "github.com/caos/zitadel/internal/org/model"
+import (
+	"github.com/caos/logging"
+	org_model "github.com/caos/zitadel/internal/org/model"
+	"github.com/golang/protobuf/ptypes"
+)
 
 func addOrgMemberToModel(member *AddOrgMemberRequest) *org_model.OrgMember {
 	memberModel := org_model.NewOrgMember(member.OrgId, member.UserId)
@@ -14,4 +18,20 @@ func changeOrgMemberToModel(member *ChangeOrgMemberRequest) *org_model.OrgMember
 	memberModel.Roles = member.Roles
 
 	return memberModel
+}
+
+func orgMemberFromModel(member *org_model.OrgMember) *OrgMember {
+	creationDate, err := ptypes.TimestampProto(member.CreationDate)
+	logging.Log("GRPC-jC5wY").OnError(err).Debug("date parse failed")
+
+	changeDate, err := ptypes.TimestampProto(member.ChangeDate)
+	logging.Log("GRPC-Nc2jJ").OnError(err).Debug("date parse failed")
+
+	return &OrgMember{
+		UserId:       member.UserID,
+		CreationDate: creationDate,
+		ChangeDate:   changeDate,
+		Roles:        member.Roles,
+		Sequence:     member.Sequence,
+	}
 }
