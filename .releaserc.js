229d3d0ed1b664a0f378083e7eafcc342b8a4acc
Florian Forster <florian@caos.ch>
2020-05-13 23:13:02 +0200

    artifact paths

--- .releaserc.js
+++ .releaserc.js
@@ -1,13 +1,13 @@
 module.exports = {
-    branches: ["master", "matrix"],
+    branches: ["master"],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         ["@semantic-release/github", {
             "assets": [
-              {"path": ".artifacts/zitadel-darwin-amd64", "label": "Darwin x86_64"},
-              {"path": ".artifacts/zitadel-linux-amd64", "label": "Linux x86_64"},
-              {"path": ".artifacts/zitadel-windows-amd64", "label": "Windows x86_64"}
+              {"path": ".artifacts/zitadel-darwin-amd64/zitadel-darwin-amd64", "label": "Darwin x86_64"},
+              {"path": ".artifacts/zitadel-linux-amd64/zitadel-linux-amd64", "label": "Linux x86_64"},
+              {"path": ".artifacts/zitadel-windows-amd64/zitadel-windows-amd64", "label": "Windows x86_64"}
             ]
         }],
         ["@semantic-release/exec", {
