c2acd514dbe4ebbb957e0764f62d570f1c8decac
Livio Amstutz <livio.a@gmail.com>
2020-05-20 11:10:14 +0200

    add missing exports

--- cmd/zitadel/caos_local.sh
+++ cmd/zitadel/caos_local.sh
@@ -27,3 +27,5 @@ export ZITADEL_ISSUER=http://localhost:50022
 export ZITADEL_ACCOUNTS=http://localhost:50030
 export ZITADEL_AUTHORIZE=http://localhost:50022
 export ZITADEL_OAUTH=http://localhost:50022
+export ZITADEL_CONSOLE=http://localhost:4200
+export CAOS_OIDC_DEV=true
