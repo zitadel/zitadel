3afb4fce794bd854f65225fa5c16203161619acb
Livio Amstutz <livio.a@gmail.com>
2020-05-25 16:08:02 +0200

    fix MfaTypesSetupPossible

--- internal/user/model/user_view.go
+++ internal/user/model/user_view.go
@@ -88,6 +88,8 @@ func (r *UserSearchRequest) AppendMyOrgQuery(orgID string) {
 func (u *UserView) MfaTypesSetupPossible(level req_model.MfaLevel) []req_model.MfaType {
 	types := make([]req_model.MfaType, 0)
 	switch level {
+	default:
+		fallthrough
 	case req_model.MfaLevelSoftware:
 		if u.OTPState != MFASTATE_READY {
 			types = append(types, req_model.MfaTypeOTP)
