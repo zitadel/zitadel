ce9af9ad610e647e974723462d35f0914d8bd93e
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-06-03 07:14:23 +0200

    fix tests

--- internal/org/repository/eventsourcing/model/org_model_test.go
+++ internal/org/repository/eventsourcing/model/org_model_test.go
@@ -11,12 +11,12 @@ import (
 func TestOrgFromEvents(t *testing.T) {
 	type args struct {
 		event []*es_models.Event
-		org   *model2.Org
+		org   *Org
 	}
 	tests := []struct {
 		name   string
 		args   args
-		result *model2.Org
+		result *Org
 	}{
 		{
 			name: "org from events, ok",
@@ -24,9 +24,9 @@ func TestOrgFromEvents(t *testing.T) {
 				event: []*es_models.Event{
 					{AggregateID: "ID", Sequence: 1, Type: OrgAdded},
 				},
-				org: &model2.Org{Name: "OrgName"},
+				org: &Org{Name: "OrgName"},
 			},
-			result: &model2.Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_ACTIVE), Name: "OrgName"},
+			result: &Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_ACTIVE), Name: "OrgName"},
 		},
 		{
 			name: "org from events, nil org",
@@ -36,7 +36,7 @@ func TestOrgFromEvents(t *testing.T) {
 				},
 				org: nil,
 			},
-			result: &model2.Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_ACTIVE)},
+			result: &Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_ACTIVE)},
 		},
 	}
 	for _, tt := range tests {
@@ -45,7 +45,7 @@ func TestOrgFromEvents(t *testing.T) {
 				data, _ := json.Marshal(tt.args.org)
 				tt.args.event[0].Data = data
 			}
-			result, _ := model2.OrgFromEvents(tt.args.org, tt.args.event...)
+			result, _ := OrgFromEvents(tt.args.org, tt.args.event...)
 			if result.Name != tt.result.Name {
 				t.Errorf("got wrong result name: expected: %v, actual: %v ", tt.result.Name, result.Name)
 			}
@@ -56,42 +56,42 @@ func TestOrgFromEvents(t *testing.T) {
 func TestAppendEvent(t *testing.T) {
 	type args struct {
 		event *es_models.Event
-		org   *model2.Org
+		org   *Org
 	}
 	tests := []struct {
 		name   string
 		args   args
-		result *model2.Org
+		result *Org
 	}{
 		{
 			name: "append added event",
 			args: args{
 				event: &es_models.Event{AggregateID: "ID", Sequence: 1, Type: OrgAdded},
-				org:   &model2.Org{Name: "OrgName"},
+				org:   &Org{Name: "OrgName"},
 			},
-			result: &model2.Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_ACTIVE), Name: "OrgName"},
+			result: &Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_ACTIVE), Name: "OrgName"},
 		},
 		{
 			name: "append change event",
 			args: args{
 				event: &es_models.Event{AggregateID: "ID", Sequence: 1, Type: OrgChanged, Data: []byte(`{"domain": "OrgDomain"}`)},
-				org:   &model2.Org{Name: "OrgName", Domain: "asdf"},
+				org:   &Org{Name: "OrgName", Domain: "asdf"},
 			},
-			result: &model2.Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_ACTIVE), Name: "OrgName", Domain: "OrgDomain"},
+			result: &Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_ACTIVE), Name: "OrgName", Domain: "OrgDomain"},
 		},
 		{
 			name: "append deactivate event",
 			args: args{
 				event: &es_models.Event{AggregateID: "ID", Sequence: 1, Type: OrgDeactivated},
 			},
-			result: &model2.Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_INACTIVE)},
+			result: &Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_INACTIVE)},
 		},
 		{
 			name: "append reactivate event",
 			args: args{
 				event: &es_models.Event{AggregateID: "ID", Sequence: 1, Type: OrgReactivated},
 			},
-			result: &model2.Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_ACTIVE)},
+			result: &Org{ObjectRoot: es_models.ObjectRoot{AggregateID: "ID"}, State: int32(model.ORGSTATE_ACTIVE)},
 		},
 	}
 	for _, tt := range tests {
@@ -100,7 +100,7 @@ func TestAppendEvent(t *testing.T) {
 				data, _ := json.Marshal(tt.args.org)
 				tt.args.event.Data = data
 			}
-			result := &model2.Org{}
+			result := &Org{}
 			result.AppendEvent(tt.args.event)
 			if result.State != tt.result.State {
 				t.Errorf("got wrong result state: expected: %v, actual: %v ", tt.result.State, result.State)
@@ -117,8 +117,8 @@ func TestAppendEvent(t *testing.T) {
 
 func TestChanges(t *testing.T) {
 	type args struct {
-		existing *model2.Org
-		new      *model2.Org
+		existing *Org
+		new      *Org
 	}
 	type res struct {
 		changesLen int
@@ -131,8 +131,8 @@ func TestChanges(t *testing.T) {
 		{
 			name: "org name changes",
 			args: args{
-				existing: &model2.Org{Name: "Name"},
-				new:      &model2.Org{Name: "NameChanged"},
+				existing: &Org{Name: "Name"},
+				new:      &Org{Name: "NameChanged"},
 			},
 			res: res{
 				changesLen: 1,
@@ -141,8 +141,8 @@ func TestChanges(t *testing.T) {
 		{
 			name: "org domain changes",
 			args: args{
-				existing: &model2.Org{Name: "Name", Domain: "old domain"},
-				new:      &model2.Org{Name: "Name", Domain: "new domain"},
+				existing: &Org{Name: "Name", Domain: "old domain"},
+				new:      &Org{Name: "Name", Domain: "new domain"},
 			},
 			res: res{
 				changesLen: 1,
@@ -151,8 +151,8 @@ func TestChanges(t *testing.T) {
 		{
 			name: "no changes",
 			args: args{
-				existing: &model2.Org{Name: "Name"},
-				new:      &model2.Org{Name: "Name"},
+				existing: &Org{Name: "Name"},
+				new:      &Org{Name: "Name"},
 			},
 			res: res{
 				changesLen: 0,
