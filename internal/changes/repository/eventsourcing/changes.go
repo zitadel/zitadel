7e288f78147bc2f058bf26b7173a7df437ee1422
Michael Wäger <michael@caos.ch>
2020-06-09 15:07:02 +0200

    refactoring

--- internal/changes/repository/eventsourcing/changes.go
+++ internal/changes/repository/eventsourcing/changes.go
@@ -24,12 +24,12 @@ func (es *ChangesEventstore) Changes(ctx context.Context, aggregateType es_model
 	query := ChangesQuery(id, lastSequence, aggregateTypeQuery)
 
 	events, err := es.Eventstore.FilterEvents(context.Background(), query)
-	if len(events) == 0 {
-		return nil, caos_errs.ThrowNotFound(nil, "EVENT-FpQqK", "no objects found")
-	}
 	if err != nil {
 		logging.Log("EVENT-ZRffs").WithError(err).Warn("eventstore unavailable")
 		return nil, errors.ThrowInternal(err, "EVENT-328b1", "unable to get current user")
+	}
+	if len(events) == 0 {
+		return nil, caos_errs.ThrowNotFound(nil, "EVENT-FpQqK", "no objects found")
 	}
 
 	result := make([]*model.Change, 0)
