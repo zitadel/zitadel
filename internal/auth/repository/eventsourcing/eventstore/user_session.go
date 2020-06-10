136dc4b326b660202c8cb27180f9b8d0d698f71b
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-06-10 07:59:13 +0200

    fix: alway do permission check

--- internal/auth/repository/eventsourcing/eventstore/user_session.go
+++ internal/auth/repository/eventsourcing/eventstore/user_session.go
@@ -2,10 +2,11 @@ package eventstore
 
 import (
 	"context"
-	"github.com/caos/zitadel/internal/api/auth"
 	"github.com/caos/zitadel/internal/auth/repository/eventsourcing/view"
+	caos_errs "github.com/caos/zitadel/internal/errors"
 	usr_model "github.com/caos/zitadel/internal/user/model"
 	"github.com/caos/zitadel/internal/user/repository/view/model"
+	"github.com/caos/zitadel/pkg/auth/api/oidc"
 )
 
 type UserSessionRepo struct {
@@ -13,7 +14,11 @@ type UserSessionRepo struct {
 }
 
 func (repo *UserSessionRepo) GetMyUserSessions(ctx context.Context) ([]*usr_model.UserSessionView, error) {
-	userSessions, err := repo.View.UserSessionsByUserID(auth.GetCtxData(ctx).UserID)
+	agentID, ok := oidc.UserAgentIDFromCtx(ctx)
+	if !ok {
+		return nil, caos_errs.ThrowInternal(nil, "EVENT-s8kWs", "Could not read agentid")
+	}
+	userSessions, err := repo.View.UserSessionsByAgentID(agentID)
 	if err != nil {
 		return nil, err
 	}
