42beae775d96aaf4409ed9e36fb73b7be778ab21
Max Peintner <max@caos.ch>
2020-06-10 15:30:05 +0200

    lint

--- console/src/app/pages/projects/granted-project-detail/granted-project-detail.component.spec.ts
+++ console/src/app/pages/projects/granted-project-detail/granted-project-detail.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GrantedProjectDetailComponent } from './granted-project-detail.component';
 
 describe('GrantedProjectDetailComponent', () => {
-  let component: GrantedProjectDetailComponent;
-  let fixture: ComponentFixture<GrantedProjectDetailComponent>;
+    let component: GrantedProjectDetailComponent;
+    let fixture: ComponentFixture<GrantedProjectDetailComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GrantedProjectDetailComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [GrantedProjectDetailComponent],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GrantedProjectDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(GrantedProjectDetailComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
