8dd6082b17d5bb0114a6d2f5baf51ed85a529dd5
Fabi <38692350+fgerschwiler@users.noreply.github.com>
2020-06-15 14:57:19 +0200

    feat: add project creator role on register user (#222)
    * feat: add project creator role on register user
    
    * fix: better error message
    
    * fix: tests

--- internal/auth/repository/user.go
+++ internal/auth/repository/user.go
@@ -2,12 +2,13 @@ package repository
 
 import (
 	"context"
+	org_model "github.com/caos/zitadel/internal/org/model"
 
 	"github.com/caos/zitadel/internal/user/model"
 )
 
 type UserRepository interface {
-	Register(ctx context.Context, user *model.User, resourceOwner string) (*model.User, error)
+	Register(ctx context.Context, user *model.User, member *org_model.OrgMember, resourceOwner string) (*model.User, error)
 
 	myUserRepo
 	SkipMfaInit(ctx context.Context, userID string) error
