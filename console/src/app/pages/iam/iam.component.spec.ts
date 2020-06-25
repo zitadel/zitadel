ed85ad3b97ac53aa83b70bccb8107df5ed06eac6
Max Peintner <max@caos.ch>
2020-06-25 12:49:58 +0200

    lint

--- console/src/app/pages/iam/iam.component.spec.ts
+++ console/src/app/pages/iam/iam.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IamComponent } from './iam.component';
 
 describe('IamComponent', () => {
-  let component: IamComponent;
-  let fixture: ComponentFixture<IamComponent>;
+    let component: IamComponent;
+    let fixture: ComponentFixture<IamComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ IamComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [IamComponent],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(IamComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(IamComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- console/src/app/pages/iam/iam.component.ts
+++ console/src/app/pages/iam/iam.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
     selector: 'app-iam',
     templateUrl: './iam.component.html',
-    styleUrls: ['./iam.component.scss']
+    styleUrls: ['./iam.component.scss'],
 })
 export class IamComponent implements OnInit {
 
-    constructor(private adminService: AdminService) {
+    constructor() {
 
     }
 
