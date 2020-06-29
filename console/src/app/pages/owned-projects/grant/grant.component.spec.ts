ab959dd94bb571303a1f221af7680ffcf8cdab07
Max Peintner <max@caos.ch>
2020-06-29 17:00:26 +0200

    lint

--- console/src/app/pages/owned-projects/grant/grant.component.spec.ts
+++ console/src/app/pages/owned-projects/grant/grant.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GrantComponent } from './grant.component';
 
 describe('GrantComponent', () => {
-  let component: GrantComponent;
-  let fixture: ComponentFixture<GrantComponent>;
+    let component: GrantComponent;
+    let fixture: ComponentFixture<GrantComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GrantComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [GrantComponent],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GrantComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(GrantComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
