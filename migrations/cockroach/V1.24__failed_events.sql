69c5ab5495b16eb39aa8754cad4ce5d94bb0ed15
Livio Amstutz <livio.a@gmail.com>
2020-06-29 10:38:52 +0200

    Update V1.24__failed_events.sql

--- migrations/cockroach/V1.24__failed_events.sql
+++ migrations/cockroach/V1.24__failed_events.sql
@@ -1,9 +1,5 @@
-BEGIN;
-
 ALTER TABLE management.failed_event RENAME TO management.failed_events;
 ALTER TABLE auth.failed_event RENAME TO auth.failed_events;
 ALTER TABLE notification.failed_event RENAME TO notification.failed_events;
 ALTER TABLE authz.failed_event RENAME TO authz.failed_events;
-ALTER TABLE admin.failed_event RENAME TO admin.failed_events;
-
-COMMIT;
+ALTER TABLE admin_api.failed_event RENAME TO admin_api.failed_events;
