be66caabc58476234a2384c9c7816cc90a17fb9b
Max Peintner <max@caos.ch>
2020-06-30 15:41:39 +0200

    lint

--- console/src/app/modules/avatar/avatar.component.ts
+++ console/src/app/modules/avatar/avatar.component.ts
@@ -29,7 +29,24 @@ export class AvatarComponent implements OnInit {
     }
 
     getColor(userName: string): string {
-        const colors = ["#e51c23", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#5677fc", "#03a9f4", "#00bcd4", "#009688", "#259b24", "#8bc34a", "#afb42b", "#ff9800", "#ff5722", "#795548", "#607d8b"];
+        const colors = [
+            '#e51c23',
+            '#e91e63',
+            '#9c27b0',
+            '#673ab7',
+            '#3f51b5',
+            '#5677fc',
+            '#03a9f4',
+            '#00bcd4',
+            '#009688',
+            '#259b24',
+            '#8bc34a',
+            '#afb42b',
+            '#ff9800',
+            '#ff5722',
+            '#795548',
+            '#607d8b',
+        ];
 
         let hash = 0;
         if (userName.length === 0) {
