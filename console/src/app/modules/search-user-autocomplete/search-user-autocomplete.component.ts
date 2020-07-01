8066cd32e1218e98c882d8d61052285d4c4835b3
Max Peintner <max@caos.ch>
2020-07-01 15:16:30 +0200

    ignore case searches

--- console/src/app/modules/search-user-autocomplete/search-user-autocomplete.component.ts
+++ console/src/app/modules/search-user-autocomplete/search-user-autocomplete.component.ts
@@ -52,7 +52,7 @@ export class SearchUserAutocompleteComponent {
                 const query = new UserSearchQuery();
                 query.setKey(UserSearchKey.USERSEARCHKEY_EMAIL);
                 query.setValue(value);
-                query.setMethod(SearchMethod.SEARCHMETHOD_CONTAINS);
+                query.setMethod(SearchMethod.SEARCHMETHOD_CONTAINS_IGNORE_CASE);
                 if (this.target === UserTarget.SELF) {
                     return from(this.userService.SearchUsers(10, 0, [query]));
                 } else {
