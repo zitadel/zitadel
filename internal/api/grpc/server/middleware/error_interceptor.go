e5e4981867d43f5ae4805a4d4a5221e3714c63d4
adlerhurst <silvan.reusser@gmail.com>
2020-07-01 11:38:55 +0200

    feat(translations): event type translations

--- internal/api/grpc/server/middleware/error_interceptor.go
+++ internal/api/grpc/server/middleware/error_interceptor.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+
 	"github.com/caos/logging"
 	"github.com/caos/zitadel/internal/i18n"
 	"github.com/rakyll/statik/fs"
@@ -17,13 +18,13 @@ func ErrorHandler(defaultLanguage language.Tag) func(ctx context.Context, req in
 	dir, err := fs.NewWithNamespace("zitadel")
 	logging.Log("ERROR-7usEW").OnError(err).Panic("unable to get zitadel namespace")
 
-	i18n, err := i18n.NewTranslator(dir, i18n.TranslatorConfig{DefaultLanguage: defaultLanguage})
+	translator, err := i18n.NewTranslator(dir, i18n.TranslatorConfig{DefaultLanguage: defaultLanguage})
 	if err != nil {
 		logging.Log("ERROR-Sk8sf").OnError(err).Panic("unable to get i18n translator")
 	}
 
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		resp, err := handler(ctx, req)
-		return resp, grpc_util.CaosToGRPCError(err, ctx, i18n)
+		return resp, grpc_util.CaosToGRPCError(err, ctx, translator)
 	}
 }
