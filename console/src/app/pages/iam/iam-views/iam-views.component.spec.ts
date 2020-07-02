8d885afd6048bd1d5ab375a15317552ecb3f39e8
Max Peintner <max@caos.ch>
2020-07-02 17:06:45 +0200

    lint

--- console/src/app/pages/iam/iam-views/iam-views.component.spec.ts
+++ console/src/app/pages/iam/iam-views/iam-views.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IamViewsComponent } from './iam-views.component';
 
 describe('IamViewsComponent', () => {
-  let component: IamViewsComponent;
-  let fixture: ComponentFixture<IamViewsComponent>;
+    let component: IamViewsComponent;
+    let fixture: ComponentFixture<IamViewsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ IamViewsComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [IamViewsComponent],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(IamViewsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(IamViewsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
