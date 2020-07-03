32f7f6f64c25f0c7d07e96a0b4a2dcafb320b89c
Florian Forster <florian@caos.ch>
2020-07-03 16:35:50 +0200

    chore: build file for grpc-web

--- build/console/generate-grpc.sh
+++ build/console/generate-grpc.sh
@@ -2,8 +2,6 @@
 
 set -eux
 
-GOPATH=${GOPATH:-~/go}
-
 GEN_PATH=console/src/app/proto/generated
 
 echo "Remove old files"
@@ -12,9 +10,10 @@ rm -rf $GEN_PATH
 echo "Create folders"
 mkdir -p $GEN_PATH
 
-echo "Go Get additional protos files"
-go get github.com/envoyproxy/protoc-gen-validate
-go get github.com/grpc-ecosystem/grpc-gateway
+echo "Download additional protofiles"
+wget -P tmp/validate https://raw.githubusercontent.com/envoyproxy/protoc-gen-validate/v0.4.0/validate/validate.proto
+wget -P tmp/protoc-gen-swagger/options https://raw.githubusercontent.com/grpc-ecosystem/grpc-gateway/v1.14.6/protoc-gen-swagger/options/annotations.proto
+wget -P tmp/protoc-gen-swagger/options https://raw.githubusercontent.com/grpc-ecosystem/grpc-gateway/v1.14.6/protoc-gen-swagger/options/openapiv2.proto
 
 echo "Generate grpc"
 
@@ -25,28 +24,9 @@ protoc \
   -I=pkg/admin/api/proto \
   -I=internal/protoc/protoc-gen-authoption \
   -I=console/node_modules/google-proto-files \
-  -I=${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate \
-  -I=${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway \
+  -I=tmp \
   --js_out=import_style=commonjs,binary:$GEN_PATH \
   --grpc-web_out=import_style=commonjs+dts,mode=grpcweb:$GEN_PATH \
   pkg/management/api/proto/*.proto \
   pkg/admin/api/proto/*.proto \
   pkg/auth/api/proto/*.proto
-
-echo "Generate annotations js file (compatibility)"
-
-mkdir -p $GEN_PATH/google/api/
-touch $GEN_PATH/google/api/annotations_pb.js
-echo "export {}" > $GEN_PATH/google/api/annotations_pb.d.ts
-
-mkdir -p $GEN_PATH/validate
-touch $GEN_PATH/validate/validate_pb.js
-echo "export {}" > $GEN_PATH/validate/validate_pb.d.ts
-
-mkdir -p $GEN_PATH/protoc-gen-swagger/options
-touch $GEN_PATH/protoc-gen-swagger/options/annotations_pb.js
-echo "export {}" > $GEN_PATH/protoc-gen-swagger/options/annotations_pb.d.ts
-
-mkdir -p $GEN_PATH/authoption
-touch $GEN_PATH/authoption/options_pb.js
-echo "export {}" > $GEN_PATH/authoption/options_pb.d.ts
