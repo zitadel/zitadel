d89027d8b7cbed252163081fafb592cf89804305
Florian Forster <florian@caos.ch>
2020-07-03 16:45:38 +0200

    chore: reduce package.json

--- build/console/generate-grpc.sh
+++ build/console/generate-grpc.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-GEN_PATH=console/src/app/proto/generated
+GEN_PATH=src/app/proto/generated
 
 echo "Remove old files"
 rm -rf $GEN_PATH
@@ -19,14 +19,32 @@ echo "Generate grpc"
 
 protoc \
   -I=/usr/local/include \
-  -I=pkg/management/api/proto \
-  -I=pkg/auth/api/proto \
-  -I=pkg/admin/api/proto \
-  -I=internal/protoc/protoc-gen-authoption \
-  -I=console/node_modules/google-proto-files \
+  -I=../pkg/management/api/proto \
+  -I=../pkg/auth/api/proto \
+  -I=../pkg/admin/api/proto \
+  -I=../internal/protoc/protoc-gen-authoption \
+  -I=node_modules/google-proto-files \
   -I=tmp \
   --js_out=import_style=commonjs,binary:$GEN_PATH \
   --grpc-web_out=import_style=commonjs+dts,mode=grpcweb:$GEN_PATH \
-  pkg/management/api/proto/*.proto \
-  pkg/admin/api/proto/*.proto \
-  pkg/auth/api/proto/*.proto
+  ../pkg/management/api/proto/*.proto \
+  ../pkg/admin/api/proto/*.proto \
+  ../pkg/auth/api/proto/*.proto
+
+echo "Generate annotations js file (compatibility)"
+
+mkdir -p $GEN_PATH/google/api/
+touch $GEN_PATH/google/api/annotations_pb.js
+echo "export {}" > $GEN_PATH/google/api/annotations_pb.d.ts
+
+mkdir -p $GEN_PATH/validate
+touch $GEN_PATH/validate/validate_pb.js
+echo "export {}" > $GEN_PATH/validate/validate_pb.d.ts
+
+mkdir -p $GEN_PATH/protoc-gen-swagger/options
+touch $GEN_PATH/protoc-gen-swagger/options/annotations_pb.js
+echo "export {}" > $GEN_PATH/protoc-gen-swagger/options/annotations_pb.d.ts
+
+mkdir -p $GEN_PATH/authoption
+touch $GEN_PATH/authoption/options_pb.js
+echo "export {}" > $GEN_PATH/authoption/options_pb.d.ts
