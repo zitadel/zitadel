dbe59bb40708d88a0b08dd7d0b57c7bbaef1db25
Livio Amstutz <livio.a@gmail.com>
2020-07-07 14:56:55 +0200

    logging

--- internal/api/http/server.go
+++ internal/api/http/server.go
@@ -17,14 +17,14 @@ func Serve(ctx context.Context, handler http.Handler, port, servername string) {
 	go func() {
 		<-ctx.Done()
 		err := server.Shutdown(ctx)
-		logging.Log("HTTP-m7kBlq").OnError(err).Warnf("error during graceful shutdown of http server (%s)", servername)
+		logging.LogWithFields("HTTP-m7kBlq", "name", servername).OnError(err).Warnf("error during graceful shutdown of http server (%s)", servername)
 	}()
 
 	go func() {
 		err := server.Serve(listener)
-		logging.Log("HTTP-tBHR60").OnError(err).Panicf("http serve (%s) failed", servername)
+		logging.LogWithFields("HTTP-tBHR60", "name", servername).OnError(err).Panicf("http serve (%s) failed", servername)
 	}()
-	logging.LogWithFields("HTTP-KHh0Cb", "port", port).Infof("http server (%s) is listening", servername)
+	logging.LogWithFields("HTTP-KHh0Cb", "name", servername, "port", port).Infof("http server (%s) is listening", servername)
 }
 
 func RegisterHandler(mux *http.ServeMux, prefix string, handler http.Handler) {
