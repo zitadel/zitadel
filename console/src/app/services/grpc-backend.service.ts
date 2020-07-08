e94dee376ae8212aa384fc6a46b2add3b6b6002d
Max Peintner <max@caos.ch>
2020-07-08 14:47:59 +0200

    organize interceptors

--- console/src/app/services/grpc-backend.service.ts
+++ console/src/app/services/grpc-backend.service.ts
@@ -1,31 +1,27 @@
 import { Inject, Injectable } from '@angular/core';
 import { Metadata } from 'grpc-web';
-import {
-  GrpcDefaultHandler,
-  GrpcHandler,
-  GrpcInterceptorHandler,
-  GrpcRequestFn,
-} from './grpc-handler';
-import { GRPC_INTERCEPTORS, GrpcInterceptor } from './grpc-interceptor';
+
+import { GrpcDefaultHandler, GrpcHandler, GrpcInterceptorHandler, GrpcRequestFn } from './grpc-handler';
+import { GRPC_INTERCEPTORS, GrpcInterceptor } from './interceptors/grpc-interceptor';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class GrpcBackendService {
-  constructor(
-    @Inject(GRPC_INTERCEPTORS) private readonly interceptors: GrpcInterceptor[],
-  ) {}
+    constructor(
+        @Inject(GRPC_INTERCEPTORS) private readonly interceptors: GrpcInterceptor[],
+    ) { }
 
-  public async runRequest<TReq, TResp>(
-    requestFn: GrpcRequestFn<TReq, TResp>,
-    request: TReq,
-    metadata: Metadata = {},
-  ): Promise<TResp> {
-    const defaultHandler: GrpcHandler = new GrpcDefaultHandler(requestFn);
-    const chain = this.interceptors.reduceRight(
-      (next, interceptor) => new GrpcInterceptorHandler(next, interceptor),
-      defaultHandler,
-    );
-    return chain.handle(request, metadata);
-  }
+    public async runRequest<TReq, TResp>(
+        requestFn: GrpcRequestFn<TReq, TResp>,
+        request: TReq,
+        metadata: Metadata = {},
+    ): Promise<TResp> {
+        const defaultHandler: GrpcHandler = new GrpcDefaultHandler(requestFn);
+        const chain = this.interceptors.reduceRight(
+            (next, interceptor) => new GrpcInterceptorHandler(next, interceptor),
+            defaultHandler,
+        );
+        return chain.handle(request, metadata);
+    }
 }
