12756daa686c34916c4cc340f07cd027c45cc7b1
Max Peintner <max@caos.ch>
2020-07-17 11:37:04 +0200

    lint

--- console/src/app/modules/refresh-table/refresh-table.component.spec.ts
+++ console/src/app/modules/refresh-table/refresh-table.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RefreshTableComponent } from './refresh-table.component';
 
 describe('RefreshTableComponent', () => {
-  let component: RefreshTableComponent;
-  let fixture: ComponentFixture<RefreshTableComponent>;
+    let component: RefreshTableComponent;
+    let fixture: ComponentFixture<RefreshTableComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RefreshTableComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [RefreshTableComponent],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RefreshTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RefreshTableComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
