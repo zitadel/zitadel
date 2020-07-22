edca6c689bb82781c4809971b16628db40123e79
Max Peintner <max@caos.ch>
2020-07-22 09:59:57 +0200

    org contributors, iam contributors

--- console/src/app/pages/orgs/org-detail/org-detail.component.ts
+++ console/src/app/pages/orgs/org-detail/org-detail.component.ts
@@ -3,11 +3,22 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { Subscription } from 'rxjs';
+import { BehaviorSubject, from, Observable, of, Subscription } from 'rxjs';
+import { catchError, finalize, map } from 'rxjs/operators';
+import { CreationType, MemberCreateDialogComponent } from 'src/app/modules/add-member-dialog/member-create-dialog.component';
 import { ChangeType } from 'src/app/modules/changes/changes.component';
 import { WarnDialogComponent } from 'src/app/modules/warn-dialog/warn-dialog.component';
-import { Org, OrgDomainView, OrgMember, OrgMemberSearchResponse, OrgState } from 'src/app/proto/generated/management_pb';
+import {
+    Org,
+    OrgDomainView,
+    OrgMember,
+    OrgMemberSearchResponse,
+    OrgMemberView,
+    OrgState,
+    User,
+} from 'src/app/proto/generated/management_pb';
 import { OrgService } from 'src/app/services/org.service';
 import { ToastService } from 'src/app/services/toast.service';
 
@@ -34,11 +45,19 @@ export class OrgDetailComponent implements OnInit, OnDestroy {
     public domains: OrgDomainView.AsObject[] = [];
     public primaryDomain: string = '';
 
+    // members
+    private loadingSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    public loading$: Observable<boolean> = this.loadingSubject.asObservable();
+    public totalMemberResult: number = 0;
+    public membersSubject: BehaviorSubject<OrgMemberView.AsObject[]>
+        = new BehaviorSubject<OrgMemberView.AsObject[]>([]);
+
     constructor(
         private dialog: MatDialog,
         public translate: TranslateService,
         private orgService: OrgService,
         private toast: ToastService,
+        private router: Router,
     ) { }
 
     public ngOnInit(): void {
@@ -54,6 +73,18 @@ export class OrgDetailComponent implements OnInit, OnDestroy {
             this.org = org.toObject();
         }).catch(error => {
             this.toast.showError(error);
+        });
+
+        this.loadingSubject.next(true);
+        from(this.orgService.SearchMyOrgMembers(100, 0)).pipe(
+            map(resp => {
+                this.totalMemberResult = resp.toObject().totalResult;
+                return resp.toObject().resultList;
+            }),
+            catchError(() => of([])),
+            finalize(() => this.loadingSubject.next(false)),
+        ).subscribe(members => {
+            this.membersSubject.next(members);
         });
 
         this.orgService.SearchMyOrgDomains(0, 100).then(result => {
@@ -118,5 +149,37 @@ export class OrgDetailComponent implements OnInit, OnDestroy {
                 });
             }
         });
+    }
+
+    public openAddMember(): void {
+        const dialogRef = this.dialog.open(MemberCreateDialogComponent, {
+            data: {
+                creationType: CreationType.ORG,
+            },
+            width: '400px',
+        });
+
+        dialogRef.afterClosed().subscribe(resp => {
+            if (resp) {
+                const users: User.AsObject[] = resp.users;
+                const roles: string[] = resp.roles;
+
+                if (users && users.length && roles && roles.length) {
+                    Promise.all(users.map(user => {
+                        return this.orgService.AddMyOrgMember(user.id, roles);
+                    })).then(() => {
+                        this.toast.showInfo('ORG.TOAST.MEMBERADDED', true);
+                    }).catch(error => {
+                        this.toast.showError(error);
+                    });
+                }
+            }
+        });
+    }
+
+    public showDetail(): void {
+        if (this.org?.state === OrgState.ORGSTATE_ACTIVE) {
+            this.router.navigate(['org/members']);
+        }
     }
 }
