1904c95bc7058155f1569b9943294842fcf1d02e
Max Peintner <max@caos.ch>
2020-07-29 16:32:18 +0200

    lint

--- console/src/app/modules/detail-layout/detail-layout.component.spec.ts
+++ console/src/app/modules/detail-layout/detail-layout.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DetailLayoutComponent } from './detail-layout.component';
 
 describe('DetailLayoutComponent', () => {
-  let component: DetailLayoutComponent;
-  let fixture: ComponentFixture<DetailLayoutComponent>;
+    let component: DetailLayoutComponent;
+    let fixture: ComponentFixture<DetailLayoutComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DetailLayoutComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [DetailLayoutComponent],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DetailLayoutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DetailLayoutComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
