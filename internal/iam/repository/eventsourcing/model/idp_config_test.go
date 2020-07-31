b3e97b369ad991c31aeddd8cbc6a63d7e63c864b
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-07-31 08:12:19 +0200

    fix: tests

---
+++ internal/iam/repository/eventsourcing/model/idp_config_test.go
@@ -0,0 +1,213 @@
+package model
+
+import (
+	"encoding/json"
+	es_models "github.com/caos/zitadel/internal/eventstore/models"
+	"github.com/caos/zitadel/internal/iam/model"
+	"testing"
+)
+
+func TestIdpConfigChanges(t *testing.T) {
+	type args struct {
+		existing *IDPConfig
+		new      *IDPConfig
+	}
+	type res struct {
+		changesLen int
+	}
+	tests := []struct {
+		name string
+		args args
+		res  res
+	}{
+		{
+			name: "idp config name changes",
+			args: args{
+				existing: &IDPConfig{IDPConfigID: "IDPConfigID", Name: "Name"},
+				new:      &IDPConfig{IDPConfigID: "IDPConfigID", Name: "NameChanged"},
+			},
+			res: res{
+				changesLen: 2,
+			},
+		},
+		{
+			name: "no changes",
+			args: args{
+				existing: &IDPConfig{IDPConfigID: "IDPConfigID", Name: "Name"},
+				new:      &IDPConfig{IDPConfigID: "IDPConfigID", Name: "Name"},
+			},
+			res: res{
+				changesLen: 1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			changes := tt.args.existing.Changes(tt.args.new)
+			if len(changes) != tt.res.changesLen {
+				t.Errorf("got wrong changes len: expected: %v, actual: %v ", tt.res.changesLen, len(changes))
+			}
+		})
+	}
+}
+
+func TestAppendAddIdpConfigEvent(t *testing.T) {
+	type args struct {
+		iam   *Iam
+		idp   *IDPConfig
+		event *es_models.Event
+	}
+	tests := []struct {
+		name   string
+		args   args
+		result *Iam
+	}{
+		{
+			name: "append add idp config event",
+			args: args{
+				iam:   &Iam{},
+				idp:   &IDPConfig{Name: "IdpConfig"},
+				event: &es_models.Event{},
+			},
+			result: &Iam{IDPs: []*IDPConfig{&IDPConfig{Name: "IdpConfig"}}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.args.idp != nil {
+				data, _ := json.Marshal(tt.args.idp)
+				tt.args.event.Data = data
+			}
+			tt.args.iam.appendAddIdpConfigEvent(tt.args.event)
+			if len(tt.args.iam.IDPs) != 1 {
+				t.Errorf("got wrong result should have one app actual: %v ", len(tt.args.iam.IDPs))
+			}
+			if tt.args.iam.IDPs[0] == tt.result.IDPs[0] {
+				t.Errorf("got wrong result: expected: %v, actual: %v ", tt.result.IDPs[0], tt.args.iam.IDPs[0])
+			}
+		})
+	}
+}
+
+func TestAppendChangeIdpConfigEvent(t *testing.T) {
+	type args struct {
+		project *Iam
+		app     *IDPConfig
+		event   *es_models.Event
+	}
+	tests := []struct {
+		name   string
+		args   args
+		result *Iam
+	}{
+		{
+			name: "append change idp config event",
+			args: args{
+				project: &Iam{IDPs: []*IDPConfig{&IDPConfig{Name: "IdpConfig"}}},
+				app:     &IDPConfig{Name: "IdpConfig Change"},
+				event:   &es_models.Event{},
+			},
+			result: &Iam{IDPs: []*IDPConfig{&IDPConfig{Name: "IdpConfig Change"}}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.args.app != nil {
+				data, _ := json.Marshal(tt.args.app)
+				tt.args.event.Data = data
+			}
+			tt.args.project.appendChangeIdpConfigEvent(tt.args.event)
+			if len(tt.args.project.IDPs) != 1 {
+				t.Errorf("got wrong result should have one app actual: %v ", len(tt.args.project.IDPs))
+			}
+			if tt.args.project.IDPs[0] == tt.result.IDPs[0] {
+				t.Errorf("got wrong result: expected: %v, actual: %v ", tt.result.IDPs[0], tt.args.project.IDPs[0])
+			}
+		})
+	}
+}
+
+func TestAppendRemoveIDPEvent(t *testing.T) {
+	type args struct {
+		iam   *Iam
+		idp   *IDPConfig
+		event *es_models.Event
+	}
+	tests := []struct {
+		name   string
+		args   args
+		result *Iam
+	}{
+		{
+			name: "append remove idp config event",
+			args: args{
+				iam:   &Iam{IDPs: []*IDPConfig{&IDPConfig{IDPConfigID: "IDPConfigID", Name: "IdpConfig"}}},
+				idp:   &IDPConfig{IDPConfigID: "IDPConfigID", Name: "IdpConfig"},
+				event: &es_models.Event{},
+			},
+			result: &Iam{IDPs: []*IDPConfig{}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.args.idp != nil {
+				data, _ := json.Marshal(tt.args.idp)
+				tt.args.event.Data = data
+			}
+			tt.args.iam.appendRemoveIdpConfigEvent(tt.args.event)
+			if len(tt.args.iam.IDPs) != 0 {
+				t.Errorf("got wrong result should have no apps actual: %v ", len(tt.args.iam.IDPs))
+			}
+		})
+	}
+}
+
+func TestAppendAppStateEvent(t *testing.T) {
+	type args struct {
+		iam   *Iam
+		idp   *IDPConfig
+		event *es_models.Event
+		state model.IDPConfigState
+	}
+	tests := []struct {
+		name   string
+		args   args
+		result *Iam
+	}{
+		{
+			name: "append deactivate application event",
+			args: args{
+				iam:   &Iam{IDPs: []*IDPConfig{&IDPConfig{IDPConfigID: "IDPConfigID", Name: "IdpConfig", State: int32(model.IDPConfigStateActive)}}},
+				idp:   &IDPConfig{IDPConfigID: "IDPConfigID"},
+				event: &es_models.Event{},
+				state: model.IDPConfigStateInactive,
+			},
+			result: &Iam{IDPs: []*IDPConfig{&IDPConfig{IDPConfigID: "IDPConfigID", Name: "IdpConfig", State: int32(model.IDPConfigStateInactive)}}},
+		},
+		{
+			name: "append reactivate application event",
+			args: args{
+				iam:   &Iam{IDPs: []*IDPConfig{&IDPConfig{IDPConfigID: "IDPConfigID", Name: "IdpConfig", State: int32(model.IDPConfigStateInactive)}}},
+				idp:   &IDPConfig{IDPConfigID: "IDPConfigID"},
+				event: &es_models.Event{},
+				state: model.IDPConfigStateActive,
+			},
+			result: &Iam{IDPs: []*IDPConfig{&IDPConfig{IDPConfigID: "IDPConfigID", Name: "IdpConfig", State: int32(model.IDPConfigStateActive)}}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.args.idp != nil {
+				data, _ := json.Marshal(tt.args.idp)
+				tt.args.event.Data = data
+			}
+			tt.args.iam.appendIdpConfigStateEvent(tt.args.event, tt.args.state)
+			if len(tt.args.iam.IDPs) != 1 {
+				t.Errorf("got wrong result should have one app actual: %v ", len(tt.args.iam.IDPs))
+			}
+			if tt.args.iam.IDPs[0] == tt.result.IDPs[0] {
+				t.Errorf("got wrong result: expected: %v, actual: %v ", tt.result.IDPs[0], tt.args.iam.IDPs[0])
+			}
+		})
+	}
+}
