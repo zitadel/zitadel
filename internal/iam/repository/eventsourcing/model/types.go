f0f406102bebe5b90ca09651ae8ea796d30fd644
Livio Amstutz <livio.a@gmail.com>
2020-08-25 09:32:30 +0200

    Update types.go

--- internal/iam/repository/eventsourcing/model/types.go
+++ internal/iam/repository/eventsourcing/model/types.go
@@ -8,7 +8,7 @@ const (
 	IamSetupStarted  models.EventType = "iam.setup.started"
 	IamSetupDone     models.EventType = "iam.setup.done"
 	GlobalOrgSet     models.EventType = "iam.global.org.set"
-	IamProjectSet    models.EventType = "iam.iam.iam.set"
+	IamProjectSet    models.EventType = "iam.project.iam.set"
 	IamMemberAdded   models.EventType = "iam.member.added"
 	IamMemberChanged models.EventType = "iam.member.changed"
 	IamMemberRemoved models.EventType = "iam.member.removed"
