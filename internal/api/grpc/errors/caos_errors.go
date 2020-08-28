93a10002368949747395a0f51409817d2e2cd005
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-08-28 08:39:23 +0200

    fix: remove unused code

--- internal/api/grpc/errors/caos_errors.go
+++ internal/api/grpc/errors/caos_errors.go
@@ -55,31 +55,3 @@ func ExtractCaosError(err error) (c codes.Code, msg, id string, ok bool) {
 		return codes.Unknown, err.Error(), "", false
 	}
 }
-
-//
-//func SetErrorMessageIfCaosErr(err error) {
-//	switch caosErr := err.(type) {
-//	case *caos_errs.AlreadyExistsError:
-//		return codes.AlreadyExists, caosErr.GetMessage(), caosErr.GetID(), true
-//	case *caos_errs.DeadlineExceededError:
-//		return codes.DeadlineExceeded, caosErr.GetMessage(), caosErr.GetID(), true
-//	case *caos_errs.InternalError:
-//		return codes.Internal, caosErr.GetMessage(), caosErr.GetID(), true
-//	case *caos_errs.InvalidArgumentError:
-//		return codes.InvalidArgument, caosErr.GetMessage(), caosErr.GetID(), true
-//	case *caos_errs.NotFoundError:
-//		return codes.NotFound, caosErr.GetMessage(), caosErr.GetID(), true
-//	case *caos_errs.PermissionDeniedError:
-//		return codes.PermissionDenied, caosErr.GetMessage(), caosErr.GetID(), true
-//	case *caos_errs.PreconditionFailedError:
-//		return codes.FailedPrecondition, caosErr.GetMessage(), caosErr.GetID(), true
-//	case *caos_errs.UnauthenticatedError:
-//		return codes.Unauthenticated, caosErr.GetMessage(), caosErr.GetID(), true
-//	case *caos_errs.UnavailableError:
-//		return codes.Unavailable, caosErr.GetMessage(), caosErr.GetID(), true
-//	case *caos_errs.UnimplementedError:
-//		return codes.Unimplemented, caosErr.GetMessage(), caosErr.GetID(), true
-//	default:
-//		return codes.Unknown, err.Error(), "", false
-//	}
-//}
