824134e4e9f2c8eddeb806521c6a729aca46dab4
Max Peintner <max@caos.ch>
2020-08-28 15:27:02 +0200

    update clientid, fix interceptor

--- console/src/app/services/interceptors/auth.interceptor.ts
+++ console/src/app/services/interceptors/auth.interceptor.ts
@@ -8,18 +8,19 @@ import { StorageService } from '../storage.service';
 
 const authorizationKey = 'Authorization';
 const bearerPrefix = 'Bearer';
-
+const accessTokenStorageKey = 'access_token';
 @Injectable({ providedIn: 'root' })
 export class AuthInterceptor<TReq = unknown, TResp = unknown> implements UnaryInterceptor<TReq, TResp> {
     constructor(private authenticationService: AuthenticationService, private storageService: StorageService) { }
 
     public async intercept(request: Request<TReq, TResp>, invoker: any): Promise<UnaryResponse<TReq, TResp>> {
-        const accessToken = await this.authenticationService.authenticationChanged.pipe(
+        await this.authenticationService.authenticationChanged.pipe(
             filter((authed) => !!authed),
             first(),
         ).toPromise();
 
         const metadata = request.getMetadata();
+        const accessToken = this.storageService.getItem(accessTokenStorageKey);
         metadata[authorizationKey] = `${bearerPrefix} ${accessToken}`;
 
         return invoker(request).then((response: any) => {
