3ff5aa372f49a7dd86397014310b33472bb92534
Max Peintner <max@caos.ch>
2020-08-29 18:16:46 +0200

    feat(console): user services list and create (#663)
    * fix search user view, user detail form
    
    * rm log
    
    * machine list
    
    * generic table component
    
    * create user service
    
    * proove table for undefined values
    
    * tmp disable user link if machine
    
    * lint
    
    * lint styles
    
    * user table lint
    
    * Update console/src/assets/i18n/de.json
    
    Co-authored-by: Florian Forster <florian@caos.ch>

--- console/src/app/pages/users/user-list/user-list.component.ts
+++ console/src/app/pages/users/user-list/user-list.component.ts
@@ -1,91 +1,17 @@
-import { SelectionModel } from '@angular/cdk/collections';
-import { Component, EventEmitter, OnDestroy, Output, ViewChild } from '@angular/core';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import { UserSearchResponse, UserView } from 'src/app/proto/generated/management_pb';
-import { MgmtUserService } from 'src/app/services/mgmt-user.service';
-import { ToastService } from 'src/app/services/toast.service';
 
+export enum UserType {
+    HUMAN = 'human',
+    MACHINE = 'machine',
+}
 @Component({
     selector: 'app-user-list',
     templateUrl: './user-list.component.html',
     styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent implements OnDestroy {
-    @ViewChild(MatPaginator) public paginator!: MatPaginator;
-    public dataSource: MatTableDataSource<UserView.AsObject> = new MatTableDataSource<UserView.AsObject>();
-    public userResult!: UserSearchResponse.AsObject;
-    private loadingSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    public loading$: Observable<boolean> = this.loadingSubject.asObservable();
-    public displayedColumns: string[] = ['select', 'firstname', 'lastname', 'username', 'email', 'state'];
-    public selection: SelectionModel<UserView.AsObject> = new SelectionModel<UserView.AsObject>(true, []);
-    @Output() public changedSelection: EventEmitter<Array<UserView.AsObject>> = new EventEmitter();
+export class UserListComponent {
+    public UserType: any = UserType;
 
-    private subscription?: Subscription;
-
-    constructor(public translate: TranslateService, private route: ActivatedRoute, private userService: MgmtUserService,
-        private toast: ToastService) {
-        this.subscription = this.route.params.subscribe(() => this.getData(10, 0));
-
-        this.selection.changed.subscribe(() => {
-            this.changedSelection.emit(this.selection.selected);
-        });
-    }
-
-    public isAllSelected(): boolean {
-        const numSelected = this.selection.selected.length;
-        const numRows = this.dataSource.data.length;
-        return numSelected === numRows;
-    }
-
-    public masterToggle(): void {
-        this.isAllSelected() ?
-            this.selection.clear() :
-            this.dataSource.data.forEach(row => this.selection.select(row));
-    }
-
-    public ngOnDestroy(): void {
-        this.subscription?.unsubscribe();
-    }
-
-    public changePage(event: PageEvent): void {
-        this.getData(event.pageSize, event.pageIndex * event.pageSize);
-    }
-
-    public deactivateSelectedUsers(): void {
-        Promise.all(this.selection.selected.map(value => {
-            return this.userService.DeactivateUser(value.id);
-        })).then(() => {
-            this.toast.showInfo('USER.TOAST.SELECTEDDEACTIVATED', true);
-            this.getData(10, 0);
-        });
-    }
-
-    public reactivateSelectedUsers(): void {
-        Promise.all(this.selection.selected.map(value => {
-            return this.userService.ReactivateUser(value.id);
-        })).then(() => {
-            this.toast.showInfo('USER.TOAST.SELECTEDREACTIVATED', true);
-            this.getData(10, 0);
-        });
-    }
-
-    private async getData(limit: number, offset: number): Promise<void> {
-        this.loadingSubject.next(true);
-        this.userService.SearchUsers(limit, offset).then(resp => {
-            this.userResult = resp.toObject();
-            this.dataSource.data = this.userResult.resultList;
-            this.loadingSubject.next(false);
-        }).catch(error => {
-            this.toast.showError(error);
-            this.loadingSubject.next(false);
-        });
-    }
-
-    public refreshPage(): void {
-        this.getData(this.paginator.pageSize, this.paginator.pageIndex * this.paginator.pageSize);
-    }
+    constructor(public translate: TranslateService) { }
 }
---
+++ console/src/app/pages/users/user-list/user-table/user-table.component.ts
@@ -0,0 +1,98 @@
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { TranslateService } from '@ngx-translate/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { UserView } from 'src/app/proto/generated/auth_pb';
+import { UserSearchKey, UserSearchQuery, UserSearchResponse } from 'src/app/proto/generated/management_pb';
+import { MgmtUserService } from 'src/app/services/mgmt-user.service';
+import { ToastService } from 'src/app/services/toast.service';
+
+import { UserType } from '../user-list.component';
+
+@Component({
+    selector: 'app-user-table',
+    templateUrl: './user-table.component.html',
+    styleUrls: ['./user-table.component.scss'],
+})
+export class UserTableComponent implements OnInit {
+    public UserType: any = UserType;
+    @Input() userType: UserType = UserType.HUMAN;
+    @ViewChild(MatPaginator) public paginator!: MatPaginator;
+    public dataSource: MatTableDataSource<UserView.AsObject> = new MatTableDataSource<UserView.AsObject>();
+    public selection: SelectionModel<UserView.AsObject> = new SelectionModel<UserView.AsObject>(true, []);
+    public userResult!: UserSearchResponse.AsObject;
+    private loadingSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    public loading$: Observable<boolean> = this.loadingSubject.asObservable();
+    @Input() public displayedColumns: string[] = ['select', 'firstname', 'lastname', 'username', 'email', 'state'];
+
+    @Output() public changedSelection: EventEmitter<Array<UserView.AsObject>> = new EventEmitter();
+
+    constructor(public translate: TranslateService, private userService: MgmtUserService,
+        private toast: ToastService) {
+        this.selection.changed.subscribe(() => {
+            this.changedSelection.emit(this.selection.selected);
+        });
+    }
+
+    ngOnInit(): void {
+        this.getData(10, 0, this.userType);
+    }
+
+    public isAllSelected(): boolean {
+        const numSelected = this.selection.selected.length;
+        const numRows = this.dataSource.data.length;
+        return numSelected === numRows;
+    }
+
+    public masterToggle(): void {
+        this.isAllSelected() ?
+            this.selection.clear() :
+            this.dataSource.data.forEach(row => this.selection.select(row));
+    }
+
+
+    public changePage(event: PageEvent): void {
+        this.getData(event.pageSize, event.pageIndex * event.pageSize, this.userType);
+    }
+
+    public deactivateSelectedUsers(): void {
+        Promise.all(this.selection.selected.map(value => {
+            return this.userService.DeactivateUser(value.id);
+        })).then(() => {
+            this.toast.showInfo('USER.TOAST.SELECTEDDEACTIVATED', true);
+            this.getData(10, 0, this.userType);
+        });
+    }
+
+    public reactivateSelectedUsers(): void {
+        Promise.all(this.selection.selected.map(value => {
+            return this.userService.ReactivateUser(value.id);
+        })).then(() => {
+            this.toast.showInfo('USER.TOAST.SELECTEDREACTIVATED', true);
+            this.getData(10, 0, this.userType);
+        });
+    }
+
+    private async getData(limit: number, offset: number, filterTypeValue: UserType): Promise<void> {
+        this.loadingSubject.next(true);
+        const query = new UserSearchQuery();
+        query.setKey(UserSearchKey.USERSEARCHKEY_TYPE);
+        query.setValue(filterTypeValue);
+
+        this.userService.SearchUsers(limit, offset).then(resp => {
+            this.userResult = resp.toObject();
+            this.dataSource.data = this.userResult.resultList;
+            console.log(this.userResult.resultList);
+            this.loadingSubject.next(false);
+        }).catch(error => {
+            this.toast.showError(error);
+            this.loadingSubject.next(false);
+        });
+    }
+
+    public refreshPage(): void {
+        this.getData(this.paginator.pageSize, this.paginator.pageIndex * this.paginator.pageSize, this.userType);
+    }
+}
