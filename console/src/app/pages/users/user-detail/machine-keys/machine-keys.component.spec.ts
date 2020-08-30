bac69d875242e5d02be3a21687fd1acbb67a2819
Max Peintner <max@caos.ch>
2020-08-30 17:06:24 +0200

    lint ts, scss

--- console/src/app/pages/users/user-detail/machine-keys/machine-keys.component.spec.ts
+++ console/src/app/pages/users/user-detail/machine-keys/machine-keys.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MachineKeysComponent } from './machine-keys.component';
 
 describe('MachineKeysComponent', () => {
-  let component: MachineKeysComponent;
-  let fixture: ComponentFixture<MachineKeysComponent>;
+    let component: MachineKeysComponent;
+    let fixture: ComponentFixture<MachineKeysComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MachineKeysComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [MachineKeysComponent],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MachineKeysComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MachineKeysComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
