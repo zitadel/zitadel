d082ededabf829c9fb8b6d41a4ed83f7ef69e17d
Max Peintner <max@caos.ch>
2020-09-09 15:59:58 +0200

    download button

--- console/src/app/pages/users/user-detail/machine-keys/show-key-dialog/show-key-dialog.component.ts
+++ console/src/app/pages/users/user-detail/machine-keys/show-key-dialog/show-key-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { saveAs } from 'file-saver';
 import { AddMachineKeyResponse } from 'src/app/proto/generated/management_pb';
 
 @Component({
@@ -15,6 +16,12 @@ export class ShowKeyDialogComponent {
         @Inject(MAT_DIALOG_DATA) public data: any,
     ) {
         this.addedKey = data.key;
+    }
+
+    public saveFile(): void {
+        const json = atob(this.addedKey.keyDetails.toString());
+        const blob = new Blob([json], { type: 'text/plain;charset=utf-8' });
+        saveAs(blob, `${this.addedKey.id}.json`);
     }
 
     public closeDialog(): void {
