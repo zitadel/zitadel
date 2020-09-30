b5d4bc656a763d7029cf092cfcd525636ffaa523
Stefan Benz <stefan@caos.ch>
2020-09-30 16:33:51 +0200

    fix(operator): changed pipeline to release operator

--- .releaserc.js
+++ .releaserc.js
@@ -5,13 +5,13 @@ module.exports = {
         "@semantic-release/release-notes-generator",
         ["@semantic-release/github", {
             "assets": [
-              {"path": ".artifacts/zitadel-darwin-amd64/zitadel-darwin-amd64", "label": "Darwin x86_64"},
-              {"path": ".artifacts/zitadel-linux-amd64/zitadel-linux-amd64", "label": "Linux x86_64"},
-              {"path": ".artifacts/zitadel-windows-amd64/zitadel-windows-amd64", "label": "Windows x86_64"}
+              {"path": ".artifacts/zitadel-darwin-amd64/zitadel-darwin-amd64", "label": "Zitadel Darwin x86_64"},
+              {"path": ".artifacts/zitadel-linux-amd64/zitadel-linux-amd64", "label": "Zitadel Linux x86_64"},
+              {"path": ".artifacts/zitadel-windows-amd64/zitadel-windows-amd64", "label": "Zitadel Windows x86_64"},
+              {"path": ".artifacts/zitadel-darwin-amd64/zitadelctl-darwin-amd64", "label": "Zitadelctl Darwin x86_64"},
+              {"path": ".artifacts/zitadel-linux-amd64/zitadelctl-linux-amd64", "label": "Zitadelctl Linux x86_64"},
+              {"path": ".artifacts/zitadel-windows-amd64/zitadelctl-windows-amd64", "label": "Zitadelctl Windows x86_64"}
             ]
         }],
-        ["@semantic-release/exec", {
-            "publishCmd": "echo '::set-env name=CAOS_NEXT_VERSION::${nextRelease.version}'"
-            }],
     ]
 };
