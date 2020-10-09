3be458b9cb7e09f89d5c188c6b9b970519f084ce
Max Peintner <max@caos.ch>
2020-10-09 11:01:37 +0200

    rm accounts log

--- console/src/app/modules/accounts-card/accounts-card.component.ts
+++ console/src/app/modules/accounts-card/accounts-card.component.ts
@@ -20,7 +20,6 @@ export class AccountsCardComponent implements OnInit {
     constructor(public authService: AuthenticationService, private router: Router, private userService: GrpcAuthService) {
         this.userService.getMyUserSessions().then(sessions => {
             this.users = sessions.toObject().userSessionsList;
-            console.log(sessions.toObject());
             const index = this.users.findIndex(user => user.loginName === this.profile.preferredLoginName);
             if (index > -1) {
                 this.users.splice(index, 1);
