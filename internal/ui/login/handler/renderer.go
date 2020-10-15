ef7d20d0c783128d4c674ad0609c5aca94b13def
Livio Amstutz <livio.a@gmail.com>
2020-10-15 08:55:56 +0200

    user grant required step in login

--- internal/ui/login/handler/renderer.go
+++ internal/ui/login/handler/renderer.go
@@ -208,6 +208,8 @@ func (l *Login) chooseNextStep(w http.ResponseWriter, r *http.Request, authReq *
 		l.linkUsers(w, r, authReq, err)
 	case *model.ExternalNotFoundOptionStep:
 		l.renderExternalNotFoundOption(w, r, authReq, err)
+	case *model.GrantRequiredStep:
+		l.renderInternalError(w, r, authReq, caos_errs.ThrowPreconditionFailed(nil, "APP-asb43", "Errors.User.GrantRequired"))
 	default:
 		l.renderInternalError(w, r, authReq, caos_errs.ThrowInternal(nil, "APP-ds3QF", "step no possible"))
 	}
