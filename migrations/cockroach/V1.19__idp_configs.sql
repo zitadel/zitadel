46c42c6b09244858b31415baba4d31379125c776
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-10-19 16:12:40 +0200

    fix drop column migration

--- migrations/cockroach/V1.19__idp_configs.sql
+++ migrations/cockroach/V1.19__idp_configs.sql
@@ -1,6 +1,8 @@
+SET SQL_SAFE_UPDATES=0;
 ALTER TABLE management.idp_configs DROP COLUMN logo_src;
 ALTER TABLE adminapi.idp_configs DROP COLUMN logo_src;
 ALTER TABLE auth.idp_configs DROP COLUMN logo_src;
+SET SQL_SAFE_UPDATES=1;
 
 ALTER TABLE management.idp_configs ADD COLUMN styling_type SMALLINT;
 ALTER TABLE adminapi.idp_configs ADD COLUMN styling_type SMALLINT;
