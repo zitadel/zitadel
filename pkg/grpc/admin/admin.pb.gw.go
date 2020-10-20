bde624e33e31d61f9f47ab3d69a9314579a05dd8
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-10-20 17:59:21 +0200

    fix: migrations

--- pkg/grpc/admin/admin.pb.gw.go
+++ pkg/grpc/admin/admin.pb.gw.go
@@ -110,7 +110,10 @@ func local_request_AdminService_IsOrgUnique_0(ctx context.Context, marshaler run
 	var protoReq UniqueOrgRequest
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_AdminService_IsOrgUnique_0); err != nil {
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AdminService_IsOrgUnique_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
