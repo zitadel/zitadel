6926b749c470725865cfaa8cf472d5e287d57243
Max Peintner <max@caos.ch>
2020-10-23 16:44:31 +0200

    lint

--- console/src/app/pages/grants/grants.component.ts
+++ console/src/app/pages/grants/grants.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-grants',
-  templateUrl: './grants.component.html',
-  styleUrls: ['./grants.component.scss']
+    selector: 'app-grants',
+    templateUrl: './grants.component.html',
+    styleUrls: ['./grants.component.scss'],
 })
-export class GrantsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class GrantsComponent { }
