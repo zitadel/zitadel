321f592b6677a290c0566d27a120fbe051e8e0b2
adlerhurst <silvan.reusser@gmail.com>
2020-11-04 16:24:55 +0100

    fix: cleanup files

--- internal/api/grpc/client/middleware/tracing.go
+++ internal/api/grpc/client/middleware/tracing.go
@@ -1,16 +1,8 @@
 package middleware
 
 import (
-
-	// "strings"
-
-	// "go.opencensus.io/plugin/ocgrpc"
-	// "go.opencensus.io/trace"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
-	// "google.golang.org/grpc/stats"
-	// grpc_utils "github.com/caos/zitadel/internal/api/grpc"
-	// "github.com/caos/zitadel/internal/tracing"
 )
 
 type GRPCMethod string
@@ -18,34 +10,3 @@ type GRPCMethod string
 func DefaultTracingClient() grpc.DialOption {
 	return grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor())
 }
-
-// func TracingStatsClient(ignoredMethods ...GRPCMethod) grpc.DialOption {
-// 	return grpc.WithStatsHandler(
-// 		&tracingClientHandler{
-// 			ignoredMethods,
-// 			ocgrpc.ClientHandler{
-// 				StartOptions: trace.StartOptions{
-// 					Sampler:  tracing.Sampler(),
-// 					SpanKind: trace.SpanKindClient},
-// 			},
-// 		},
-// 	)
-// }
-
-// func DefaultTracingStatsClient() grpc.DialOption {
-// 	return TracingStatsClient(grpc_utils.Healthz, grpc_utils.Readiness, grpc_utils.Validation)
-// }
-
-// type tracingClientHandler struct {
-// 	IgnoredMethods []GRPCMethod
-// 	ocgrpc.ClientHandler
-// }
-
-// func (s *tracingClientHandler) TagRPC(ctx context.Context, tagInfo *stats.RPCTagInfo) context.Context {
-// 	for _, method := range s.IgnoredMethods {
-// 		if strings.HasSuffix(tagInfo.FullMethodName, string(method)) {
-// 			return ctx
-// 		}
-// 	}
-// 	return s.ClientHandler.TagRPC(ctx, tagInfo)
-// }
--- internal/api/grpc/server/middleware/tracing.go
+++ internal/api/grpc/server/middleware/tracing.go
@@ -1,15 +1,6 @@
 package middleware
 
 import (
-	// "context"
-	// "strings"
-
-	// "go.opencensus.io/plugin/ocgrpc"
-	// "go.opencensus.io/trace"
-	// "google.golang.org/grpc/stats"
-
-	// grpc_utils "github.com/caos/zitadel/internal/api/grpc"
-	// "github.com/caos/zitadel/internal/tracing"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 )
@@ -17,37 +8,3 @@ import (
 func DefaultTracingServer() grpc.UnaryServerInterceptor {
 	return otelgrpc.UnaryServerInterceptor()
 }
-
-// type GRPCMethod string
-
-// func TracingStatsServer(ignoredMethods ...GRPCMethod) grpc.ServerOption {
-// 	return grpc.StatsHandler(
-// 		&tracingServerHandler{
-// 			ignoredMethods,
-// 			ocgrpc.ServerHandler{
-// 				StartOptions: trace.StartOptions{
-// 					Sampler:  tracing.Sampler(),
-// 					SpanKind: trace.SpanKindServer,
-// 				},
-// 			},
-// 		},
-// 	)
-// }
-
-// func DefaultTracingStatsServer() grpc.ServerOption {
-// 	return TracingStatsServer(grpc_utils.Healthz, grpc_utils.Readiness, grpc_utils.Validation)
-// }
-
-// type tracingServerHandler struct {
-// 	IgnoredMethods []GRPCMethod
-// 	ocgrpc.ServerHandler
-// }
-
-// func (s *tracingServerHandler) TagRPC(ctx context.Context, tagInfo *stats.RPCTagInfo) context.Context {
-// 	for _, method := range s.IgnoredMethods {
-// 		if strings.HasSuffix(tagInfo.FullMethodName, string(method)) {
-// 			return ctx
-// 		}
-// 	}
-// 	return s.ServerHandler.TagRPC(ctx, tagInfo)
-// }
