d3761b6608bb1cdfe5bffa19d923422a4e73df2d
Fabiennne <fabienne.gerschwiler@gmail.com>
2020-11-09 08:15:20 +0100

    feat: tests

--- internal/user/repository/eventsourcing/model/web_auth_n.go
+++ internal/user/repository/eventsourcing/model/web_auth_n.go
@@ -3,18 +3,10 @@ package model
 import (
 	"encoding/json"
 	"github.com/caos/logging"
-	"github.com/caos/zitadel/internal/crypto"
 	caos_errs "github.com/caos/zitadel/internal/errors"
 	es_models "github.com/caos/zitadel/internal/eventstore/models"
 	"github.com/caos/zitadel/internal/user/model"
 )
-
-type OTP struct {
-	es_models.ObjectRoot
-
-	Secret *crypto.CryptoValue `json:"otpSecret,omitempty"`
-	State  int32               `json:"-"`
-}
 
 type WebAuthNToken struct {
 	es_models.ObjectRoot
@@ -39,6 +31,10 @@ type WebAuthNVerify struct {
 	SignCount       uint32 `json:"signCount"`
 }
 
+type WebAuthNTokenID struct {
+	WebauthNTokenID string `json:"webAuthNTokenId"`
+}
+
 func GetWebauthn(webauthnTokens []*WebAuthNToken, id string) (int, *WebAuthNToken) {
 	for i, webauthn := range webauthnTokens {
 		if webauthn.WebauthNTokenID == id {
@@ -46,22 +42,6 @@ func GetWebauthn(webauthnTokens []*WebAuthNToken, id string) (int, *WebAuthNToke
 		}
 	}
 	return -1, nil
-}
-
-func OTPFromModel(otp *model.OTP) *OTP {
-	return &OTP{
-		ObjectRoot: otp.ObjectRoot,
-		Secret:     otp.Secret,
-		State:      int32(otp.State),
-	}
-}
-
-func OTPToModel(otp *OTP) *model.OTP {
-	return &model.OTP{
-		ObjectRoot: otp.ObjectRoot,
-		Secret:     otp.Secret,
-		State:      model.MfaState(otp.State),
-	}
 }
 
 func WebAuthNsToModel(u2fs []*WebAuthNToken) []*model.WebAuthNToken {
@@ -119,30 +99,6 @@ func WebAuthNVerifyFromModel(webAuthN *model.WebAuthNToken) *WebAuthNVerify {
 	}
 }
 
-func (u *Human) appendOTPAddedEvent(event *es_models.Event) error {
-	u.OTP = &OTP{
-		State: int32(model.MfaStateNotReady),
-	}
-	return u.OTP.setData(event)
-}
-
-func (u *Human) appendOTPVerifiedEvent() {
-	u.OTP.State = int32(model.MfaStateReady)
-}
-
-func (u *Human) appendOTPRemovedEvent() {
-	u.OTP = nil
-}
-
-func (o *OTP) setData(event *es_models.Event) error {
-	o.ObjectRoot.AppendEvent(event)
-	if err := json.Unmarshal(event.Data, o); err != nil {
-		logging.Log("EVEN-d9soe").WithError(err).Error("could not unmarshal event data")
-		return caos_errs.ThrowInternal(err, "MODEL-lo023", "could not unmarshal event")
-	}
-	return nil
-}
-
 func (u *Human) appendU2FAddedEvent(event *es_models.Event) error {
 	webauthn := new(WebAuthNToken)
 	err := webauthn.setData(event)
@@ -182,13 +138,13 @@ func (u *Human) appendU2FRemovedEvent(event *es_models.Event) error {
 	if err != nil {
 		return err
 	}
-	if i, token := GetWebauthn(u.U2FTokens, webauthn.WebauthNTokenID); token != nil {
-		err = u.U2FTokens[i].setData(event)
-		if err != nil {
-			return err
+	for i := len(u.U2FTokens) - 1; i >= 0; i-- {
+		if u.U2FTokens[i].WebauthNTokenID == webauthn.WebauthNTokenID {
+			copy(u.U2FTokens[i:], u.U2FTokens[i+1:])
+			u.U2FTokens[len(u.U2FTokens)-1] = nil
+			u.U2FTokens = u.U2FTokens[:len(u.U2FTokens)-1]
+			return nil
 		}
-		webauthn.State = int32(model.MfaStateReady)
-		return nil
 	}
 	return nil
 }
