77aeb0ef35a38b740992d78f968f7dc4f3c104c7
Livio Amstutz <livio.a@gmail.com>
2020-11-16 14:00:17 +0100

    migration

--- migrations/cockroach/V1.22__webauthn.sql
+++ migrations/cockroach/V1.22__webauthn.sql
@@ -1,3 +1,9 @@
 ALTER TABLE management.users ADD COLUMN u2f_verified_ids TEXT ARRAY;
 ALTER TABLE auth.users ADD COLUMN u2f_verified_ids TEXT ARRAY;
 ALTER TABLE adminapi.users ADD COLUMN u2f_verified_ids TEXT ARRAY;
+
+ALTER TABLE management.users ADD COLUMN passwordless_verified_ids TEXT ARRAY;
+ALTER TABLE auth.users ADD COLUMN passwordless_verified_ids TEXT ARRAY;
+ALTER TABLE adminapi.users ADD COLUMN passwordless_verified_ids TEXT ARRAY;
+
+ALTER TABLE auth.user_sessions ADD COLUMN passwordless_verification TIMESTAMPTZ;
