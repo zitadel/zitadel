ec24210cdda82c4df8604e78c843ab471df3fe05
adlerhurst <silvan.reusser@gmail.com>
2020-11-19 14:00:19 +0100

    fix: mig

--- migrations/cockroach/V1.22__user_grant_owners.sql
+++ migrations/cockroach/V1.22__user_grant_owners.sql
@@ -1,8 +1,5 @@
 ALTER TABLE management.user_grants ADD COLUMN project_owner STRING;
-ALTER TABLE management.user_grants RENAME COLUMN resource_owner TO grant_owner;
 
 ALTER TABLE auth.user_grants ADD COLUMN project_owner STRING;
-ALTER TABLE auth.user_grants RENAME COLUMN resource_owner TO grant_owner;
 
 ALTER TABLE authz.user_grants ADD COLUMN project_owner STRING;
-ALTER TABLE authz.user_grants RENAME COLUMN resource_owner TO grant_owner;
