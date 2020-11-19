03a56f7d4615e03324bb9f012f8207f0347983dd
Max Peintner <max@caos.ch>
2020-11-19 09:57:45 +0100

    fix input directive

--- console/src/app/pages/users/user-detail/detail-form/detail-form.module.ts
+++ console/src/app/pages/users/user-detail/detail-form/detail-form.module.ts
@@ -5,7 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { TranslateModule } from '@ngx-translate/core';
-import { FormFieldModule } from 'src/app/modules/form-field/form-field.module';
+import { InputModule } from 'src/app/modules/input/input.module';
 
 import { DetailFormComponent } from './detail-form.component';
 
@@ -22,7 +22,7 @@ import { DetailFormComponent } from './detail-form.component';
         MatButtonModule,
         MatIconModule,
         TranslateModule,
-        FormFieldModule,
+        InputModule,
     ],
     exports: [
         DetailFormComponent,
