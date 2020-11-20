a44c921113c9f44d12b0a414178f88a880659b11
Stefan Benz <stefan@caos.ch>
2020-11-20 14:56:43 +0100

    fix(operator): correct and align operator pipeline with zitadel pipeline

--- .releaserc.js
+++ .releaserc.js
@@ -3,6 +3,21 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/github"
+        ["@semantic-release/github", {
+            "assets": [
+                {
+                    "path": ".artifacts/zitadel-darwin-amd64/zitadelctl",
+                    "label": "Zitadelctl Darwin x86_64"
+                },
+                {
+                    "path": ".artifacts/zitadel-linux-amd64/zitadelctl",
+                    "label": "Zitadelctl Linux x86_64"
+                },
+                {
+                    "path": ".artifacts/zitadel-windows-amd64/zitadelctl",
+                    "label": "Zitadelctl Windows x86_64"
+                }
+            ]
+        }],
     ]
 };
