ce5c23a32050657e96d786ba3bfc33b1b368e678
Maximilian Peintner <csaq7175@uibk.ac.at>
2020-12-07 11:10:09 +0100

    rm local grpc, u2f dialog

---
+++ console/src/app/pages/users/user-detail/auth-user-detail/dialog-u2f/dialog-u2f.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+    selector: 'app-dialog-u2f',
+    templateUrl: './dialog-u2f.component.html',
+    styleUrls: ['./dialog-u2f.component.scss'],
+})
+export class DialogU2FComponent {
+    public code: string = '';
+    constructor(public dialogRef: MatDialogRef<DialogU2FComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: string) { }
+
+    public closeDialog(): void {
+        this.dialogRef.close();
+    }
+
+    public closeDialogWithCode(): void {
+        this.dialogRef.close(this.code);
+    }
+}
