e77e9f7e0077b441a7abd76a936e526bcf731a6b
Maximilian Peintner <csaq7175@uibk.ac.at>
2020-12-08 15:42:18 +0100

    spinner, remove, attribute col

--- console/src/app/pages/users/user-detail/auth-user-detail/auth-user-mfa/auth-user-mfa.component.ts
+++ console/src/app/pages/users/user-detail/auth-user-detail/auth-user-mfa/auth-user-mfa.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
-import { TranslateService } from '@ngx-translate/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { WarnDialogComponent } from 'src/app/modules/warn-dialog/warn-dialog.component';
 import { MfaOtpResponse, MFAState, MfaType, MultiFactor, WebAuthNResponse } from 'src/app/proto/generated/auth_pb';
@@ -11,6 +10,28 @@ import { ToastService } from 'src/app/services/toast.service';
 
 import { DialogOtpComponent } from '../dialog-otp/dialog-otp.component';
 import { DialogU2FComponent } from '../dialog-u2f/dialog-u2f.component';
+
+export function _base64ToArrayBuffer(base64: string): any {
+  const binaryString = atob(base64);
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
+export function _arrayBufferToBase64( buffer: any): string {
+  let binary = '';
+  const bytes = new Uint8Array( buffer );
+  const len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode( bytes[ i ] );
+  }
+  return btoa( binary ).replace(/\+/g, '-')
+  .replace(/\//g, '_')
+  .replace(/=/g, '');
+}
 
 export interface WebAuthNOptions {
   challenge: string;
@@ -28,7 +49,7 @@ export interface WebAuthNOptions {
     styleUrls: ['./auth-user-mfa.component.scss'],
 })
 export class AuthUserMfaComponent implements OnInit, OnDestroy {
-    public displayedColumns: string[] = ['type', 'state', 'actions'];
+    public displayedColumns: string[] = ['type', 'attr', 'state', 'actions'];
     private loadingSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     public loading$: Observable<boolean> = this.loadingSubject.asObservable();
 
@@ -43,8 +64,7 @@ export class AuthUserMfaComponent implements OnInit, OnDestroy {
     public otpAvailable: boolean = false;
     constructor(private service: GrpcAuthService,
       private toast: ToastService,
-      private dialog: MatDialog,
-      private translate: TranslateService) { }
+      private dialog: MatDialog) { }
 
     public ngOnInit(): void {
         this.getMFAs();
@@ -83,56 +103,21 @@ export class AuthUserMfaComponent implements OnInit, OnDestroy {
             const webauthn: WebAuthNResponse.AsObject = u2fresp.toObject();
             const credOptions: CredentialCreationOptions = JSON.parse(atob(webauthn.publicKey as string));
 
-            console.log(credOptions);
             if (credOptions.publicKey?.challenge) {
-              credOptions.publicKey.challenge = this._base64ToArrayBuffer(credOptions.publicKey.challenge as any);
-              credOptions.publicKey.user.id = this._base64ToArrayBuffer(credOptions.publicKey.user.id as any);
-              console.log(credOptions);
+              credOptions.publicKey.challenge = _base64ToArrayBuffer(credOptions.publicKey.challenge as any);
+              credOptions.publicKey.user.id = _base64ToArrayBuffer(credOptions.publicKey.user.id as any);
               const dialogRef = this.dialog.open(DialogU2FComponent, {
                 width: '400px',
+                data: {
+                  credOptions,
+                },
               });
 
-              dialogRef.afterClosed().subscribe(tokenname => {
-                if (tokenname && credOptions.publicKey) {
-                  navigator.credentials.create(credOptions).then((resp) => {
-                    console.log(resp);
-
-                      if (resp &&
-                        (resp as any).response.attestationObject &&
-                        (resp as any).response.clientDataJSON &&
-                        (resp as any).rawId) {
-
-                        const attestationObject = new Uint8Array((resp as any).response.attestationObject);
-                        const clientDataJSON = new Uint8Array((resp as any).response.clientDataJSON);
-                        const rawId = new Uint8Array((resp as any).rawId);
-
-                        const data = JSON.stringify({
-                            id: resp.id,
-                            rawId: this._arrayBufferToBase64(rawId),
-                            type: resp.type,
-                            response: {
-                                attestationObject: this._arrayBufferToBase64(attestationObject),
-                                clientDataJSON: this._arrayBufferToBase64(clientDataJSON),
-                            },
-                        });
-
-                        console.log(data);
-
-                        const base64 = btoa(data);
-                        console.log(base64);
-                        this.service.VerifyMyMfaU2F(base64, tokenname).then(() => {
-                          this.translate.get('USER.MFA.U2F_SUCCESS').subscribe(msg => {
-                            this.toast.showInfo(msg);
-                          });
-                        }).catch(error => {
-                          this.toast.showError(error);
-                        });
-                      } else {
-                        this.translate.get('USER.MFA.U2F_ERROR').subscribe(msg => {
-                          this.toast.showInfo(msg);
-                        });
-                      }
-                  });
+              dialogRef.afterClosed().subscribe(done => {
+                if (done) {
+                  this.getMFAs();
+                } else {
+                  this.getMFAs();
                 }
               });
             }
@@ -199,23 +184,5 @@ export class AuthUserMfaComponent implements OnInit, OnDestroy {
         });
     }
 
-    private _base64ToArrayBuffer(base64: string): any {
-      const binaryString = window.atob(base64);
-      const len = binaryString.length;
-      const bytes = new Uint8Array(len);
-      for (let i = 0; i < len; i++) {
-          bytes[i] = binaryString.charCodeAt(i);
-      }
-      return bytes.buffer;
-    }
 
-    private _arrayBufferToBase64( buffer: any): string {
-      let binary = '';
-      const bytes = new Uint8Array( buffer );
-      const len = bytes.byteLength;
-      for (let i = 0; i < len; i++) {
-          binary += String.fromCharCode( bytes[ i ] );
-      }
-      return window.btoa( binary );
-  }
 }
