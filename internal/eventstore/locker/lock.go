f16ac9e6f17256df53247b333061643745fe76a5
adlerhurst <silvan.reusser@gmail.com>
2020-12-08 19:00:58 +0100

    fix(spooler): improve renew

--- internal/eventstore/locker/lock.go
+++ internal/eventstore/locker/lock.go
@@ -12,6 +12,8 @@ import (
 )
 
 const (
+	queryStmtFmt = "with l as (select * from %s where view_name = $1) " +
+	"select case when (select count(*) from l) = 0 then true else (select case when (locker_id = $2 or locked_until < now()) then true else false end from l) end"
 	insertStmtFormat = "INSERT INTO %s" +
 		" (locker_id, locked_until, view_name) VALUES ($1, now()+$2::INTERVAL, $3)" +
 		" ON CONFLICT (view_name)" +
@@ -27,6 +29,14 @@ type lock struct {
 }
 
 func Renew(dbClient *sql.DB, lockTable, lockerID, viewModel string, waitTime time.Duration) error {
+	var shouldInsert bool
+	err := dbClient.QueryRow(fmt.Sprintf(queryStmtFmt, lockTable), viewModel, lockerID).Scan(&shouldInsert)
+	if err !=nil{
+		return caos_errs.ThrowInternal(nil, "SPOOL-qcRP8", "query row failed")
+	}
+	if !shouldInsert { 
+		return caos_errs.ThrowAlreadyExists(nil, "SPOOL-7WTO6", "view already locked")
+	}
 	return crdb.ExecuteTx(context.Background(), dbClient, nil, func(tx *sql.Tx) error {
 		insert := fmt.Sprintf(insertStmtFormat, lockTable)
 		result, err := tx.Exec(insert,
