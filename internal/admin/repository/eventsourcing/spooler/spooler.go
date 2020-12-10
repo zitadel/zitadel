ad8b0e1fe9da112f9fa79b7990e49eb3f2038b87
adlerhurst <silvan.reusser@gmail.com>
2020-12-11 00:23:26 +0100

    revert

--- internal/admin/repository/eventsourcing/spooler/spooler.go
+++ internal/admin/repository/eventsourcing/spooler/spooler.go
@@ -2,6 +2,7 @@ package spooler
 
 import (
 	"database/sql"
+
 	"github.com/caos/zitadel/internal/config/systemdefaults"
 
 	"github.com/caos/zitadel/internal/admin/repository/eventsourcing/handler"
@@ -13,16 +14,14 @@ import (
 type SpoolerConfig struct {
 	BulkLimit             uint64
 	FailureCountUntilSkip uint64
-	ConcurrentWorkers     int
 	Handlers              handler.Configs
 }
 
 func StartSpooler(c SpoolerConfig, es eventstore.Eventstore, view *view.View, sql *sql.DB, repos handler.EventstoreRepos, defaults systemdefaults.SystemDefaults) *spooler.Spooler {
 	spoolerConfig := spooler.Config{
-		Eventstore:        es,
-		Locker:            &locker{dbClient: sql},
-		ConcurrentWorkers: c.ConcurrentWorkers,
-		ViewHandlers:      handler.Register(c.Handlers, c.BulkLimit, c.FailureCountUntilSkip, view, es, repos, defaults),
+		Eventstore:   es,
+		Locker:       &locker{dbClient: sql},
+		ViewHandlers: handler.Register(c.Handlers, c.BulkLimit, c.FailureCountUntilSkip, view, es, repos, defaults),
 	}
 	spool := spoolerConfig.New()
 	spool.Start()
