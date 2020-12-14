9e0c7435df09fe52e1ddacffad49e5964c4bd5ff
adlerhurst <silvan.reusser@gmail.com>
2020-12-14 11:23:58 +0100

    fix(otel): add context to newExporter

--- internal/tracing/otel/config.go
+++ internal/tracing/otel/config.go
@@ -1,6 +1,8 @@
 package otel
 
 import (
+	"context"
+
 	"github.com/caos/zitadel/internal/tracing"
 	"go.opentelemetry.io/otel/exporters/otlp"
 	sdk_trace "go.opentelemetry.io/otel/sdk/trace"
@@ -14,7 +16,7 @@ type Config struct {
 
 func (c *Config) NewTracer() error {
 	sampler := sdk_trace.ParentBased(sdk_trace.TraceIDRatioBased(c.Fraction))
-	exporter, err := otlp.NewExporter(otlp.WithAddress(c.Endpoint), otlp.WithInsecure())
+	exporter, err := otlp.NewExporter(context.Background(), otlp.WithAddress(c.Endpoint), otlp.WithInsecure())
 	if err != nil {
 		return err
 	}
