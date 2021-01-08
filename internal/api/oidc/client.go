76d11c74cd3dc4e979b2bfb2836dec127c71d5b7
Livio Amstutz <livio.a@gmail.com>
2021-01-08 09:34:29 +0100

    introspect

--- internal/api/oidc/client.go
+++ internal/api/oidc/client.go
@@ -85,6 +85,25 @@ func (o *OPStorage) AuthorizeClientIDSecret(ctx context.Context, id string, secr
 	return o.repo.AuthorizeOIDCApplication(ctx, id, secret)
 }
 
+func (o *OPStorage) SetUserinfoFromToken(ctx context.Context, userInfo oidc.UserInfoSetter, tokenID, subject, origin string) (err error) {
+	ctx, span := tracing.NewSpan(ctx)
+	defer func() { span.EndWithError(err) }()
+	token, err := o.repo.TokenByID(ctx, subject, tokenID)
+	if err != nil {
+		return errors.ThrowPermissionDenied(nil, "OIDC-Dsfb2", "token is not valid or has expired")
+	}
+	if token.ApplicationID != "" {
+		app, err := o.repo.ApplicationByClientID(ctx, token.ApplicationID)
+		if err != nil {
+			return err
+		}
+		if origin != "" && !http.IsOriginAllowed(app.OriginAllowList, origin) {
+			return errors.ThrowPermissionDenied(nil, "OIDC-da1f3", "origin is not allowed")
+		}
+	}
+	return o.SetUserinfoFromScopes(ctx, userInfo, token.UserID, token.ApplicationID, token.Scopes)
+}
+
 func (o *OPStorage) GetUserinfoFromToken(ctx context.Context, tokenID, subject, origin string) (_ oidc.UserInfo, err error) {
 	ctx, span := tracing.NewSpan(ctx)
 	defer func() { span.EndWithError(err) }()
@@ -102,6 +121,74 @@ func (o *OPStorage) GetUserinfoFromToken(ctx context.Context, tokenID, subject, 
 		}
 	}
 	return o.GetUserinfoFromScopes(ctx, token.UserID, token.ApplicationID, token.Scopes)
+}
+
+func (o *OPStorage) SetUserinfoFromScopes(ctx context.Context, userInfo oidc.UserInfoSetter, userID, applicationID string, scopes []string) (err error) {
+	ctx, span := tracing.NewSpan(ctx)
+	defer func() { span.EndWithError(err) }()
+	user, err := o.repo.UserByID(ctx, userID)
+	if err != nil {
+		return err
+	}
+	roles := make([]string, 0)
+	for _, scope := range scopes {
+		switch scope {
+		case oidc.ScopeOpenID:
+			userInfo.SetSubject(user.ID)
+		case oidc.ScopeEmail:
+			if user.HumanView == nil {
+				continue
+			}
+			userInfo.SetEmail(user.Email, user.IsEmailVerified)
+		case oidc.ScopeProfile:
+			userInfo.SetPreferredUsername(user.PreferredLoginName)
+			userInfo.SetUpdatedAt(user.ChangeDate)
+			if user.HumanView != nil {
+				userInfo.SetName(user.DisplayName)
+				userInfo.SetFamilyName(user.LastName)
+				userInfo.SetGivenName(user.FirstName)
+				userInfo.SetNickname(user.NickName)
+				userInfo.SetGender(oidc.Gender(getGender(user.Gender)))
+				locale, _ := language.Parse(user.PreferredLanguage)
+				userInfo.SetLocale(locale)
+			} else {
+				userInfo.SetName(user.MachineView.Name)
+			}
+		case oidc.ScopePhone:
+			if user.HumanView == nil {
+				continue
+			}
+			userInfo.SetPhone(user.Phone, user.IsPhoneVerified)
+		case oidc.ScopeAddress:
+			if user.HumanView == nil {
+				continue
+			}
+			if user.StreetAddress == "" && user.Locality == "" && user.Region == "" && user.PostalCode == "" && user.Country == "" {
+				continue
+			}
+			userInfo.SetAddress(oidc.NewUserInfoAddress(user.StreetAddress, user.Locality, user.Region, user.PostalCode, user.Country, ""))
+		default:
+			if strings.HasPrefix(scope, ScopeProjectRolePrefix) {
+				roles = append(roles, strings.TrimPrefix(scope, ScopeProjectRolePrefix))
+			}
+			if strings.HasPrefix(scope, model.OrgDomainPrimaryScope) {
+				userInfo.AppendClaims(model.OrgDomainPrimaryClaim, strings.TrimPrefix(scope, model.OrgDomainPrimaryScope))
+			}
+		}
+	}
+
+	if len(roles) == 0 || applicationID == "" {
+		return nil
+	}
+	projectRoles, err := o.assertRoles(ctx, userID, applicationID, roles)
+	if err != nil {
+		return err
+	}
+	if len(projectRoles) > 0 {
+		userInfo.AppendClaims(ClaimProjectRoles, projectRoles)
+	}
+
+	return nil
 }
 
 func (o *OPStorage) GetUserinfoFromScopes(ctx context.Context, userID, applicationID string, scopes []string) (_ oidc.UserInfo, err error) {
