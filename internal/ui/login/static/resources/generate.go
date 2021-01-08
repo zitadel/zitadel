b3f7a2df9fe25878d12d9711aabcefaa9daa24b5
Max Peintner <max@caos.ch>
2021-01-08 12:43:50 +0100

    footer, main style, qr code fix, mfa type fix, account sel, checkbox

--- internal/ui/login/static/resources/generate.go
+++ internal/ui/login/static/resources/generate.go
@@ -2,5 +2,6 @@ package resources
 
 // scss
 
+//go:generate sass themes/scss/main.scss themes/zitadel/css/main.css
 //go:generate sass themes/scss/bundle.scss themes/zitadel/css/bundle.css
 //go:generate sass themes/scss/styles/zitadel.scss themes/zitadel/css/zitadel.css
--- internal/ui/login/static/resources/scripts/copy_to_clipboard.js
+++ internal/ui/login/static/resources/scripts/copy_to_clipboard.js
@@ -1,6 +1,18 @@
 const copyToClipboard = str => {
     navigator.clipboard.writeText(str);
-}
+    // const icon = copyButton.getElementsByClassName('las')[0];
+    // if (icon && icon.classList.contains('la-clipboard')) {
+    //     icon.remove('la-clipboard');
+    //     icon.classList.add('la-clipboard-check');
 
-let copyButton = document.getElementsByClassName("copy")[0];
+    //     setTimeout(() => {
+    //         if (icon.classList.contains('la-clipboard-check')) {
+    //             icon.classList.remove('la-clipboard-check');
+    //             icon.classList.add('la-clipboard');
+    //         }
+    //     }, 3000);
+    // }
+};
+
+let copyButton = document.getElementById("copy");
 copyButton.addEventListener("click", copyToClipboard(copyButton.getAttribute("data-copy")));
