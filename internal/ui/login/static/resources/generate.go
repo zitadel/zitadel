5769d2d4bdea4c6fb2d7e658fdcce1ee3c863e84
Max Peintner <max@caos.ch>
2021-01-14 16:12:58 +0100

    remove material icons, unused assets, fix icon font

--- internal/ui/login/static/resources/generate.go
+++ internal/ui/login/static/resources/generate.go
@@ -4,4 +4,4 @@ package resources
 
 //go:generate sass themes/scss/main.scss themes/zitadel/css/main.css
 //go:generate sass themes/scss/bundle.scss themes/zitadel/css/bundle.css
-//go:generate sass themes/scss/styles/zitadel.scss themes/zitadel/css/zitadel.css
+//go:generate sass themes/scss/zitadel.scss themes/zitadel/css/zitadel.css
