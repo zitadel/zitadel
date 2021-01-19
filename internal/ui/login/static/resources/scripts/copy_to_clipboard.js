d7342bb3aaef16deb0221b6b507d4124d7951d7b
Max Peintner <max@caos.ch>
2021-01-19 15:57:03 +0100

    clear comments

--- internal/ui/login/static/resources/scripts/copy_to_clipboard.js
+++ internal/ui/login/static/resources/scripts/copy_to_clipboard.js
@@ -1,17 +1,5 @@
 const copyToClipboard = str => {
     navigator.clipboard.writeText(str);
-    // const icon = copyButton.getElementsByClassName('las')[0];
-    // if (icon && icon.classList.contains('la-clipboard')) {
-    //     icon.remove('la-clipboard');
-    //     icon.classList.add('la-clipboard-check');
-
-    //     setTimeout(() => {
-    //         if (icon.classList.contains('la-clipboard-check')) {
-    //             icon.classList.remove('la-clipboard-check');
-    //             icon.classList.add('la-clipboard');
-    //         }
-    //     }, 3000);
-    // }
 };
 
 let copyButton = document.getElementById("copy");
