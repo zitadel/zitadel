f765ca22c51a8b6bb7536a6e82799262939fbc43
Fabi <38692350+fgerschwiler@users.noreply.github.com>
2021-01-22 13:16:45 +0100

    Update internal/v2/command/user_grant.go
    Co-authored-by: Livio Amstutz <livio.a@gmail.com>

--- internal/v2/command/user_grant.go
+++ internal/v2/command/user_grant.go
@@ -38,12 +38,9 @@ func (r *CommandSide) addUserGrant(ctx context.Context, userGrant *domain.UserGr
 	if !exists {
 		return nil, nil, caos_errs.ThrowPreconditionFailed(nil, "COMMAND-4M0fs", "Errors.User.NotFound")
 	}
-	exists, err = r.checkProjectExists(ctx, userGrant.ProjectID, resourceOwner)
+	err = r.checkProjectExists(ctx, userGrant.ProjectID, resourceOwner)
 	if err != nil {
 		return nil, nil, err
-	}
-	if !exists {
-		return nil, nil, caos_errs.ThrowPreconditionFailed(nil, "COMMAND-4M0fs", "Errors.Project.NotFound")
 	}
 	userGrant.AggregateID, err = r.idGenerator.Next()
 	if err != nil {
