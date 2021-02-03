5f5ec0a2392dac630775f00fa230d19b77bf0d72
Max Peintner <max@caos.ch>
2021-02-03 16:12:21 +0100

    set links from component data

--- console/src/app/modules/onboarding/onboarding.component.ts
+++ console/src/app/modules/onboarding/onboarding.component.ts
@@ -6,15 +6,11 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
     templateUrl: './onboarding.component.html',
     styleUrls: ['./onboarding.component.scss']
 })
-export class OnboardingComponent implements OnInit {
+export class OnboardingComponent {
     public steps = [
-        { titleI18nKey: 'ONBOARDING.STEPS.1.TITLE', descI18nKey: 'ONBOARDING.STEPS.1.DESC', docs: "https://docs.zitadel.ch/use" },
-        { titleI18nKey: 'ONBOARDING.STEPS.2.TITLE', descI18nKey: 'ONBOARDING.STEPS.2.DESC', docs: "https://docs.zitadel.ch/use" },
-        { titleI18nKey: 'ONBOARDING.STEPS.3.TITLE', descI18nKey: 'ONBOARDING.STEPS.3.DESC' },
+        { titleI18nKey: 'ONBOARDING.STEPS.1.TITLE', descI18nKey: 'ONBOARDING.STEPS.1.DESC', docs: "https://docs.zitadel.ch/use", link: ['/projects', 'create'] },
+        { titleI18nKey: 'ONBOARDING.STEPS.2.TITLE', descI18nKey: 'ONBOARDING.STEPS.2.DESC', docs: "https://docs.zitadel.ch/use", link: ['/projects'] },
+        { titleI18nKey: 'ONBOARDING.STEPS.3.TITLE', descI18nKey: 'ONBOARDING.STEPS.3.DESC', link: ['/iam', 'policies'] },
     ];
-    constructor(public authenticationService: AuthenticationService) { }
-
-    ngOnInit(): void {
-    }
-
+    constructor() { }
 }
