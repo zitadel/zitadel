7c513d9ad4b131aed4eedc7af5a289e76a385699
Stefan Benz <stefan@caos.ch>
2021-02-08 15:58:11 +0100

    fix(pipeline): corrected download of artifacts to globally defined folder

--- .releaserc.js
+++ .releaserc.js
@@ -6,15 +6,15 @@ module.exports = {
         ["@semantic-release/github", {
             "assets": [
                 {
-                    "path": "./artifacts/zitadel-darwin-amd64",
+                    "path": "./artifacts/zitadel-darwin-amd64/zitadel-darwin-amd64",
                     "label": "Zitadelctl Darwin x86_64"
                 },
                 {
-                    "path": "./artifacts/zitadel-linux-amd64",
+                    "path": "./artifacts/zitadel-linux-amd64/zitadel-linux-amd64",
                     "label": "Zitadelctl Linux x86_64"
                 },
                 {
-                    "path": "./artifacts/zitadel-windows-amd64.exe",
+                    "path": "./artifacts/zitadel-windows-amd64/zitadel-windows-amd64.exe",
                     "label": "Zitadelctl Windows x86_64"
                 }
             ]
