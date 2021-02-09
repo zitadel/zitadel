e08483fe397f97b2d4ac862687ee6ef5c58a071a
Fabiennne <fabienne.gerschwiler@gmail.com>
2021-02-09 16:49:34 +0100

    feat: setup step 10

--- internal/v2/command/setup_step10.go
+++ internal/v2/command/setup_step10.go
@@ -2,15 +2,14 @@ package command
 
 import (
 	"context"
-	iam_model "github.com/caos/zitadel/internal/iam/model"
-
+	"github.com/caos/logging"
 	"github.com/caos/zitadel/internal/v2/domain"
 	iam_repo "github.com/caos/zitadel/internal/v2/repository/iam"
 )
 
 type Step10 struct {
-	DefaultMailTemplate iam_model.MailTemplate
-	DefaultMailTexts    []iam_model.MailText
+	DefaultMailTemplate domain.MailTemplate
+	DefaultMailTexts    []domain.MailText
 }
 
 func (s *Step10) Step() domain.Step {
@@ -23,23 +22,19 @@ func (s *Step10) execute(ctx context.Context, commandSide *CommandSide) error {
 
 func (r *CommandSide) SetupStep10(ctx context.Context, step *Step10) error {
 	fn := func(iam *IAMWriteModel) (*iam_repo.Aggregate, error) {
-		//multiFactorModel := NewIAMMultiFactorWriteModel()
-		//iamAgg := IAMAggregateFromWriteModel(&multiFactorModel.MultiFactoryWriteModel.WriteModel)
-		//if !step.Passwordless {
-		//	return iamAgg, nil
-		//}
-		//err := setPasswordlessAllowedInPolicy(ctx, r, iamAgg)
-		//if err != nil {
-		//	return nil, err
-		//}
-		//logging.Log("SETUP-AEG2t").Info("allowed passwordless in login policy")
-		//err = r.addMultiFactorToDefaultLoginPolicy(ctx, iamAgg, multiFactorModel, domain.MultiFactorTypeU2FWithPIN)
-		//if err != nil {
-		//	return nil, err
-		//}
-		//logging.Log("SETUP-ADfng").Info("added passwordless to MFA login policy")
-		//return iamAgg, err
-		return nil, nil
+		iamAgg := IAMAggregateFromWriteModel(&iam.WriteModel)
+		err := r.addDefaultMailTemplate(ctx, iamAgg, NewIAMMailTemplateWriteModel(), &step.DefaultMailTemplate)
+		if err != nil {
+			return nil, err
+		}
+		for _, text := range step.DefaultMailTexts {
+			r.addDefaultMailText(ctx, iamAgg, NewIAMMailTextWriteModel(text.MailTextType, text.Language), &text)
+			if err != nil {
+				return nil, err
+			}
+		}
+		logging.Log("SETUP-3N9fs").Info("default mail template/text set up")
+		return iamAgg, nil
 	}
 	return r.setup(ctx, step, fn)
 }
