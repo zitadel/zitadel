dcae8e6adb495b3a226a182df04c92dbda51c96d
Fabiennne <fabienne.gerschwiler@gmail.com>
2021-02-16 11:25:16 +0100

    fix: my usermemberships

---
+++ migrations/cockroach/V1.30__user_memberships.sql
@@ -0,0 +1,46 @@
+BEGIN;
+
+TRUNCATE TABLE authz.user_memberships;
+
+INSERT INTO authz.user_memberships (
+    user_id,
+    member_type,
+    aggregate_id,
+    object_id,
+    roles,
+    display_name,
+    resource_owner,
+    resource_owner_name,
+    creation_date,
+    change_date,
+    sequence
+)
+SELECT
+    user_id,
+    member_type,
+    aggregate_id,
+    object_id,
+    roles,
+    display_name,
+    resource_owner,
+    resource_owner_name,
+    creation_date,
+    change_date,
+    sequence
+FROM auth.user_memberships;
+
+INSERT INTO authz.current_sequences (
+     view_name,
+     event_timestamp,
+     current_sequence,
+     last_successful_spooler_run,
+     aggregate_type
+     )
+SELECT view_name,
+    event_timestamp,
+    current_sequence,
+    last_successful_spooler_run,
+    aggregate_type
+FROM auth.current_sequences;
+
+COMMIT;
