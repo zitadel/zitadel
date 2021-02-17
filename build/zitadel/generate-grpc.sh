4be39cc9d9e851db000d5d1f2df5500b22733704
adlerhurst <silvan.reusser@gmail.com>
2021-02-17 16:23:11 +0100

    hobla

--- build/zitadel/generate-grpc.sh
+++ build/zitadel/generate-grpc.sh
@@ -21,13 +21,13 @@ protoc \
   --go_out ${GOPATH}/src \
   --go-grpc_out ${GOPATH}/src \
   --go-grpc_opt paths=source_relative \
+  --authoption_out=/proto/output \
+  --validate_out=lang=go:${GOPATH}/src \
+  /proto/include/zitadel/admin.proto
   # --grpc-gateway_out $GOPATH/src/github.com/caos/zitadel/pkg/grpc \
   # --grpc-gateway_opt logtostderr=true \
   # --openapiv2_out $GOPATH/src/github.com/caos/zitadel/openapi/v2 \
   # --openapiv2_opt logtostderr=true \ 
-  --authoption_out=/proto/output \
-  --validate_out=lang=go:${GOPATH}/src \
-  /proto/include/zitadel/admin.proto
 
 protoc \
   -I=/proto/include \
@@ -37,11 +37,11 @@ protoc \
   --grpc-gateway_out $GOPATH/src/github.com/caos/zitadel/pkg/grpc \
   --grpc-gateway_opt logtostderr=true,paths=source_relative \
   --grpc-gateway_opt allow_delete_body=true \
-  # --openapiv2_out $GOPATH/src/github.com/caos/zitadel/openapi/v2 \
-  # --openapiv2_opt logtostderr=true \ 
   --authoption_out=/proto/output \
   --validate_out=lang=go:${GOPATH}/src \
   /proto/include/zitadel/management.proto
+  # --openapiv2_out $GOPATH/src/github.com/caos/zitadel/openapi/v2 \
+  # --openapiv2_opt logtostderr=true \ 
 
 protoc \
   -I=/proto/include \
