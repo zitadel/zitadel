2457c00cede9b12e75f143f338136aeab158059b
adlerhurst <silvan.reusser@gmail.com>
2021-02-17 16:19:34 +0100

    is onenapi the problem?

--- build/zitadel/generate-grpc.sh
+++ build/zitadel/generate-grpc.sh
@@ -7,6 +7,8 @@ echo "Generate grpc"
 # output folder for openapi v2
 mkdir -p $GOPATH/src/github.com/caos/zitadel/openapi/v2
 
+ls /proto/include
+
 protoc \
   -I=/proto/include \
   --go_out $GOPATH/src \
@@ -19,10 +21,10 @@ protoc \
   --go_out ${GOPATH}/src \
   --go-grpc_out ${GOPATH}/src \
   --go-grpc_opt paths=source_relative \
-  --grpc-gateway_out $GOPATH/src/github.com/caos/zitadel/pkg/grpc \
-  --grpc-gateway_opt logtostderr=true,paths=source_relative \
-  --openapiv2_out $GOPATH/src/github.com/caos/zitadel/openapi/v2 \
-  --openapiv2_opt logtostderr=true,paths=source_relative \ 
+  # --grpc-gateway_out $GOPATH/src/github.com/caos/zitadel/pkg/grpc \
+  # --grpc-gateway_opt logtostderr=true \
+  # --openapiv2_out $GOPATH/src/github.com/caos/zitadel/openapi/v2 \
+  # --openapiv2_opt logtostderr=true \ 
   --authoption_out=/proto/output \
   --validate_out=lang=go:${GOPATH}/src \
   /proto/include/zitadel/admin.proto
@@ -35,8 +37,8 @@ protoc \
   --grpc-gateway_out $GOPATH/src/github.com/caos/zitadel/pkg/grpc \
   --grpc-gateway_opt logtostderr=true,paths=source_relative \
   --grpc-gateway_opt allow_delete_body=true \
-  --openapiv2_out $GOPATH/src/github.com/caos/zitadel/openapi/v2 \
-  --openapiv2_opt logtostderr=true \ 
+  # --openapiv2_out $GOPATH/src/github.com/caos/zitadel/openapi/v2 \
+  # --openapiv2_opt logtostderr=true \ 
   --authoption_out=/proto/output \
   --validate_out=lang=go:${GOPATH}/src \
   /proto/include/zitadel/management.proto
