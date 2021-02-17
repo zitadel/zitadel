d50f8c8d8f3b4c092fb81ce7f42e92d34d244d18
adlerhurst <silvan.reusser@gmail.com>
2021-02-17 22:05:04 +0100

    grpc generation in one file

--- build/zitadel/generate-grpc.sh
+++ build/zitadel/generate-grpc.sh
@@ -5,8 +5,23 @@ set -eux
 echo "Generate grpc"
 
 OPENAPI_PATH=${GOPATH}/src/github.com/caos/zitadel/openapi/v2
-GRPC_PATH=${GOPATH}/src/github.com/caos/zitadel/pkg/grpc
+ZITADEL_PATH=${GOPATH}/src/github.com/caos/zitadel
+GRPC_PATH=${ZITADEL_PATH}/pkg/grpc
 PROTO_PATH=/proto/include/zitadel
+
+protoc \
+  -I=/proto/include/ \
+  --go_out $GOPATH/src \
+  --go-grpc_out $GOPATH/src \
+  ${PROTO_PATH}/options.proto
+
+go-bindata \
+  -pkg main \
+  -prefix internal/protoc/protoc-gen-authoption \
+  -o ${ZITADEL_PATH}/internal/protoc/protoc-gen-authoption/templates.gen.go \
+  ${ZITADEL_PATH}/internal/protoc/protoc-gen-authoption/templates
+
+go install ${ZITADEL_PATH}/internal/protoc/protoc-gen-authoption
 
 # output folder for openapi v2
 mkdir -p ${OPENAPI_PATH}
@@ -15,19 +30,17 @@ protoc \
   -I=/proto/include \
   --go_out $GOPATH/src \
   --go-grpc_out $GOPATH/src \
-  --go-grpc_opt paths=source_relative \
   ${PROTO_PATH}/message.proto
 
 protoc \
   -I=/proto/include \
   --go_out ${GOPATH}/src \
   --go-grpc_out ${GOPATH}/src \
-  --go-grpc_opt paths=source_relative \
-  --grpc-gateway_out ${GRPC_PATH} \
+  --grpc-gateway_out ${GOPATH}/src \
   --grpc-gateway_opt logtostderr=true \
   --openapiv2_out ${OPENAPI_PATH} \
   --openapiv2_opt logtostderr=true \
-  --authoption_out=${GRPC_PATH} \
+  --authoption_out=${GOPATH}/src \
   --validate_out=lang=go:${GOPATH}/src \
   ${PROTO_PATH}/admin.proto
 
@@ -35,13 +48,12 @@ protoc \
   -I=/proto/include \
   --go_out $GOPATH/src \
   --go-grpc_out $GOPATH/src \
-  --go-grpc_opt paths=source_relative \
-  --grpc-gateway_out ${GRPC_PATH} \
+  --grpc-gateway_out ${GOPATH}/src \
   --grpc-gateway_opt logtostderr=true \
   --grpc-gateway_opt allow_delete_body=true \
   --openapiv2_out ${OPENAPI_PATH} \
   --openapiv2_opt logtostderr=true \
-  --authoption_out=${GRPC_PATH} \
+  --authoption_out=${GOPATH}/src \
   --validate_out=lang=go:${GOPATH}/src \
   ${PROTO_PATH}/management.proto
 
@@ -49,12 +61,11 @@ protoc \
   -I=/proto/include \
   --go_out $GOPATH/src \
   --go-grpc_out $GOPATH/src \
-  --go-grpc_opt paths=source_relative \
-  --grpc-gateway_out ${GRPC_PATH} \
+  --grpc-gateway_out ${GOPATH}/src \
   --grpc-gateway_opt logtostderr=true \
   --openapiv2_out ${OPENAPI_PATH} \
   --openapiv2_opt logtostderr=true \
-  --authoption_out=${GRPC_PATH} \
+  --authoption_out=${GOPATH}/src \
   --validate_out=lang=go:${GOPATH}/src \
   ${PROTO_PATH}/auth.proto
 
