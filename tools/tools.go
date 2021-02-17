7f472ac1da433e268e31b5041ba662b979b25e51
adlerhurst <silvan.reusser@gmail.com>
2021-02-17 14:54:22 +0100

    update proto dependencies

--- tools/tools.go
+++ tools/tools.go
@@ -7,12 +7,14 @@ import (
 	_ "github.com/envoyproxy/protoc-gen-validate"
 	//proto custom authoptions
 	_ "github.com/go-bindata/go-bindata/v3/go-bindata"
-	//proto
-	_ "github.com/golang/protobuf/protoc-gen-go"
-	//proto gateway
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	//proto gateway
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	// gateway grpc to rest
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	// openapi v2 descriptions
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	// grpc generator
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	//protoc
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 	//generate static files
 	_ "github.com/rakyll/statik"
 	//proto
