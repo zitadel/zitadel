40449109519d6e954e82266a4e2805d64b21294e
Livio Amstutz <livio.a@gmail.com>
2021-02-18 11:19:56 +0100

    fix step 11

--- internal/v2/command/setup_step11.go
+++ internal/v2/command/setup_step11.go
@@ -22,12 +22,16 @@ func (s *Step11) execute(ctx context.Context, commandSide *CommandSide) error {
 func (r *CommandSide) SetupStep11(ctx context.Context, step *Step11) error {
 	fn := func(iam *IAMWriteModel) (*iam_repo.Aggregate, error) {
 		iamAgg := IAMAggregateFromWriteModel(&iam.WriteModel)
-		uniqueConstraints := NewUniqueConstraintReadModel(ctx, r)
-		err := r.eventstore.FilterToQueryReducer(ctx, uniqueConstraints)
-		if err != nil {
-			return nil, err
+		var uniqueContraintMigrations []*domain.UniqueConstraintMigration
+		if step.MigrateV1EventstoreToV2 {
+			uniqueConstraints := NewUniqueConstraintReadModel(ctx, r)
+			err := r.eventstore.FilterToQueryReducer(ctx, uniqueConstraints)
+			if err != nil {
+				return nil, err
+			}
+			uniqueContraintMigrations = uniqueConstraints.UniqueConstraints
 		}
-		iamAgg.PushEvents(iam_repo.NewMigrateUniqueConstraintEvent(ctx, uniqueConstraints.UniqueConstraints))
+		iamAgg.PushEvents(iam_repo.NewMigrateUniqueConstraintEvent(ctx, uniqueContraintMigrations))
 		logging.Log("SETUP-M9fsd").Info("migrate v1 eventstore to v2")
 		return iamAgg, nil
 	}
