116a73c13856535d6150fa578e6bf42debdbab16
Fabiennne <fabienne.gerschwiler@gmail.com>
2021-02-18 08:13:30 +0100

    fix: index

--- migrations/cockroach/V1.31__indexes.sql
+++ migrations/cockroach/V1.31__indexes.sql
@@ -1,1 +1,2 @@
-CREATE INDEX ON eventstore.events (event_type, resource_owner);
+CREATE INDEX event_type ON eventstore.events (event_type);
+CREATE INDEX resource_owner ON eventstore.events (resource_owner);
