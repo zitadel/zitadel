aa03800af274ef03bfe4ab010448fd173a465066
Livio Amstutz <livio.a@gmail.com>
2021-02-24 10:30:23 +0100

    set password for queries on tests

--- internal/eventstore/repository/sql/local_crdb_test.go
+++ internal/eventstore/repository/sql/local_crdb_test.go
@@ -99,7 +99,7 @@ func setPasswordNULL() error {
 		"authpassword",
 		"notificationpassword",
 		"authzpassword",
-		"queries",
+		"queriespassword",
 	}
 	for _, name := range passwordNames {
 		if err := os.Setenv(name, "NULL"); err != nil {
