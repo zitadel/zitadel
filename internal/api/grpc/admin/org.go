bcbd33a243d6b52ebbb686ffeae3d251589d831a
adlerhurst <silvan.reusser@gmail.com>
2021-02-24 17:52:57 +0100

    start admin

---
+++ internal/api/grpc/admin/org.go
@@ -0,0 +1,44 @@
+package admin
+
+import (
+	"context"
+
+	org_grpc "github.com/caos/zitadel/internal/api/grpc/org"
+	"github.com/caos/zitadel/internal/errors"
+	admin_pb "github.com/caos/zitadel/pkg/grpc/admin"
+	"github.com/golang/protobuf/ptypes/empty"
+)
+
+func (s *Server) IsOrgUnique(ctx context.Context, req *admin_pb.IsOrgUniqueRequest) (*admin_pb.IsOrgUniqueResponse, error) {
+	isUnique, err := s.org.IsOrgUnique(ctx, req.Name, req.Domain)
+	return &admin_pb.IsOrgUniqueResponse{IsUnique: isUnique}, err
+}
+
+func (s *Server) GetOrgByID(ctx context.Context, req *admin_pb.GetOrgByIDRequest) (*admin_pb.GetOrgByIDResponse, error) {
+	org, err := s.org.OrgByID(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &admin_pb.GetOrgByIDResponse{Org: org_grpc.OrgToPb(org)}, nil
+}
+
+func (s *Server) ListOrgs(ctx context.Context, req *admin_pb.ListOrgsRequest) (*admin_pb.ListOrgsResponse, error) {
+	query, err := orgListRequestToModel(req)
+	if err != nil {
+		return nil, err
+	}
+	orgs, err := s.org.SearchOrgs(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	return &admin_pb.ListOrgsResponse{Result: org_grpc.OrgsToPb(orgs.Result)}, nil
+}
+
+func (s *Server) SetUpOrg(ctx context.Context, req *admin_pb.SetUpOrgRequest) (*admin_pb.SetUpOrgResponse, error) {
+	human, _ := userCreateRequestToDomain(orgSetUp.User)
+	if human == nil {
+		return &empty.Empty{}, errors.ThrowPreconditionFailed(nil, "ADMIN-4nd9f", "Errors.User.NotHuman")
+	}
+	err = s.command.SetUpOrg(ctx, orgCreateRequestToDomain(orgSetUp.Org), human)
+	return &admin_pb.SetUpOrgResponse{}, nil
+}
