d6222a03e9a74f16e7c9896fbfc5fbb127a5f67a
Elio Bischof <eliobischof@gmail.com>
2021-02-25 16:27:16 +0100

    test: pass

--- operator/zitadel/kinds/iam/zitadel/ingress/adapt_test.go
+++ operator/zitadel/kinds/iam/zitadel/ingress/adapt_test.go
@@ -5,20 +5,17 @@ import (
 
 	"github.com/caos/zitadel/operator/zitadel/kinds/iam/zitadel/configuration"
 
-	kubernetesmock "github.com/caos/orbos/pkg/kubernetes/mock"
-	"github.com/golang/mock/gomock"
-
 	"github.com/caos/orbos/mntr"
 	"github.com/caos/orbos/pkg/labels/mocklabels"
 )
 
 func TestAdaptFunc(t *testing.T) {
-	q, _, err := AdaptFunc(mntr.Monitor{}, mocklabels.Api, "", "", 0, "", 0, "", 0, &configuration.Ingress{
-		Subdomains: &configuration.Subdomains{},
-	})
-	if err != nil {
-		t.Fatal(err)
+	cover := func(controller string) {
+		AdaptFunc(mntr.Monitor{}, mocklabels.Api, "", "", 0, "", 0, "", 0, &configuration.Ingress{
+			Subdomains: &configuration.Subdomains{},
+			Controller: controller,
+		})
 	}
-
-	q(kubernetesmock.NewMockClientInt(gomock.NewController(t)), make(map[string]interface{}))
+	cover("NGINX")
+	cover("Ambassador")
 }
