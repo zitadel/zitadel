88368265e8fcf63a7241a4d8b032ff2e3e4102b8
Livio Amstutz <livio.a@gmail.com>
2021-03-17 11:15:11 +0100

    add setup step

--- internal/command/iam_features.go
+++ internal/command/iam_features.go
@@ -2,13 +2,32 @@ package command
 
 import (
 	"context"
+
 	caos_errs "github.com/caos/zitadel/internal/errors"
+	"github.com/caos/zitadel/internal/repository/iam"
 
 	"github.com/caos/zitadel/internal/domain"
 )
 
 func (c *Commands) SetDefaultFeatures(ctx context.Context, features *domain.Features) (*domain.ObjectDetails, error) {
 	existingFeatures := NewIAMFeaturesWriteModel()
+	setEvent, err := c.setDefaultFeatures(ctx, existingFeatures, features)
+	if err != nil {
+		return nil, err
+	}
+
+	pushedEvents, err := c.eventstore.PushEvents(ctx, setEvent)
+	if err != nil {
+		return nil, err
+	}
+	err = AppendAndReduce(existingFeatures, pushedEvents...)
+	if err != nil {
+		return nil, err
+	}
+	return writeModelToObjectDetails(&existingFeatures.WriteModel), nil
+}
+
+func (c *Commands) setDefaultFeatures(ctx context.Context, existingFeatures *IAMFeaturesWriteModel, features *domain.Features) (*iam.FeaturesSetEvent, error) {
 	err := c.eventstore.FilterToQueryReducer(ctx, existingFeatures)
 	if err != nil {
 		return nil, err
@@ -31,14 +50,5 @@ func (c *Commands) SetDefaultFeatures(ctx context.Context, features *domain.Feat
 	if !hasChanged {
 		return nil, caos_errs.ThrowPreconditionFailed(nil, "Features-GE4h2", "Errors.Features.NotChanged")
 	}
-
-	pushedEvents, err := c.eventstore.PushEvents(ctx, setEvent)
-	if err != nil {
-		return nil, err
-	}
-	err = AppendAndReduce(existingFeatures, pushedEvents...)
-	if err != nil {
-		return nil, err
-	}
-	return writeModelToObjectDetails(&existingFeatures.WriteModel), nil
+	return setEvent, nil
 }
