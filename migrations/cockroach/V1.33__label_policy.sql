aa65da15879ad3b04eb9bfecb9689180ad317e3f
fabi <fabienne.gerschwiler@gmail.com>
2021-03-24 09:21:04 +0100

    fix: migrations

--- migrations/cockroach/V1.33__label_policy.sql
+++ migrations/cockroach/V1.33__label_policy.sql
@@ -1,6 +1,6 @@
 
-ALTER TABLE management.label_policy ADD COLUMN hide_login_name_suffix BOOLEAN;
-ALTER TABLE adminapi.label_policy ADD COLUMN hide_login_name_suffix BOOLEAN;
+ALTER TABLE management.label_policies ADD COLUMN hide_login_name_suffix BOOLEAN;
+ALTER TABLE adminapi.label_policies ADD COLUMN hide_login_name_suffix BOOLEAN;
 
 
 CREATE TABLE auth.label_policies (
