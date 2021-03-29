a8576be85edb876004bc073aa0d8fa5460ac76ad
adlerhurst <silvan.reusser@gmail.com>
2021-03-29 15:08:25 +0200

    running example

--- examples/client/go.mod
+++ examples/client/go.mod
@@ -1,3 +1,12 @@
 module github.com/caos/zitadel/examples/client
 
 go 1.15
+
+require (
+	github.com/caos/zitadel v0.118.2
+	github.com/envoyproxy/protoc-gen-validate v0.5.0
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	google.golang.org/genproto v0.0.0-20210325224202-eed09b1b5210
+	google.golang.org/grpc v1.36.1
+	google.golang.org/protobuf v1.26.0
+)
--- examples/client/main.go
+++ examples/client/main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"crypto/x509"
+	"log"
+	"strings"
+
+	pb "github.com/caos/zitadel/examples/client/zitadel/admin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+const zitadelAPI = "api.zitadel.ch:433"
+
+func main() {
+	conn, err := grpc.Dial(zitadelAPI, grpc.WithTransportCredentials(cert()))
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewAdminServiceClient(conn)
+	res, err := client.Healthz(context.TODO(), nil)
+	log.Println(res, err)
+}
+
+func cert() credentials.TransportCredentials {
+	ca, err := x509.SystemCertPool()
+	if err != nil {
+		log.Println("unable to load cert pool")
+	}
+	if ca == nil {
+		ca = x509.NewCertPool()
+	}
+
+	servernameWithoutPort := strings.Split(zitadelAPI, ":")[0]
+	return credentials.NewClientTLSFromCert(ca, servernameWithoutPort)
+}
