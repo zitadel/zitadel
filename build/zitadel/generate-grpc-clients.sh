b608f080801deb7578307052c061e758df6a788f
adlerhurst <silvan.reusser@gmail.com>
2021-03-29 14:30:48 +0200

    message

--- build/zitadel/generate-grpc-clients.sh
+++ build/zitadel/generate-grpc-clients.sh
@@ -12,12 +12,20 @@ else
     GRPC_OUT=$GO_OUT
 fi
 
+if [ -n $3 ]; then
+    GO_MESSAGE_IMPORT=$3
+else
+    echo "need message import"
+    exit 3
+fi
+
 generate () {
     protoc \
         -I=/.tmp/protos \
         -I=/go/src/github.com/caos/zitadel/pkg/grpc/message \
         -I=/go/src/github.com/caos/zitadel/internal/protoc/protoc-gen-authoption \
         -I=$1 \
+        --go_opt=Mproto/message.proto=${GO_MESSAGE_IMPORT} \
         --go_out ${GO_OUT} \
         --go-grpc_out ${GO_OUT} \
         $1/$2
