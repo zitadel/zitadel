0a31890e98b7b15f11504f12735f668f11a0e0dd
adlerhurst <silvan.reusser@gmail.com>
2021-04-06 17:44:33 +0200

    all sub protos

--- build/zitadel/generate-grpc-clients.sh
+++ build/zitadel/generate-grpc-clients.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 if [ -n $1 ]; then
-    GO_MESSAGE_IMPORT=$1/zitadel/message
+    ZITADEL_IMPORT=$1/zitadel
 else
     echo "need message import"
     exit 3
@@ -10,7 +10,16 @@ fi
 protoc \
     -I=/proto/include \
     -I=/proto/include/zitadel \
-    --go_opt=Mzitadel/message.proto=${GO_MESSAGE_IMPORT} \
+    --go_opt=Mzitadel/app.proto=${ZITADEL_IMPORT}/app \
+    --go_opt=Mzitadel/authn.proto=${ZITADEL_IMPORT}/authn \
+    --go_opt=Mzitadel/change.proto=${ZITADEL_IMPORT}/change \
+    --go_opt=Mzitadel/member.proto=${ZITADEL_IMPORT}/member \
+    --go_opt=Mzitadel/message.proto=${ZITADEL_IMPORT}/message \
+    --go_opt=Mzitadel/object.proto=${ZITADEL_IMPORT}/object \
+    --go_opt=Mzitadel/org.proto=${ZITADEL_IMPORT}/org \
+    --go_opt=Mzitadel/policy.proto=${ZITADEL_IMPORT}/policy \
+    --go_opt=Mzitadel/project.proto=${ZITADEL_IMPORT}/project \
+    --go_opt=Mzitadel/user.proto=${ZITADEL_IMPORT}/user \
     --go_out /go/src \
     --go-grpc_out /go/src \
     $(find /proto/include/zitadel -iname *.proto)
