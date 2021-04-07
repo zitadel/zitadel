c02022b7efe4fa2f98d92756f50ef20fb936764f
fabi <fabienne.gerschwiler@gmail.com>
2021-04-07 11:24:48 +0200

    fix: list granted project roles

--- internal/api/grpc/management/project.go
+++ internal/api/grpc/management/project.go
@@ -71,6 +71,26 @@ func (s *Server) ListGrantedProjects(ctx context.Context, req *mgmt_pb.ListGrant
 	}, nil
 }
 
+func (s *Server) ListGrantedProjectRoles(ctx context.Context, req *mgmt_pb.ListGrantedProjectRolesRequest) (*mgmt_pb.ListGrantedProjectRolesResponse, error) {
+	queries, err := ListGrantedProjectRolesRequestToModel(req)
+	if err != nil {
+		return nil, err
+	}
+	queries.AppendMyOrgQuery(authz.GetCtxData(ctx).OrgID)
+	roles, err := s.project.SearchProjectGrantRoles(ctx, req.ProjectId, req.GrantId, queries)
+	if err != nil {
+		return nil, err
+	}
+	return &mgmt_pb.ListGrantedProjectRolesResponse{
+		Result: project_grpc.RolesToPb(roles.Result),
+		Details: object_grpc.ToListDetails(
+			roles.TotalResult,
+			roles.Sequence,
+			roles.Timestamp,
+		),
+	}, nil
+}
+
 func (s *Server) ListProjectChanges(ctx context.Context, req *mgmt_pb.ListProjectChangesRequest) (*mgmt_pb.ListProjectChangesResponse, error) {
 	sequence, limit, asc := change_grpc.ChangeQueryToModel(req.Query)
 	features, err := s.features.GetOrgFeatures(ctx, authz.GetCtxData(ctx).OrgID)
