e4dd9bfb3eec40930d1452c3a67ad6448a1d2110
Elio Bischof <eliobischof@gmail.com>
2021-04-13 15:55:23 +0200

    chore: pass compile time data

--- build/operator/ldflags.sh
+++ build/operator/ldflags.sh
@@ -7,4 +7,4 @@ if [ "${VERSION}" == "" ]; then
   VERSION="$(git rev-parse --abbrev-ref HEAD | sed -e 's/heads\///')"
 fi
 
-echo -n "-extldflags '-static' -X main.Version=${VERSION} -X main.githubClientID=${GITHUBOAUTHCLIENTID} -X main.githubClientSecret=${GITHUBOAUTHCLIENTSECRET}"
+echo -n "-extldflags -static -X main.Version=${VERSION} -X main.githubClientID=${GITHUBOAUTHCLIENTID} -X main.githubClientSecret=${GITHUBOAUTHCLIENTSECRET}"
