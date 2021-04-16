badc26ba793936f7b25c428946102de69a9f5237
adlerhurst <silvan.reusser@gmail.com>
2021-04-16 16:39:18 +0200

    statements

--- internal/eventstore/handler.go
+++ internal/eventstore/handler.go
@@ -2,7 +2,10 @@ package eventstore
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/caos/logging"
@@ -93,8 +96,33 @@ type Column struct {
 	Value interface{}
 }
 
+func columnsToQuery(cols []Column) (names []string, parameters []string, values []interface{}) {
+	names = make([]string, len(cols))
+	values = make([]interface{}, len(cols))
+	parameters = make([]string, len(cols))
+	for i, col := range cols {
+		names[i] = col.Name
+		values[i] = col.Value
+		parameters[i] = "$" + strconv.Itoa(i+1)
+
+	}
+	return names, parameters, values
+}
+
+func columnsToWhere(cols []Column, paramOffset int) (wheres []string, values []interface{}) {
+	wheres = make([]string, len(cols))
+	values = make([]interface{}, len(cols))
+
+	for i, col := range cols {
+		wheres[i] = "(" + col.Name + " = " + strconv.Itoa(i+1+paramOffset) + ")"
+		values[i] = col.Value
+	}
+
+	return wheres, values
+}
+
 type Statement interface {
-	ToSQL() (stmt string, args []interface{})
+	Prepare(ctx context.Context, tx *sql.Tx) func() (sql.Result, error)
 }
 
 type CreateStatement struct {
@@ -102,24 +130,54 @@ type CreateStatement struct {
 	Values    []Column
 }
 
-func (stmt *CreateStatement) ToSQL() (query string, args []interface{}) {
-	// db := sql.OpenDB(nil)
-	return fmt.Sprintf("INSERT INTO %s", stmt.TableName), nil
+func (stmt *CreateStatement) Prepare(ctx context.Context, tx *sql.Tx) func() (sql.Result, error) {
+	cols, params, args := columnsToQuery(stmt.Values)
+	query := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", stmt.TableName, strings.Join(cols, ", "), strings.Join(params, ", "))
+	statement, err := tx.PrepareContext(ctx, query)
+
+	return func() (sql.Result, error) {
+		if err != nil {
+			return nil, err
+		}
+		return statement.ExecContext(ctx, args)
+	}
 }
 
 type UpdateStatement struct {
-	PK     []Column
-	Values []Column
+	TableName string
+	PK        []Column
+	Values    []Column
 }
 
-func (stmt *UpdateStatement) ToSQL() (string, []interface{}) {
-	return "", nil
+func (stmt *UpdateStatement) Prepare(ctx context.Context, tx *sql.Tx) func() (sql.Result, error) {
+	cols, params, args := columnsToQuery(stmt.Values)
+	wheres, whereArgs := columnsToWhere(stmt.PK, len(params))
+	args = append(args, whereArgs)
+	query := fmt.Sprintf("UPDATE %s SET (%s) = (%s) WHERE %s", stmt.TableName, strings.Join(cols, ", "), strings.Join(params, ", "), strings.Join(wheres, " AND "))
+	statement, err := tx.PrepareContext(ctx, query)
+
+	return func() (sql.Result, error) {
+		if err != nil {
+			return nil, err
+		}
+		return statement.ExecContext(ctx, args)
+	}
 }
 
 type DeleteStatement struct {
-	PK []Column
+	TableName string
+	PK        []Column
 }
 
-func (stmt *DeleteStatement) ToSQL() (string, []interface{}) {
-	return "", nil
+func (stmt *DeleteStatement) Prepare(ctx context.Context, tx *sql.Tx) func() (sql.Result, error) {
+	wheres, args := columnsToWhere(stmt.PK, 0)
+	query := fmt.Sprintf("DELETE FROM %s WHERE %s", stmt.TableName, strings.Join(wheres, " AND "))
+	statement, err := tx.PrepareContext(ctx, query)
+
+	return func() (sql.Result, error) {
+		if err != nil {
+			return nil, err
+		}
+		return statement.ExecContext(ctx, args)
+	}
 }
