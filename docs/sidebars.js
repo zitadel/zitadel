de4b9c160ae7b8df8cfffcaaf3824302dc901240
Florian Forster <florian@caos.ch>
2021-04-20 23:26:12 +0200

    fix(console): link to quickstarts

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -5,11 +5,13 @@ module.exports = {
       type: 'category',
       label: 'User',
       items: ['manuals/user'],
+      collapsed: false,
     },
     {
       type: 'category',
       label: 'Administrator',
       items: ['manuals/admin-managers'],
+      collapsed: false,
     },
   ],
   quickstarts: [
@@ -18,11 +20,13 @@ module.exports = {
       type: 'category',
       label: 'Single Page Applications',
       items: ['quickstarts/angular'],
+      collapsed: false,
     },
     {
       type: 'category',
       label: 'Identity Aware Proxy',
       items: ['quickstarts/oauth2-proxy'],
+      collapsed: false,
     }
   ],
   guides: [
@@ -31,6 +35,7 @@ module.exports = {
       type: 'category',
       label: 'Get to know ZITADEL',
       items: ['guides/get-started', 'guides/organizations', 'guides/projects', 'guides/oauth-recommended-flows', 'guides/serviceusers', 'guides/access-zitadel-apis', 'guides/identity-brokering'],
+      collapsed: false,
     },
   ],
   apis: [
@@ -56,11 +61,13 @@ module.exports = {
         'apis/proto/object',
         'apis/proto/options',
       ],
+      collapsed: false,
     },
     {
       type: 'category',
       label: 'OpenID Connect & OAuth',
       items: ['apis/openidoauth/endpoints', 'apis/openidoauth/scopes', 'apis/openidoauth/claims', 'apis/openidoauth/authn-methods', 'apis/openidoauth/grant-types'],
+      collapsed: false,
     },
   ],
   concepts: [
