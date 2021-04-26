292b7086cbb2e2ce78c6dcc48d18e9869297292c
Fabiennne <fabienne.gerschwiler@gmail.com>
2021-04-26 17:04:52 +0200

    fix: asset migrations

--- migrations/cockroach/V1.42__assets.sql
+++ migrations/cockroach/V1.42__assets.sql
@@ -1,7 +1,7 @@
 CREATE TABLE eventstore.assets (
     id TEXT,
 	asset TEXT,
-	PRIMARY KEY (unique_type, unique_field)
+	PRIMARY KEY (id)
 );
 
 GRANT DELETE ON TABLE eventstore.assets to eventstore;
