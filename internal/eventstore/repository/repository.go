1ba74754858978465dc890045af8179d8a2fcf44
Fabi <38692350+fgerschwiler@users.noreply.github.com>
2021-05-03 08:03:59 +0200

    Update internal/eventstore/repository/repository.go
    Co-authored-by: Livio Amstutz <livio.a@gmail.com>

--- internal/eventstore/repository/repository.go
+++ internal/eventstore/repository/repository.go
@@ -9,7 +9,6 @@ type Repository interface {
 	//Health checks if the connection to the storage is available
 	Health(ctx context.Context) error
 	// PushEvents adds all events of the given aggregates to the eventstreams of the aggregates.
-	// if assets are pushed, they will be added to the assets table only referenced by id in eventstore (history)
 	// if unique constraints are pushed, they will be added to the unique table for checking unique constraint violations
 	// This call is transaction save. The transaction will be rolled back if one event fails
 	Push(ctx context.Context, events []*Event, uniqueConstraints ...*UniqueConstraint) error
