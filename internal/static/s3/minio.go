fe74ec0ca1cdcd379b3b84cf90e46f1ac92b2480
fabi <fabienne.gerschwiler@gmail.com>
2021-05-03 08:22:11 +0200

    fix: error handling

--- internal/static/s3/minio.go
+++ internal/static/s3/minio.go
@@ -27,7 +27,7 @@ func NewMinio(config S3Config) (*Minio, error) {
 		Region: config.Location,
 	})
 	if err != nil {
-		return nil, err
+		return nil, caos_errs.ThrowInternal(err, "MINIO-4m90d", "Errors.Assets.Store.NotInitialized")
 	}
 	return &Minio{
 		Client:   minioClient,
@@ -41,18 +41,22 @@ func (m *Minio) CreateBucket(ctx context.Context, name, location string) error {
 	}
 	exists, err := m.Client.BucketExists(ctx, name)
 	if err != nil {
-		return err
+		return caos_errs.ThrowInternal(err, "MINIO-4m90d", "Errors.Assets.Bucket.Internal")
 	}
 	if exists {
 		return caos_errs.ThrowAlreadyExists(nil, "MINIO-9n3MK", "Errors.Assets.Bucket.AlreadyExists")
 	}
-	return m.Client.MakeBucket(ctx, name, minio.MakeBucketOptions{Region: location})
+	err = m.Client.MakeBucket(ctx, name, minio.MakeBucketOptions{Region: location})
+	if err != nil {
+		return caos_errs.ThrowInternal(err, "MINIO-4m90d", "Errors.Assets.Bucket.CreateFailed")
+	}
+	return nil
 }
 
 func (m *Minio) ListBuckets(ctx context.Context) ([]*domain.BucketInfo, error) {
 	infos, err := m.Client.ListBuckets(ctx)
 	if err != nil {
-		return nil, err
+		return nil, caos_errs.ThrowInternal(err, "MINIO-390OP", "Errors.Assets.Bucket.ListFailed")
 	}
 	buckets := make([]*domain.BucketInfo, len(infos))
 	for i, info := range infos {
@@ -65,13 +69,17 @@ func (m *Minio) ListBuckets(ctx context.Context) ([]*domain.BucketInfo, error) {
 }
 
 func (m *Minio) RemoveBucket(ctx context.Context, name string) error {
-	return m.Client.RemoveBucket(ctx, name)
+	err := m.Client.RemoveBucket(ctx, name)
+	if err != nil {
+		return caos_errs.ThrowInternal(err, "MINIO-338Hs", "Errors.Assets.Bucket.RemoveFailed")
+	}
+	return nil
 }
 
 func (m *Minio) PutObject(ctx context.Context, bucketName, objectName, contentType string, object io.Reader, objectSize int64) (*domain.AssetInfo, error) {
 	info, err := m.Client.PutObject(ctx, bucketName, objectName, object, objectSize, minio.PutObjectOptions{ContentType: contentType})
 	if err != nil {
-		return nil, err
+		return nil, caos_errs.ThrowInternal(err, "MINIO-590sw", "Errors.Assets.Object.PutFailed")
 	}
 	return &domain.AssetInfo{
 		Bucket:       info.Bucket,
@@ -87,11 +95,11 @@ func (m *Minio) PutObject(ctx context.Context, bucketName, objectName, contentTy
 func (m *Minio) GetObjectInfo(ctx context.Context, bucketName, objectName string) (*domain.AssetInfo, error) {
 	object, err := m.Client.GetObject(ctx, bucketName, objectName, minio.GetObjectOptions{})
 	if err != nil {
-		return nil, err
+		return nil, caos_errs.ThrowInternal(err, "MINIO-1vySX", "Errors.Assets.Object.GetFailed")
 	}
 	info, err := object.Stat()
 	if err != nil {
-		return nil, err
+		return nil, caos_errs.ThrowInternal(err, "MINIO-F96xF", "Errors.Assets.Object.GetFailed")
 	}
 	return m.objectToAssetInfo(bucketName, info), nil
 }
@@ -101,24 +109,24 @@ func (m *Minio) GetObjectPresignedURL(ctx context.Context, bucketName, objectNam
 	reqParams.Set("response-content-disposition", fmt.Sprintf("attachment; filename=\"%s\"", objectName))
 	presignedURL, err := m.Client.PresignedGetObject(ctx, bucketName, objectName, expiration, reqParams)
 	if err != nil {
-		return nil, err
+		return nil, caos_errs.ThrowInternal(err, "MINIO-19Mp0", "Errors.Assets.Object.PresignedTokenFailed")
 	}
-	return presignedURL, err
+	return presignedURL, nil
 }
 
-func (m *Minio) ListObjectInfos(ctx context.Context, bucketName, prefix string) ([]*domain.AssetInfo, error) {
+func (m *Minio) ListObjectInfos(ctx context.Context, bucketName, prefix string, recursive bool) ([]*domain.AssetInfo, error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
 	objectCh := m.Client.ListObjects(ctx, bucketName, minio.ListObjectsOptions{
 		Prefix:    prefix,
-		Recursive: true,
+		Recursive: recursive,
 	})
 	assetInfos := make([]*domain.AssetInfo, 0)
 	for object := range objectCh {
 		if object.Err != nil {
-			logging.LogWithFields("MINIO-wC8sd", "bucket-name", bucketName, "prefix", prefix).WithError(object.Err).Debug("unable to ger object")
-			return nil, nil
+			logging.LogWithFields("MINIO-wC8sd", "bucket-name", bucketName, "prefix", prefix).WithError(object.Err).Debug("unable to get object")
+			return nil, caos_errs.ThrowInternal(object.Err, "MINIO-1m09S", "Errors.Assets.Object.ListFailed")
 		}
 		assetInfos = append(assetInfos, m.objectToAssetInfo(bucketName, object))
 	}
@@ -126,7 +134,11 @@ func (m *Minio) ListObjectInfos(ctx context.Context, bucketName, prefix string) 
 }
 
 func (m *Minio) RemoveObject(ctx context.Context, bucketName, objectName string) error {
-	return m.Client.RemoveObject(ctx, bucketName, objectName, minio.RemoveObjectOptions{})
+	err := m.Client.RemoveObject(ctx, bucketName, objectName, minio.RemoveObjectOptions{})
+	if err != nil {
+		return caos_errs.ThrowInternal(err, "MINIO-x85RT", "Errors.Assets.Object.RemoveFailed")
+	}
+	return nil
 }
 
 func (m *Minio) objectToAssetInfo(bucketName string, object minio.ObjectInfo) *domain.AssetInfo {
