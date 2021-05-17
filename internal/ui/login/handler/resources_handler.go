ac5222e75b24e03fbf71a6c0320011d6c5a8548f
fabi <fabienne.gerschwiler@gmail.com>
2021-05-17 09:14:11 +0200

    feat: dynamic handler

--- internal/ui/login/handler/resources_handler.go
+++ internal/ui/login/handler/resources_handler.go
@@ -2,8 +2,32 @@ package handler
 
 import (
 	"net/http"
+	"time"
+
+	"github.com/caos/zitadel/internal/domain"
 )
+
+type dynamicResourceData struct {
+	OrgID         string `schema:"orgId"`
+	DefaultPolicy bool   `schema:"default-policy"`
+}
 
 func (l *Login) handleResources(staticDir http.FileSystem) http.Handler {
 	return http.FileServer(staticDir)
+}
+
+func (l *Login) handleDynamicResources(w http.ResponseWriter, r *http.Request) {
+	data := new(dynamicResourceData)
+	err := l.getParseData(r, data)
+
+	bucketName := domain.IAMID
+	if data.OrgID != "" && !data.DefaultPolicy {
+		bucketName = data.OrgID
+	}
+	presignedURL, err := l.staticStorage.GetObjectPresignedURL(r.Context(), bucketName, domain.CssPath+"/"+domain.CssVariablesFileName, time.Hour*1)
+	if err != nil {
+		return ""
+	}
+	return presignedURL.String()
+
 }
