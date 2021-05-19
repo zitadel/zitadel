5ca23fb9b4972ae0bdf40cb5f6eb09a7080f5850
Livio Amstutz <livio.a@gmail.com>
2021-05-19 07:29:18 +0200

    remove test branch again

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ["main", "livio-a-patch-1"],
+    branches: ["main"],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
