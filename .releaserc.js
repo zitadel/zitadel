dc081744b21f35e2c806cae5dfb37fdf6e7e0162
Livio Amstutz <livio.a@gmail.com>
2021-05-19 07:07:29 +0200

    try branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ["main"],
+    branches: ["main", "livio-a-patch-1"],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
