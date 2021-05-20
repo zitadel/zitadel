98187c16b3d239892b1b6807d6d3d6611aba2dee
Livio Amstutz <livio.a@gmail.com>
2021-05-20 14:32:22 +0200

    only current branch in releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ["main", "key-clientid"],
+    branches: ["key-clientid"],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
