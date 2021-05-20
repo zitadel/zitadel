1290c9d44ce8ec05c966ba1fc9e6bcdeea3569fc
Livio Amstutz <livio.a@gmail.com>
2021-05-20 13:46:49 +0200

    build on branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ["main"],
+    branches: ["main", "key-clientid"],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
