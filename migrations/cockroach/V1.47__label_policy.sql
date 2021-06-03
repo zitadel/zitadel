cf1e6bdf86301024110b984a935969ade19a0ca7
Livio Amstutz <livio.a@gmail.com>
2021-06-03 16:31:52 +0200

    fix migration

--- migrations/cockroach/V1.47__label_policy.sql
+++ migrations/cockroach/V1.47__label_policy.sql
@@ -102,4 +102,4 @@ ALTER TABLE auth.features ADD COLUMN label_policy_watermark BOOLEAN;
 ALTER TABLE authz.features RENAME COLUMN label_policy TO label_policy_private_label;
 ALTER TABLE authz.features ADD COLUMN label_policy_watermark BOOLEAN;
 ALTER TABLE management.features RENAME COLUMN label_policy TO label_policy_private_label;
-ALTER TABLE management.features
+ALTER TABLE management.features ADD COLUMN label_policy_watermark BOOLEAN;
