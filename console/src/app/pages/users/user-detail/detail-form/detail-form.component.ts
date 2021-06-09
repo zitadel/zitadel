f4d3809600e35dc09bbf18427a31fe8d1a9b9e5c
Max Peintner <max@caos.ch>
2021-06-09 11:55:57 +0200

    trigger dialog, styles

--- console/src/app/pages/users/user-detail/detail-form/detail-form.component.ts
+++ console/src/app/pages/users/user-detail/detail-form/detail-form.component.ts
@@ -1,92 +1,106 @@
 import { Component, EventEmitter, Input, OnChanges, OnDestroy, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 import { Gender, Human, User } from 'src/app/proto/generated/zitadel/user_pb';
 
+import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
 
 @Component({
-    selector: 'app-detail-form',
-    templateUrl: './detail-form.component.html',
-    styleUrls: ['./detail-form.component.scss'],
+  selector: 'app-detail-form',
+  templateUrl: './detail-form.component.html',
+  styleUrls: ['./detail-form.component.scss'],
 })
 export class DetailFormComponent implements OnDestroy, OnChanges {
-    @Input() public username!: string;
-    @Input() public user!: Human.AsObject;
-    @Input() public disabled: boolean = false;
-    @Input() public genders: Gender[] = [];
-    @Input() public languages: string[] = ['de', 'en'];
-    @Output() public submitData: EventEmitter<User> = new EventEmitter<User>();
-    @Output() public changedLanguage: EventEmitter<string> = new EventEmitter<string>();
+  @Input() public username!: string;
+  @Input() public user!: Human.AsObject;
+  @Input() public disabled: boolean = false;
+  @Input() public genders: Gender[] = [];
+  @Input() public languages: string[] = ['de', 'en'];
+  @Output() public submitData: EventEmitter<User> = new EventEmitter<User>();
+  @Output() public changedLanguage: EventEmitter<string> = new EventEmitter<string>();
 
-    public profileForm!: FormGroup;
+  public profileForm!: FormGroup;
 
-    private sub: Subscription = new Subscription();
+  private sub: Subscription = new Subscription();
 
-    constructor(private fb: FormBuilder) {
-        this.profileForm = this.fb.group({
-            userName: [{ value: '', disabled: true }, [
-                Validators.required,
-            ]],
-            firstName: [{ value: '', disabled: this.disabled }, Validators.required],
-            lastName: [{ value: '', disabled: this.disabled }, Validators.required],
-            nickName: [{ value: '', disabled: this.disabled }],
-            displayName: [{ value: '', disabled: this.disabled }, Validators.required],
-            gender: [{ value: 0, disabled: this.disabled }],
-            preferredLanguage: [{ value: '', disabled: this.disabled }],
-        });
-    }
+  constructor(private fb: FormBuilder, private dialog: MatDialog) {
+    this.profileForm = this.fb.group({
+      userName: [{ value: '', disabled: true }, [
+        Validators.required,
+      ]],
+      firstName: [{ value: '', disabled: this.disabled }, Validators.required],
+      lastName: [{ value: '', disabled: this.disabled }, Validators.required],
+      nickName: [{ value: '', disabled: this.disabled }],
+      displayName: [{ value: '', disabled: this.disabled }, Validators.required],
+      gender: [{ value: 0, disabled: this.disabled }],
+      preferredLanguage: [{ value: '', disabled: this.disabled }],
+    });
+  }
 
-    public ngOnChanges(): void {
-        this.profileForm = this.fb.group({
-            userName: [{ value: '', disabled: true }, [
-                Validators.required,
-            ]],
-            firstName: [{ value: '', disabled: this.disabled }, Validators.required],
-            lastName: [{ value: '', disabled: this.disabled }, Validators.required],
-            nickName: [{ value: '', disabled: this.disabled }],
-            displayName: [{ value: '', disabled: this.disabled }, Validators.required],
-            gender: [{ value: 0, disabled: this.disabled }],
-            preferredLanguage: [{ value: '', disabled: this.disabled }],
-        });
+  public ngOnChanges(): void {
+    this.profileForm = this.fb.group({
+      userName: [{ value: '', disabled: true }, [
+        Validators.required,
+      ]],
+      firstName: [{ value: '', disabled: this.disabled }, Validators.required],
+      lastName: [{ value: '', disabled: this.disabled }, Validators.required],
+      nickName: [{ value: '', disabled: this.disabled }],
+      displayName: [{ value: '', disabled: this.disabled }, Validators.required],
+      gender: [{ value: 0, disabled: this.disabled }],
+      preferredLanguage: [{ value: '', disabled: this.disabled }],
+    });
 
-        this.profileForm.patchValue({ userName: this.username, ...this.user.profile });
+    this.profileForm.patchValue({ userName: this.username, ...this.user.profile });
 
-        if (this.preferredLanguage) {
-            this.sub = this.preferredLanguage.valueChanges.subscribe(value => {
-                this.changedLanguage.emit(value);
-            });
-        }
+    if (this.preferredLanguage) {
+      this.sub = this.preferredLanguage.valueChanges.subscribe(value => {
+        this.changedLanguage.emit(value);
+      });
     }
+  }
 
-    public ngOnDestroy(): void {
-        this.sub.unsubscribe();
-    }
+  public ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
-    public submitForm(): void {
-        this.submitData.emit(this.profileForm.value);
-    }
+  public submitForm(): void {
+    this.submitData.emit(this.profileForm.value);
+  }
 
-    public get userName(): AbstractControl | null {
-        return this.profileForm.get('userName');
-    }
+  public openUploadDialog(): void {
+    const dialogRef = this.dialog.open(ProfilePictureComponent, {
+      data: {},
+      width: '400px',
+    });
 
-    public get firstName(): AbstractControl | null {
-        return this.profileForm.get('firstName');
-    }
-    public get lastName(): AbstractControl | null {
-        return this.profileForm.get('lastName');
-    }
-    public get nickName(): AbstractControl | null {
-        return this.profileForm.get('nickName');
-    }
-    public get displayName(): AbstractControl | null {
-        return this.profileForm.get('displayName');
-    }
-    public get gender(): AbstractControl | null {
-        return this.profileForm.get('gender');
-    }
-    public get preferredLanguage(): AbstractControl | null {
-        return this.profileForm.get('preferredLanguage');
-    }
+    dialogRef.afterClosed().subscribe(resp => {
+      if (resp) {
+      }
+    });
+  }
+
+  public get userName(): AbstractControl | null {
+    return this.profileForm.get('userName');
+  }
+
+  public get firstName(): AbstractControl | null {
+    return this.profileForm.get('firstName');
+  }
+  public get lastName(): AbstractControl | null {
+    return this.profileForm.get('lastName');
+  }
+  public get nickName(): AbstractControl | null {
+    return this.profileForm.get('nickName');
+  }
+  public get displayName(): AbstractControl | null {
+    return this.profileForm.get('displayName');
+  }
+  public get gender(): AbstractControl | null {
+    return this.profileForm.get('gender');
+  }
+  public get preferredLanguage(): AbstractControl | null {
+    return this.profileForm.get('preferredLanguage');
+  }
 
 }
--- console/src/app/pages/users/user-detail/detail-form/detail-form.module.ts
+++ console/src/app/pages/users/user-detail/detail-form/detail-form.module.ts
@@ -5,16 +5,20 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { TranslateModule } from '@ngx-translate/core';
+import { DropzoneModule } from 'src/app/directives/dropzone/dropzone.module';
 import { AvatarModule } from 'src/app/modules/avatar/avatar.module';
 import { InputModule } from 'src/app/modules/input/input.module';
 
 import { DetailFormComponent } from './detail-form.component';
+import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
 
 @NgModule({
   declarations: [
     DetailFormComponent,
+    ProfilePictureComponent,
   ],
   imports: [
+    DropzoneModule,
     AvatarModule,
     CommonModule,
     FormsModule,
