8107ee5eb7633ff2ea11e42ae6a7789cfc4d91eb
Maximilian Panne <maximilian.panne@gmail.com>
2021-06-16 08:33:26 +0200

    fix: update tos and privacy policy links

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -105,11 +105,11 @@ module.exports = {
             },
             {
               label: 'Terms and Conditions',
-              href: 'https://zitadel.ch/pdf/tos.pdf',
+              href: 'https://docs.zitadel.ch/docs/legal/terms-of-service',
             },
             {
               label: 'Privacy Policy',
-              href: 'https://zitadel.ch/pdf/privacy.pdf',
+              href: 'https://docs.zitadel.ch/docs/legal/privacy-policy',
             },
           ],
         },
