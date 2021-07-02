43e32a0d536c336a9ece4f0ff28b2430373fb5f2
Livio Amstutz <livio.a@gmail.com>
2021-07-02 14:42:06 +0200

    fix test

--- operator/zitadel/kinds/iam/zitadel/deployment/container_test.go
+++ operator/zitadel/kinds/iam/zitadel/deployment/container_test.go
@@ -7,11 +7,12 @@ import (
 	"github.com/caos/zitadel/operator/common"
 
 	"github.com/caos/orbos/pkg/kubernetes/k8s"
-	"github.com/caos/zitadel/operator/helpers"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/util/intstr"
+
+	"github.com/caos/zitadel/operator/helpers"
 )
 
 func TestDeployment_GetContainer(t *testing.T) {
@@ -85,21 +86,20 @@ func TestDeployment_GetContainer(t *testing.T) {
 					Key:                  "ZITADEL_ASSET_STORAGE_SECRET_ACCESS_KEY",
 				},
 			}},
+		{Name: "SENTRY_DSN",
+			ValueFrom: &corev1.EnvVarSource{
+				SecretKeyRef: &corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{Name: secretVarsName},
+					Key:                  "SENTRY_DSN",
+				},
+			},
+		},
 		{
 			Name: "CR_TEST_PASSWORD",
 			ValueFrom: &corev1.EnvVarSource{
 				SecretKeyRef: &corev1.SecretKeySelector{
 					LocalObjectReference: corev1.LocalObjectReference{Name: secretPasswordsName},
 					Key:                  "test",
-				},
-			},
-		},
-		{
-			Name: "SENTRY_DSN",
-			ValueFrom: &corev1.EnvVarSource{
-				SecretKeyRef: &corev1.SecretKeySelector{
-					LocalObjectReference: corev1.LocalObjectReference{Name: secretPasswordsName},
-					Key:                  "SENTRY_DSN",
 				},
 			},
 		},
