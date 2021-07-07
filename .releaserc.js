5b0b2af4478cb58d9e0c92aff7a3894bd896dc1d
Livio Amstutz <livio.a@gmail.com>
2021-07-07 11:33:54 +0200

    chore: build version on 1.17 maintenance branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,8 @@
 module.exports = {
-    branches: ["main"],
+    branches: [
+        {name: "main"},
+        {name: "1-17-x", range: "1.17.x"},
+    ],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
