90348d547e67329b1013cfa57899fd7b44b011d7
Max Peintner <max@caos.ch>
2021-07-12 15:36:36 +0200

    login text requests

--- console/src/app/modules/policies/login-texts/login-texts.component.ts
+++ console/src/app/modules/policies/login-texts/login-texts.component.ts
@@ -12,6 +12,28 @@ import {
   GetDefaultLoginTextsRequest,
   SetCustomLoginTextsRequest,
 } from 'src/app/proto/generated/zitadel/management_pb';
+import {
+  EmailVerificationDoneScreenText,
+  EmailVerificationScreenText,
+  ExternalUserNotFoundScreenText,
+  FooterText,
+  InitializeUserDoneScreenText,
+  InitializeUserScreenText,
+  InitMFADoneScreenText,
+  InitMFAOTPScreenText,
+  InitMFAPromptScreenText,
+  InitMFAU2FScreenText,
+  InitPasswordDoneScreenText,
+  InitPasswordScreenText,
+  LinkingUserDoneScreenText,
+  LoginScreenText,
+  LogoutDoneScreenText,
+  MFAProvidersText,
+  PasswordChangeDoneScreenText,
+  PasswordChangeScreenText,
+  PasswordResetDoneScreenText,
+  PasswordScreenText,
+} from 'src/app/proto/generated/zitadel/text_pb';
 import { AdminService } from 'src/app/services/admin.service';
 import { ManagementService } from 'src/app/services/mgmt.service';
 
@@ -146,27 +168,235 @@ const REQUESTMAP = {
     set: new AdminSetCustomLoginTextsRequest(),
     setFcn: (map: Partial<AdminSetCustomLoginTextsRequest.AsObject>): AdminSetCustomLoginTextsRequest => {
       const req = new AdminSetCustomLoginTextsRequest();
-      // req.setEmailVerificationDoneText(map.emailVerificationDoneText ?? '');
-      // req.setEmailVerificationText(map.emailVerificationText ?? '');
-      // req.setExternalUserNotFoundText(map.externalUserNotFoundText ?? '');
-      // req.setFooterText(map.footerText ?? '');
-      // req.setInitMfaDoneText(map.initMfaDoneText ?? '');
-      // req.setInitMfaOtpText(map.initMfaOtpText ?? '');
-      // req.setInitMfaPromptText(map.initMfaPromptText ?? '');
-      // req.setInitMfaU2fText(map.initMfaU2fText ?? '');
-      // req.setInitPasswordDoneText(map.initPasswordDoneText ?? '');
-      // req.setInitPasswordText(map.initPasswordText ?? '');
-      // req.setInitializeDoneText(map.initializeDoneText ?? '');
-      // req.setInitializeUserText(map.initializeUserText ?? '');
       req.setLanguage(map.language ?? '');
-      // req.setLinkingUserDoneText(map.linkingUserDoneText ?? '');
-      // req.setLoginText(map.loginText ?? '');
-      // req.setLogoutText(map.logoutText ?? '');
-      // req.setMfaProvidersText(map.mfaProvidersText ?? '');
-      // req.setPasswordChangeDoneText(map.passwordChangeDoneText ?? '');
-      // req.setPasswordChangeText(map.passwordChangeText ?? '');
-      // req.setPasswordResetDoneText(map.passwordResetDoneText ?? '');
-      // req.setPasswordText(map.passwordText ?? '');
+
+      map.emailVerificationDoneText ? () => {
+        const r = new EmailVerificationDoneScreenText();
+        r.setCancelButtonText(map.emailVerificationDoneText?.cancelButtonText ?? '');
+        r.setDescription(map.emailVerificationDoneText?.description ?? '');
+        r.setLoginButtonText(map.emailVerificationDoneText?.loginButtonText ?? '');
+        r.setNextButtonText(map.initializeUserText?.nextButtonText ?? '');
+        r.setTitle(map.initializeUserText?.title ?? '');
+
+        req.setEmailVerificationDoneText(r);
+      } : null;
+
+      map.emailVerificationText ? () => {
+        const r = new EmailVerificationScreenText();
+        r.setCodeLabel(map.emailVerificationText?.codeLabel ?? '');
+        r.setDescription(map.emailVerificationText?.description ?? '');
+        r.setNextButtonText(map.emailVerificationText?.nextButtonText ?? '');
+        r.setResendButtonText(map.emailVerificationText?.resendButtonText ?? '');
+        r.setTitle(map.emailVerificationText?.title ?? '');
+
+        req.setEmailVerificationText(r);
+      } : null;
+
+      map.externalUserNotFoundText ? () => {
+        const r = new ExternalUserNotFoundScreenText();
+        r.setAutoRegisterButtonText(map.externalUserNotFoundText?.autoRegisterButtonText ?? '');
+        r.setDescription(map.externalUserNotFoundText?.description ?? '');
+        r.setLinkButtonText(map.externalUserNotFoundText?.linkButtonText ?? '');
+        r.setTitle(map.externalUserNotFoundText?.title ?? '');
+
+        req.setExternalUserNotFoundText(r);
+      } : null;
+
+      map.footerText ? () => {
+        const r = new FooterText();
+        r.setHelp(map.footerText?.help ?? '');
+        r.setHelpLink(map.footerText?.helpLink ?? '');
+        r.setPrivacyPolicy(map.footerText?.privacyPolicy ?? '');
+        r.setPrivacyPolicyLink(map.footerText?.privacyPolicyLink ?? '');
+        r.setTos(map.footerText?.tos ?? '');
+        r.setTosLink(map.footerText?.tosLink ?? '');
+
+        req.setFooterText(r);
+      } : null;
+
+      map.initMfaDoneText ? () => {
+        const r = new InitMFADoneScreenText();
+        r.setCancelButtonText(map.initMfaDoneText?.cancelButtonText ?? '');
+        r.setDescription(map.initMfaDoneText?.description ?? '');
+        r.setNextButtonText(map.initMfaDoneText?.nextButtonText ?? '');
+        r.setTitle(map.initMfaDoneText?.title ?? '');
+
+        req.setInitMfaDoneText(r);
+      } : null;
+
+      map.initMfaOtpText ? () => {
+        const r = new InitMFAOTPScreenText();
+        r.setCancelButtonText(map.initMfaOtpText?.cancelButtonText ?? '');
+        r.setCodeLabel(map.initMfaOtpText?.codeLabel ?? '');
+        r.setDescription(map.initMfaOtpText?.description ?? '');
+        r.setDescriptionOtp(map.initMfaOtpText?.descriptionOtp ?? '');
+        r.setNextButtonText(map.initMfaOtpText?.nextButtonText ?? '');
+        r.setSecretLabel(map.initMfaOtpText?.secretLabel ?? '');
+        r.setTitle(map.initMfaOtpText?.title ?? '');
+
+        req.setInitMfaOtpText(r);
+      } : null;
+
+      map.initMfaPromptText ? () => {
+        const r = new InitMFAPromptScreenText();
+        r.setDescription(map.initMfaPromptText?.description ?? '');
+        r.setNextButtonText(map.initMfaPromptText?.nextButtonText ?? '');
+        r.setOtpOption(map.initMfaPromptText?.otpOption ?? '');
+        r.setSkipButtonText(map.initMfaPromptText?.skipButtonText ?? '');
+        r.setTitle(map.initMfaPromptText?.title ?? '');
+        r.setU2fOption(map.initMfaPromptText?.otpOption ?? '');
+
+        req.setInitMfaPromptText(r);
+      } : null;
+
+      map.initMfaU2fText ? () => {
+        const r = new InitMFAU2FScreenText();
+        r.setDescription(map.initMfaU2fText?.description ?? '');
+        r.setErrorRetry(map.initMfaU2fText?.errorRetry ?? '');
+        r.setNotSupported(map.initMfaU2fText?.notSupported ?? '');
+        r.setRegisterTokenButtonText(map.initMfaU2fText?.registerTokenButtonText ?? '');
+        r.setTitle(map.initMfaU2fText?.title ?? '');
+        r.setTokenNameLabel(map.initMfaU2fText?.tokenNameLabel ?? '');
+
+        req.setInitMfaU2fText(r);
+      } : null;
+
+      map.initPasswordDoneText ? () => {
+        const r = new InitPasswordDoneScreenText();
+        r.setCancelButtonText(map.initPasswordDoneText?.cancelButtonText ?? '');
+        r.setDescription(map.initPasswordDoneText?.description ?? '');
+        r.setNextButtonText(map.initPasswordDoneText?.nextButtonText ?? '');
+        r.setTitle(map.initPasswordDoneText?.title ?? '');
+
+        req.setInitPasswordDoneText(r);
+      } : null;
+
+      map.initPasswordText ? () => {
+        const r = new InitPasswordScreenText();
+        r.setCodeLabel(map.initPasswordText?.description ?? '');
+        r.setDescription(map.initPasswordText?.description ?? '');
+        r.setNewPasswordConfirmLabel(map.initPasswordText?.newPasswordConfirmLabel ?? '');
+        r.setNewPasswordLabel(map.initPasswordText?.newPasswordLabel ?? '');
+        r.setNextButtonText(map.initPasswordText?.nextButtonText ?? '');
+        r.setResendButtonText(map.initPasswordText?.resendButtonText ?? '');
+        r.setTitle(map.initPasswordText?.title ?? '');
+
+        req.setInitPasswordText(r);
+      } : null;
+
+      map.initializeDoneText ? () => {
+        const r = new InitializeUserDoneScreenText();
+        r.setCancelButtonText(map.initializeDoneText?.cancelButtonText ?? '');
+        r.setDescription(map.initializeDoneText?.description ?? '');
+        r.setNextButtonText(map.initializeDoneText?.nextButtonText ?? '');
+        r.setTitle(map.initializeDoneText?.title ?? '');
+
+        req.setInitializeDoneText(r);
+      } : null;
+
+      map.initializeUserText ? () => {
+        const initializeUserTextRequest = new InitializeUserScreenText();
+        initializeUserTextRequest.setCodeLabel(map.initializeUserText?.codeLabel ?? '');
+        initializeUserTextRequest.setDescription(map.initializeUserText?.description ?? '');
+        initializeUserTextRequest.setNewPasswordConfirmLabel(map.initializeUserText?.newPasswordConfirmLabel ?? '');
+        initializeUserTextRequest.setNewPasswordLabel(map.initializeUserText?.newPasswordLabel ?? '');
+        initializeUserTextRequest.setNextButtonText(map.initializeUserText?.nextButtonText ?? '');
+        initializeUserTextRequest.setResendButtonText(map.initializeUserText?.resendButtonText ?? '');
+        initializeUserTextRequest.setTitle(map.initializeUserText?.title ?? '');
+
+        req.setInitializeUserText(initializeUserTextRequest);
+      } : null;
+
+      map.linkingUserDoneText ? () => {
+        const r = new LinkingUserDoneScreenText();
+        r.setCancelButtonText(map.linkingUserDoneText?.cancelButtonText ?? '');
+        r.setDescription(map.linkingUserDoneText?.description ?? '');
+        r.setNextButtonText(map.linkingUserDoneText?.nextButtonText ?? '');
+        r.setTitle(map.linkingUserDoneText?.title ?? '');
+
+        req.setLinkingUserDoneText(r);
+      } : null;
+
+      map.loginText ? () => {
+        const r = new LoginScreenText();
+        r.setDescription(map.loginText?.description ?? '');
+        r.setDescriptionLinkingProcess(map.loginText?.descriptionLinkingProcess ?? '');
+        r.setExternalUserDescription(map.loginText?.externalUserDescription ?? '');
+        r.setLoginNameLabel(map.loginText?.loginNameLabel ?? '');
+        r.setLoginNamePlaceholder(map.loginText?.loginNamePlaceholder ?? '');
+        r.setNextButtonText(map.loginText?.nextButtonText ?? '');
+        r.setRegisterButtonText(map.loginText?.registerButtonText ?? '');
+        r.setTitle(map.loginText?.title ?? '');
+        r.setTitleLinkingProcess(map.loginText?.titleLinkingProcess ?? '');
+        r.setUserMustBeMemberOfOrg(map.loginText?.userMustBeMemberOfOrg ?? '');
+        r.setUserNamePlaceholder(map.loginText?.userNamePlaceholder ?? '');
+
+        req.setLoginText(r);
+      } : null;
+
+      map.logoutText ? () => {
+        const r = new LogoutDoneScreenText();
+        r.setDescription(map.logoutText?.description ?? '');
+        r.setLoginButtonText(map.logoutText?.loginButtonText ?? '');
+        r.setTitle(map.logoutText?.title ?? '');
+
+        req.setLogoutText(r);
+      } : null;
+
+      map.mfaProvidersText ? () => {
+        const r = new MFAProvidersText();
+        r.setChooseOther(map.mfaProvidersText?.chooseOther ?? '');
+        r.setOtp(map.mfaProvidersText?.otp ?? '');
+        r.setU2f(map.mfaProvidersText?.u2f ?? '');
+
+        req.setMfaProvidersText(r);
+      } : null;
+
+      map.passwordChangeDoneText ? () => {
+        const r = new PasswordChangeDoneScreenText();
+        r.setDescription(map.passwordChangeDoneText?.description ?? '');
+        r.setNextButtonText(map.passwordChangeDoneText?.nextButtonText ?? '');
+        r.setTitle(map.passwordChangeDoneText?.title ?? '');
+
+        req.setPasswordChangeDoneText(r);
+      } : null;
+
+      map.passwordChangeText ? () => {
+        const r = new PasswordChangeScreenText();
+        r.setDescription(map.passwordChangeText?.description ?? '');
+        r.setNextButtonText(map.passwordChangeText?.nextButtonText ?? '');
+        r.setTitle(map.passwordChangeText?.title ?? '');
+
+        req.setPasswordChangeText(r);
+      } : null;
+
+      map.passwordResetDoneText ? () => {
+        const r = new PasswordResetDoneScreenText();
+        r.setDescription(map.passwordResetDoneText?.description ?? '');
+        r.setNextButtonText(map.passwordResetDoneText?.nextButtonText ?? '');
+        r.setTitle(map.passwordResetDoneText?.title ?? '');
+
+        req.setPasswordResetDoneText(r);
+      } : null;
+
+      map.passwordText ? () => {
+        const r = new PasswordScreenText();
+        r.setBackButtonText(map.passwordText?.backButtonText ?? '');
+        r.setConfirmation(map.passwordText?.confirmation ?? '');
+        r.setDescription(map.passwordText?.description ?? '');
+        r.setHasLowercase(map.passwordText?.hasLowercase ?? '');
+        r.setHasNumber(map.passwordText?.hasNumber ?? '');
+        r.setHasSymbol(map.passwordText?.hasSymbol ?? '');
+        r.setHasUppercase(map.passwordText?.hasUppercase ?? '');
+        r.setMinLength(map.passwordText?.minLength ?? '');
+        r.setNextButtonText(map.passwordText?.nextButtonText ?? '');
+        r.setPasswordLabel(map.passwordText?.passwordLabel ?? '');
+        r.setResetLinkText(map.passwordText?.resetLinkText ?? '');
+        r.setTitle(map.passwordText?.title ?? '');
+
+        req.setPasswordText(r);
+      } : null;
+
       // req.setPasswordlessText(map.passwordlessText ?? '');
       // req.setRegistrationOptionText(map.registrationOptionText ?? '');
       // req.setRegistrationOrgText(map.registrationOrgText ?? '');
@@ -264,7 +494,7 @@ export class LoginTextsComponent implements OnDestroy {
 
   public updateCurrentValues(values: { [key: string]: string; }): void {
     const req = REQUESTMAP[this.serviceType].setFcn;
-    const mappedValues = req(values);
+    const mappedValues = req({ [this.currentSubMap]: values });
 
     console.log(mappedValues.toObject());
   }
@@ -277,7 +507,8 @@ export class LoginTextsComponent implements OnDestroy {
     return prom.then(res => {
       if (res.customText) {
         delete res.customText.details;
-        return Object.assign({}, res.customText as unknown as { [key: string]: string; });
+        console.log(Object.assign({}, res.customText));
+        return Object.assign({}, res.customText);
       } else {
         return {};
       }
