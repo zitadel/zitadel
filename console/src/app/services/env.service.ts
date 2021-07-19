a7113db5da0323045fed2a2ca2d9af5b4d475d99
Max Peintner <max@caos.ch>
2021-07-19 12:25:59 +0200

    lint

--- console/src/app/services/env.service.ts
+++ console/src/app/services/env.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EnvService {
   private env$!: Observable<any>;
