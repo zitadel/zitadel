aca63355784e300ebe55aa117049ae3a0127fc58
Max Peintner <max@caos.ch>
2021-08-09 12:00:14 +0200

    lint styles

--- console/src/app/pages/users/user-detail/membership-detail/membership-detail.component.ts
+++ console/src/app/pages/users/user-detail/membership-detail/membership-detail.component.ts
@@ -81,7 +81,7 @@ export class MembershipDetailComponent implements AfterViewInit {
                 console.error(err);
               });
             }
-          })
+          }),
         );
       })).subscribe();
   }
