f4660a568b2b9d47c06888d28bdda0df24788037
Fabi <38692350+fgerschwiler@users.noreply.github.com>
2021-08-11 08:22:20 +0200

    Update internal/command/setup_step4.go
    Co-authored-by: Silvan <silvan.reusser@gmail.com>

--- internal/command/setup_step4.go
+++ internal/command/setup_step4.go
@@ -19,7 +19,9 @@ func (s *Step4) Step() domain.Step {
 func (s *Step4) execute(ctx context.Context, commandSide *Commands) error {
 	return commandSide.SetupStep4(ctx, s)
 }
-
+//This step should not be executed when a new instance is setup, because its not used anymore
+//SetupStep4 is no op in favour of step 18.
+//Password lockout policy is replaced by lockout policy
 func (c *Commands) SetupStep4(ctx context.Context, step *Step4) error {
 	fn := func(iam *IAMWriteModel) ([]eventstore.EventPusher, error) {
 		return nil, nil
