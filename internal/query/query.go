9ff622ae2f93d6953ecab14b60f08bf723dbb05a
adlerhurst <silvan.reusser@gmail.com>
2021-08-25 13:47:58 +0200

    fix: fill old events with sequence

--- internal/query/query.go
+++ internal/query/query.go
@@ -44,11 +44,10 @@ func StartQueries(ctx context.Context, es *eventstore.Eventstore, projections pr
 		return nil, err
 	}
 
-	// turned off for this release
-	// err = projection.Start(ctx, es, projections)
-	// if err != nil {
-	// 	return nil, err
-	// }
+	err = projection.Start(ctx, es, projections)
+	if err != nil {
+		return nil, err
+	}
 
 	return repo, nil
 }
