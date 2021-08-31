c884a11f1be7e9df7ec72184ea4fd8b611a30cbf
Max Peintner <max@caos.ch>
2021-08-31 08:25:24 +0200

    feat(console): project privatelabelling, catch query param to set org context (#2277)
    * feat: privatelabeling setting, query param for context
    
    * lint
    
    * Update console/src/assets/i18n/de.json
    
    Co-authored-by: Fabi <38692350+fgerschwiler@users.noreply.github.com>
    
    * Update console/src/assets/i18n/de.json
    
    Co-authored-by: Fabi <38692350+fgerschwiler@users.noreply.github.com>
    
    * Update console/src/assets/i18n/en.json
    
    Co-authored-by: Fabi <38692350+fgerschwiler@users.noreply.github.com>
    
    * Update console/src/assets/i18n/en.json
    
    Co-authored-by: Fabi <38692350+fgerschwiler@users.noreply.github.com>
    
    Co-authored-by: Fabi <38692350+fgerschwiler@users.noreply.github.com>

--- console/src/app/pages/projects/granted-projects/granted-project-detail/granted-project-detail.component.ts
+++ console/src/app/pages/projects/granted-projects/granted-project-detail/granted-project-detail.component.ts
@@ -9,7 +9,7 @@ import { CreationType, MemberCreateDialogComponent } from 'src/app/modules/add-m
 import { ChangeType } from 'src/app/modules/changes/changes.component';
 import { UserGrantContext } from 'src/app/modules/user-grants/user-grants-datasource';
 import { Member } from 'src/app/proto/generated/zitadel/member_pb';
-import { GrantedProject, ProjectState } from 'src/app/proto/generated/zitadel/project_pb';
+import { GrantedProject, ProjectGrantState } from 'src/app/proto/generated/zitadel/project_pb';
 import { User } from 'src/app/proto/generated/zitadel/user_pb';
 import { ManagementService } from 'src/app/services/mgmt.service';
 import { ToastService } from 'src/app/services/toast.service';
@@ -24,7 +24,7 @@ export class GrantedProjectDetailComponent implements OnInit, OnDestroy {
   public grantId: string = '';
   public project!: GrantedProject.AsObject;
 
-  public ProjectState: any = ProjectState;
+  public ProjectGrantState: any = ProjectGrantState;
   public ChangeType: any = ChangeType;
 
   private subscription?: Subscription;
