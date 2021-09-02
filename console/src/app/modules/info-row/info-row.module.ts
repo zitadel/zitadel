502eab6b0dec67e72bb2749655e2ed2894585026
Max Peintner <max@caos.ch>
2021-09-02 15:46:43 +0200

    card cleanup, actions for user detail

--- console/src/app/modules/info-row/info-row.module.ts
+++ console/src/app/modules/info-row/info-row.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
 import { CopyToClipboardModule } from 'src/app/directives/copy-to-clipboard/copy-to-clipboard.module';
@@ -21,6 +22,7 @@ import { InfoRowComponent } from './info-row.component';
     MatTooltipModule,
     TranslateModule,
     CopyToClipboardModule,
+    MatButtonModule,
     LocalizedDatePipeModule,
     TimestampToDatePipeModule,
   ],
