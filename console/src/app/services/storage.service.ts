f99d6619c514c329055606fe1da4ffec081134c5
Christoph Bühler <cbuehler@rootd.ch>
2021-09-10 11:56:20 +0200

    feat(org-selector): Remember the last selected org and order the list.
    This remembers the last selected org
    in localstorage instead of session storage to improve the UX. Furthermore,
    the list of organizations is ordered by
    name instead of just the returned
    result set.

--- console/src/app/services/storage.service.ts
+++ console/src/app/services/storage.service.ts
@@ -7,28 +7,35 @@ const STORAGE_PREFIX = 'zitadel';
     providedIn: 'root',
 })
 export class StorageService implements OAuthStorage {
-    private storage: Storage = window.sessionStorage;
+    private sessionStorage: Storage = window.sessionStorage;
+    private localStorage: Storage = window.localStorage;
 
     constructor() { }
 
-    public setItem<TValue = string>(key: string, value: TValue): void {
-        this.storage.setItem(this.getPrefixedKey(key), JSON.stringify(value));
+    public setItem<TValue = string>(key: string, value: TValue, location: StorageLocation = StorageLocation.session): void {
+        this.getStorage(location).setItem(this.getPrefixedKey(key), JSON.stringify(value));
     }
 
-    public getItem<TResult = string>(key: string): TResult | null {
-        const result = this.storage.getItem(this.getPrefixedKey(key));
+    public getItem<TResult = string>(key: string, location: StorageLocation = StorageLocation.session): TResult | null {
+        const result = this.getStorage(location).getItem(this.getPrefixedKey(key));
         if (result) {
             return JSON.parse(result);
         }
         return null;
     }
 
-    public removeItem(key: string): void {
-        this.storage.removeItem(this.getPrefixedKey(key));
+    public removeItem(key: string, location: StorageLocation = StorageLocation.session): void {
+        this.getStorage(location).removeItem(this.getPrefixedKey(key));
     }
 
     public getPrefixedKey(key: string): string {
         return `${STORAGE_PREFIX}:${key}`;
+    }
+
+    private getStorage(location: StorageLocation): Storage {
+      return location === StorageLocation.session
+        ? this.sessionStorage
+        : this.localStorage;
     }
 }
 
@@ -39,4 +46,9 @@ export class StorageConfig {
 
 export enum StorageKey {
     organization = 'organization',
+}
+
+export enum StorageLocation {
+  session,
+  local,
 }
