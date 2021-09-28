dd68aa79f57107b057059876578f097393a2a2dc
adlerhurst <silvan.reusser@gmail.com>
2021-09-28 02:16:55 +0200

    fix: init provider

--- internal/auth/repository/eventsourcing/repository.go
+++ internal/auth/repository/eventsourcing/repository.go
@@ -98,6 +98,7 @@ func Start(conf Config, authZ authz.Config, systemDefaults sd.SystemDefaults, co
 		es,
 		userRepo,
 		eventstore.AuthRequestRepo{
+			PrivacyPolicyProvider:      queries,
 			Command:                    command,
 			OrgViewProvider:            queries,
 			AuthRequests:               authReq,
