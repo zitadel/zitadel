89adc7a6c7027ef51b1859964f273c6c0b27380d
Livio Amstutz <livio.a@gmail.com>
2021-09-28 14:34:35 +0200

    fix: typo in migration 1.73

--- migrations/cockroach/V1.73__auth_request_times.sql
+++ migrations/cockroach/V1.73__auth_request_times.sql
@@ -1,2 +1,2 @@
-ALTER TABLE auth.auth_requests ADD COLUMN creation_data TIMESTAMPTZ;
-ALTER TABLE auth.auth_requests ADD COLUMN change_data TIMESTAMPTZ;
+ALTER TABLE auth.auth_requests ADD COLUMN creation_date TIMESTAMPTZ;
+ALTER TABLE auth.auth_requests ADD COLUMN change_date TIMESTAMPTZ;
