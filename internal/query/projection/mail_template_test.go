a1f4a06d27f0199f0622fd7daa4110e8b6ddc561
Fabi <38692350+fgerschwiler@users.noreply.github.com>
2021-10-29 16:11:47 +0200

    feat: Message projection (#2591)
    * feat: mail template projection
    
    * feat: message text projection
    
    * feat: message text projection tests
    
    * feat: custom text projection
    
    * feat: custom text projection

---
+++ internal/query/projection/mail_template_test.go
@@ -0,0 +1,200 @@
+package projection
+
+import (
+	"testing"
+
+	"github.com/caos/zitadel/internal/domain"
+	"github.com/caos/zitadel/internal/errors"
+	"github.com/caos/zitadel/internal/eventstore"
+	"github.com/caos/zitadel/internal/eventstore/handler"
+	"github.com/caos/zitadel/internal/eventstore/repository"
+	"github.com/caos/zitadel/internal/repository/iam"
+	"github.com/caos/zitadel/internal/repository/org"
+)
+
+func TestMailTemplateProjection_reduces(t *testing.T) {
+	type args struct {
+		event func(t *testing.T) eventstore.EventReader
+	}
+	tests := []struct {
+		name   string
+		args   args
+		reduce func(event eventstore.EventReader) (*handler.Statement, error)
+		want   wantReduce
+	}{
+		{
+			name: "org.reduceAdded",
+			args: args{
+				event: getEvent(testEvent(
+					repository.EventType(org.MailTemplateAddedEventType),
+					org.AggregateType,
+					[]byte(`{
+						"template": "PHRhYmxlPjwvdGFibGU+"
+					}`),
+				), org.MailTemplateAddedEventMapper),
+			},
+			reduce: (&MailTemplateProjection{}).reduceAdded,
+			want: wantReduce{
+				aggregateType:    eventstore.AggregateType("org"),
+				sequence:         15,
+				previousSequence: 10,
+				projection:       MailTemplateTable,
+				executer: &testExecuter{
+					executions: []execution{
+						{
+							expectedStmt: "INSERT INTO zitadel.projections.mail_templates (aggregate_id, creation_date, change_date, sequence, state, is_default, template) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+							expectedArgs: []interface{}{
+								"agg-id",
+								anyArg{},
+								anyArg{},
+								uint64(15),
+								domain.PolicyStateActive,
+								false,
+								[]byte("<table></table>"),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:   "org.reduceChanged",
+			reduce: (&MailTemplateProjection{}).reduceChanged,
+			args: args{
+				event: getEvent(testEvent(
+					repository.EventType(org.MailTemplateChangedEventType),
+					org.AggregateType,
+					[]byte(`{
+						"template": "PHRhYmxlPjwvdGFibGU+"
+		}`),
+				), org.MailTemplateChangedEventMapper),
+			},
+			want: wantReduce{
+				aggregateType:    eventstore.AggregateType("org"),
+				sequence:         15,
+				previousSequence: 10,
+				projection:       MailTemplateTable,
+				executer: &testExecuter{
+					executions: []execution{
+						{
+							expectedStmt: "UPDATE zitadel.projections.mail_templates SET (change_date, sequence, template) = ($1, $2, $3) WHERE (aggregate_id = $4)",
+							expectedArgs: []interface{}{
+								anyArg{},
+								uint64(15),
+								[]byte("<table></table>"),
+								"agg-id",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:   "org.reduceRemoved",
+			reduce: (&MailTemplateProjection{}).reduceRemoved,
+			args: args{
+				event: getEvent(testEvent(
+					repository.EventType(org.MailTemplateRemovedEventType),
+					org.AggregateType,
+					nil,
+				), org.MailTemplateRemovedEventMapper),
+			},
+			want: wantReduce{
+				aggregateType:    eventstore.AggregateType("org"),
+				sequence:         15,
+				previousSequence: 10,
+				projection:       MailTemplateTable,
+				executer: &testExecuter{
+					executions: []execution{
+						{
+							expectedStmt: "DELETE FROM zitadel.projections.mail_templates WHERE (aggregate_id = $1)",
+							expectedArgs: []interface{}{
+								"agg-id",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:   "iam.reduceAdded",
+			reduce: (&MailTemplateProjection{}).reduceAdded,
+			args: args{
+				event: getEvent(testEvent(
+					repository.EventType(iam.MailTemplateAddedEventType),
+					iam.AggregateType,
+					[]byte(`{
+						"template": "PHRhYmxlPjwvdGFibGU+"
+					}`),
+				), iam.MailTemplateAddedEventMapper),
+			},
+			want: wantReduce{
+				aggregateType:    eventstore.AggregateType("iam"),
+				sequence:         15,
+				previousSequence: 10,
+				projection:       MailTemplateTable,
+				executer: &testExecuter{
+					executions: []execution{
+						{
+							expectedStmt: "INSERT INTO zitadel.projections.mail_templates (aggregate_id, creation_date, change_date, sequence, state, is_default, template) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+							expectedArgs: []interface{}{
+								"agg-id",
+								anyArg{},
+								anyArg{},
+								uint64(15),
+								domain.PolicyStateActive,
+								true,
+								[]byte("<table></table>"),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:   "iam.reduceChanged",
+			reduce: (&MailTemplateProjection{}).reduceChanged,
+			args: args{
+				event: getEvent(testEvent(
+					repository.EventType(iam.MailTemplateChangedEventType),
+					iam.AggregateType,
+					[]byte(`{
+						"template": "PHRhYmxlPjwvdGFibGU+"
+					}`),
+				), iam.MailTemplateChangedEventMapper),
+			},
+			want: wantReduce{
+				aggregateType:    eventstore.AggregateType("iam"),
+				sequence:         15,
+				previousSequence: 10,
+				projection:       MailTemplateTable,
+				executer: &testExecuter{
+					executions: []execution{
+						{
+							expectedStmt: "UPDATE zitadel.projections.mail_templates SET (change_date, sequence, template) = ($1, $2, $3) WHERE (aggregate_id = $4)",
+							expectedArgs: []interface{}{
+								anyArg{},
+								uint64(15),
+								[]byte("<table></table>"),
+								"agg-id",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			event := baseEvent(t)
+			got, err := tt.reduce(event)
+			if _, ok := err.(errors.InvalidArgument); !ok {
+				t.Errorf("no wrong event mapping: %v, got: %v", err, got)
+			}
+
+			event = tt.args.event(t)
+			got, err = tt.reduce(event)
+			assertReduce(t, got, err, tt.want)
+		})
+	}
+}
---
+++ internal/query/projection/message_texts.go
@@ -0,0 +1,234 @@
+package projection
+
+import (
+	"context"
+
+	"github.com/caos/logging"
+	"github.com/caos/zitadel/internal/domain"
+	"github.com/caos/zitadel/internal/errors"
+	"github.com/caos/zitadel/internal/eventstore"
+	"github.com/caos/zitadel/internal/eventstore/handler"
+	"github.com/caos/zitadel/internal/eventstore/handler/crdb"
+	"github.com/caos/zitadel/internal/repository/iam"
+	"github.com/caos/zitadel/internal/repository/org"
+	"github.com/caos/zitadel/internal/repository/policy"
+)
+
+type MessageTextProjection struct {
+	crdb.StatementHandler
+}
+
+const (
+	MessageTextTable = "zitadel.projections.message_texts"
+
+	MessageTextAggregateIDCol  = "aggregate_id"
+	MessageTextCreationDateCol = "creation_date"
+	MessageTextChangeDateCol   = "change_date"
+	MessageTextSequenceCol     = "sequence"
+	MessageTextStateCol        = "state"
+	MessageTextTypeCol         = "type"
+	MessageTextLanguageCol     = "language"
+	MessageTextTitleCol        = "title"
+	MessageTextPreHeaderCol    = "pre_header"
+	MessageTextSubjectCol      = "subject"
+	MessageTextGreetingCol     = "greeting"
+	MessageTextTextCol         = "text"
+	MessageTextButtonTextCol   = "button_text"
+	MessageTextFooterCol       = "footer_text"
+)
+
+func NewMessageTextProjection(ctx context.Context, config crdb.StatementHandlerConfig) *MessageTextProjection {
+	p := &MessageTextProjection{}
+	config.ProjectionName = MessageTextTable
+	config.Reducers = p.reducers()
+	p.StatementHandler = crdb.NewStatementHandler(ctx, config)
+	return p
+}
+
+func (p *MessageTextProjection) reducers() []handler.AggregateReducer {
+	return []handler.AggregateReducer{
+		{
+			Aggregate: org.AggregateType,
+			EventRedusers: []handler.EventReducer{
+				{
+					Event:  org.CustomTextSetEventType,
+					Reduce: p.reduceAdded,
+				},
+				{
+					Event:  org.CustomTextRemovedEventType,
+					Reduce: p.reduceRemoved,
+				},
+				{
+					Event:  org.CustomTextTemplateRemovedEventType,
+					Reduce: p.reduceTemplateRemoved,
+				},
+			},
+		},
+		{
+			Aggregate: iam.AggregateType,
+			EventRedusers: []handler.EventReducer{
+				{
+					Event:  iam.CustomTextSetEventType,
+					Reduce: p.reduceAdded,
+				},
+				{
+					Event:  iam.CustomTextRemovedEventType,
+					Reduce: p.reduceRemoved,
+				},
+				{
+					Event:  iam.CustomTextTemplateRemovedEventType,
+					Reduce: p.reduceTemplateRemoved,
+				},
+			},
+		},
+	}
+}
+
+func (p *MessageTextProjection) reduceAdded(event eventstore.EventReader) (*handler.Statement, error) {
+	var templateEvent policy.CustomTextSetEvent
+	switch e := event.(type) {
+	case *org.CustomTextSetEvent:
+		templateEvent = e.CustomTextSetEvent
+	case *iam.CustomTextSetEvent:
+		templateEvent = e.CustomTextSetEvent
+	default:
+		logging.LogWithFields("PROJE-2N9fg", "seq", event.Sequence(), "expectedTypes", []eventstore.EventType{org.CustomTextSetEventType, iam.CustomTextSetEventType}).Error("wrong event type")
+		return nil, errors.ThrowInvalidArgument(nil, "PROJE-2n90r", "reduce.wrong.event.type")
+	}
+	if !isMessageTemplate(templateEvent.Template) {
+		return nil, nil
+	}
+
+	cols := []handler.Column{
+		handler.NewCol(MessageTextAggregateIDCol, templateEvent.Aggregate().ID),
+		handler.NewCol(MessageTextCreationDateCol, templateEvent.CreationDate()),
+		handler.NewCol(MessageTextChangeDateCol, templateEvent.CreationDate()),
+		handler.NewCol(MessageTextSequenceCol, templateEvent.Sequence()),
+		handler.NewCol(MessageTextStateCol, domain.PolicyStateActive),
+		handler.NewCol(MessageTextTypeCol, templateEvent.Template),
+		handler.NewCol(MessageTextLanguageCol, templateEvent.Language.String()),
+	}
+	if isTitle(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextTitleCol, templateEvent.Text))
+	}
+	if isPreHeader(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextPreHeaderCol, templateEvent.Text))
+	}
+	if isSubject(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextSubjectCol, templateEvent.Text))
+	}
+	if isGreeting(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextGreetingCol, templateEvent.Text))
+	}
+	if isText(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextTextCol, templateEvent.Text))
+	}
+	if isButtonText(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextButtonTextCol, templateEvent.Text))
+	}
+	if isFooterText(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextFooterCol, templateEvent.Text))
+	}
+	return crdb.NewUpsertStatement(
+		&templateEvent,
+		cols), nil
+}
+
+func (p *MessageTextProjection) reduceRemoved(event eventstore.EventReader) (*handler.Statement, error) {
+	var templateEvent policy.CustomTextRemovedEvent
+	switch e := event.(type) {
+	case *org.CustomTextRemovedEvent:
+		templateEvent = e.CustomTextRemovedEvent
+	case *iam.CustomTextRemovedEvent:
+		templateEvent = e.CustomTextRemovedEvent
+	default:
+		logging.LogWithFields("PROJE-3m022", "seq", event.Sequence(), "expectedTypes", []eventstore.EventType{org.CustomTextRemovedEventType, iam.CustomTextRemovedEventType}).Error("wrong event type")
+		return nil, errors.ThrowInvalidArgument(nil, "PROJE-fm0ge", "reduce.wrong.event.type")
+	}
+	if !isMessageTemplate(templateEvent.Template) {
+		return nil, nil
+	}
+	cols := []handler.Column{
+		handler.NewCol(MessageTextChangeDateCol, templateEvent.CreationDate()),
+		handler.NewCol(MessageTextSequenceCol, templateEvent.Sequence()),
+	}
+	if isTitle(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextTitleCol, ""))
+	}
+	if isPreHeader(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextPreHeaderCol, ""))
+	}
+	if isSubject(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextSubjectCol, ""))
+	}
+	if isGreeting(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextGreetingCol, ""))
+	}
+	if isText(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextTextCol, ""))
+	}
+	if isButtonText(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextButtonTextCol, ""))
+	}
+	if isFooterText(templateEvent.Key) {
+		cols = append(cols, handler.NewCol(MessageTextFooterCol, ""))
+	}
+	return crdb.NewUpdateStatement(
+		&templateEvent,
+		cols,
+		[]handler.Condition{
+			handler.NewCond(MessageTextAggregateIDCol, templateEvent.Aggregate().ID),
+			handler.NewCond(MessageTextTypeCol, templateEvent.Template),
+			handler.NewCond(MessageTextLanguageCol, templateEvent.Language.String()),
+		},
+	), nil
+}
+
+func (p *MessageTextProjection) reduceTemplateRemoved(event eventstore.EventReader) (*handler.Statement, error) {
+	templateEvent, ok := event.(*org.CustomTextTemplateRemovedEvent)
+	if !ok {
+		logging.LogWithFields("PROJE-m03ng", "seq", event.Sequence(), "expectedType", org.CustomTextTemplateRemovedEventType).Error("wrong event type")
+		return nil, errors.ThrowInvalidArgument(nil, "PROJE-2n9rs", "reduce.wrong.event.type")
+	}
+	if !isMessageTemplate(templateEvent.Template) {
+		return nil, nil
+	}
+	return crdb.NewDeleteStatement(
+		templateEvent,
+		[]handler.Condition{
+			handler.NewCond(MessageTextAggregateIDCol, templateEvent.Aggregate().ID),
+			handler.NewCond(MessageTextTypeCol, templateEvent.Template),
+			handler.NewCond(MessageTextLanguageCol, templateEvent.Language),
+		},
+	), nil
+}
+
+func isMessageTemplate(template string) bool {
+	return template == domain.InitCodeMessageType ||
+		template == domain.PasswordResetMessageType ||
+		template == domain.VerifyEmailMessageType ||
+		template == domain.VerifyPhoneMessageType ||
+		template == domain.DomainClaimedMessageType ||
+		template == domain.PasswordlessRegistrationMessageType
+}
+func isTitle(key string) bool {
+	return key == domain.MessageTitle
+}
+func isPreHeader(key string) bool {
+	return key == domain.MessagePreHeader
+}
+func isSubject(key string) bool {
+	return key == domain.MessageSubject
+}
+func isGreeting(key string) bool {
+	return key == domain.MessageGreeting
+}
+func isText(key string) bool {
+	return key == domain.MessageText
+}
+func isButtonText(key string) bool {
+	return key == domain.MessageButtonText
+}
+func isFooterText(key string) bool {
+	return key == domain.MessageFooterText
+}
