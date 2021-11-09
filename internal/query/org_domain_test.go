f14ab056384f702dd558333ca81cf7b30eafb783
adlerhurst <silvan.reusser@gmail.com>
2021-11-09 17:06:23 +0100

    test(queries): org domain prepare funcs

---
+++ internal/query/org_domain_test.go
@@ -0,0 +1,214 @@
+package query
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/caos/zitadel/internal/domain"
+)
+
+func Test_DomainDomainPrepares(t *testing.T) {
+	type want struct {
+		sqlExpectations sqlExpectation
+		err             checkErr
+	}
+	tests := []struct {
+		name    string
+		prepare interface{}
+		want    want
+		object  interface{}
+	}{
+		{
+			name:    "prepareDomainsQuery no result",
+			prepare: prepareDomainsQuery,
+			want: want{
+				sqlExpectations: mockQueries(
+					regexp.QuoteMeta(`SELECT zitadel.projections.org_domains.creation_date,`+
+						` zitadel.projections.org_domains.change_date,`+
+						` zitadel.projections.org_domains.sequence,`+
+						` zitadel.projections.org_domains.domain,`+
+						` zitadel.projections.org_domains.org_id,`+
+						` zitadel.projections.org_domains.is_verified,`+
+						` zitadel.projections.org_domains.is_primary,`+
+						` zitadel.projections.org_domains.validation_type,`+
+						` COUNT(*) OVER ()`+
+						` FROM zitadel.projections.org_domains`),
+					nil,
+					nil,
+				),
+			},
+			object: &Domains{Domains: []*Domain{}},
+		},
+		{
+			name:    "prepareDomainsQuery one result",
+			prepare: prepareDomainsQuery,
+			want: want{
+				sqlExpectations: mockQueries(
+					regexp.QuoteMeta(`SELECT zitadel.projections.org_domains.creation_date,`+
+						` zitadel.projections.org_domains.change_date,`+
+						` zitadel.projections.org_domains.sequence,`+
+						` zitadel.projections.org_domains.domain,`+
+						` zitadel.projections.org_domains.org_id,`+
+						` zitadel.projections.org_domains.is_verified,`+
+						` zitadel.projections.org_domains.is_primary,`+
+						` zitadel.projections.org_domains.validation_type,`+
+						` COUNT(*) OVER ()`+
+						` FROM zitadel.projections.org_domains`),
+					[]string{
+						"id",
+						"creation_date",
+						"change_date",
+						"resource_owner",
+						"org_state",
+						"sequence",
+						"name",
+						"primary_domain",
+						"count",
+					},
+					[][]driver.Value{
+						{
+							testNow,
+							testNow,
+							uint64(20211109),
+							"zitadel.ch",
+							"ro",
+							true,
+							true,
+							domain.OrgDomainValidationTypeHTTP,
+						},
+					},
+				),
+			},
+			object: &Domains{
+				SearchResponse: SearchResponse{
+					Count: 1,
+				},
+				Domains: []*Domain{
+					{
+						CreationDate:   testNow,
+						ChangeDate:     testNow,
+						Sequence:       20211109,
+						Domain:         "zitadel.ch",
+						OrgID:          "ro",
+						IsVerified:     true,
+						IsPrimary:      true,
+						ValidationType: domain.OrgDomainValidationTypeHTTP,
+					},
+				},
+			},
+		},
+		{
+			name:    "prepareDomainsQuery multiple result",
+			prepare: prepareDomainsQuery,
+			want: want{
+				sqlExpectations: mockQueries(
+					regexp.QuoteMeta(`SELECT zitadel.projections.org_domains.creation_date,`+
+						` zitadel.projections.org_domains.change_date,`+
+						` zitadel.projections.org_domains.sequence,`+
+						` zitadel.projections.org_domains.domain,`+
+						` zitadel.projections.org_domains.org_id,`+
+						` zitadel.projections.org_domains.is_verified,`+
+						` zitadel.projections.org_domains.is_primary,`+
+						` zitadel.projections.org_domains.validation_type,`+
+						` COUNT(*) OVER ()`+
+						` FROM zitadel.projections.org_domains`),
+					[]string{
+						"id",
+						"creation_date",
+						"change_date",
+						"resource_owner",
+						"org_state",
+						"sequence",
+						"name",
+						"primary_domain",
+						"count",
+					},
+					[][]driver.Value{
+						{
+							testNow,
+							testNow,
+							uint64(20211109),
+							"zitadel.ch",
+							"ro",
+							true,
+							true,
+							domain.OrgDomainValidationTypeHTTP,
+						},
+						{
+							testNow,
+							testNow,
+							uint64(20211109),
+							"zitadel.ch",
+							"ro",
+							false,
+							false,
+							domain.OrgDomainValidationTypeDNS,
+						},
+					},
+				),
+			},
+			object: &Domains{
+				SearchResponse: SearchResponse{
+					Count: 2,
+				},
+				Domains: []*Domain{
+					{
+						CreationDate:   testNow,
+						ChangeDate:     testNow,
+						Sequence:       20211109,
+						Domain:         "zitadel.ch",
+						OrgID:          "ro",
+						IsVerified:     true,
+						IsPrimary:      true,
+						ValidationType: domain.OrgDomainValidationTypeHTTP,
+					},
+					{
+						CreationDate:   testNow,
+						ChangeDate:     testNow,
+						Sequence:       20211109,
+						Domain:         "zitadel.ch",
+						OrgID:          "ro",
+						IsVerified:     false,
+						IsPrimary:      false,
+						ValidationType: domain.OrgDomainValidationTypeDNS,
+					},
+				},
+			},
+		},
+		{
+			name:    "prepareDomainsQuery sql err",
+			prepare: prepareDomainsQuery,
+			want: want{
+				sqlExpectations: mockQueryErr(
+					regexp.QuoteMeta(`SELECT zitadel.projections.org_domains.creation_date,`+
+						` zitadel.projections.org_domains.change_date,`+
+						` zitadel.projections.org_domains.sequence,`+
+						` zitadel.projections.org_domains.domain,`+
+						` zitadel.projections.org_domains.org_id,`+
+						` zitadel.projections.org_domains.is_verified,`+
+						` zitadel.projections.org_domains.is_primary,`+
+						` zitadel.projections.org_domains.validation_type,`+
+						` COUNT(*) OVER ()`+
+						` FROM zitadel.projections.org_domains`),
+					sql.ErrConnDone,
+				),
+				err: func(err error) (error, bool) {
+					if !errors.Is(err, sql.ErrConnDone) {
+						return fmt.Errorf("err should be sql.ErrConnDone got: %w", err), false
+					}
+					return nil, true
+				},
+			},
+			object: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assertPrepare(t, tt.prepare, tt.object, tt.want.sqlExpectations, tt.want.err)
+		})
+	}
+}
