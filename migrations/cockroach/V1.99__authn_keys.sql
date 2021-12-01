b2ace0e78aee97fe0a3f4d33aacf411e1b12adbf
Livio Amstutz <livio.a@gmail.com>
2021-12-01 10:26:18 +0100

    single table for state change

--- migrations/cockroach/V1.99__authn_keys.sql
+++ migrations/cockroach/V1.99__authn_keys.sql
@@ -1,22 +1,15 @@
 CREATE TABLE zitadel.projections.authn_keys(
     id STRING
     , creation_date TIMESTAMPTZ NOT NULL
-    , change_date TIMESTAMPTZ NOT NULL
     , resource_owner STRING NOT NULL
     , aggregate_id STRING NOT NULL
     , sequence INT8 NOT NULL
 
     , object_id STRING NOT NULL
     , expiration TIMESTAMPTZ NOT NULL
+    , identifier STRING NOT NULL
+    , public_key BYTES NOT NULL
+    , enabled BOOLEAN NOT NULL DEFAULT true
 
     , PRIMARY KEY (id)
-);
-
-CREATE TABLE zitadel.projections.authn_keys_authentication(
-	key_id STRING REFERENCES zitadel.projections.authn_keys (id) ON DELETE CASCADE
-	
-    , identifier STRING NOT NULL
-    , key BYTES NOT NULL
-
-    , PRIMARY KEY (key_id)
 );
