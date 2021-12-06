d0228b71a960a50d8af0f08a999cc995f84f4909
adlerhurst <silvan.reusser@gmail.com>
2021-12-06 23:35:33 +0100

    fix(queries): project grant members

--- internal/query/project_grant_member.go
+++ internal/query/project_grant_member.go
@@ -1,6 +1,14 @@
 package query
 
-import "github.com/caos/zitadel/internal/query/projection"
+import (
+	"context"
+	"database/sql"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/caos/zitadel/internal/errors"
+	"github.com/caos/zitadel/internal/query/projection"
+	"github.com/lib/pq"
+)
 
 var (
 	projectGrantMemberTable = table{
@@ -39,3 +47,132 @@ var (
 		table: projectGrantMemberTable,
 	}
 )
+
+type ProjectGrantMembersQuery struct {
+	MembersQuery
+	ProjectID, GrantID string
+}
+
+func (q *ProjectGrantMembersQuery) toQuery(query sq.SelectBuilder) sq.SelectBuilder {
+	return q.MembersQuery.
+		toQuery(query).
+		Where(sq.Eq{
+			ProjectGrantMemberProjectID.identifier(): q.ProjectID,
+			ProjectGrantMemberGrantID.identifier():   q.GrantID,
+		})
+}
+
+func (q *Queries) ProjectGrantMembers(ctx context.Context, queries *ProjectGrantMembersQuery) (*Members, error) {
+	query, scan := prepareProjectGrantMembersQuery()
+	stmt, args, err := queries.toQuery(query).ToSql()
+	if err != nil {
+		return nil, errors.ThrowInvalidArgument(err, "QUERY-USNwM", "Errors.Query.InvalidRequest")
+	}
+
+	currentSequence, err := q.latestSequence(ctx, projectGrantMemberTable)
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := q.client.QueryContext(ctx, stmt, args...)
+	if err != nil {
+		return nil, errors.ThrowInternal(err, "QUERY-Pdg1I", "Errors.Internal")
+	}
+	members, err := scan(rows)
+	if err != nil {
+		return nil, err
+	}
+	members.LatestSequence = currentSequence
+	return members, err
+}
+
+func prepareProjectGrantMembersQuery() (sq.SelectBuilder, func(*sql.Rows) (*Members, error)) {
+	return sq.Select(
+			ProjectGrantMemberCreationDate.identifier(),
+			ProjectGrantMemberChangeDate.identifier(),
+			ProjectGrantMemberSequence.identifier(),
+			ProjectGrantMemberResourceOwner.identifier(),
+			ProjectGrantMemberUserID.identifier(),
+			ProjectGrantMemberRoles.identifier(),
+			LoginNameNameCol.identifier(),
+			HumanEmailCol.identifier(),
+			HumanFirstNameCol.identifier(),
+			HumanLastNameCol.identifier(),
+			HumanDisplayNameCol.identifier(),
+			MachineNameCol.identifier(),
+			HumanAvaterURLCol.identifier(),
+			countColumn.identifier(),
+		).From(projectGrantMemberTable.identifier()).
+			LeftJoin(join(HumanUserIDCol, ProjectGrantMemberUserID)).
+			LeftJoin(join(MachineUserIDCol, ProjectGrantMemberUserID)).
+			LeftJoin(join(LoginNameUserIDCol, ProjectGrantMemberUserID)).
+			Where(
+				sq.Eq{LoginNameIsPrimaryCol.identifier(): true},
+			).PlaceholderFormat(sq.Dollar),
+		func(rows *sql.Rows) (*Members, error) {
+			members := make([]*Member, 0)
+			var count uint64
+
+			for rows.Next() {
+				member := new(Member)
+				roles := pq.StringArray{}
+
+				var (
+					preferredLoginName = sql.NullString{}
+					email              = sql.NullString{}
+					firstName          = sql.NullString{}
+					lastName           = sql.NullString{}
+					displayName        = sql.NullString{}
+					machineName        = sql.NullString{}
+					avatarURL          = sql.NullString{}
+				)
+
+				err := rows.Scan(
+					&member.CreationDate,
+					&member.ChangeDate,
+					&member.Sequence,
+					&member.ResourceOwner,
+					&member.UserID,
+					&roles,
+					&preferredLoginName,
+					&email,
+					&firstName,
+					&lastName,
+					&displayName,
+					&machineName,
+					&avatarURL,
+
+					&count,
+				)
+
+				if err != nil {
+					return nil, err
+				}
+
+				member.Roles = roles
+				member.PreferredLoginName = preferredLoginName.String
+				member.Email = email.String
+				member.FirstName = firstName.String
+				member.LastName = lastName.String
+				member.AvatarURL = avatarURL.String
+				if displayName.Valid {
+					member.DisplayName = displayName.String
+				} else {
+					member.DisplayName = machineName.String
+				}
+
+				members = append(members, member)
+			}
+
+			if err := rows.Close(); err != nil {
+				return nil, errors.ThrowInternal(err, "QUERY-EqJFc", "Errors.Query.CloseRows")
+			}
+
+			return &Members{
+				Members: members,
+				SearchResponse: SearchResponse{
+					Count: count,
+				},
+			}, nil
+		}
+}
---
+++ internal/query/project_grant_member_test.go
@@ -0,0 +1,277 @@
+package query
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/lib/pq"
+)
+
+var (
+	projectGrantMembersQuery = regexp.QuoteMeta("SELECT" +
+		" zitadel.projections.project_grant_members.creation_date" +
+		", zitadel.projections.project_grant_members.change_date" +
+		", zitadel.projections.project_grant_members.sequence" +
+		", zitadel.projections.project_grant_members.resource_owner" +
+		", zitadel.projections.project_grant_members.user_id" +
+		", zitadel.projections.project_grant_members.roles" +
+		", zitadel.projections.login_names.login_name" +
+		", zitadel.projections.users_humans.email" +
+		", zitadel.projections.users_humans.first_name" +
+		", zitadel.projections.users_humans.last_name" +
+		", zitadel.projections.users_humans.display_name" +
+		", zitadel.projections.users_machines.name" +
+		", zitadel.projections.users_humans.avater_key" +
+		", COUNT(*) OVER () " +
+		"FROM zitadel.projections.project_grant_members " +
+		"LEFT JOIN zitadel.projections.users_humans " +
+		"ON zitadel.projections.project_grant_members.user_id = zitadel.projections.users_humans.user_id " +
+		"LEFT JOIN zitadel.projections.users_machines " +
+		"ON zitadel.projections.project_grant_members.user_id = zitadel.projections.users_machines.user_id " +
+		"LEFT JOIN zitadel.projections.login_names " +
+		"ON zitadel.projections.project_grant_members.user_id = zitadel.projections.login_names.user_id " +
+		"WHERE zitadel.projections.login_names.is_primary = $1")
+	projectGrantMembersColumns = []string{
+		"creation_date",
+		"change_date",
+		"sequence",
+		"resource_owner",
+		"user_id",
+		"roles",
+		"login_name",
+		"email",
+		"first_name",
+		"last_name",
+		"display_name",
+		"name",
+		"avater_key",
+		"count",
+	}
+)
+
+func Test_ProjectGrantMemberPrepares(t *testing.T) {
+	type want struct {
+		sqlExpectations sqlExpectation
+		err             checkErr
+	}
+	tests := []struct {
+		name    string
+		prepare interface{}
+		want    want
+		object  interface{}
+	}{
+		{
+			name:    "prepareProjectGrantMembersQuery no result",
+			prepare: prepareProjectGrantMembersQuery,
+			want: want{
+				sqlExpectations: mockQueries(
+					projectGrantMembersQuery,
+					nil,
+					nil,
+				),
+			},
+			object: &Members{
+				Members: []*Member{},
+			},
+		},
+		{
+			name:    "prepareProjectGrantMembersQuery human found",
+			prepare: prepareProjectGrantMembersQuery,
+			want: want{
+				sqlExpectations: mockQueries(
+					projectGrantMembersQuery,
+					projectGrantMembersColumns,
+					[][]driver.Value{
+						{
+							testNow,
+							testNow,
+							uint64(20211206),
+							"ro",
+							"user-id",
+							pq.StringArray{"role-1", "role-2"},
+							"gigi@caos-ag.zitadel.ch",
+							"gigi@caos.ch",
+							"first-name",
+							"last-name",
+							"display name",
+							nil,
+							nil,
+						},
+					},
+				),
+			},
+			object: &Members{
+				SearchResponse: SearchResponse{
+					Count: 1,
+				},
+				Members: []*Member{
+					{
+						CreationDate:       testNow,
+						ChangeDate:         testNow,
+						Sequence:           20211206,
+						ResourceOwner:      "ro",
+						UserID:             "user-id",
+						Roles:              []string{"role-1", "role-2"},
+						PreferredLoginName: "gigi@caos-ag.zitadel.ch",
+						Email:              "gigi@caos.ch",
+						FirstName:          "first-name",
+						LastName:           "last-name",
+						DisplayName:        "display name",
+						AvatarURL:          "",
+					},
+				},
+			},
+		},
+		{
+			name:    "prepareProjectGrantMembersQuery machine found",
+			prepare: prepareProjectGrantMembersQuery,
+			want: want{
+				sqlExpectations: mockQueries(
+					projectGrantMembersQuery,
+					projectGrantMembersColumns,
+					[][]driver.Value{
+						{
+							testNow,
+							testNow,
+							uint64(20211206),
+							"ro",
+							"user-id",
+							pq.StringArray{"role-1", "role-2"},
+							"machine@caos-ag.zitadel.ch",
+							nil,
+							nil,
+							nil,
+							nil,
+							"machine-name",
+							nil,
+						},
+					},
+				),
+			},
+			object: &Members{
+				SearchResponse: SearchResponse{
+					Count: 1,
+				},
+				Members: []*Member{
+					{
+						CreationDate:       testNow,
+						ChangeDate:         testNow,
+						Sequence:           20211206,
+						ResourceOwner:      "ro",
+						UserID:             "user-id",
+						Roles:              []string{"role-1", "role-2"},
+						PreferredLoginName: "machine@caos-ag.zitadel.ch",
+						Email:              "",
+						FirstName:          "",
+						LastName:           "",
+						DisplayName:        "machine-name",
+						AvatarURL:          "",
+					},
+				},
+			},
+		},
+		{
+			name:    "prepareProjectGrantMembersQuery multiple users",
+			prepare: prepareProjectGrantMembersQuery,
+			want: want{
+				sqlExpectations: mockQueries(
+					projectGrantMembersQuery,
+					projectGrantMembersColumns,
+					[][]driver.Value{
+						{
+							testNow,
+							testNow,
+							uint64(20211206),
+							"ro",
+							"user-id-1",
+							pq.StringArray{"role-1", "role-2"},
+							"gigi@caos-ag.zitadel.ch",
+							"gigi@caos.ch",
+							"first-name",
+							"last-name",
+							"display name",
+							nil,
+							nil,
+						},
+						{
+							testNow,
+							testNow,
+							uint64(20211206),
+							"ro",
+							"user-id-2",
+							pq.StringArray{"role-1", "role-2"},
+							"machine@caos-ag.zitadel.ch",
+							nil,
+							nil,
+							nil,
+							nil,
+							"machine-name",
+							nil,
+						},
+					},
+				),
+			},
+			object: &Members{
+				SearchResponse: SearchResponse{
+					Count: 2,
+				},
+				Members: []*Member{
+					{
+						CreationDate:       testNow,
+						ChangeDate:         testNow,
+						Sequence:           20211206,
+						ResourceOwner:      "ro",
+						UserID:             "user-id-1",
+						Roles:              []string{"role-1", "role-2"},
+						PreferredLoginName: "gigi@caos-ag.zitadel.ch",
+						Email:              "gigi@caos.ch",
+						FirstName:          "first-name",
+						LastName:           "last-name",
+						DisplayName:        "display name",
+						AvatarURL:          "",
+					},
+					{
+						CreationDate:       testNow,
+						ChangeDate:         testNow,
+						Sequence:           20211206,
+						ResourceOwner:      "ro",
+						UserID:             "user-id-2",
+						Roles:              []string{"role-1", "role-2"},
+						PreferredLoginName: "machine@caos-ag.zitadel.ch",
+						Email:              "",
+						FirstName:          "",
+						LastName:           "",
+						DisplayName:        "machine-name",
+						AvatarURL:          "",
+					},
+				},
+			},
+		},
+		{
+			name:    "prepareProjectGrantMembersQuery sql err",
+			prepare: prepareProjectGrantMembersQuery,
+			want: want{
+				sqlExpectations: mockQueryErr(
+					projectGrantMembersQuery,
+					sql.ErrConnDone,
+				),
+				err: func(err error) (error, bool) {
+					if !errors.Is(err, sql.ErrConnDone) {
+						return fmt.Errorf("err should be sql.ErrConnDone got: %w", err), false
+					}
+					return nil, true
+				},
+			},
+			object: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assertPrepare(t, tt.prepare, tt.object, tt.want.sqlExpectations, tt.want.err)
+		})
+	}
+}
