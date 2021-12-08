65a6fb638bc7e4ef4a026d4d2bf2b99236a63351
Livio Amstutz <livio.a@gmail.com>
2021-12-08 16:16:48 +0100

    fix: add authnkeys projection (#2801)
    * begin authn keys
    
    * single table for state change
    
    * add key type
    
    * rename migration
    
    * format imports
    
    * fix test

---
+++ migrations/cockroach/V1.101__authn_keys.sql
@@ -0,0 +1,16 @@
+CREATE TABLE zitadel.projections.authn_keys(
+    id STRING
+    , creation_date TIMESTAMPTZ NOT NULL
+    , resource_owner STRING NOT NULL
+    , aggregate_id STRING NOT NULL
+    , sequence INT8 NOT NULL
+
+    , object_id STRING NOT NULL
+    , expiration TIMESTAMPTZ NOT NULL
+    , identifier STRING NOT NULL
+    , public_key BYTES NOT NULL
+    , enabled BOOLEAN NOT NULL DEFAULT true
+    , type INT2 NOT NULL DEFAULT 0
+
+    , PRIMARY KEY (id)
+);
