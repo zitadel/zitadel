d1cb7fdc9f0b35c4be94206c1ef0b0aeb7ecf007
Max Peintner <max@caos.ch>
2021-12-10 16:14:24 +0100

    fix: new context component (#2823)

---
+++ console/src/app/modules/org-context/org-context.module.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { HasRoleModule } from 'src/app/directives/has-role/has-role.module';
+import { OutsideClickModule } from 'src/app/directives/outside-click/outside-click.module';
+
+import { InputModule } from '../input/input.module';
+import { OrgContextComponent } from './org-context.component';
+
+@NgModule({
+  declarations: [OrgContextComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    RouterModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    InputModule,
+    OutsideClickModule,
+    TranslateModule,
+    MatButtonModule,
+    HasRoleModule,
+  ],
+  exports: [OrgContextComponent],
+})
+export class OrgContextModule {}
