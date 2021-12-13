c6303d2c2fa9f491b36b5e274c4801f396b3e230
Max Peintner <max@caos.ch>
2021-12-13 13:57:20 +0100

    shortcuts for settings

--- console/src/app/modules/shortcuts/shortcuts.component.ts
+++ console/src/app/modules/shortcuts/shortcuts.component.ts
@@ -1,25 +1,75 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { Org } from 'src/app/proto/generated/zitadel/org_pb';
+import { GrpcAuthService } from 'src/app/services/grpc-auth.service';
+import { StorageLocation, StorageService } from 'src/app/services/storage.service';
+
+import { GridPolicy, POLICIES } from '../policy-grid/policies';
 
 @Component({
   selector: 'cnsl-shortcuts',
   templateUrl: './shortcuts.component.html',
   styleUrls: ['./shortcuts.component.scss'],
 })
-export class ShortcutsComponent implements OnInit {
-  public shortcuts = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+export class ShortcutsComponent implements OnDestroy {
+  public shortcuts: GridPolicy[] = [];
+  public POLICIES: GridPolicy[] = POLICIES;
 
-  public all = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  public all = POLICIES; // ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
-  constructor() {}
+  private destroy$: Subject<void> = new Subject();
+  public editState: boolean = false;
+  constructor(private storageService: StorageService, private auth: GrpcAuthService) {
+    const org: Org.AsObject | null = this.storageService.getItem('organization', StorageLocation.session);
+    if (org && org.id) {
+      this.loadShortcuts(org);
+    }
 
-  ngOnInit(): void {}
+    this.auth.activeOrgChanged.pipe(takeUntil(this.destroy$)).subscribe((org) => {
+      console.log(org.name, org.id);
+      this.loadShortcuts(org);
+    });
+  }
 
-  drop(event: CdkDragDrop<string[]>) {
+  public loadShortcuts(org: Org.AsObject): void {
+    const shortcuts = this.storageService.getItem(`shortcuts:${org.id}`, StorageLocation.local);
+    if (shortcuts) {
+      const parsed = JSON.parse(shortcuts);
+      if (parsed) {
+        this.organizeItems(parsed);
+      }
+    } else {
+      this.organizeItems([]);
+    }
+  }
+
+  private organizeItems(list: GridPolicy[]): void {
+    console.log(list);
+    this.shortcuts = list;
+    const filtered = POLICIES.filter((p) => !list.find((l) => l.i18nTitle === p.i18nTitle));
+    this.all = filtered;
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  public drop(event: CdkDragDrop<GridPolicy[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      this.saveToStorage(event.container);
     } else {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+      this.saveToStorage(event.container);
+    }
+  }
+
+  public saveToStorage(list: CdkDropList): void {
+    const org: Org.AsObject | null = this.storageService.getItem('organization', StorageLocation.session);
+    if (org && org.id) {
+      this.storageService.setItem(`shortcuts:${org.id}`, JSON.stringify(list.data), StorageLocation.local);
     }
   }
 }
--- console/src/app/modules/shortcuts/shortcuts.module.ts
+++ console/src/app/modules/shortcuts/shortcuts.module.ts
@@ -1,12 +1,17 @@
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { HasRoleModule } from 'src/app/directives/has-role/has-role.module';
 
 import { ShortcutsComponent } from './shortcuts.component';
 
 @NgModule({
   declarations: [ShortcutsComponent],
-  imports: [CommonModule, DragDropModule],
+  imports: [CommonModule, MatButtonModule, RouterModule, DragDropModule, HasRoleModule, TranslateModule, MatIconModule],
   exports: [ShortcutsComponent],
 })
 export class ShortcutsModule {}
