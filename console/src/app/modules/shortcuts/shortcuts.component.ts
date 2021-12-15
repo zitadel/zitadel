0b3c6d14aaa102ee6c9c2e0778f536decde670c3
Max Peintner <max@caos.ch>
2021-12-15 10:43:59 +0100

    shortcuts

--- console/src/app/modules/shortcuts/shortcuts.component.ts
+++ console/src/app/modules/shortcuts/shortcuts.component.ts
@@ -5,7 +5,37 @@ import { Org } from 'src/app/proto/generated/zitadel/org_pb';
 import { GrpcAuthService } from 'src/app/services/grpc-auth.service';
 import { StorageLocation, StorageService } from 'src/app/services/storage.service';
 
-import { GridPolicy, POLICIES } from '../policy-grid/policies';
+import { POLICIES } from '../policy-grid/policies';
+
+export interface ShortcutItem {
+  title?: string;
+  desc?: string;
+  i18nTitle?: string;
+  i18nDesc?: string;
+  routerLink: any;
+  withRole: string[];
+  icon?: string;
+  svgIcon?: string;
+  avatarSrc?: string;
+  color?: string;
+  disabled?: boolean;
+}
+
+const PROFILE_SHORTCUT: ShortcutItem = {
+  routerLink: ['/users', 'me'],
+  i18nTitle: 'USER.TITLE',
+  icon: 'las la-cog',
+  withRole: [''],
+  disabled: true,
+};
+
+const CREATE_ORG: ShortcutItem = {
+  i18nTitle: 'ORG.PAGES.CREATE',
+  routerLink: ['/org', 'create'],
+  withRole: ['org.create', 'iam.write'],
+  icon: 'las la-plus',
+  disabled: true,
+};
 
 @Component({
   selector: 'cnsl-shortcuts',
@@ -13,10 +43,25 @@ import { GridPolicy, POLICIES } from '../policy-grid/policies';
   styleUrls: ['./shortcuts.component.scss'],
 })
 export class ShortcutsComponent implements OnDestroy {
-  public shortcuts: GridPolicy[] = [];
-  public POLICIES: GridPolicy[] = POLICIES;
+  public main: ShortcutItem[] = [PROFILE_SHORTCUT, CREATE_ORG];
+  public secondary: ShortcutItem[] = [];
+  public third: ShortcutItem[] = [];
 
-  public all = POLICIES; // ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  public ALL_POLICIES = POLICIES.map((p) => {
+    const policy: ShortcutItem = {
+      i18nTitle: p.i18nTitle,
+      i18nDesc: p.i18nDesc,
+      routerLink: p.orgRouterLink,
+      withRole: p.orgWithRole,
+      icon: p.icon ?? '',
+      svgIcon: p.svgIcon ?? '',
+      color: p.color ?? '',
+      disabled: false,
+    };
+    return policy;
+  });
+
+  public all: ShortcutItem[] = this.ALL_POLICIES;
 
   private destroy$: Subject<void> = new Subject();
   public editState: boolean = false;
@@ -33,21 +78,49 @@ export class ShortcutsComponent implements OnDestroy {
   }
 
   public loadShortcuts(org: Org.AsObject): void {
-    const shortcuts = this.storageService.getItem(`shortcuts:${org.id}`, StorageLocation.local);
-    if (shortcuts) {
-      const parsed = JSON.parse(shortcuts);
-      if (parsed) {
-        this.organizeItems(parsed);
+    ['main', 'secondary', 'third'].map((listName) => {
+      const shortcuts = this.storageService.getItem(`shortcuts:${listName}:${org.id}`, StorageLocation.local);
+      if (shortcuts) {
+        const parsed = JSON.parse(shortcuts);
+        if (parsed) {
+          switch (listName) {
+            case 'main':
+              const f = parsed.filter(
+                (shortcut: ShortcutItem) =>
+                  JSON.stringify(shortcut) !== JSON.stringify(PROFILE_SHORTCUT) &&
+                  JSON.stringify(shortcut) !== JSON.stringify(CREATE_ORG),
+              );
+              this.main = [PROFILE_SHORTCUT, CREATE_ORG, ...f];
+              break;
+            case 'secondary':
+              this.secondary = parsed;
+              break;
+            case 'third':
+              this.third = parsed;
+              break;
+          }
+          this.organizeAllItems();
+        }
+      } else {
+        switch (listName) {
+          case 'main':
+            this.main = [PROFILE_SHORTCUT, CREATE_ORG];
+            break;
+          case 'secondary':
+            this.secondary = [];
+            break;
+          case 'third':
+            this.third = [];
+            break;
+        }
+        this.organizeAllItems();
       }
-    } else {
-      this.organizeItems([]);
-    }
+    });
   }
 
-  private organizeItems(list: GridPolicy[]): void {
-    console.log(list);
-    this.shortcuts = list;
-    const filtered = POLICIES.filter((p) => !list.find((l) => l.i18nTitle === p.i18nTitle));
+  private organizeAllItems(): void {
+    const list = [this.main, this.secondary, this.third].flat();
+    const filtered = this.ALL_POLICIES.filter((p) => !list.find((l) => l.i18nTitle === p.i18nTitle));
     this.all = filtered;
   }
 
@@ -56,20 +129,24 @@ export class ShortcutsComponent implements OnDestroy {
     this.destroy$.complete();
   }
 
-  public drop(event: CdkDragDrop<GridPolicy[]>) {
+  public drop(event: CdkDragDrop<ShortcutItem[]>, listName: string) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-      this.saveToStorage(event.container);
+      if (listName) {
+        this.saveToStorage(event.container, listName);
+      }
     } else {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
-      this.saveToStorage(event.container);
+      if (listName) {
+        this.saveToStorage(event.container, listName);
+      }
     }
   }
 
-  public saveToStorage(list: CdkDropList): void {
+  public saveToStorage(list: CdkDropList, listName: string): void {
     const org: Org.AsObject | null = this.storageService.getItem('organization', StorageLocation.session);
     if (org && org.id) {
-      this.storageService.setItem(`shortcuts:${org.id}`, JSON.stringify(list.data), StorageLocation.local);
+      this.storageService.setItem(`shortcuts:${listName}:${org.id}`, JSON.stringify(list.data), StorageLocation.local);
     }
   }
 }
