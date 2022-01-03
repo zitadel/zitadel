dc79512ce553d2e31d3666bbce06864dd36c86a0
Elio Bischof <eliobischof@gmail.com>
2022-01-03 18:20:46 +0100

    initialize zitadel for e2e tests

--- console/cypress.sh
+++ console/cypress.sh
@@ -13,4 +13,4 @@ if ! command -v cypress &> /dev/null; then
     NPX="npx" 
 fi
 
-$NPX cypress $ACTION --port 4201 --env org_owner_password="${E2E_ORG_OWNER_PW}",org_owner_viewer_password="${E2E_ORG_OWNER_VIEWER_PW}",org_project_creator_password="${E2E_ORG_PROJECT_CREATOR_PW}",consoleUrl=${E2E_CONSOLE_URL},apiCallsDomain="${E2E_API_CALLS_DOMAIN}",serviceAccountKey="${E2E_SERVICEACCOUNT_KEY}",zitadelProjectResourceId="${E2E_ZITADEL_PROJECT_RESOURCE_ID}" "$@"
+$NPX cypress $ACTION --port 4201 --env org_owner_password="${E2E_ORG_OWNER_PW}",org_owner_viewer_password="${E2E_ORG_OWNER_VIEWER_PW}",org_project_creator_password="${E2E_ORG_PROJECT_CREATOR_PW}",login_policy_user_password="${E2E_LOGIN_POLICY_USER_PW}",password_complexity_user_password="${E2E_PASSWORD_COMPLEXITY_USER_PW}",consoleUrl=${E2E_CONSOLE_URL},apiCallsDomain="${E2E_API_CALLS_DOMAIN}",serviceAccountKey="${E2E_SERVICEACCOUNT_KEY}",zitadelProjectResourceId="${E2E_ZITADEL_PROJECT_RESOURCE_ID}" "$@"
--- console/cypress/integration/init/init.ts
+++ console/cypress/integration/init/init.ts
@@ -8,13 +8,59 @@ describe('initialize organisation', () => {
         const adminPw = 'Password1!'
 
         login(User.IAMAdminUser, false, adminPw, null, null, () => {
-// TODO: Not always
+            // Login as zitadel admin for the first time
             cy.contains('button', 'skip').click()
             cy.get('#change-old-password').type(adminPw)
             cy.get('#change-new-password').type(adminPw)
             cy.get('#change-password-confirmation').type(adminPw)
             cy.contains('button', 'next').click()
             cy.contains('button', 'next').click()
+        })
+
+        // Create org
+        cy.visit("http://localhost:4200/org/create")
+        cy.contains('Use your personal account as organisation owner').click({ force: true })
+        cy.get('[formcontrolname="name"]').type('caos-demo')
+        cy.contains('button', 'CREATE').click()
+        cy.contains('button', 'Global').click()
+        cy.contains('button', 'caos-demo').click()
+  
+        // Create sa
+        cy.visit("http://localhost:4200/users/create-machine")
+        cy.get('[formcontrolname="userName"]').type("e2e")
+        cy.get('[formcontrolname="name"]').type("e2e")
+        cy.get('[formcontrolname="description"]').type("User who calls the ZITADEL API for preparing end-to-end tests")
+        cy.contains('button', 'Create').click()
+
+        // Create and download sa key
+        cy.contains('div .card', 'Keys').contains('a', 'New').click()     
+        cy.contains('button', 'Add').click()
+        cy.contains('button', 'Download').click()
+
+        // Create e2e users
+        // tmp
+/*        cy.visit("http://localhost:4200/users/me")
+        cy.contains('button', 'Global').click()
+        cy.contains('button', 'caos-demo').click()*/
+
+        ;[User.OrgOwner,  User.OrgOwnerViewer, User.OrgProjectCreator, User.LoginPolicyUser, User.PasswordComplexityUser].forEach(user => {
+            cy.visit("http://localhost:4200/users/create")
+            cy.get('[formcontrolname="email"]').type("dummy@example.com")
+            cy.get('[formcontrolname="userName"]').type(`${user}_user_name`, { force: true })
+            cy.get('[formcontrolname="firstName"]').type(`${user}_first_name`)
+            cy.get('[formcontrolname="lastName"]').type(`${user}_last_name`)
+            cy.get('mat-checkbox').click()
+            cy.contains('button', 'Create').click()
+            cy.url().should(url => {
+                expect(url).to.match(/http\:\/\/localhost\:4200\/users\/[0-9]+/)
+            })
+            cy.url().then(url => {
+                cy.visit(`${url}/password`)
+                const pw = Cypress.env(`${user}_password`)
+                cy.get('[formcontrolname="password"]').type(pw)
+                cy.get('[formcontrolname="confirmPassword"]').type(pw)
+                cy.contains('button', 'Set New Password').click()
+            })
         })
     })
 })
