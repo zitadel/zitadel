b0948c87b317ee1a514d459a8bc34e5d74341750
Elio Bischof <eliobischof@gmail.com>
2022-01-05 15:08:51 +0100

    fix channel mock generation

--- internal/notification/channels/gen_mock.go
+++ internal/notification/channels/gen_mock.go
@@ -1,4 +1,4 @@
 package channels
 
-//go:generate mockgen -package mock -destination ./mock/provider.mock.go github.com/caos/zitadel/internal/notification/providers NotificationProvider
-//go:generate mockgen -package mock -destination ./mock/message.mock.go github.com/caos/zitadel/internal/notification/providers Message
+//go:generate mockgen -package mock -destination ./mock/channel.mock.go github.com/caos/zitadel/internal/notification/channels NotificationChannel
+//go:generate mockgen -package mock -destination ./mock/message.mock.go github.com/caos/zitadel/internal/notification/channels Message
