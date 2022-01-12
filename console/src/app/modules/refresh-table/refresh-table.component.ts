db487ae2639cce16ace7cb7adfc17258ac0a800f
Max Peintner <max@caos.ch>
2022-01-12 15:56:06 +0100

    refresh table underline, user table

--- console/src/app/modules/refresh-table/refresh-table.component.ts
+++ console/src/app/modules/refresh-table/refresh-table.component.ts
@@ -21,11 +21,7 @@ const rotate = animation([
   selector: 'cnsl-refresh-table',
   templateUrl: './refresh-table.component.html',
   styleUrls: ['./refresh-table.component.scss'],
-  animations: [
-    trigger('rotate', [
-      transition('* => *', [useAnimation(rotate, { params: { time: '1s' } })]),
-    ]),
-  ],
+  animations: [trigger('rotate', [transition('* => *', [useAnimation(rotate, { params: { time: '1s' } })])])],
 })
 export class RefreshTableComponent implements OnInit {
   @Input() public selection: SelectionModel<any> = new SelectionModel<any>(true, []);
@@ -36,8 +32,9 @@ export class RefreshTableComponent implements OnInit {
   @Input() public emitRefreshOnPreviousRoutes: string[] = [];
   @Output() public refreshed: EventEmitter<void> = new EventEmitter();
   @Input() public hideRefresh: boolean = false;
+  @Input() public showBorder: boolean = false;
 
-  constructor(private refreshService: RefreshService) { }
+  constructor(private refreshService: RefreshService) {}
 
   ngOnInit(): void {
     if (this.emitRefreshAfterTimeoutInMs) {
@@ -46,8 +43,10 @@ export class RefreshTableComponent implements OnInit {
       }, this.emitRefreshAfterTimeoutInMs);
     }
 
-    if (this.emitRefreshOnPreviousRoutes.length && this.refreshService.previousUrls
-      .some(url => this.emitRefreshOnPreviousRoutes.includes(url))) {
+    if (
+      this.emitRefreshOnPreviousRoutes.length &&
+      this.refreshService.previousUrls.some((url) => this.emitRefreshOnPreviousRoutes.includes(url))
+    ) {
       setTimeout(() => {
         this.emitRefresh();
       }, 1000);
