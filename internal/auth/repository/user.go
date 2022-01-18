9eca3e8012a96bd59b257cfbe5ad70afdcbf8847
Livio Amstutz <livio.a@gmail.com>
2022-01-18 10:51:21 +0100

    cleanup

--- internal/auth/repository/user.go
+++ internal/auth/repository/user.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"time"
 
-	"github.com/caos/zitadel/internal/domain"
 	"github.com/caos/zitadel/internal/user/model"
 )
 
@@ -18,7 +17,7 @@ type UserRepository interface {
 
 	//SearchUsers(ctx context.Context, request *model.UserSearchRequest) (*model.UserSearchResponse, error)
 
-	SearchUserMetadata(ctx context.Context, userID string) (*domain.MetadataSearchResponse, error)
+	//SearchUserMetadata(ctx context.Context, userID string) (*domain.MetadataSearchResponse, error)
 }
 
 type myUserRepo interface {
@@ -40,6 +39,6 @@ type myUserRepo interface {
 
 	MyUserChanges(ctx context.Context, lastSequence uint64, limit uint64, sortAscending bool, retention time.Duration) (*model.UserChanges, error)
 
-	GetMyMetadataByKey(ctx context.Context, key string) (*domain.Metadata, error)
-	SearchMyMetadata(ctx context.Context, req *domain.MetadataSearchRequest) (*domain.MetadataSearchResponse, error)
+	//GetMyMetadataByKey(ctx context.Context, key string) (*domain.Metadata, error)
+	//SearchMyMetadata(ctx context.Context, req *domain.MetadataSearchRequest) (*domain.MetadataSearchResponse, error)
 }
