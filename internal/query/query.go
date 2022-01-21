ae0f024f1bdb95b4ad3f8d814788f3613aabcd11
fabi <fabienne.gerschwiler@gmail.com>
2022-01-21 13:44:33 +0100

    fix: remove unused code

--- internal/query/query.go
+++ internal/query/query.go
@@ -77,26 +77,3 @@ func StartQueries(ctx context.Context, es *eventstore.Eventstore, projections pr
 
 	return repo, nil
 }
-
-//
-//func (r *Queries) IAMByID(ctx context.Context, id string) (_ *iam_model.IAM, err error) {
-//	readModel, err := r.iamByID(ctx, id)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	return readModelToIAM(readModel), nil
-//}
-//
-//func (r *Queries) iamByID(ctx context.Context, id string) (_ *ReadModel, err error) {
-//	ctx, span := tracing.NewSpan(ctx)
-//	defer func() { span.EndWithError(err) }()
-//
-//	readModel := NewReadModel(id)
-//	err = r.eventstore.FilterToQueryReducer(ctx, readModel)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	return readModel, nil
-//}
