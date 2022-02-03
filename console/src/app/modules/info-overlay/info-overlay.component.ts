a98f4e1d13556a0625b6fcf84cee2ffe5ed4c0b8
Max Peintner <max@caos.ch>
2022-02-03 15:11:09 +0100

    lifecycle lint

--- console/src/app/modules/info-overlay/info-overlay.component.ts
+++ console/src/app/modules/info-overlay/info-overlay.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'cnsl-info-overlay',
   templateUrl: './info-overlay.component.html',
   styleUrls: ['./info-overlay.component.scss'],
 })
-export class InfoOverlayComponent implements OnInit {
+export class InfoOverlayComponent {
   public show: boolean = false;
   constructor() {}
-
-  ngOnInit(): void {}
 
   public dismiss() {
     this.show = false;
