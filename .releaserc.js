ebb7784d8feb8868794138db6df6c3c965d465b4
Florian Forster <florian@caos.ch>
2022-02-13 23:52:10 +0100

    test with goreleaser and semrel

--- .releaserc.js
+++ .releaserc.js
@@ -1,23 +1,9 @@
 module.exports = {
-    branches: ["main"],
+    branches: [
+        {name: 'main'},
+        {name: 'v2-ci', channel: 'next'},
+      ]
     plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        ["@semantic-release/github", {
-            "assets": [
-                {
-                    "path": "./artifacts/zitadelctl-darwin-amd64/zitadelctl-darwin-amd64",
-                    "label": "Zitadelctl Darwin x86_64"
-                },
-                {
-                    "path": "./artifacts/zitadelctl-linux-amd64/zitadelctl-linux-amd64",
-                    "label": "Zitadelctl Linux x86_64"
-                },
-                {
-                    "path": "./artifacts/zitadelctl-windows-amd64/zitadelctl-windows-amd64.exe",
-                    "label": "Zitadelctl Windows x86_64"
-                }
-            ]
-        }],
+        "@semantic-release/commit-analyzer"
     ]
 };
