970a47c3d4e9093eface09a95c6808c93eb04de0
Florian Forster <florian@caos.ch>
2022-02-13 23:53:54 +0100

    fix typo

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: 'v2-ci', channel: 'next'},
-      ]
+      ],
     plugins: [
         "@semantic-release/commit-analyzer"
     ]
