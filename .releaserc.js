1bf51296bd4ac564a123df6d4fe9f0face937f5b
Florian Forster <florian@caos.ch>
2022-02-14 00:09:45 +0100

    trial

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,8 @@
 module.exports = {
     branches: [
         {name: 'main'},
-        {name: 'v2-ci', channel: 'next'},
+        {name: '1.x.x', range: '1.x.x', channel: '1.x.x'}, 
+        {name: 'v2', channel: 'next', prerelease: true},
       ],
     plugins: [
         "@semantic-release/commit-analyzer"
