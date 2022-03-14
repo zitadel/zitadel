0b7f127e029ef5b69965cfcb98a2ffe798f803ce
Livio Amstutz <livio.a@gmail.com>
2022-03-14 07:23:17 +0100

    Update 07_events_table.sql

--- cmd/admin/initialise/sql/07_events_table.sql
+++ cmd/admin/initialise/sql/07_events_table.sql
@@ -12,7 +12,7 @@ CREATE TABLE eventstore.events (
 	, editor_user TEXT NOT NULL 
 	, editor_service TEXT NOT NULL
 	, resource_owner TEXT NOT NULL
-    , tenant TEXT
+	, tenant TEXT
 
 	, PRIMARY KEY (event_sequence DESC) USING HASH WITH BUCKET_COUNT = 10
 	, INDEX agg_type_agg_id (aggregate_type, aggregate_id)
