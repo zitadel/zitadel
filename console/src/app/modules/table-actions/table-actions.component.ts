37f5337d2480476335ee6017b0a3788935668731
Max Peintner <max@caos.ch>
2022-03-17 17:10:12 +0100

    lifecycle

--- console/src/app/modules/table-actions/table-actions.component.ts
+++ console/src/app/modules/table-actions/table-actions.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'cnsl-table-actions',
   templateUrl: './table-actions.component.html',
-  styleUrls: ['./table-actions.component.scss']
+  styleUrls: ['./table-actions.component.scss'],
 })
-export class TableActionsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TableActionsComponent {
+  constructor() {}
 }
