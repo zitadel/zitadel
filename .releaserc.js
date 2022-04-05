0eea0faba2b937426ee1c0361800f762384d616e
Elio Bischof <eliobischof@gmail.com>
2022-04-05 08:21:44 +0200

    fix: prerelease

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: '1.x.x', range: '1.x.x', channel: '1.x.x'},
-        {name: 'workflow-dispatch', prerelease: true },
+        {name: 'workflow-dispatch', prerelease: true},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
