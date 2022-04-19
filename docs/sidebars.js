3f7dfd9825fb3cb2bd103b6b5b293086de23624d
adlerhurst <silvan.reusser@gmail.com>
2022-04-19 20:05:38 +0200

    docs: add app guide

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -44,6 +44,7 @@ module.exports = {
         "guides/basics/get-started",
         "guides/basics/organizations",
         "guides/basics/projects",
+        "guides/basics/applications",
       ],
     },
     {
