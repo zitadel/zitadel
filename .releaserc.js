e3bbcf7b477938912f862c97149abb41d93a22fa
Livio Amstutz <livio.a@gmail.com>
2022-04-27 12:02:50 +0200

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -3,6 +3,7 @@ module.exports = {
         {name: 'main'},
         {name: '1.x.x', range: '1.x.x', channel: '1.x.x'},
         {name: 'v2', prerelease: true},
+        {name: 'lower-headers', prerelease: true},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
