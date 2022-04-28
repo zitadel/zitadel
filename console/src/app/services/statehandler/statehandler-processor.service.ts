074842d17ef668024ead80e51ee8b2ac569727a2
Max Peintner <max@caos.ch>
2022-04-28 15:00:42 +0200

    statehandler

--- console/src/app/services/statehandler/statehandler-processor.service.ts
+++ console/src/app/services/statehandler/statehandler-processor.service.ts
@@ -4,34 +4,36 @@ import { Observable, of } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
 
 export abstract class StatehandlerProcessorService {
-    public abstract createState(url: string): Observable<string | undefined>;
-    public abstract restoreState(state?: string): void;
+  public abstract createState(url: string): Observable<string | undefined>;
+  public abstract restoreState(state?: string): void;
 }
 
 @Injectable()
-export class StatehandlerProcessorServiceImpl
-    implements StatehandlerProcessorService {
-    constructor(private location: Location) { }
+export class StatehandlerProcessorServiceImpl implements StatehandlerProcessorService {
+  constructor(private location: Location) {}
 
-    public createState(url: string): Observable<string> {
-        const externalUrl = this.location.prepareExternalUrl(url);
-        const urlId = uuidv4();
+  public createState(url: string): Observable<string> {
+    const externalUrl = this.location.prepareExternalUrl(url);
+    const urlId = uuidv4();
 
-        sessionStorage.setItem(urlId, externalUrl);
-
-        return of(urlId);
+    if (externalUrl.includes('login_hint=')) {
+      sessionStorage.setItem(urlId, externalUrl);
     }
 
-    public restoreState(state?: string): void {
-        if (state === undefined) {
-            return;
-        }
+    return of(urlId);
+  }
 
-        const url = sessionStorage.getItem(state);
-        if (url === null) {
-            return;
-        }
-
-        window.location.href = window.location.origin + url;
+  public restoreState(state?: string): void {
+    if (state === undefined) {
+      return;
     }
+
+    const url = sessionStorage.getItem(state);
+    if (url === null) {
+      return;
+    }
+
+    sessionStorage.removeItem(state);
+    window.location.href = window.location.origin + url;
+  }
 }
