8744f933c715795bb1ff0872eb49c4124e9dd10e
Livio Amstutz <livio.a@gmail.com>
2022-05-13 13:59:33 +0200

    fix channel order

--- internal/notification/senders/sms.go
+++ internal/notification/senders/sms.go
@@ -3,15 +3,17 @@ package senders
 import (
 	"context"
 
+	"github.com/zitadel/zitadel/internal/notification/channels"
 	"github.com/zitadel/zitadel/internal/notification/channels/fs"
 	"github.com/zitadel/zitadel/internal/notification/channels/log"
 	"github.com/zitadel/zitadel/internal/notification/channels/twilio"
 )
 
 func SMSChannels(ctx context.Context, twilioConfig *twilio.TwilioConfig, getFileSystemProvider func(ctx context.Context) (*fs.FSConfig, error), getLogProvider func(ctx context.Context) (*log.LogConfig, error)) (chain *Chain, err error) {
-	chain = debugChannels(ctx, getFileSystemProvider, getLogProvider)
+	channels := make([]channels.NotificationChannel, 0, 3)
 	if twilioConfig != nil {
-		chain.channels = append(chain.channels, twilio.InitTwilioChannel(*twilioConfig))
+		channels = append(channels, twilio.InitTwilioChannel(*twilioConfig))
 	}
-	return chain, nil
+	channels = append(channels, debugChannels(ctx, getFileSystemProvider, getLogProvider)...)
+	return chainChannels(channels...), nil
 }
