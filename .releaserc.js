35be37c1b0aed075b8032befa5f6a3645e661477
Elio Bischof <eliobischof@gmail.com>
2022-05-17 11:14:32 +0200

    fix: release on v2-alpha

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,8 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: '1.x.x', range: '1.x.x', channel: '1.x.x'},
-        {name: 'v2', prerelease: true},
+        {name: 'v2-alpha', prerelease: true},
+        {name: 'v2-alpha-publish-releases', prerelease: true},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
