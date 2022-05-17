a0c8ae8e033edb6faf758868019e9fe8cd41832d
Elio Bischof <eliobischof@gmail.com>
2022-05-17 11:38:25 +0200

    remove test release channel

--- .releaserc.js
+++ .releaserc.js
@@ -3,7 +3,6 @@ module.exports = {
         {name: 'main'},
         {name: '1.x.x', range: '1.x.x', channel: '1.x.x'},
         {name: 'v2-alpha', prerelease: true},
-        {name: 'v2-alpha-publish-releases', prerelease: true},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
