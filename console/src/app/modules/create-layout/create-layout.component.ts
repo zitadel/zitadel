7221db71003dac134bedb80dda51a5062bae424d
Max Peintner <max@caos.ch>
2022-05-20 15:27:03 +0200

    lint

--- console/src/app/modules/create-layout/create-layout.component.ts
+++ console/src/app/modules/create-layout/create-layout.component.ts
@@ -1,18 +1,16 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'cnsl-create-layout',
   templateUrl: './create-layout.component.html',
   styleUrls: ['./create-layout.component.scss'],
 })
-export class CreateLayoutComponent implements OnInit {
+export class CreateLayoutComponent {
   @Input() currentCreateStep: number = 1;
   @Input() createSteps: number = 1;
   @Input() title: string = '';
   @Output() closed: EventEmitter<void> = new EventEmitter();
   constructor() {}
-
-  ngOnInit(): void {}
 
   close() {
     this.closed.emit();
