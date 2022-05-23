0fc176aa5dc73b29146124bd496d8698e5616f8d
Max Peintner <max@caos.ch>
2022-05-23 15:45:10 +0200

    text styles, statehandler fix

--- console/src/app/services/statehandler/statehandler-processor.service.ts
+++ console/src/app/services/statehandler/statehandler-processor.service.ts
@@ -1,37 +1,36 @@
 import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
 import { v4 as uuidv4 } from 'uuid';
 
 export abstract class StatehandlerProcessorService {
-  public abstract createState(url: string): Observable<string | undefined>;
+  public abstract createState(url: string): string;
   public abstract restoreState(state?: string): void;
 }
 
 @Injectable()
 export class StatehandlerProcessorServiceImpl implements StatehandlerProcessorService {
-  constructor(private location: Location) {}
+  constructor(private location: Location, private router: Router) {}
 
-  public createState(url: string): Observable<string> {
+  public createState(url: string): string {
     const externalUrl = this.location.prepareExternalUrl(url);
     const urlId = uuidv4();
-
     sessionStorage.setItem(urlId, externalUrl);
-
-    return of(urlId);
+    return urlId;
   }
 
   public restoreState(state?: string): void {
     if (state === undefined) {
       return;
+    } else {
+      const url = sessionStorage.getItem(state);
+      if (url === null) {
+        return;
+      } else {
+        sessionStorage.removeItem(state);
+        // window.location.replace(window.location.origin + url);
+        this.router.navigate([url]);
+      }
     }
-
-    const url = sessionStorage.getItem(state);
-    if (url === null) {
-      return;
-    }
-
-    sessionStorage.removeItem(state);
-    window.location.href = window.location.origin + url;
   }
 }
