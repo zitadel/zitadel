21a0e4a972df1be4124b0227df21e46fa9c1a6b9
Livio Amstutz <livio.a@gmail.com>
2022-06-01 09:50:28 +0200

    feat: get current label and privacy policies (#3748)

---
+++ internal/api/grpc/auth/policy.go
@@ -0,0 +1,29 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/zitadel/zitadel/internal/api/authz"
+	policy_grpc "github.com/zitadel/zitadel/internal/api/grpc/policy"
+	auth_pb "github.com/zitadel/zitadel/pkg/grpc/auth"
+)
+
+func (s *Server) GetMyLabelPolicy(ctx context.Context, _ *auth_pb.GetMyLabelPolicyRequest) (*auth_pb.GetMyLabelPolicyResponse, error) {
+	policy, err := s.query.ActiveLabelPolicyByOrg(ctx, authz.GetCtxData(ctx).OrgID)
+	if err != nil {
+		return nil, err
+	}
+	return &auth_pb.GetMyLabelPolicyResponse{
+		Policy: policy_grpc.ModelLabelPolicyToPb(policy, s.assetsAPIDomain(ctx)),
+	}, nil
+}
+
+func (s *Server) GetMyPrivacyPolicy(ctx context.Context, _ *auth_pb.GetMyPrivacyPolicyRequest) (*auth_pb.GetMyPrivacyPolicyResponse, error) {
+	policy, err := s.query.PrivacyPolicyByOrg(ctx, authz.GetCtxData(ctx).OrgID)
+	if err != nil {
+		return nil, err
+	}
+	return &auth_pb.GetMyPrivacyPolicyResponse{
+		Policy: policy_grpc.ModelPrivacyPolicyToPb(policy),
+	}, nil
+}
