b3a2d04845ee428df1de0064f20381e1ea2e6faf
adlerhurst <silvan.reusser@gmail.com>
2022-07-19 14:45:22 +0200

    init for cockroach

--- cmd/initialise/helper.go
+++ cmd/initialise/helper.go
@@ -8,11 +8,6 @@ import (
 )
 
 func exec(db *sql.DB, stmt string, possibleErrCodes []string, args ...interface{}) error {
-	// s, err := db.Prepare(stmt)
-	// if err != nil {
-	// 	return err
-	// }
-	// _, err = s.Exec(args...)
 	_, err := db.Exec(stmt, args...)
 	pgErr := new(pgconn.PgError)
 	if errors.As(err, &pgErr) {
