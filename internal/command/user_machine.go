8448f88f94423a02b9998766038ee42520e04197
Fabi <38692350+hifabienne@users.noreply.github.com>
2022-07-28 16:33:59 +0200

    fix: remove user login must be domain check on machine users (#4065)
    * fix: remove user login must be domain check on machine users
    
    * fix: test
    
    Co-authored-by: Livio Spring <livio.a@gmail.com>

--- internal/command/user_machine.go
+++ internal/command/user_machine.go
@@ -16,9 +16,6 @@ func (c *Commands) AddMachine(ctx context.Context, orgID string, machine *domain
 	if err != nil {
 		return nil, caos_errs.ThrowPreconditionFailed(err, "COMMAND-3M9fs", "Errors.Org.DomainPolicy.NotFound")
 	}
-	if !domainPolicy.UserLoginMustBeDomain {
-		return nil, caos_errs.ThrowPreconditionFailed(nil, "COMMAND-6M0ds", "Errors.User.Invalid")
-	}
 	userID, err := c.idGenerator.Next()
 	if err != nil {
 		return nil, err
