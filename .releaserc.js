86fd7aef5a2552f0963aa3d04ce2f0bd57da177a
Livio Amstutz <livio.a@gmail.com>
2022-07-29 09:00:28 +0200

    chore: update branches in releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,6 @@
 module.exports = {
     branches: [
-        {name: 'main'},
+        {name: 'v1',  range: '1.x.x', channel: '1.x.x'},
         {name: '1.x.x', range: '1.x.x', channel: '1.x.x'},
         {name: 'v2-alpha', prerelease: true},
         {name: 'v2-alpha-import', prerelease: true},
