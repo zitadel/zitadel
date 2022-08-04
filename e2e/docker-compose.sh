a2f0957c733bc60f6802c9cf09560400ee0e6420
Elio Bischof <eliobischof@gmail.com>
2022-08-04 10:40:36 +0200

    use compose run and zitadel nw

--- e2e/docker-compose.sh
+++ e2e/docker-compose.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-COMPOSE_DOCKER_CLI_BUILD=1 docker compose --file ${projectRoot}/e2e/docker-compose-workdir.yaml --file ${projectRoot}/docs/docs/guides/deploy/docker-compose.yaml --file ${projectRoot}/e2e/docker-compose-overwrite.yaml --env-file ${projectRoot}/e2e/compose.env "$@"
+COMPOSE_DOCKER_CLI_BUILD=1 docker compose --file ${projectRoot}/e2e/docker-compose-workdir.yaml --file ${projectRoot}/docs/docs/guides/deploy/docker-compose.yaml --file ${projectRoot}/e2e/docker-compose-overwrite.yaml "$@"
