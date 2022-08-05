265ea64c05c3f40362629db31c27583925755d88
Christian Jakob <christian@caos.ch>
2022-08-05 14:32:55 +0200

    deleted unneeded shellscript

--- e2e/docker-compose.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-COMPOSE_DOCKER_CLI_BUILD=1 docker compose --file ${SCRIPTPATH}/docker-compose-workdir.yaml --file ${SCRIPTPATH}/../docs/docs/guides/deploy/docker-compose.yaml --file ${SCRIPTPATH}/docker-compose-overwrite.yaml "$@"
