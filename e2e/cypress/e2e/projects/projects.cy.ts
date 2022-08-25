a57800fba6813c20c63bc0e07d98081551da97b4
Elio Bischof <eliobischof@gmail.com>
2022-08-25 13:20:19 +0200

    unskip remove project tests

--- e2e/cypress/e2e/projects/projects.cy.ts
+++ e2e/cypress/e2e/projects/projects.cy.ts
@@ -27,7 +27,7 @@ describe("projects", () => {
     });
   });
 
-  describe.skip("remove project", () => {
+  describe("remove project", () => {
     describe("list view", () => {
       beforeEach("ensure it exists", () => {
         apiAuth().then((api) => {
@@ -42,7 +42,7 @@ describe("projects", () => {
         cy.contains("tr", testProjectNameDeleteList, { timeout: 1000 })
           .find('[data-e2e="delete-project-button"]')
           .click({ force: true });
-        cy.get('[data-e2e="confirm-dialog-input"]').type(
+        cy.get('[data-e2e="confirm-dialog-input"]').focus().type(
           testProjectNameDeleteList
         );
         cy.get('[data-e2e="confirm-dialog-button"]').click();
@@ -65,7 +65,7 @@ describe("projects", () => {
           .find('[data-e2e="delete-project-button"]')
           .trigger("mouseover")
           .click();
-        cy.get('[data-e2e="confirm-dialog-input"]').type(
+        cy.get('[data-e2e="confirm-dialog-input"]').focus().type(
           testProjectNameDeleteGrid
         );
         cy.get('[data-e2e="confirm-dialog-button"]').click();
