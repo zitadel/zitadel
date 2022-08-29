dcc5dcd9b5df7f8d4c1c24e68ff51e74ff0720ed
Elio Bischof <eliobischof@gmail.com>
2022-08-29 17:09:51 +0200

    cleanup

--- e2e/cypress/e2e/machines/machines.cy.ts
+++ e2e/cypress/e2e/machines/machines.cy.ts
@@ -22,20 +22,16 @@ describe("machines", () => {
     });
 
     it("should add a machine", () => {
-      cy.get('[data-e2e="action-key-add"]')
-        .parents('[data-e2e="create-user-button"]')
+      cy.get('[data-e2e="create-user-button"]')
         .click();
       cy.url().should("contain", "users/create-machine");
       //force needed due to the prefilled username prefix
       cy.get('[formcontrolname="userName"]')
-        .focus()
         .type(testMachineUserNameAdd);
       cy.get('[formcontrolname="name"]')
-      .focus()
-      .type("e2emachinename");
+        .type("e2emachinename");
       cy.get('[formcontrolname="description"]')
-      .focus()
-      .type("e2emachinedescription");
+        .type("e2emachinedescription");
       cy.get('[data-e2e="create-button"]').click();
       cy.get(".data-e2e-success");
       cy.wait(200);
