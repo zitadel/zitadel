cae7c70f95efb740dc270322249ed699132f9b60
Livio Spring <livio.a@gmail.com>
2022-09-07 07:41:15 +0200

    create prerelease

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
-        {name: 'saml-v2', prerelease: 'beta'},
+        {name: 'v2-saml', prerelease: 'beta'},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
