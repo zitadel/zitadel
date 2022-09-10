2cd9662da9c10fe62ecea099fee76b6885e87a77
Elio Bischof <eliobischof@gmail.com>
2022-09-10 03:08:09 +0200

    improve application test

--- e2e/cypress/e2e/applications/applications.cy.ts
+++ e2e/cypress/e2e/applications/applications.cy.ts
@@ -34,34 +34,43 @@ describe("applications", () => {
         .should("be.visible")
         .click();
       cy.get('[formcontrolname="name"]')
+        .focus()
         .type(testAppName);
       cy.get('[for="WEB"]')
         .click();
       cy.get('[data-e2e="continue-button-nameandtype"]')
         .click();
       cy.get('[for="PKCE"]')
+        .should('be.visible')
         .click();
       cy.get('[data-e2e="continue-button-authmethod"]')
         .click();
-      cy.get("cnsl-redirect-uris")
-        .eq(0)
+      cy.get('[data-e2e="redirect-uris"] input')
+        .focus()
         .type("http://localhost:3000/api/auth/callback/zitadel");
-      cy.get("cnsl-redirect-uris")
-        .eq(1)
+      cy.get('[data-e2e="postlogout-uris"] input')
+        .focus()
         .type("http://localhost:3000");
       cy.get('[data-e2e="continue-button-redirecturis"]')
         .click();
       cy.get('[data-e2e="create-button"]')
         .click()
-        .then(() => {
-          cy.get("[id*=overlay]")
-            .should("exist");
-        });
+      cy.get("[id*=overlay]")
+        .should("exist");
       cy.get(".data-e2e-success");
-      cy.wait(200);
+      cy.get('[data-e2e="client-id-copy"]')
+        .click()
+      const expectClientId = new RegExp(`^.*[0-9]+\\\@${testProjectName}.*$`)
+      cy.contains('[data-e2e="client-id"]', expectClientId)
+      cy.window()
+        .then(win => {
+            win.navigator.clipboard.readText().then(copiedClientId => {
+              win.focus()
+              expect(expectClientId.test(copiedClientId)).to.be.true
+            })
+        })
       cy.get(".data-e2e-failure", { timeout: 0 })
         .should("not.exist");
-      //TODO: check client ID/Secret
     });
   })
 
