8646d3d5caa0c4d21e4b7d3dada9475155deb164
Florian Forster <florian@zitadel.com>
2022-09-12 17:25:24 +0200

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,8 +1,7 @@
 module.exports = {
     branches: [
         {name: 'main'},
-        {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
-        {name: 'v2-saml', prerelease: 'beta'},
+        {name: '1.87.x', range: '1.87.x', channel: '1.87.x'}
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
