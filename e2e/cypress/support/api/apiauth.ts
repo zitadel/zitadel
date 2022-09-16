d6b8be94d34d25bcead44ab7dfd1b21f9480c2a5
Max Peintner <max@caos.ch>
2022-09-16 08:50:16 +0200

    format

--- e2e/cypress/support/api/apiauth.ts
+++ e2e/cypress/support/api/apiauth.ts
@@ -1,15 +1,15 @@
-import { login, User } from 'support/login/users'
+import { login, User } from 'support/login/users';
 
 export interface apiCallProperties {
-    authHeader: string
-    mgntBaseURL: string
+  authHeader: string;
+  mgntBaseURL: string;
 }
 
 export function apiAuth(): Cypress.Chainable<apiCallProperties> {
-    return login(User.IAMAdminUser, 'Password1!', false, true).then(token => {
-        return <apiCallProperties>{
-            authHeader: `Bearer ${token}`,
-            mgntBaseURL: `${Cypress.env("BACKEND_URL")}/management/v1/`,
-        }
-    })
+  return login(User.IAMAdminUser, 'Password1!', false, true).then((token) => {
+    return <apiCallProperties>{
+      authHeader: `Bearer ${token}`,
+      mgntBaseURL: `${Cypress.env('BACKEND_URL')}/management/v1/`,
+    };
+  });
 }
