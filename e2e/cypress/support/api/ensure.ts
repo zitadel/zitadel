53350f42653ded60e5315cde6ace5482c1b9dabe
Livio Spring <livio.a@gmail.com>
2022-09-23 10:19:15 +0200

    lint e2e

--- e2e/cypress/support/api/ensure.ts
+++ e2e/cypress/support/api/ensure.ts
@@ -41,43 +41,43 @@ export function ensureSomethingExists(
 }
 
 export function ensureSomethingIsSet(
-    api: apiCallProperties,
-    path: string,
-    find: (entity: any) => SearchResult,
-    createPath: string,
-    body: any,
+  api: apiCallProperties,
+  path: string,
+  find: (entity: any) => SearchResult,
+  createPath: string,
+  body: any,
 ): Cypress.Chainable<number> {
-    return getSomething(api, path, find)
-        .then((sRes) => {
-            if (sRes.entity) {
-                return cy.wrap({
-                    id: sRes.entity.id,
-                    initialSequence: 0,
-                });
-            }
-            return cy
-                .request({
-                    method: 'PUT',
-                    url: createPath,
-                    headers: {
-                        Authorization: api.authHeader,
-                    },
-                    body: body,
-                    failOnStatusCode: false,
-                    followRedirect: false,
-                })
-                .then((cRes) => {
-                    expect(cRes.status).to.equal(200);
-                    return {
-                        id: cRes.body.id,
-                        initialSequence: sRes.sequence,
-                    };
-                });
-        })
-        .then((data) => {
-            awaitDesiredById(90, (entity) => !!entity, data.initialSequence, api, path, find);
-            return cy.wrap<number>(data.id);
+  return getSomething(api, path, find)
+    .then((sRes) => {
+      if (sRes.entity) {
+        return cy.wrap({
+          id: sRes.entity.id,
+          initialSequence: 0,
         });
+      }
+      return cy
+        .request({
+          method: 'PUT',
+          url: createPath,
+          headers: {
+            Authorization: api.authHeader,
+          },
+          body: body,
+          failOnStatusCode: false,
+          followRedirect: false,
+        })
+        .then((cRes) => {
+          expect(cRes.status).to.equal(200);
+          return {
+            id: cRes.body.id,
+            initialSequence: sRes.sequence,
+          };
+        });
+    })
+    .then((data) => {
+      awaitDesiredById(90, (entity) => !!entity, data.initialSequence, api, path, find);
+      return cy.wrap<number>(data.id);
+    });
 }
 
 export function ensureSomethingDoesntExist(
@@ -138,21 +138,21 @@ function searchSomething(
 }
 
 function getSomething(
-    api: apiCallProperties,
-    searchPath: string,
-    find: (entity: any) => SearchResult,
+  api: apiCallProperties,
+  searchPath: string,
+  find: (entity: any) => SearchResult,
 ): Cypress.Chainable<SearchResult> {
-    return cy
-        .request({
-            method: 'GET',
-            url: searchPath,
-            headers: {
-                Authorization: api.authHeader,
-            },
-        })
-        .then((res) => {
-            return find(res.body);
-        });
+  return cy
+    .request({
+      method: 'GET',
+      url: searchPath,
+      headers: {
+        Authorization: api.authHeader,
+      },
+    })
+    .then((res) => {
+      return find(res.body);
+    });
 }
 
 function awaitDesired(
@@ -190,7 +190,7 @@ function awaitDesiredById(
     if (!foundExpectedEntity || !foundExpectedSequence) {
       expect(trials, `trying ${trials} more times`).to.be.greaterThan(0);
       cy.wait(1000);
-        awaitDesiredById(trials - 1, expectEntity, initialSequence, api, path, find);
+      awaitDesiredById(trials - 1, expectEntity, initialSequence, api, path, find);
     }
   });
 }
