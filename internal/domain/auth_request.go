7dfa1925cc00d1fc1b36f0c8674d8a6e1a237e66
Livio Spring <livio.a@gmail.com>
2022-09-23 14:08:10 +0200

    feat: restrict login to specific org by id (scope) (#4294)
    * feat: add new org scope
    
    * change default of UserLoginMustBeDomain to false
    
    * return resource owner claims
    
    * fix: use email style for first user
    
    * fix: ensure email style for default users (backwards compatibility)
    
    * change to external domain (as it was before UserLoginMustBeDomain change)
    
    * update e2e tests to use email style usernames
    
    * document new scope
    
    * lint e2e
    
    Co-authored-by: Fabi <38692350+hifabienne@users.noreply.github.com>

--- internal/domain/auth_request.go
+++ internal/domain/auth_request.go
@@ -37,6 +37,7 @@ type AuthRequest struct {
 	RequestedOrgID           string
 	RequestedOrgName         string
 	RequestedPrimaryDomain   string
+	RequestedOrgDomain       bool
 	ApplicationResourceOwner string
 	PrivateLabelingSetting   PrivateLabelingSetting
 	SelectedIDPConfigID      string
@@ -159,6 +160,18 @@ func (a *AuthRequest) GetScopeOrgPrimaryDomain() string {
 		for _, scope := range request.Scopes {
 			if strings.HasPrefix(scope, OrgDomainPrimaryScope) {
 				return strings.TrimPrefix(scope, OrgDomainPrimaryScope)
+			}
+		}
+	}
+	return ""
+}
+
+func (a *AuthRequest) GetScopeOrgID() string {
+	switch request := a.Request.(type) {
+	case *AuthRequestOIDC:
+		for _, scope := range request.Scopes {
+			if strings.HasPrefix(scope, OrgIDScope) {
+				return strings.TrimPrefix(scope, OrgIDScope)
 			}
 		}
 	}
