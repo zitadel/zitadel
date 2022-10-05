d4e89da5f0f69ed78744b7a71aff7deef491b69d
adlerhurst <silvan.reusser@gmail.com>
2022-10-05 21:49:33 +0200

    fix: create pre release

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
-        {name: "timestamp-as-sequence", channel: "rc"}
+        {name: "timestamp-as-sequence", prerelease: true}
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
