5039950914f826b639ece3c3161a71038c32283c
Florian Forster <florian@zitadel.com>
2022-10-17 23:54:10 +0200

    still broken

--- tools/install.sh
+++ tools/install.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-cd $GOPATH/src/github.com/zitadel/zitadel/tools
+cd tools
 for imp in `cat tools.go | grep "_" | sed -E "s/_ \"(.*.+)\"/\1/g"`; do
     echo "installing $imp"
     go install $imp
