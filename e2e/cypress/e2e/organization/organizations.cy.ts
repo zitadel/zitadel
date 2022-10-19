8dbdcd13868a0888a95124eaf47da84d48a18ecf
Max Peintner <max@caos.ch>
2022-10-19 10:05:48 +0200

    change e2e test

--- e2e/cypress/e2e/organization/organizations.cy.ts
+++ e2e/cypress/e2e/organization/organizations.cy.ts
@@ -1,25 +1,30 @@
-import { renameOrg } from 'support/api/orgs';
+import { ensureOrgExists, renameOrg } from 'support/api/orgs';
 import { apiAuth } from '../../support/api/apiauth';
 
+const orgListPath = `/orgs`;
 const orgPath = `/org`;
+
+const orgNameOnCreation = 'e2eneworg';
 const testOrgNameChange = 'e2erenametest';
 
 describe('organizations', () => {
   describe('rename', () => {
     beforeEach(() => {
-      apiAuth().as('api');
-      cy.visit(orgPath);
-    });
-
-    afterEach(() => {
       apiAuth()
         .as('api')
         .then((api) => {
-          renameOrg(api, 'ZITADEL');
+          ensureOrgExists(api, orgNameOnCreation)
+            .as('newOrgId')
+            .then((newOrgId) => {
+              cy.visit(orgListPath);
+            });
         });
     });
 
     it('should rename the organization', () => {
+      const rowSelector = `tr:contains(${orgNameOnCreation})`;
+      cy.get(rowSelector).children('.mat-cell').first().click({ force: true });
+
       cy.get('[data-e2e="actions"]').click();
       cy.get('[data-e2e="rename"]', { timeout: 1000 }).should('be.visible').click();
 
