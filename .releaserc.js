27282977b99108053e1fe8a59ebcb863676cee65
Livio Spring <livio.a@gmail.com>
2022-10-27 12:53:01 +0200

    maintenance branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,7 @@
 module.exports = {
     branches: [
         {name: 'main'},
+        {name: '2.8.x', range: '2.8.x', channel: '2.8.x'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'}
     ],
     plugins: [
