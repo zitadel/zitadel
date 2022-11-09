765b39ff6ab37e8d89ab81d293b36a0453dccb6a
Livio Spring <livio.a@gmail.com>
2022-11-09 07:40:29 +0100

    test(e2e): table driven tests for humans and machines

--- e2e/cypress/e2e/humans/humans.cy.ts
+++ e2e/cypress/e2e/humans/humans.cy.ts
@@ -1,53 +1,67 @@
 import { apiAuth } from '../../support/api/apiauth';
 import { ensureHumanUserExists, ensureUserDoesntExist } from '../../support/api/users';
 import { loginname } from '../../support/login/users';
+import { ensureDomainPolicy } from '../../support/api/policies';
 
 describe('humans', () => {
   const humansPath = `/users?type=human`;
-  const testHumanUserNameAdd = 'e2ehumanusernameadd';
-  const testHumanUserNameRemove = 'e2ehumanusernameremove';
 
-  beforeEach(() => {
-    apiAuth().as('api');
-  });
-
-  describe('add', () => {
-    beforeEach(`ensure it doesn't exist already`, function () {
-      ensureUserDoesntExist(this.api, testHumanUserNameAdd);
-      cy.visit(humansPath);
+  [
+    { mustBeDomain: true, addName: 'e2ehumanusernameadd', removeName: 'e2ehumanusernameremove' },
+    { mustBeDomain: false, addName: 'e2ehumanusernameadd', removeName: 'e2ehumanusernameremove' },
+    { mustBeDomain: false, addName: 'e2ehumanusernameadd@test.com', removeName: 'e2ehumanusernameremove@test.com' },
+  ].forEach((user) => {
+    beforeEach(() => {
+      apiAuth().as('api');
     });
 
-    it('should add a user', () => {
-      cy.get('[data-e2e="create-user-button"]').click();
-      cy.url().should('contain', 'users/create');
-      cy.get('[formcontrolname="email"]').type('dummy@dummy.com');
-      //force needed due to the prefilled username prefix
-      cy.get('[formcontrolname="userName"]').type(testHumanUserNameAdd);
-      cy.get('[formcontrolname="firstName"]').type('e2ehumanfirstname');
-      cy.get('[formcontrolname="lastName"]').type('e2ehumanlastname');
-      cy.get('[formcontrolname="phone"]').type('+41 123456789');
-      cy.get('[data-e2e="create-button"]').click();
-      cy.get('.data-e2e-success');
-      cy.contains('[data-e2e="copy-loginname"]', testHumanUserNameAdd).click();
-      cy.clipboardMatches(testHumanUserNameAdd);
-      cy.shouldNotExist({ selector: '.data-e2e-failure' });
-    });
-  });
+    describe(`add "${user.addName}" with domain setting "${user.mustBeDomain}"`, () => {
+      beforeEach(`ensure it doesn't exist already`, function () {
+        ensureDomainPolicy(this.api, user.mustBeDomain, false, false);
+        ensureUserDoesntExist(this.api, user.addName);
+        cy.visit(humansPath);
+      });
 
-  describe('remove', () => {
-    beforeEach('ensure it exists', function () {
-      ensureHumanUserExists(this.api, testHumanUserNameRemove);
-      cy.visit(humansPath);
+      it('should add a user', () => {
+        cy.get('[data-e2e="create-user-button"]').click();
+        cy.url().should('contain', 'users/create');
+        cy.get('[formcontrolname="email"]').type('dummy@dummy.com');
+        //force needed due to the prefilled username prefix
+        cy.get('[formcontrolname="userName"]').type(user.addName);
+        cy.get('[formcontrolname="firstName"]').type('e2ehumanfirstname');
+        cy.get('[formcontrolname="lastName"]').type('e2ehumanlastname');
+        cy.get('[formcontrolname="phone"]').type('+41 123456789');
+        cy.get('[data-e2e="create-button"]').click();
+        cy.get('.data-e2e-success');
+        let loginName = user.addName;
+        if (user.mustBeDomain) {
+          loginName = loginname(user.addName, Cypress.env('ORGANIZATION'));
+        }
+        cy.contains('[data-e2e="copy-loginname"]', loginName).click();
+        cy.clipboardMatches(loginName);
+        cy.shouldNotExist({ selector: '.data-e2e-failure' });
+      });
     });
 
-    it('should delete a human user', () => {
-      const rowSelector = `tr:contains(${testHumanUserNameRemove})`;
-      cy.get(rowSelector).find('[data-e2e="enabled-delete-button"]').click({ force: true });
-      cy.get('[data-e2e="confirm-dialog-input"]').focus().type(testHumanUserNameRemove);
-      cy.get('[data-e2e="confirm-dialog-button"]').click();
-      cy.get('.data-e2e-success');
-      cy.shouldNotExist({ selector: rowSelector, timeout: 2000 });
-      cy.shouldNotExist({ selector: '.data-e2e-failure' });
+    describe(`remove "${user.removeName}" with domain setting "${user.mustBeDomain}"`, () => {
+      beforeEach('ensure it exists', function () {
+        ensureHumanUserExists(this.api, user.removeName);
+        cy.visit(humansPath);
+      });
+
+      let loginName = user.removeName;
+      if (user.mustBeDomain) {
+        loginName = loginname(user.removeName, Cypress.env('ORGANIZATION'));
+      }
+      it('should delete a human user', () => {
+        const rowSelector = `tr:contains(${user.removeName})`;
+        cy.get(rowSelector).find('[data-e2e="enabled-delete-button"]').click({ force: true });
+        cy.get('[data-e2e="confirm-dialog-input"]').focus().type(loginName);
+        cy.get('[data-e2e="confirm-dialog-button"]').click();
+        cy.get('.data-e2e-success');
+        cy.shouldNotExist({ selector: rowSelector, timeout: 2000 });
+        cy.shouldNotExist({ selector: '.data-e2e-failure' });
+      });
     });
   });
 });
