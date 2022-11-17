e17995b7963424f54ee6e7a37364dcd0b3ce9554
Livio Spring <livio.a@gmail.com>
2022-11-17 08:01:38 +0100

    change sql statement to only create index

--- cmd/setup/05.sql
+++ cmd/setup/05.sql
@@ -1,11 +1,11 @@
-ALTER TABLE adminapi.failed_events ALTER PRIMARY KEY USING COLUMNS (instance_id, view_name, failed_sequence);
-ALTER TABLE auth.failed_events ALTER PRIMARY KEY USING COLUMNS (instance_id, view_name, failed_sequence);
-ALTER TABLE projections.failed_events ALTER PRIMARY KEY USING COLUMNS (instance_id, projection_name, failed_sequence);
+CREATE INDEX instance_id_idx ON adminapi.current_sequences (instance_id);
+CREATE INDEX instance_id_idx ON auth.current_sequences (instance_id);
+CREATE INDEX instance_id_idx ON projections.current_sequences (instance_id);
+
+CREATE INDEX instance_id_idx ON adminapi.failed_events (instance_id);
+CREATE INDEX instance_id_idx ON auth.failed_events (instance_id);
+CREATE INDEX instance_id_idx ON projections.failed_events (instance_id);
 
 ALTER TABLE adminapi.failed_events ADD COLUMN last_failed TIMESTAMPTZ;
 ALTER TABLE auth.failed_events ADD COLUMN last_failed TIMESTAMPTZ;
 ALTER TABLE projections.failed_events ADD COLUMN last_failed TIMESTAMPTZ;
-
-ALTER TABLE adminapi.current_sequences ALTER PRIMARY KEY USING COLUMNS (instance_id, view_name);
-ALTER TABLE auth.current_sequences ALTER PRIMARY KEY USING COLUMNS (instance_id, view_name);
-ALTER TABLE projections.current_sequences ALTER PRIMARY KEY USING COLUMNS (instance_id, projection_name, aggregate_type);
