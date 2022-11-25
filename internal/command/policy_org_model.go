faaa56806390141703638186522511024a5aa708
Livio Spring <livio.a@gmail.com>
2022-11-25 14:05:58 +0100

    fix: handle UserLoginMustBeDomain changes correctly

--- internal/command/policy_org_model.go
+++ internal/command/policy_org_model.go
@@ -1,9 +1,14 @@
 package command
 
 import (
+	"context"
+	"strings"
+
 	"github.com/zitadel/zitadel/internal/domain"
 	"github.com/zitadel/zitadel/internal/eventstore"
+	"github.com/zitadel/zitadel/internal/repository/org"
 	"github.com/zitadel/zitadel/internal/repository/policy"
+	"github.com/zitadel/zitadel/internal/repository/user"
 )
 
 type PolicyDomainWriteModel struct {
@@ -38,4 +43,89 @@ func (wm *PolicyDomainWriteModel) Reduce() error {
 		}
 	}
 	return wm.WriteModel.Reduce()
+}
+
+type DomainPolicyUsernamesWriteModel struct {
+	eventstore.WriteModel
+
+	Domain string
+	Users  map[string]string
+}
+
+func NewDomainPolicyUsernamesWriteModel(orgID string) *DomainPolicyUsernamesWriteModel {
+	return &DomainPolicyUsernamesWriteModel{
+		WriteModel: eventstore.WriteModel{
+			ResourceOwner: orgID,
+		},
+		Users: make(map[string]string),
+	}
+}
+
+func (wm *DomainPolicyUsernamesWriteModel) AppendEvents(events ...eventstore.Event) {
+	wm.WriteModel.AppendEvents(events...)
+}
+
+func (wm *DomainPolicyUsernamesWriteModel) Reduce() error {
+	for _, event := range wm.Events {
+		switch e := event.(type) {
+		case *org.DomainPrimarySetEvent:
+			wm.Domain = e.Domain
+		case *user.HumanAddedEvent:
+			wm.Users[e.Aggregate().ID] = e.UserName
+		case *user.HumanRegisteredEvent:
+			wm.Users[e.Aggregate().ID] = e.UserName
+		case *user.MachineAddedEvent:
+			wm.Users[e.Aggregate().ID] = e.UserName
+		case *user.UsernameChangedEvent:
+			wm.Users[e.Aggregate().ID] = e.UserName
+		case *user.DomainClaimedEvent:
+			wm.Users[e.Aggregate().ID] = e.UserName
+		case *user.UserRemovedEvent:
+			delete(wm.Users, e.Aggregate().ID)
+		}
+	}
+	return wm.WriteModel.Reduce()
+}
+
+func (wm *DomainPolicyUsernamesWriteModel) Query() *eventstore.SearchQueryBuilder {
+	return eventstore.NewSearchQueryBuilder(eventstore.ColumnsEvent).
+		ResourceOwner(wm.ResourceOwner).
+		AddQuery().
+		AggregateTypes(org.AggregateType, user.AggregateType).
+		EventTypes(
+			org.OrgDomainPrimarySetEventType,
+			user.HumanAddedType,
+			user.HumanRegisteredType,
+			user.MachineAddedEventType,
+			user.UserUserNameChangedType,
+			user.UserDomainClaimedType,
+			user.UserRemovedType,
+		).
+		Builder()
+}
+
+func (wm *DomainPolicyUsernamesWriteModel) NewUsernameChangedEvents(ctx context.Context, userLoginMustBeDomain bool) []eventstore.Command {
+	events := make([]eventstore.Command, 0, len(wm.Users))
+	for id, name := range wm.Users {
+		var newName string
+		if userLoginMustBeDomain {
+			// if the UserLoginMustBeDomain will be true, then it's currently false
+			// which means the usernames might already be suffixed by the domain
+			// so let's remove a potential duplicate suffix
+			newName = strings.TrimSuffix(name, "@"+wm.Domain)
+		} else {
+			// the UserLoginMustBeDomain is currently true
+			// which means the usernames must be suffixed to ensure their uniqueness
+			// and the preferred login name remains the same
+			newName = name + "@" + wm.Domain
+		}
+		events = append(events, user.NewUsernameChangedEvent(ctx,
+			&user.NewAggregate(id, wm.ResourceOwner).Aggregate,
+			name,
+			newName,
+			userLoginMustBeDomain,
+			user.UsernameChangedEventWithPolicyChange()),
+		)
+	}
+	return events
 }
--- internal/repository/user/user.go
+++ internal/repository/user/user.go
@@ -395,9 +395,10 @@ func DomainClaimedSentEventMapper(event *repository.Event) (eventstore.Event, er
 type UsernameChangedEvent struct {
 	eventstore.BaseEvent `json:"-"`
 
-	UserName              string `json:"userName"`
-	oldUserName           string `json:"-"`
-	userLoginMustBeDomain bool   `json:"-"`
+	UserName                 string `json:"userName"`
+	oldUserName              string `json:"-"`
+	userLoginMustBeDomain    bool   `json:"-"`
+	oldUserLoginMustBeDomain bool   `json:"-"`
 }
 
 func (e *UsernameChangedEvent) Data() interface{} {
@@ -406,7 +407,7 @@ func (e *UsernameChangedEvent) Data() interface{} {
 
 func (e *UsernameChangedEvent) UniqueConstraints() []*eventstore.EventUniqueConstraint {
 	return []*eventstore.EventUniqueConstraint{
-		NewRemoveUsernameUniqueConstraint(e.oldUserName, e.Aggregate().ResourceOwner, e.userLoginMustBeDomain),
+		NewRemoveUsernameUniqueConstraint(e.oldUserName, e.Aggregate().ResourceOwner, e.oldUserLoginMustBeDomain),
 		NewAddUsernameUniqueConstraint(e.UserName, e.Aggregate().ResourceOwner, e.userLoginMustBeDomain),
 	}
 }
@@ -417,16 +418,30 @@ func NewUsernameChangedEvent(
 	oldUserName,
 	newUserName string,
 	userLoginMustBeDomain bool,
+	opts ...UsernameChangedEventOption,
 ) *UsernameChangedEvent {
-	return &UsernameChangedEvent{
+	event := &UsernameChangedEvent{
 		BaseEvent: *eventstore.NewBaseEventForPush(
 			ctx,
 			aggregate,
 			UserUserNameChangedType,
 		),
-		UserName:              newUserName,
-		oldUserName:           oldUserName,
-		userLoginMustBeDomain: userLoginMustBeDomain,
+		UserName:                 newUserName,
+		oldUserName:              oldUserName,
+		userLoginMustBeDomain:    userLoginMustBeDomain,
+		oldUserLoginMustBeDomain: userLoginMustBeDomain,
+	}
+	for _, opt := range opts {
+		opt(event)
+	}
+	return event
+}
+
+type UsernameChangedEventOption func(*UsernameChangedEvent)
+
+func UsernameChangedEventWithPolicyChange() UsernameChangedEventOption {
+	return func(e *UsernameChangedEvent) {
+		e.oldUserLoginMustBeDomain = !e.userLoginMustBeDomain
 	}
 }
 
