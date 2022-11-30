ebc8e6a808ce339cbeba730f9ec883645bb6953f
Stefan Benz <stefan@caos.ch>
2022-11-30 16:40:49 +0100

    chore: delete org-delete as prerelease

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,6 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
-        {name: 'org-delete', prerelease: 'true'}
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
