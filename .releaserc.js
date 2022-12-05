05dbdaadd284c3f0760fc4827576ca79f34bf01f
adlerhurst <silvan.reusser@gmail.com>
2022-12-05 11:23:40 +0100

    chore: add release tag

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
+        {name: 'projection-without-storage', prerelease: 'test-projection'}
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
