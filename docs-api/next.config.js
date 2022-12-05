2c5aeb2c902ef5bab4e890101e5c6dc8117aa626
Max Peintner <max@caos.ch>
2022-12-05 17:57:43 +0100

    nextjs config

--- docs-api/next.config.js
+++ docs-api/next.config.js
@@ -1,8 +1,6 @@
 const withMarkdoc = require("@markdoc/next.js");
 
-module.exports = withMarkdoc({
+module.exports = withMarkdoc()({
   basePath: "/docs/api",
-  target: "serverless",
-})({
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdoc"],
 });
