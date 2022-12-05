b3600d266e1717b6cfce8bbc28616c8833131ab4
Max Peintner <max@caos.ch>
2022-12-05 17:51:01 +0100

    set serverless in nextconfig

--- docs-api/next.config.js
+++ docs-api/next.config.js
@@ -2,6 +2,7 @@ const withMarkdoc = require("@markdoc/next.js");
 
 module.exports = withMarkdoc({
   basePath: "/docs/api",
+  target: "serverless",
 })({
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdoc"],
 });
