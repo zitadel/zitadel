51887ce6fea18cfd567f7fa19aa1aafc5fb26aeb
Max Peintner <max@caos.ch>
2022-12-20 15:26:12 +0100

    card

--- docs-api/components/Card.tsx
+++ docs-api/components/Card.tsx
@@ -22,7 +22,7 @@ export function Card({
   console.log(protocol);
   return (
     <div className="my-4 bg-white dark:bg-background-dark-400 border border-border-light dark:border-border-dark rounded-md w-full">
-      <div className="py-2 px-4 bg-black/10 text-sm dark:bg-white/10 flex flex-row items-center justify-between">
+      <div className="py-2 px-4 bg-black/10 text-sm dark:bg-white/10 flex flex-row items-center justify-between rounded-t-md">
         {title}
         {hasLanguageToggle && <LanguageSwitcher initial={language} />}
         {hasProtocolToggle && <ProtocolSwitcher initial={protocol} />}
