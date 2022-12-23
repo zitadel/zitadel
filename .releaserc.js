842144cb03fa005f4152598998b342a67b648526
Stefan Benz <stefan@caos.ch>
2022-12-23 15:14:56 +0100

    fix: remove release from 2.16.x branch

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,6 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
-        {name: '2.16.x', range: '2.16.x', channel: '2.16.x'},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
