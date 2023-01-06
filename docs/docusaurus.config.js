14c3ff1f77ad4712c743d4522e6ca1746cd2ef31
Florian Forster <florian@zitadel.com>
2023-01-06 23:18:21 +0100

    cleanup

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -199,8 +199,8 @@ module.exports = {
         docs: {
           routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
+          showLastUpdateAuthor: false,
+          showLastUpdateTime: false,
           editUrl: "https://github.com/zitadel/zitadel/edit/main/docs/",
           remarkPlugins: [require("mdx-mermaid")],
         },
