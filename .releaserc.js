916ce90fd5a6f0081c9f8fc093e762fb4d99d0cf
Stefan Benz <stefan@caos.ch>
2023-01-12 16:56:47 +0100

    fix: add build of prerelease for branch

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
+        {name: 'system-listiammembers', prerelease: 'iammembers'}
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
