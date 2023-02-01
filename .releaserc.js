9296c6ad0ab439078cbaecbeadfc2d95e8ccdc62
Livio Spring <livio.a@gmail.com>
2023-02-01 06:53:33 +0100

    create prerelease

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
+        {name: 'v2.19-rc', prerelease: 'rc'},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
