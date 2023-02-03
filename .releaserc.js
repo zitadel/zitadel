93ec964bebb434d30fca0636313d31d78e5db69d
Livio Spring <livio.a@gmail.com>
2023-02-03 09:03:34 +0100

    maintenance branch

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
+        {name: '2.18.x', range: '2.18.x', channel: '2.18.x'},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
