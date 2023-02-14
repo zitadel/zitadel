dd4ac31dbe36981a67e4a5a6d9b72fa1ba8c5aec
Elio Bischof <eliobischof@gmail.com>
2023-02-14 20:06:52 +0100

    resolve conflicts

--- e2e/cypress/e2e/organization/organizations.cy.ts
+++ e2e/cypress/e2e/organization/organizations.cy.ts
@@ -1,5 +1,4 @@
 import { ensureOrgExists, ensureOrgIsDefault, isDefaultOrg } from 'support/api/orgs';
-import { apiAuth } from '../../support/api/apiauth';
 import { v4 as uuidv4 } from 'uuid';
 import { Context } from 'support/commands';
 
