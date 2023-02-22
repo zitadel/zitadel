44da8d160e8f21ce65d6602f0ae0d9261a17dccb
Livio Spring <livio.a@gmail.com>
2023-02-22 16:43:04 +0100

    build release

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     branches: [
         {name: 'main'},
-        {name: '1.87.x', range: '1.87.x', channel: '1.87.x'}
+        {name: 'v2.20.x', range: '2.20.x', channel: '2.20.x'}
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
