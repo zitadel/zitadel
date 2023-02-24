896330355e222cc78c77be67a4be8438300dd8ea
Max Peintner <max@caos.ch>
2023-02-24 14:22:59 +0100

    Update console/src/app/modules/nav/nav.module.ts
    Co-authored-by: Silvan <silvan.reusser@gmail.com>

--- console/src/app/modules/nav/nav.module.ts
+++ console/src/app/modules/nav/nav.module.ts
@@ -13,7 +13,6 @@ import { HasRoleModule } from 'src/app/directives/has-role/has-role.module';
 import { HasRolePipeModule } from 'src/app/pipes/has-role-pipe/has-role-pipe.module';
 
 import { NavComponent } from './nav.component';
-import { OnboardingCardComponent } from '../onboarding-card/onboarding-card.component';
 import OnboardingCardModule from '../onboarding-card/onboarding-card.module';
 import { MatLegacyProgressBarModule } from '@angular/material/legacy-progress-bar';
 
