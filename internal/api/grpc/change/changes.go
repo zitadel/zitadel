bd00c1ca1a29cd18a5b0e5e3f66ba78b600adcab
adlerhurst <silvan.reusser@gmail.com>
2023-03-07 13:54:46 +0100

    refactor: remove unused code

--- internal/api/grpc/change/changes.go
+++ internal/api/grpc/change/changes.go
@@ -10,13 +10,6 @@ import (
 	"github.com/zitadel/zitadel/pkg/grpc/message"
 )
 
-// func ChangeQueryToQuery(query *change_pb.ChangeQuery) (sequence uint64, limit uint64, asc bool) {
-// 	if query == nil {
-// 		return 0, 0, false
-// 	}
-// 	return query.Sequence, uint64(query.Limit), query.Asc
-// }
-
 func EventsToChangesPb(changes []*query.Event, assetAPIPrefix string) []*change_pb.Change {
 	c := make([]*change_pb.Change, len(changes))
 	for i, change := range changes {
