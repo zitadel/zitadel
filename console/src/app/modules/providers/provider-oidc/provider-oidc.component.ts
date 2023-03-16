f0e0191c7bca0b90fc6ebcfcbcc5142b3c5bef1a
Max Peintner <max@caos.ch>
2023-03-16 07:53:46 +0100

    refactor(console): common idp provider styles (#5450)
    common idp provider styles

--- console/src/app/modules/providers/provider-oidc/provider-oidc.component.ts
+++ console/src/app/modules/providers/provider-oidc/provider-oidc.component.ts
@@ -27,7 +27,6 @@ import { PolicyComponentServiceType } from '../../policies/policy-component-type
 @Component({
   selector: 'cnsl-provider-oidc',
   templateUrl: './provider-oidc.component.html',
-  styleUrls: ['./provider-oidc.component.scss'],
 })
 export class ProviderOIDCComponent {
   public showOptional: boolean = false;
