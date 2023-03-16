b8fc03033c1f28414982cb2bb088593649bb9237
mffap <mpa@zitadel.com>
2023-03-16 09:52:12 +0200

    docs: update security policies (#5452)
    * docs(legal): vulnerability disclosure policy
    
    * update security.md
    
    * exception
    
    * add link to sidebar
    
    * Apply suggestions from code review
    
    Co-authored-by: Florian Forster <florian@zitadel.com>
    
    * use main for release channel
    
    * review
    
    * fallback emails
    
    * typos, wording
    
    ---------
    
    Co-authored-by: Florian Forster <florian@zitadel.com>

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -462,6 +462,7 @@ module.exports = {
         "legal/privacy-policy",
         "legal/acceptable-use-policy",
         "legal/rate-limit-policy",
+        "legal/vulnerability-disclosure-policy",
       ],
     },
   ],
