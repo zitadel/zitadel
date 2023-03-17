a3b36a0138d184293a79e3e4a2b24bbed1962926
Silvan <silvan.reusser@gmail.com>
2023-03-17 10:14:06 +0100

    refactor(changes): use `queries.SearchEvents` (#5388)
    * refactor(changes): use `queries.SearchEvents`
    
    ---------
    
    Co-authored-by: Livio Spring <livio.a@gmail.com>

--- cmd/start/start.go
+++ cmd/start/start.go
@@ -235,7 +235,7 @@ func startAPIs(
 	if err := apis.RegisterServer(ctx, system.CreateServer(commands, queries, adminRepo, config.Database.DatabaseName(), config.DefaultInstance, config.ExternalDomain)); err != nil {
 		return err
 	}
-	if err := apis.RegisterServer(ctx, admin.CreateServer(config.Database.DatabaseName(), commands, queries, config.SystemDefaults, adminRepo, config.ExternalSecure, keys.User)); err != nil {
+	if err := apis.RegisterServer(ctx, admin.CreateServer(config.Database.DatabaseName(), commands, queries, config.SystemDefaults, adminRepo, config.ExternalSecure, keys.User, config.AuditLogRetention)); err != nil {
 		return err
 	}
 	if err := apis.RegisterServer(ctx, management.CreateServer(commands, queries, config.SystemDefaults, keys.User, config.ExternalSecure, config.AuditLogRetention)); err != nil {
