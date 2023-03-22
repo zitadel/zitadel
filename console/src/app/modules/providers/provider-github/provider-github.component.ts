e9df06df38e90c665c7a14248546401d0bf3941b
Max Peintner <max@caos.ch>
2023-03-22 09:53:07 +0100

    fix(console): initialize provider options (#5520)
    fix: provider options initializer

--- console/src/app/modules/providers/provider-github/provider-github.component.ts
+++ console/src/app/modules/providers/provider-github/provider-github.component.ts
@@ -30,7 +30,7 @@ import { PolicyComponentServiceType } from '../../policies/policy-component-type
 })
 export class ProviderGithubComponent {
   public showOptional: boolean = false;
-  public options: Options = new Options();
+  public options: Options = new Options().setIsCreationAllowed(true).setIsLinkingAllowed(true);
   public id: string | null = '';
   public serviceType: PolicyComponentServiceType = PolicyComponentServiceType.MGMT;
   private service!: ManagementService | AdminService;
