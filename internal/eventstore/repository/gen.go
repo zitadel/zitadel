415ea1fc35ea6ffffea4d9f58ba4d41e9147f683
Elio Bischof <eliobischof@gmail.com>
2023-03-27 08:47:33 +0200

    add comment about stringer usage

--- internal/eventstore/repository/gen.go
+++ internal/eventstore/repository/gen.go
@@ -2,3 +2,5 @@ package repository
 
 //go:generate stringer -type Operation -trimprefix Operation
 //go:generate stringer -type Field -trimprefix Field
+
+// The Stringer implementations are used in the custom mock matcher implementation for repository.Filter
