e3c917e5c1988ad8388239e7987ee876a5592442
Elio Bischof <eliobischof@gmail.com>
2023-03-27 13:38:53 +0200

    remove enum stringer implementations

--- internal/eventstore/repository/gen.go
+++
@@ -1,6 +0,0 @@
-package repository
-
-//go:generate stringer -type Operation -trimprefix Operation
-//go:generate stringer -type Field -trimprefix Field
-
-// The Stringer implementations are used in the custom mock matcher implementation for repository.Filter
