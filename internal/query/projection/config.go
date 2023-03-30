887e2f474d02536122d6be66eda3c8145735f653
Elio Bischof <elio@zitadel.com>
2023-03-30 13:01:27 +0200

    fix: use fixed active instances duration (#5567)
    * fix: use fixed active instances duration
    
    * fix active instances tests
    
    * fix syntax error
    
    * run pipeline
    
    ---------
    
    Co-authored-by: Silvan <silvan.reusser@gmail.com>

--- internal/query/projection/config.go
+++ internal/query/projection/config.go
@@ -5,20 +5,20 @@ import (
 )
 
 type Config struct {
-	RequeueEvery            time.Duration
-	RetryFailedAfter        time.Duration
-	MaxFailureCount         uint
-	ConcurrentInstances     uint
-	BulkLimit               uint64
-	Customizations          map[string]CustomConfig
-	HandleInactiveInstances bool
+	RequeueEvery          time.Duration
+	RetryFailedAfter      time.Duration
+	MaxFailureCount       uint
+	ConcurrentInstances   uint
+	BulkLimit             uint64
+	Customizations        map[string]CustomConfig
+	HandleActiveInstances time.Duration
 }
 
 type CustomConfig struct {
-	RequeueEvery            *time.Duration
-	RetryFailedAfter        *time.Duration
-	MaxFailureCount         *uint
-	ConcurrentInstances     *uint
-	BulkLimit               *uint64
-	HandleInactiveInstances *bool
+	RequeueEvery          *time.Duration
+	RetryFailedAfter      *time.Duration
+	MaxFailureCount       *uint
+	ConcurrentInstances   *uint
+	BulkLimit             *uint64
+	HandleActiveInstances *time.Duration
 }
