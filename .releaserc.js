fb9360382fdd2c56894ca095c6fc7b7e190636d0
Elio Bischof <eliobischof@gmail.com>
2023-04-06 15:07:44 +0200

    release

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,8 @@
 module.exports = {
     branches: [
-        {name: 'main'},
-        {name: '1.87.x', range: '1.87.x', channel: '1.87.x'}
+        { name: 'main' },
+        { name: '1.87.x', range: '1.87.x', channel: '1.87.x' },
+        { name: "onboarding-preview", prerelease: true }
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
