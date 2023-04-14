8ca829d085235301543adee60dc9543fc59487b7
adlerhurst <silvan.reusser@gmail.com>
2023-04-14 17:05:33 +0200

    chore: release

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     branches: [
         {name: 'main'},
-        {name: 'fix(eventstore)--search', prerelease: true, channel: 'dev'},
+        {name: 'fix(eventstore)--search', prerelease: 'correct-creation-date'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'}
     ],
     plugins: [
