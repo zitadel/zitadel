1f2ad97a3a4de7618816ad8eba09d1473c857c60
adlerhurst <silvan.reusser@gmail.com>
2023-04-18 16:08:13 +0200

    chore: reset release definition

--- .releaserc.js
+++ .releaserc.js
@@ -1,8 +1,8 @@
 module.exports = {
     branches: [
         {name: 'main'},
-        {name: 'fix(eventstore)--search', prerelease: 'correct-creation-date'},
-        {name: '1.87.x', range: '1.87.x', channel: '1.87.x'}
+        {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
+        {name: 'next'}
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
