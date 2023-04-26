b0025d22b1070c74f9e91984fce5506c8b80cc9a
adlerhurst <silvan.reusser@gmail.com>
2023-04-26 14:01:33 +0200

    chore: prerelease

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,8 @@
 module.exports = {
     branches: [
         {name: 'main'},
-        {name: 'next'}
+        {name: 'next'},
+        {name: 'optimise-step-10', prerelease: true}
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
