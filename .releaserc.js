5e719405bc74f98a10dbf6c459cb8ea18d0b67c0
adlerhurst <silvan.reusser@gmail.com>
2023-04-26 18:45:04 +0200

    chore: update releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -3,7 +3,7 @@ module.exports = {
         {name: 'main', channel: 'next'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
         {name: 'next', prerelease: true},
-        {name: 'eventstore-phase-3', prerelease: true, channel: 'projection-creation-date'},
+        {name: 'eventstore-phase-3', prerelease: true},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
