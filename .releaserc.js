8825dde42cda1f2c73fd1132ccd69ca368fa6a5d
adlerhurst <silvan.reusser@gmail.com>
2023-04-27 16:02:18 +0200

    chore: change tag name

--- .releaserc.js
+++ .releaserc.js
@@ -3,7 +3,7 @@ module.exports = {
         {name: 'main', channel: 'next'},
         {name: '1.87.x', range: '1.87.x', channel: '1.87.x'},
         {name: 'next', prerelease: true},
-        {name: 'eventstore-phase-3', prerelease: true},
+        {name: 'eventstore-phase-3', prerelease: 'alpha'},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
