b782b63dea664d997205d1131f337a84d55b0405
Max Peintner <max@caos.ch>
2023-04-27 17:44:08 +0200

    rm logs

--- apps/login/app/registeruser/route.ts
+++ apps/login/app/registeruser/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  console.log(body);
   if (body) {
     const { email, password, firstName, lastName } = body;
 
