e611be180251bd641d7e2591c89b90cf15790c13
Stefan Benz <stefan@caos.ch>
2023-04-28 15:49:05 +0200

    feat: add v2alpha policies service

--- internal/api/grpc/policy/v2/policy.go
+++ internal/api/grpc/policy/v2/policy.go
@@ -7,6 +7,7 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/zitadel/zitadel/internal/api/authz"
+	"github.com/zitadel/zitadel/internal/api/grpc/text"
 	"github.com/zitadel/zitadel/internal/domain"
 	"github.com/zitadel/zitadel/internal/query"
 	object "github.com/zitadel/zitadel/pkg/grpc/object/v2alpha"
@@ -26,18 +27,22 @@ func (s *Server) GetLoginPolicy(ctx context.Context, req *policy.GetLoginPolicyR
 	return &policy.GetLoginPolicyResponse{
 		Policy: modelLoginPolicyToPb(current),
 		Details: &object.Details{
-			Sequence:      current.Sequence,
-			ChangeDate:    timestamppb.New(current.ChangeDate),
-			ResourceOwner: current.OrgID,
+			Sequence:          current.Sequence,
+			ChangeDate:        timestamppb.New(current.ChangeDate),
+			ResourceOwner:     current.OrgID,
+			ResourceOwnerType: IsDefaultToResourceOwnerTypePb(current.IsDefault),
 		},
 	}, nil
 }
 
-func modelLoginPolicyToPb(current *query.LoginPolicy) *policy.LoginPolicy {
-	links := make([]string, len(current.IDPLinks))
-	for i, d := range current.IDPLinks {
-		links[i] = d.IDPID
+func IsDefaultToResourceOwnerTypePb(isDefault bool) object.ResourceOwnerType {
+	if isDefault {
+		return object.ResourceOwnerType_RESOURCE_OWNER_TYPE_INSTANCE
 	}
+	return object.ResourceOwnerType_RESOURCE_OWNER_TYPE_ORG
+}
+
+func modelLoginPolicyToPb(current *query.LoginPolicy) *policy.LoginPolicy {
 	multi := make([]policy.MultiFactorType, len(current.MultiFactors))
 	for i, typ := range current.MultiFactors {
 		multi[i] = ModelMultiFactorTypeToPb(typ)
@@ -48,7 +53,6 @@ func modelLoginPolicyToPb(current *query.LoginPolicy) *policy.LoginPolicy {
 	}
 
 	return &policy.LoginPolicy{
-		IsDefault:                  current.IsDefault,
 		AllowUsernamePassword:      current.AllowUsernamePassword,
 		AllowRegister:              current.AllowRegister,
 		AllowExternalIdp:           current.AllowExternalIDPs,
@@ -67,7 +71,6 @@ func modelLoginPolicyToPb(current *query.LoginPolicy) *policy.LoginPolicy {
 		MultiFactorCheckLifetime:   durationpb.New(current.MultiFactorCheckLifetime),
 		SecondFactors:              second,
 		MultiFactors:               multi,
-		Idps:                       links,
 	}
 }
 
@@ -92,6 +95,7 @@ func ModelSecondFactorTypeToPb(secondFactorType domain.SecondFactorType) policy.
 		return policy.SecondFactorType_SECOND_FACTOR_TYPE_UNSPECIFIED
 	}
 }
+
 func ModelMultiFactorTypeToPb(typ domain.MultiFactorType) policy.MultiFactorType {
 	switch typ {
 	case domain.MultiFactorTypeU2FWithPIN:
@@ -114,16 +118,16 @@ func (s *Server) GetPasswordPolicy(ctx context.Context, req *policy.GetPasswordP
 	return &policy.GetPasswordPolicyResponse{
 		Policy: ModelPasswordPolicyToPb(current),
 		Details: &object.Details{
-			Sequence:      current.Sequence,
-			ChangeDate:    timestamppb.New(current.ChangeDate),
-			ResourceOwner: current.ResourceOwner,
+			Sequence:          current.Sequence,
+			ChangeDate:        timestamppb.New(current.ChangeDate),
+			ResourceOwner:     current.ResourceOwner,
+			ResourceOwnerType: IsDefaultToResourceOwnerTypePb(current.IsDefault),
 		},
 	}, nil
 }
 
 func ModelPasswordPolicyToPb(current *query.PasswordComplexityPolicy) *policy.PasswordPolicy {
 	return &policy.PasswordPolicy{
-		IsDefault:    current.IsDefault,
 		MinLength:    current.MinLength,
 		HasUppercase: current.HasUppercase,
 		HasLowercase: current.HasLowercase,
@@ -132,7 +136,7 @@ func ModelPasswordPolicyToPb(current *query.PasswordComplexityPolicy) *policy.Pa
 	}
 }
 
-func (s *Server) GetLabelPolicy(ctx context.Context, req *policy.GetBrandingPolicyRequest) (*policy.GetBrandingPolicyResponse, error) {
+func (s *Server) GetBrandingPolicy(ctx context.Context, req *policy.GetBrandingPolicyRequest) (*policy.GetBrandingPolicyResponse, error) {
 	orgID := req.GetOrganisation().GetOrgId()
 	if orgID == "" {
 		orgID = authz.GetCtxData(ctx).OrgID
@@ -145,16 +149,16 @@ func (s *Server) GetLabelPolicy(ctx context.Context, req *policy.GetBrandingPoli
 	return &policy.GetBrandingPolicyResponse{
 		Policy: ModelLabelPolicyToPb(current, s.assetsAPIDomain(ctx)),
 		Details: &object.Details{
-			Sequence:      current.Sequence,
-			ChangeDate:    timestamppb.New(current.ChangeDate),
-			ResourceOwner: current.ResourceOwner,
+			Sequence:          current.Sequence,
+			ChangeDate:        timestamppb.New(current.ChangeDate),
+			ResourceOwner:     current.ResourceOwner,
+			ResourceOwnerType: IsDefaultToResourceOwnerTypePb(current.IsDefault),
 		},
 	}, nil
 }
 
 func ModelLabelPolicyToPb(current *query.LabelPolicy, assetPrefix string) *policy.BrandingPolicy {
 	return &policy.BrandingPolicy{
-		IsDefault:           current.IsDefault,
 		PrimaryColor:        current.Light.PrimaryColor,
 		BackgroundColor:     current.Light.BackgroundColor,
 		FontColor:           current.Light.FontColor,
@@ -171,4 +175,127 @@ func ModelLabelPolicyToPb(current *query.LabelPolicy, assetPrefix string) *polic
 		DisableWatermark:    current.WatermarkDisabled,
 		HideLoginNameSuffix: current.HideLoginNameSuffix,
 	}
+}
+
+func (s *Server) GetDomainPolicy(ctx context.Context, req *policy.GetDomainPolicyRequest) (*policy.GetDomainPolicyResponse, error) {
+	orgID := req.GetOrganisation().GetOrgId()
+	if orgID == "" {
+		orgID = authz.GetCtxData(ctx).OrgID
+	}
+
+	current, err := s.query.DomainPolicyByOrg(ctx, true, orgID, false)
+	if err != nil {
+		return nil, err
+	}
+	return &policy.GetDomainPolicyResponse{
+		Policy: DomainPolicyToPb(current),
+		Details: &object.Details{
+			Sequence:          current.Sequence,
+			ChangeDate:        timestamppb.New(current.ChangeDate),
+			ResourceOwner:     current.ResourceOwner,
+			ResourceOwnerType: IsDefaultToResourceOwnerTypePb(current.IsDefault),
+		},
+	}, nil
+}
+
+func DomainPolicyToPb(current *query.DomainPolicy) *policy.DomainPolicy {
+	return &policy.DomainPolicy{
+		UserLoginMustBeDomain:                  current.UserLoginMustBeDomain,
+		ValidateOrgDomains:                     current.ValidateOrgDomains,
+		SmtpSenderAddressMatchesInstanceDomain: current.SMTPSenderAddressMatchesInstanceDomain,
+	}
+}
+
+func (s *Server) GetPrivacyPolicy(ctx context.Context, req *policy.GetPrivacyPolicyRequest) (*policy.GetPrivacyPolicyResponse, error) {
+	orgID := req.GetOrganisation().GetOrgId()
+	if orgID == "" {
+		orgID = authz.GetCtxData(ctx).OrgID
+	}
+
+	current, err := s.query.PrivacyPolicyByOrg(ctx, true, orgID, false)
+	if err != nil {
+		return nil, err
+	}
+	return &policy.GetPrivacyPolicyResponse{
+		Policy: ModelPrivacyPolicyToPb(current),
+		Details: &object.Details{
+			Sequence:          current.Sequence,
+			ChangeDate:        timestamppb.New(current.ChangeDate),
+			ResourceOwner:     current.ResourceOwner,
+			ResourceOwnerType: IsDefaultToResourceOwnerTypePb(current.IsDefault),
+		},
+	}, nil
+}
+
+func ModelPrivacyPolicyToPb(current *query.PrivacyPolicy) *policy.PrivacyPolicy {
+	return &policy.PrivacyPolicy{
+		TosLink:      current.TOSLink,
+		PrivacyLink:  current.PrivacyLink,
+		HelpLink:     current.HelpLink,
+		SupportEmail: string(current.SupportEmail),
+	}
+}
+
+func (s *Server) GetLockoutPolicy(ctx context.Context, req *policy.GetLockoutPolicyRequest) (*policy.GetLockoutPolicyResponse, error) {
+	orgID := req.GetOrganisation().GetOrgId()
+	if orgID == "" {
+		orgID = authz.GetCtxData(ctx).OrgID
+	}
+
+	current, err := s.query.LockoutPolicyByOrg(ctx, true, orgID, false)
+	if err != nil {
+		return nil, err
+	}
+	return &policy.GetLockoutPolicyResponse{
+		Policy: ModelLockoutPolicyToPb(current),
+		Details: &object.Details{
+			Sequence:          current.Sequence,
+			ChangeDate:        timestamppb.New(current.ChangeDate),
+			ResourceOwner:     current.ResourceOwner,
+			ResourceOwnerType: IsDefaultToResourceOwnerTypePb(current.IsDefault),
+		},
+	}, nil
+}
+
+func ModelLockoutPolicyToPb(current *query.LockoutPolicy) *policy.LockoutPolicy {
+	return &policy.LockoutPolicy{
+		MaxPasswordAttempts: current.MaxPasswordAttempts,
+	}
+}
+
+func (s *Server) GetActiveIdentityProviders(ctx context.Context, req *policy.GetActiveIdentityProvidersRequest) (*policy.GetActiveIdentityProvidersResponse, error) {
+	orgID := req.GetOrganisation().GetOrgId()
+	if orgID == "" {
+		orgID = authz.GetCtxData(ctx).OrgID
+	}
+
+	links, err := s.query.IDPLoginPolicyLinks(ctx, orgID, &query.IDPLoginPolicyLinksSearchQuery{}, false)
+	if err != nil {
+		return nil, err
+	}
+
+	idps := make([]string, len(links.Links))
+	for i, d := range links.Links {
+		idps[i] = d.IDPID
+	}
+	return &policy.GetActiveIdentityProvidersResponse{
+		Idps:    idps,
+		Details: &object.Details{},
+	}, nil
+}
+
+func (s *Server) GetInstanceInformation(ctx context.Context, req *policy.GetInstanceInformationRequest) (*policy.GetInstanceInformationResponse, error) {
+	langs, err := s.query.Languages(ctx)
+	if err != nil {
+		return nil, err
+	}
+	instance, err := s.query.Instance(ctx, true)
+	if err != nil {
+		return nil, err
+	}
+	return &policy.GetInstanceInformationResponse{
+		SupportedLanguages: text.LanguageTagsToStrings(langs),
+		DefaultOrgId:       instance.DefaultOrgID,
+		Details:            &object.Details{},
+	}, nil
 }
