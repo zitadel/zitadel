da2f1227ff9a36bc73c4182f30973b668313471e
adlerhurst <silvan.reusser@gmail.com>
2023-04-28 14:50:41 +0200

    chore: release definition

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,12 @@
 module.exports = {
     branches: [
-        {name: 'main', channel: 'next'},
-        {name: 'next', prerelease: true}
+        // branch to create latest version
+        'next',
+        // main is the default branch and should create a draft release for each merge of frontend/backend
+        {name: 'main', channel: 'next', prerelease: 'rc'},
+        // maintainance branches for v2 minor releases
+        // TODO: replace ? with actual minor version
+        // {name: 'v2.?.x', range: '2.?.x', channel: '2.?.x'}
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
