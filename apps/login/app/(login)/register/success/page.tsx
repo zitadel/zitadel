324834dfa6e99f11797b0077f10fa443d85c69cb
Max Peintner <max@caos.ch>
2023-05-05 10:03:53 +0200

    Update apps/login/app/(login)/register/success/page.tsx
    Co-authored-by: Elio Bischof <elio@zitadel.com>

--- apps/login/app/(login)/register/success/page.tsx
+++ apps/login/app/(login)/register/success/page.tsx
@@ -7,8 +7,6 @@ type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 export default async function Page({ searchParams }: Props) {
-  //   const query = useSearchParams();
-  console.log(searchParams);
   return (
     <div className="flex flex-col items-center space-y-4">
       <h1>Register successful</h1>
