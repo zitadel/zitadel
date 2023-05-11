38ac2a4accbec02bf49187232bf40c8d33e81753
Livio Spring <livio.a@gmail.com>
2023-05-11 11:31:51 +0200

    chore: build prerelease

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: 'next'},
+        {name: 'v2.27-rc', prerelease: 'rc', channel: 'next'},
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
