3f040cd2da8792c866c7cf57be088610de08cbd2
Elio Bischof <eliobischof@gmail.com>
2023-05-11 20:46:37 +0200

    remove cookie before loading env

--- console/src/app/services/exhausted.service.ts
+++ console/src/app/services/exhausted.service.ts
@@ -8,18 +8,12 @@ import { EnvironmentService } from './environment.service';
   providedIn: 'root',
 })
 export class ExhaustedService {
-  private exhaustedKey = 'zitadel.quota.limiting';
   private isClosed = true;
 
   constructor(private envSvc: EnvironmentService, private dialog: MatDialog) {}
 
   public checkCookie() {
-    const cookieIsThere = !!document.cookie
-      .split(';')
-      .map((c) => c.trim())
-      .find((c) => c.startsWith(`${this.exhaustedKey}=`));
-
-    if (cookieIsThere) {
+    if (this.envSvc.hasExhaustedCookie) {
       return this.showExhaustedDialog();
     }
     return of(undefined);
@@ -42,14 +36,12 @@ export class ExhaustedService {
         id: 'authenticated-requests-exhausted-dialog',
       })
       .afterClosed()
-      .pipe(switchMap(() => this.resolveExhausted()));
+      .pipe(switchMap(this.resolveExhausted));
   }
 
   private resolveExhausted() {
     return this.envSvc.env.pipe(
       map((env) => {
-        // Delete the cookie
-        document.cookie = `${this.exhaustedKey}=; Path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT"`;
         // Just reload if there is no instance management url
         location.href = env.instance_management_url || location.href;
       }),
