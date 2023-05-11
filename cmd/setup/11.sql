b9a3fac3d2f48a6e924ebddc6681e25821209e18
adlerhurst <silvan.reusser@gmail.com>
2023-05-11 11:14:42 +0200

    fix(eventstore): backfill column

--- cmd/setup/11.sql
+++ cmd/setup/11.sql
@@ -1,5 +1,15 @@
-ALTER TABLE eventstore.events ADD COLUMN created_at TIMESTAMPTZ;
+BEGIN;
+-- create table with empty created_at
+ALTER TABLE eventstore.events ADD COLUMN created_at TIMESTAMPTZ DEFAULT NULL;
+COMMIT;
 
+BEGIN;
+-- backfill created_at
 UPDATE eventstore.events SET created_at = creation_date WHERE created_at IS NULL;
+COMMIT;
 
-ALTER TABLE eventstore.events ALTER COLUMN created_at SET NOT NULL DEFAULT clock_timestamp();
+BEGIN;
+-- set column rules
+ALTER TABLE eventstore.events ALTER COLUMN created_at SET DEFAULT clock_timestamp();
+ALTER TABLE eventstore.events ALTER COLUMN created_at SET NOT NULL;
+COMMIT;
