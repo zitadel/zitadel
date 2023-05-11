5a3d09d3a30000ddfbacd14340ec9768a7dd3e5a
adlerhurst <silvan.reusser@gmail.com>
2023-05-11 10:31:33 +0200

    fix(eventstore): use creation_date for existing columns

--- cmd/setup/11.sql
+++ cmd/setup/11.sql
@@ -1,1 +1,5 @@
-ALTER TABLE eventstore.events ADD COLUMN created_at TIMESTAMPTZ /*NOT NULL*/ DEFAULT clock_timestamp();
+ALTER TABLE eventstore.events ADD COLUMN created_at TIMESTAMPTZ;
+
+UPDATE eventstore.events SET created_at = creation_date WHERE created_at IS NULL;
+
+ALTER TABLE eventstore.events ALTER COLUMN created_at SET NOT NULL DEFAULT clock_timestamp();
