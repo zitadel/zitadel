7bafe7875c956e1e02e380a0cd62ad3223952ebc
Elio Bischof <eliobischof@gmail.com>
2023-05-15 05:43:59 +0200

    lint

--- console/src/app/services/environment.service.ts
+++ console/src/app/services/environment.service.ts
@@ -80,59 +80,58 @@ export class EnvironmentService {
 
   createEnvironment() {
     return of(null).pipe(
+      // Delete the exhausted cookie before the enviroment is loaded
+      tap(() => (document.cookie = `${this.exhaustedCookieKey}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC"`)),
+      switchMap(() => {
+        // Wait until the browser deleted the cookie
+        return new Promise<void>((resolve, reject) => {
+          this.awaitFiveSeconds(() => !this.hasExhaustedCookie, reject);
+          resolve();
+        });
+      }),
+      switchMap(() => this.http.get<Environment>(this.environmentJsonPath, { observe: 'response' })),
+      catchError((err) => {
+        console.error('Getting environment.json failed', err);
+        return throwError(() => err);
+      }),
+      switchMap((resp) => {
+        return new Promise<Environment>((resolve, reject) => {
+          if (resp.body === null) {
+            reject('environment.json has no body');
+            return;
+          }
+          const exhaustedResponseCookie = resp.headers.getAll('set-cookie')?.find(this.isCookie);
+          if (!exhaustedResponseCookie || !this.cookiesAreEnabled) {
+            resolve(resp.body as Environment);
+            return;
+          }
 
-          // Delete the exhausted cookie before the enviroment is loaded
-          tap(() => (document.cookie = `${this.exhaustedCookieKey}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC"`)),
-          switchMap(() => {
-            // Wait until the browser deleted the cookie
-            return new Promise<void>((resolve, reject) => {
-              this.awaitFiveSeconds(()=> !this.hasExhaustedCookie, reject)
-              resolve()
-            });
-          }),
-          switchMap(()=>this.http.get<Environment>(this.environmentJsonPath, { observe: 'response' })),
-          catchError((err) => {
-            console.error('Getting environment.json failed', err);
-            return throwError(() => err);
-          }),
-          switchMap((resp) => {
-            return new Promise<Environment>((resolve, reject) => {
-              if (resp.body === null) {
-                reject('environment.json has no body');
-                return;
-              }
-              const exhaustedResponseCookie = resp.headers.getAll('set-cookie')?.find(this.isCookie);
-              if (!exhaustedResponseCookie || !this.cookiesAreEnabled) {
-                resolve(resp.body as Environment);
-                return;
-              }
+          // The `/i` in the end of the RegExp matches without case sentitivity
+          const maxAgeRegex = /Max-Age=(\d+)/i;
+          const match = exhaustedResponseCookie.match(maxAgeRegex);
 
-              // The `/i` in the end of the RegExp matches without case sentitivity
-              const maxAgeRegex = /Max-Age=(\d+)/i;
-              const match = exhaustedResponseCookie.match(maxAgeRegex);
+          // If there is no Max-Age, we don't know if the browser should have the cookie or not.
+          if (!match) {
+            resolve(resp.body as Environment);
+            return;
+          }
 
-              // If there is no Max-Age, we don't know if the browser should have the cookie or not.
-              if (!match) {
-                resolve(resp.body as Environment);
-                return;
-              }
+          // If it is above 0, we show the exhausted dialog that either refreshes
+          // the page or redirects the user to the instance management URL.
+          if (parseInt(match[1]) > 0) {
+            this.exhaustedSvc.showExhaustedDialog(resp.body.instance_management_url);
+            return;
+          }
 
-              // If it is above 0, we show the exhausted dialog that either refreshes
-              // the page or redirects the user to the instance management URL.
-              if (parseInt(match[1]) > 0) {
-                this.exhaustedSvc.showExhaustedDialog(resp.body.instance_management_url);
-                return;
-              }
-
-              // If the Max age is 0 or below, the browser must not have the cookie.
-              // In this case, we wait for the browser to delete the cookie.
-              this.awaitFiveSeconds(()=> !this.hasExhaustedCookie, reject)
-            });
-          }),
-          // Cache the first response, then replay it
-          shareReplay(1),
-          );
-        }
+          // If the Max age is 0 or below, the browser must not have the cookie.
+          // In this case, we wait for the browser to delete the cookie.
+          this.awaitFiveSeconds(() => !this.hasExhaustedCookie, reject);
+        });
+      }),
+      // Cache the first response, then replay it
+      shareReplay(1),
+    );
+  }
 
   createWellKnown(environment$: Observable<Environment>) {
     return environment$.pipe(
@@ -148,18 +147,18 @@ export class EnvironmentService {
     );
   }
 
-  awaitFiveSeconds(condition: ()=>boolean, failWithTimeoutMessage: (msg: string) => void) {
+  awaitFiveSeconds(condition: () => boolean, failWithTimeoutMessage: (msg: string) => void) {
     let checks = 0;
     const check = () => {
-      if (condition()){
-        return
+      if (condition()) {
+        return;
       }
-      checks++
+      checks++;
       if (checks > 500) {
         failWithTimeoutMessage(`after ${checks} checks, the condition did not return true`);
-        return
+        return;
       }
       setTimeout(check, 10);
-    }
+    };
   }
 }
