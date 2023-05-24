b5522a46491569941c73609e2cf1dc477a1f7381
adlerhurst <silvan.reusser@gmail.com>
2023-05-24 09:40:53 +0200

    depth 0

--- cmd/setup/10/10_fill_table.sql
+++ cmd/setup/10/10_fill_table.sql
@@ -5,14 +5,16 @@ INSERT INTO wrong_events (
         SELECT
             instance_id
             , event_sequence
-            , creation_date AS current_cd
-            , lead(creation_date) OVER (
+            , created_at AS current_cd
+            , lead(created_at) OVER (
                 PARTITION BY instance_id
                 ORDER BY event_sequence DESC
             ) AS next_cd
         FROM
             eventstore.events
-    ) sub WHERE
+        WHERE 
+            creation_date > '2023-05-23 13:00'
+    ) sub AS OF SYSTEM TIME follower_read_timestamp() WHERE
         current_cd < next_cd
     ORDER BY
         event_sequence DESC
