7bfad856cf7e12bc14b02b168ac87df897fae445
Elio Bischof <eliobischof@gmail.com>
2023-05-25 23:31:43 +0200

    test(react): unit test react component

--- packages/zitadel-react/jest.config.ts
+++ packages/zitadel-react/jest.config.ts
@@ -2,7 +2,8 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [ '<rootDir>/jest-setup.ts' ]
 }
 
 export default jestConfig
