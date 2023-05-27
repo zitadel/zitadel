6b1f8966cc91538a1f61f32fcb1451cdf61ead88
mffap <mpa@zitadel.com>
2023-05-27 16:15:16 +0200

    works

--- docs/utils/generate_config.js
+++ docs/utils/generate_config.js
@@ -4,18 +4,7 @@ const YAML = require('yaml')
 const defaults = fs.readFileSync('../../cmd/defaults.yaml', 'utf8')
 const steps = fs.readFileSync('../../cmd/setup/steps.yaml', 'utf8')
 
-
-const docdata = [
-  {
-    key: "ZITADEL_FIRSTINSTANCE_ORG_HUMAN_USERNAME",
-    desc: `In case that UserLoginMustBeDomain is false (default) and you don't overwrite the username with an email, it will be suffixed by the org domain (org-name + domain from config). for example: zitadel-admin in org ZITADEL on domain.tld -> zitadel-admin@zitadel.domain.tld`,
-    type: undefined,
-    shortlist: true,
-  }
-]
-
-
-let doc = YAML.parseDocument(defaults)
+let doc = YAML.parseDocument(steps)
 
 let json = doc.toJSON()
 // console.log(JSON.stringify(json, null, 2))
@@ -28,42 +17,68 @@ function* deepKeys (t, pre = [])
     for (const [k, v] of Object.entries(t))
       yield* deepKeys(v, [...pre, k])
   else
-    yield [...pre]
+    yield pre
     // yield pre.join('_').toUpperCase()
 }
 
 let keys = Array.from(deepKeys(json))
 
 
-const keys2env =  function(parts) {
-  return "ZITADEL_" + parts.join("_").toUpperCase()
+
+// let env_variables = keys.map(parts => keys2env(parts))
+
+// console.log(env_variables)
+
+// // ExternalPort example
+// console.log(doc.get('ExternalPort').toString()) // String
+// console.log(doc.get('ExternalPort')) // Number
+// console.log(doc.get('ExternalPort', true).comment) // Comment on line
+// console.log(doc.get('ExternalPort', true)) // Scalar
+
+const keys2env =  function(parts, prefix = "ZITADEL_") {
+  return prefix + parts.join("_").toUpperCase()
 }
 
-function getDocData(key, data) {
-  let result = data.filter(obj => obj.key === key)
-  if (result.length === 0) {
-    return [null, null, null]
-  }
-  if (result.length > 1) {
-    throw new Error("Duplicate Keys")
-  }
-  return [result[0]['desc'], result[0]['type'], result[0]['shortlist']]
-}
+let output = []
 
-let output = keys.map(parts => [
-  keys2env(parts), 
-  doc.getIn(parts), 
-  ...getDocData(keys2env(parts), docdata),
-])
+let v = YAML.visit(doc, {
+  Scalar(key, value, path) {
 
-console.log(output)
+    // put the key names of the path in an array
+    path_array = path.filter(node => YAML.isPair(node)).map(pair => pair.key.value)
 
-console.log(doc.contents.items[4].commentBefore)
+    // we need to treat comments before a collection in such case
+    // that it's attached to the first element instead
+    let parent = path.slice(-2, -1)[0] // second to last element (aka. parent)
+    
+    if (key === 'key') {
+      
+      let description = ''
+      // console.log(`FistItem: ${parent.items[0] === path.slice(-1)[0]} Value: ${path.slice(-1)[0]}`)
+      if (parent.items[0] === path.slice(-1)[0] && parent.commentBefore !== undefined) description += parent.commentBefore // is a first item
+      if(value.commentBefore !== undefined) description += value.commentBefore
 
-console.log(doc.get('ExternalPort').toString())
-console.log(doc.get('ExternalPort'))
-console.log(doc.get('ExternalPort', true).comment)
-console.log(doc.get('ExternalPort', true))
+      output.push({
+        env: keys2env(path_array), 
+        path: path_array,
+        description: description.trim()
+      })
 
+    }
+
+    if (key === 'value') {
+      output_node = output.find(node => node.env === keys2env(path_array))
+      output_node.value = value.value 
+      if(value.comment !== undefined) output_node.description += value.comment
+    }
+
+    //if (parent.commentBefore !== undefined && parent.items[0]) output_node.commentBefore = parent.commentBefore
+
+  }, 
+
+})
+
+  console.log(output.filter(nodes => nodes.value !== undefined))
 // Metrics
-console.log(doc.contents.items)
+// console.log(doc.contents.items[4].commentBefore)
+//console.log(doc.contents.items)
