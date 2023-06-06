51a408611be2b2297234d05b05bf538d30ade29d
Max Peintner <max@caos.ch>
2023-06-06 17:11:49 +0200

    delete account api

--- apps/login/app/(login)/accounts/page.tsx
+++ apps/login/app/(login)/accounts/page.tsx
@@ -1,11 +1,10 @@
-import { Session } from "#/../../packages/zitadel-server/dist";
+import { Session } from "@zitadel/server";
 import { listSessions, server } from "#/lib/zitadel";
 import Alert from "#/ui/Alert";
-import { Avatar } from "#/ui/Avatar";
 import { getAllSessionIds } from "#/utils/cookies";
-import { UserPlusIcon, XCircleIcon } from "@heroicons/react/24/outline";
-import moment from "moment";
+import { UserPlusIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import SessionItem from "#/ui/SessionItem";
 
 async function loadSessions(): Promise<Session[]> {
   const ids = await getAllSessionIds();
@@ -23,7 +22,7 @@ async function loadSessions(): Promise<Session[]> {
 }
 
 export default async function Page() {
-  const sessions = await loadSessions();
+  let sessions = await loadSessions();
 
   return (
     <div className="flex flex-col items-center space-y-4">
@@ -35,56 +34,15 @@ export default async function Page() {
           sessions
             .filter((session) => session?.factors?.user?.loginName)
             .map((session, index) => {
-              const validPassword = session?.factors?.password?.verifiedAt;
               return (
-                <Link
+                <SessionItem
+                  session={session}
+                  reload={async () => {
+                    "use server";
+                    sessions = sessions.filter((s) => s.id !== session.id);
+                  }}
                   key={"session-" + index}
-                  href={
-                    validPassword
-                      ? `/signedin?` +
-                        new URLSearchParams({
-                          loginName: session.factors?.user?.loginName as string,
-                        })
-                      : `/password?` +
-                        new URLSearchParams({
-                          loginName: session.factors?.user?.loginName as string,
-                        })
-                  }
-                  className="group flex flex-row items-center bg-background-light-400 dark:bg-background-dark-400  border border-divider-light hover:shadow-lg dark:hover:bg-white/10 py-2 px-4 rounded-md transition-all"
-                >
-                  <div className="pr-4">
-                    <Avatar
-                      size="small"
-                      loginName={session.factors?.user?.loginName as string}
-                      name={session.factors?.user?.displayName ?? ""}
-                    />
-                  </div>
-
-                  <div className="flex flex-col">
-                    <span className="">
-                      {session.factors?.user?.displayName}
-                    </span>
-                    <span className="text-xs opacity-80">
-                      {session.factors?.user?.loginName}
-                    </span>
-                    {validPassword && (
-                      <span className="text-xs opacity-80">
-                        {moment(new Date(validPassword)).fromNow()}
-                      </span>
-                    )}
-                  </div>
-
-                  <span className="flex-grow"></span>
-                  <div className="relative flex flex-row items-center">
-                    {validPassword ? (
-                      <div className="absolute h-2 w-2 bg-green-500 rounded-full mx-2 transform right-0 group-hover:right-6 transition-all"></div>
-                    ) : (
-                      <div className="absolute h-2 w-2 bg-red-500 rounded-full mx-2 transform right-0 group-hover:right-6 transition-all"></div>
-                    )}
-
-                    <XCircleIcon className="hidden group-hover:block h-5 w-5 transition-all opacity-50 hover:opacity-100" />
-                  </div>
-                </Link>
+                />
               );
             })
         ) : (
