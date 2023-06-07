fd39de812eaefebd5d8db7e7af234b672cb0e83c
Silvan <silvan.reusser@gmail.com>
2023-06-07 18:30:19 +0200

    fix(setup): steps 10 and 11 (#5987)
    * fix(step10): count amount of wrong_events manually
    
    * fix(step11): create index for better performance

--- cmd/setup/11/11_fetch_events.sql
+++ cmd/setup/11/11_fetch_events.sql
@@ -1,1 +1,12 @@
-SELECT id, creation_date FROM eventstore.events WHERE created_at IS NULL ORDER BY event_sequence DESC, instance_id LIMIT 20 FOR UPDATE
+SELECT 
+    id
+    , creation_date 
+FROM 
+    eventstore.events 
+WHERE 
+    created_at IS NULL 
+ORDER BY 
+    event_sequence DESC
+    , instance_id 
+LIMIT $1 
+FOR UPDATE
