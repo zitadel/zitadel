16ce383d30a58028233fb0a4a0f1839da5000df3
Max Peintner <max@caos.ch>
2023-06-08 08:48:32 +0200

    rm releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -1,10 +1,4 @@
 module.exports = {
-    branches: [
-        { name: 'main' },
-        { name: 'next' },
-        { name: 'urlsafebase64', prerelease: true },
-    ],
-    plugins: [
-        "@semantic-release/commit-analyzer"
-    ]
+  branches: [{ name: "main" }, { name: "next" }],
+  plugins: ["@semantic-release/commit-analyzer"],
 };
