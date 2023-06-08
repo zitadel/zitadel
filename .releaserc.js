616d3717b90e41d3fd2cbdab2ac969f43547e0b8
adlerhurst <silvan.reusser@gmail.com>
2023-06-08 20:46:29 +0200

    add plugiins and brnach

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,11 @@
 module.exports = {
-  branches: [{ name: "main" }, { name: "next" }],
-  plugins: ["@semantic-release/commit-analyzer"],
+  branches: [
+    { name: "main" }, 
+    { name: "next" },
+    { name: "ci/improve-make", channel: "ignore-me", prerelease: true }
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator"
+  ],
 };
