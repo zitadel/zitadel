b44fb27c20d12e337742f3f1d17701be4fddbeae
Elio Bischof <eliobischof@gmail.com>
2023-06-08 06:37:35 +0200

    remove jest-setup.ts from zitadel-react

--- packages/zitadel-react/jest.config.ts
+++ packages/zitadel-react/jest.config.ts
@@ -3,7 +3,7 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: [ '<rootDir>/jest-setup.ts' ]
+  setupFilesAfterEnv: [ '@testing-library/jest-dom/extend-expect' ]
 }
 
 export default jestConfig
