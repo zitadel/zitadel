4bffd8df2eb280d8c71b07ab679a889ff63bf1da
adlerhurst <silvan.reusser@gmail.com>
2023-06-08 21:26:12 +0200

    so?

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,7 @@ module.exports = {
   branches: [
     { name: "main" }, 
     { name: "next" },
-    { name: "ci/improve-no-pr", prerelease: true }
+    { name: "ci/improve-no-pr", prerelease: "2.29-ignore-me" }
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
