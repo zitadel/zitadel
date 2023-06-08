e640c852ed43d879cfa70b4c0a203b89e362d7f7
adlerhurst <silvan.reusser@gmail.com>
2023-06-08 21:06:44 +0200

    plugins like oidc

--- .releaserc.js
+++ .releaserc.js
@@ -6,6 +6,7 @@ module.exports = {
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator"
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
   ],
 };
