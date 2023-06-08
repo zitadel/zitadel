bb96b0d06771467dcb39bc45207c79476b67cd09
Elio Bischof <eliobischof@gmail.com>
2023-06-08 07:19:25 +0200

    fix linting

--- apps/login/jest.config.ts
+++ apps/login/jest.config.ts
@@ -14,6 +14,6 @@ export default async (): Promise<Config.InitialOptions> => {
     }),
     testEnvironment: "jsdom",
     testRegex: "/__test__/.*\\.test\\.tsx?$",
-    modulePathIgnorePatterns: ["cypress"]
+    modulePathIgnorePatterns: ["cypress"],
   };
 };
