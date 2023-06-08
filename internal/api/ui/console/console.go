082eca58271a446ab32fd03e5503d17c54a8a8b4
adlerhurst <silvan.reusser@gmail.com>
2023-06-08 10:58:04 +0200

    console dist in static

--- internal/api/ui/console/console.go
+++ internal/api/ui/console/console.go
@@ -34,7 +34,7 @@ type spaHandler struct {
 }
 
 var (
-	//go:embed static/console
+	//go:embed static
 	static embed.FS
 )
 
@@ -92,7 +92,7 @@ func (f *file) Stat() (_ fs.FileInfo, err error) {
 }
 
 func Start(config Config, externalSecure bool, issuer op.IssuerFromRequest, callDurationInterceptor, instanceHandler func(http.Handler) http.Handler, limitingAccessInterceptor *middleware.AccessInterceptor, customerPortal string) (http.Handler, error) {
-	fSys, err := fs.Sub(static, "static/console")
+	fSys, err := fs.Sub(static, "static")
 	if err != nil {
 		return nil, err
 	}
