7828ce5094a9f62469a1739ec69c9390866ee5c3
Miguel Cabrerizo <doncicuto@gmail.com>
2023-06-11 14:09:57 +0200

    fix: add double router back if new

--- console/src/app/directives/back/back.directive.ts
+++ console/src/app/directives/back/back.directive.ts
@@ -1,16 +1,30 @@
 import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { NavigationService } from 'src/app/services/navigation.service';
 
 @Directive({
   selector: '[cnslBack]',
 })
 export class BackDirective {
+  new: Boolean = false;
   @HostListener('click')
   onClick(): void {
     this.navigation.back();
+    if (this.new) {
+      this.navigation.back();
+    }
   }
 
-  constructor(private navigation: NavigationService, private elRef: ElementRef, private renderer2: Renderer2) {
+  constructor(
+    private navigation: NavigationService,
+    private elRef: ElementRef,
+    private renderer2: Renderer2,
+    private route: ActivatedRoute,
+  ) {
+    this.route.queryParams.subscribe((params) => {
+      this.new = params['new'];
+    });
+
     if (navigation.isBackPossible) {
       // this.renderer2.removeStyle(this.elRef.nativeElement, 'visibility');
     } else {
