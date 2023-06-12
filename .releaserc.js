709897e70d5ca1752ef75cb2bf0d6ac76024c915
adlerhurst <silvan.reusser@gmail.com>
2023-06-12 07:06:23 +0200

    chore: maintenance releases

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: 'next'},
+        {name: 'v.2.27.x', }
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
