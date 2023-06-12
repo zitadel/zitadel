48d5fb05ebd78d5d8a98ddaeddb450de5a91de1f
adlerhurst <silvan.reusser@gmail.com>
2023-06-12 08:09:24 +0200

    chore: correct releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,7 @@ module.exports = {
     branches: [
         {name: 'main'},
         {name: 'next'},
-        {name: 'v.2.27.x', }
+        {name: 'v2.27.x', range: '2.27.x', channel: '2.27.x'}
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
