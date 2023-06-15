e566980819ddd8d796cfd319096fab4c135d3b90
adlerhurst <silvan.reusser@gmail.com>
2023-06-16 01:15:49 +0200

    fix(eventstore): add indexes

--- cmd/setup/12/cockroach/12.sql
+++ cmd/setup/12/cockroach/12.sql
@@ -15,3 +15,9 @@ COMMIT;
 BEGIN;
 ALTER TABLE eventstore.events ALTER PRIMARY KEY USING COLUMNS (instance_id, aggregate_type, aggregate_id, event_sequence DESC);
 COMMIT;
+
+BEGIN;
+CREATE INDEX IF NOT EXISTS handler_idx ON eventstore.events (instance_id, aggregate_type, event_type, created_at) STORING (aggregate_version, previous_aggregate_sequence, previous_aggregate_type_sequence, event_data, editor_user, editor_service, resource_owner);
+CREATE INDEX IF NOT EXISTS agg_id_event_idx ON eventstore.events (aggregate_type, aggregate_id, event_type) STORING (aggregate_version, previous_aggregate_sequence, previous_aggregate_type_sequence, created_at, event_data, editor_user, editor_service, resource_owner);
+CREATE INDEX IF NOT EXISTS active_instances_new ON eventstore.events (created_at DESC, instance_id) USING HASH;
+COMMIT;
--- cmd/setup/12/postgres/12.sql
+++ cmd/setup/12/postgres/12.sql
@@ -16,3 +16,9 @@ BEGIN;
 ALTER TABLE eventstore.events DROP CONSTRAINT IF EXISTS events_pkey;
 ALTER TABLE eventstore.events ADD PRIMARY KEY (instance_id, aggregate_type, aggregate_id, event_sequence);
 COMMIT;
+
+BEGIN;
+CREATE INDEX IF NOT EXISTS handler_idx ON eventstore.events (instance_id, aggregate_type, event_type, created_at) STORING (aggregate_version, previous_aggregate_sequence, previous_aggregate_type_sequence, event_data, editor_user, editor_service, resource_owner);
+CREATE INDEX IF NOT EXISTS agg_id_event_idx ON eventstore.events (aggregate_type, aggregate_id, event_type) STORING (aggregate_version, previous_aggregate_sequence, previous_aggregate_type_sequence, created_at, event_data, editor_user, editor_service, resource_owner);
+CREATE INDEX IF NOT EXISTS active_instances_new ON eventstore.events (created_at, instance_id);
+COMMIT;
