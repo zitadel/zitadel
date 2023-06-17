afca1d972c135fd4d5c7ba2c52b24c045fbcea85
adlerhurst <silvan.reusser@gmail.com>
2023-06-17 19:13:16 +0200

    fix: eventstore new active instances index

--- cmd/setup/12/cockroach/12.sql
+++ cmd/setup/12/cockroach/12.sql
@@ -10,6 +10,7 @@ DROP INDEX IF EXISTS eventstore.events@agg_type CASCADE;
 DROP INDEX IF EXISTS eventstore.events@agg_type_agg_id CASCADE;
 DROP INDEX IF EXISTS eventstore.events@agg_type_seq CASCADE;
 DROP INDEX IF EXISTS eventstore.events@max_sequence CASCADE;
+DROP INDEX IF EXISTS eventstore.events@active_instances;
 COMMIT;
 
 BEGIN;
@@ -17,16 +18,8 @@ ALTER TABLE eventstore.events ALTER PRIMARY KEY USING COLUMNS (instance_id, aggr
 COMMIT;
 
 BEGIN;
-CREATE INDEX IF NOT EXISTS es_handler_idx ON eventstore.events (instance_id, aggregate_type, event_type, created_at) STORING (aggregate_version, previous_aggregate_sequence, previous_aggregate_type_sequence, event_data, editor_user, editor_service, resource_owner);
-CREATE INDEX IF NOT EXISTS es_agg_id_event_idx ON eventstore.events (aggregate_type, aggregate_id, event_type) STORING (aggregate_version, previous_aggregate_sequence, previous_aggregate_type_sequence, created_at, event_data, editor_user, editor_service, resource_owner);
-CREATE INDEX IF NOT EXISTS es_active_instances_new ON eventstore.events (created_at DESC, instance_id) USING HASH;
+CREATE INDEX IF NOT EXISTS es_handler_idx ON eventstore.events (instance_id, aggregate_type, event_type, created_at) INCLUDE (aggregate_version, previous_aggregate_sequence, previous_aggregate_type_sequence, event_data, editor_user, editor_service, resource_owner);
+CREATE INDEX IF NOT EXISTS es_agg_id_event_idx ON eventstore.events (aggregate_type, aggregate_id, event_type) INCLUDE (aggregate_version, previous_aggregate_sequence, previous_aggregate_type_sequence, created_at, event_data, editor_user, editor_service, resource_owner);
+CREATE INDEX IF NOT EXISTS es_active_instances ON eventstore.events (created_at DESC, instance_id) USING HASH;
+CREATE INDEX IF NOT EXISTS es_global ON eventstore.events (aggregate_type, aggregate_id, created_at) INCLUDE (event_type, aggregate_version, previous_aggregate_sequence, previous_aggregate_type_sequence, event_data, editor_user, editor_service, resource_owner);
 COMMIT;
-
--- TODO: create in projection package
--- BEGIN;
--- CREATE INDEX IF NOT EXISTS im_instance ON projections.instance_members3 (instance_id) STORING (creation_date, change_date, user_owner_removed, roles, sequence, resource_owner, owner_removed); 
--- CREATE INDEX IF NOT EXISTS om_instance ON projections.org_members3 (instance_id) STORING (creation_date, change_date, user_owner_removed, roles, sequence, resource_owner, owner_removed); 
--- CREATE INDEX IF NOT EXISTS pgm_instance ON projections.project_grant_members3 (instance_id) STORING (creation_date, change_date, user_owner_removed, roles, sequence, resource_owner, owner_removed, granted_org_removed); 
--- CREATE INDEX IF NOT EXISTS p_instance ON projections.project_members3 (instance_id) STORING (creation_date, change_date, user_owner_removed, roles, sequence, resource_owner, owner_removed); 
--- CREATE INDEX IF NOT EXISTS lnu_instance_ro_id ON projections.login_names2_users (instance_id, resource_owner, id) STORING (user_name, owner_removed);
--- COMMIT;
