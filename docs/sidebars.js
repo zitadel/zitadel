4d92685b906e696dc9891f45a8ddd108b94c55d4
Fabienne <fabienne.gerschwiler@gmail.com>
2023-07-03 11:10:16 +0200

    docs: register totp guide

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -183,6 +183,7 @@ module.exports = {
             "guides/integrate/login-ui/username-password",
             "guides/integrate/login-ui/external-login",
             "guides/integrate/login-ui/passkey",
+            "guides/integrate/login-ui/mfa",
             "guides/integrate/login-ui/select-account",
             "guides/integrate/login-ui/password-reset",
             "guides/integrate/login-ui/logout",
