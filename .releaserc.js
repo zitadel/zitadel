7417fdbeb3ba4ebd65584059a80bfdf34172841b
Elio Bischof <elio@zitadel.com>
2023-07-05 22:19:58 +0200

    remove prerelease

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  branches: [{ name: "main" }, { name: "next" }, {name: "usage-telemetry", prerelease: true}],
+  branches: [{ name: "main" }, { name: "next" }],
   plugins: ["@semantic-release/commit-analyzer"],
 };
