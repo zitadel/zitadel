284859c21a8f4174cb5030ea21a064ea9ee16e2c
peintnermax <max@caos.ch>
2023-08-02 15:21:34 +0200

    lint, unit tests

--- apps/login/cypress/integration/register-idp.cy.ts
+++ apps/login/cypress/integration/register-idp.cy.ts
@@ -14,8 +14,8 @@ describe("register idps", () => {
   it("should redirect the user to the correct url", () => {
     cy.visit("/register/idp");
     cy.get('button[e2e="google"]').click();
-    cy.origin(IDP_URL, ()=> {
+    cy.origin(IDP_URL, () => {
       cy.location("href", { timeout: 10_000 }).should("eq", IDP_URL);
-    })
+    });
   });
 });
--- packages/zitadel-react/src/components/SignInWithGitlab.test.tsx
+++ packages/zitadel-react/src/components/SignInWithGitlab.test.tsx
@@ -7,9 +7,15 @@ describe("<SignInWithGitlab />", () => {
     expect(container.firstChild).toBeDefined();
   });
 
-  it("displays the correct text", () => {
+  it("displays the default text", () => {
     render(<SignInWithGitlab />);
     const signInText = screen.getByText(/Sign in with Gitlab/i);
+    expect(signInText).toBeInTheDocument();
+  });
+
+  it("displays the given text", () => {
+    render(<SignInWithGitlab name={"Gitlab"} />);
+    const signInText = screen.getByText(/Gitlab/i);
     expect(signInText).toBeInTheDocument();
   });
 });
