395f5a4a712994ab6bbda289be6fce1e9a62da11
Stefan Benz <stefan@caos.ch>
2023-08-07 18:09:41 +0200

    chore: change e2e tests from registry images to build images

--- .releaserc.js
+++ .releaserc.js
@@ -7,6 +7,18 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
-  ],
+    [
+      "@semantic-release/github",
+      {
+        "assets": [
+          { "path": ".artifacts/zitadel-linux-amd64.tar", "label": "zitadel-linux-amd64.tar" },
+          { "path": ".artifacts/zitadel-linux-arm64.tar", "label": "zitadel-linux-arm64.tar" },
+          { "path": ".artifacts/zitadel-windows-amd64.tar", "label": "zitadel-windows-amd64.tar" },
+          { "path": ".artifacts/zitadel-windows-arm64.tar", "label": "zitadel-windows-arm64.tar" },
+          { "path": ".artifacts/zitadel-darwin-amd64.tar", "label": "zitadel-darwin-amd64.tar" },
+          { "path": ".artifacts/zitadel-darwin-arm64.tar", "label": "zitadel-darwin-arm64.tar" },
+        ]
+      }
+    ]
+  ]
 };
