5cdc67a4d77d19cf8b09a650a2c9600c297e15f3
Livio Spring <livio.a@gmail.com>
2023-08-08 11:33:51 +0200

    chore: build maintenance branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,8 @@
 module.exports = {
   branches: [
     { name: "next" },
-    { name: "next-rc", prerelease: "rc" }
+    { name: "next-rc", prerelease: "rc" },
+    { name: "v2.31.x", range: "2.31.x", channel: "2.31.x" }
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
