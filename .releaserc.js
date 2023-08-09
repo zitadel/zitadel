ad2a622d630f0b2da03ef2264e815f02e52c1457
Stefan Benz <stefan@caos.ch>
2023-08-09 14:18:19 +0200

    fix: empty commit for new pre-release

--- .releaserc.js
+++ .releaserc.js
@@ -11,12 +11,12 @@ module.exports = {
       "@semantic-release/github",
       {
         "assets": [
-          { "path": ".artifacts/zitadel-linux-amd64", "label": "zitadel-linux-amd64.tar" },
-          { "path": ".artifacts/zitadel-linux-arm64", "label": "zitadel-linux-arm64.tar" },
-          { "path": ".artifacts/zitadel-windows-amd64", "label": "zitadel-windows-amd64.tar" },
-          { "path": ".artifacts/zitadel-windows-arm64", "label": "zitadel-windows-arm64.tar" },
-          { "path": ".artifacts/zitadel-darwin-amd64", "label": "zitadel-darwin-amd64.tar" },
-          { "path": ".artifacts/zitadel-darwin-arm64", "label": "zitadel-darwin-arm64.tar" },
+          { "path": ".artifacts/zitadel-linux-amd64/zitadel-linux-amd64.tar", "label": "zitadel-linux-amd64.tar" },
+          { "path": ".artifacts/zitadel-linux-arm64/zitadel-linux-arm64.tar", "label": "zitadel-linux-arm64.tar" },
+          { "path": ".artifacts/zitadel-windows-amd64/zitadel-windows-amd64.tar", "label": "zitadel-windows-amd64.tar" },
+          { "path": ".artifacts/zitadel-windows-arm64/zitadel-windows-arm64.tar", "label": "zitadel-windows-arm64.tar" },
+          { "path": ".artifacts/zitadel-darwin-amd64/zitadel-darwin-amd64.tar", "label": "zitadel-darwin-amd64.tar" },
+          { "path": ".artifacts/zitadel-darwin-arm64/zitadel-darwin-arm64.tar", "label": "zitadel-darwin-arm64.tar" },
         ]
       }
     ]
