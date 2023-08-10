0b9a251db17051606c907107a7f304e58ba0ae53
Stefan Benz <stefan@caos.ch>
2023-08-10 13:53:18 +0200

    chore: remove pipeline file changes for tests

--- .releaserc.js
+++ .releaserc.js
@@ -1,24 +1,11 @@
 module.exports = {
   branches: [
     { name: "next" },
-    { name: "next-rc", prerelease: "rc" },
-    { name: "pipeline-image", prerelease: "ignore-me" }
+    { name: "next-rc", prerelease: "rc" }
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/github",
-      {
-        "assets": [
-          { "path": "./zitadel-linux-amd64.tar.gz", "label": "zitadel-linux-amd64.tar.gz" },
-          { "path": "./zitadel-linux-arm64.tar.gz", "label": "zitadel-linux-arm64.tar.gz" },
-          { "path": "./zitadel-windows-amd64.tar.gz", "label": "zitadel-windows-amd64.tar.gz" },
-          { "path": "./zitadel-windows-arm64.tar.gz", "label": "zitadel-windows-arm64.tar.gz" },
-          { "path": "./zitadel-darwin-amd64.tar.gz", "label": "zitadel-darwin-amd64.tar.gz" },
-          { "path": "./zitadel-darwin-arm64.tar.gz", "label": "zitadel-darwin-arm64.tar.gz" },
-        ]
-      }
-    ]
+    "@semantic-release/github"
   ]
 };
