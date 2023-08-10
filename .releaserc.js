dcd1c906a06284e367f91b4aa5375238bfdd033c
Florian Forster <florian@zitadel.com>
2023-08-10 19:15:12 +0000

    debug

--- .releaserc.js
+++ .releaserc.js
@@ -1,11 +1,15 @@
 module.exports = {
   branches: [
     { name: "next" },
-    { name: "next-rc", prerelease: "rc" }
+    { name: "next-rc", prerelease: "rc" },
+    { name: "debug-artifacts-upload", prerelease: "ignore-me" }
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
+    "@semantic-release/github",
+    {
+      "draftRelease": true
+    }
   ]
 };
