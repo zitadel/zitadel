55873781e5e40627ba432483ee1378343847d6aa
Florian Forster <florian@zitadel.com>
2023-08-10 22:30:49 +0200

    debug

--- .releaserc.js
+++ .releaserc.js
@@ -2,20 +2,28 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
-    { name: "debug-artifacts-upload", prerelease: "ignore-me-ffo" }
+    { name: "debug-artifacts-upload", prerelease: "ignore-me-ffo" },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
-    {
-      "draftRelease": true,
-      "successComment": false,
-      "failComment": false,
-      "labels": false,
-      "releasedLabels": false,
-      "addReleases": false,
-      "failTitle": false,
-    }
-  ]
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: ".artifacts/zitadel-linux-amd64/zitadel-linux-amd64.tar",
+            label: "zitadel-linux-amd64.tar",
+          },
+        ],
+        draftRelease: true,
+        successComment: false,
+        failComment: false,
+        labels: false,
+        releasedLabels: false,
+        addReleases: false,
+        failTitle: false,
+      },
+    ],
+  ],
 };
