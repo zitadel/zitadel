2cb1f9d75164471e07336528873cbd7abc3b7b5c
Stefan Benz <stefan@caos.ch>
2023-08-11 11:58:47 +0200

    fix: change pipeline to add assets to the release

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
+    { name: "pipeline-upload-assets", prerelease: "ignore-me2" },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
@@ -11,30 +12,41 @@ module.exports = {
       {
         assets: [
           {
-            path: ".artifacts/zitadel-linux-amd64/zitadel-linux-amd64.tar",
-            label: "zitadel-linux-amd64.tar",
+            path: ".artifacts/zitadel-linux-amd64/zitadel-linux-amd64.tar.gz",
+            label: "zitadel-linux-amd64.tar.gz",
           },
           {
-            path: ".artifacts/zitadel-linux-arm64/zitadel-linux-arm64.tar",
-            label: "zitadel-linux-arm64.tar",
+            path: ".artifacts/zitadel-linux-arm64/zitadel-linux-arm64.tar.gz",
+            label: "zitadel-linux-arm64.tar.gz",
           },
           {
-            path: ".artifacts/zitadel-windows-amd64/zitadel-windows-amd64.tar",
-            label: "zitadel-windows-amd64.tar",
+            path: ".artifacts/zitadel-windows-amd64/zitadel-windows-amd64.tar.gz",
+            label: "zitadel-windows-amd64.tar.gz",
           },
           {
-            path: ".artifacts/zitadel-windows-arm64/zitadel-windows-arm64.tar",
-            label: "zitadel-windows-arm64.tar",
+            path: ".artifacts/zitadel-windows-arm64/zitadel-windows-arm64.tar.gz",
+            label: "zitadel-windows-arm64.tar.gz",
           },
           {
-            path: ".artifacts/zitadel-darwin-amd64/zitadel-darwin-amd64.tar",
-            label: "zitadel-darwin-amd64.tar",
+            path: ".artifacts/zitadel-darwin-amd64/zitadel-darwin-amd64.tar.gz",
+            label: "zitadel-darwin-amd64.tar.gz",
           },
           {
-            path: ".artifacts/zitadel-darwin-arm64/zitadel-darwin-arm64.tar",
-            label: "zitadel-darwin-arm64.tar",
+            path: ".artifacts/zitadel-darwin-arm64/zitadel-darwin-arm64.tar.gz",
+            label: "zitadel-darwin-arm64.tar.gz",
           },
+          {
+            path: ".artifacts/checksums.txt",
+            label: "checksums.txt",
+          }
         ],
+        draftRelease: true,
+        successComment: false,
+        failComment: false,
+        labels: false,
+        releasedLabels: false,
+        addReleases: false,
+        failTitle: false,
       },
     ],
   ],
