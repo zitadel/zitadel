132e13b8e9736b6ec582d6a5545565fef762e34a
Stefan Benz <stefan@caos.ch>
2023-08-11 09:56:51 +0200

    fix: change pipeline to add assets to the release

--- .releaserc.js
+++ .releaserc.js
@@ -7,6 +7,43 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
-  ]
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: ".artifacts/zitadel-linux-amd64/zitadel-linux-amd64.tar",
+            label: "zitadel-linux-amd64.tar",
+          },
+          {
+            path: ".artifacts/zitadel-linux-arm64/zitadel-linux-arm64.tar",
+            label: "zitadel-linux-arm64.tar",
+          },
+          {
+            path: ".artifacts/zitadel-windows-amd64/zitadel-windows-amd64.tar",
+            label: "zitadel-windows-amd64.tar",
+          },
+          {
+            path: ".artifacts/zitadel-windows-arm64/zitadel-windows-arm64.tar",
+            label: "zitadel-windows-arm64.tar",
+          },
+          {
+            path: ".artifacts/zitadel-darwin-amd64/zitadel-darwin-amd64.tar",
+            label: "zitadel-darwin-amd64.tar",
+          },
+          {
+            path: ".artifacts/zitadel-darwin-arm64/zitadel-darwin-arm64.tar",
+            label: "zitadel-darwin-arm64.tar",
+          },
+        ],
+        draftRelease: true,
+        successComment: false,
+        failComment: false,
+        labels: false,
+        releasedLabels: false,
+        addReleases: false,
+        failTitle: false,
+      },
+    ],
+  ],
 };
