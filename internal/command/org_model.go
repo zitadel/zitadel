7c9403925284444a2c4f1f47bb5ef639d5b1be5b
Miguel Cabrerizo <doncicuto@gmail.com>
2023-08-14 20:53:42 +0200

    fix: get back reduce OrgRemovedEvent

--- internal/command/org_model.go
+++ internal/command/org_model.go
@@ -33,6 +33,8 @@ func (wm *OrgWriteModel) Reduce() error {
 			wm.State = domain.OrgStateInactive
 		case *org.OrgReactivatedEvent:
 			wm.State = domain.OrgStateActive
+		case *org.OrgRemovedEvent:
+			wm.State = domain.OrgStateRemoved
 		case *org.OrgChangedEvent:
 			wm.Name = e.Name
 		case *org.DomainPrimarySetEvent:
