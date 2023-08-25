8da207fc6c229b12f0bd053fb3e49d59e6aacb17
Livio Spring <livio.a@gmail.com>
2023-08-25 15:35:05 +0200

    ignore comments

--- .releaserc.js
+++ .releaserc.js
@@ -7,6 +7,11 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
+    [
+      "@semantic-release/github",
+      {
+        successComment: false,
+      },
+    ],
   ],
 };
