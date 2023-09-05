7b3a55896786d08a0f7557ba931a9d6e6526e3ae
adlerhurst <silvan.reusser@gmail.com>
2023-09-05 18:27:53 +0200

    build: add maintenance branch

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
+    { name: "v2.35.x", range: "2.35.x", channel: "2.35.x" }
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
