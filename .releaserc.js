e51dd63ca243807a1700c4be8b28b7946e21bcb6
Livio Spring <livio.a@gmail.com>
2023-09-13 15:50:45 +0200

    create rc

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
+    { name: "quota-projection", prerelease: "alpha" },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
