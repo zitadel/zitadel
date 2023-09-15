7835ebdfe0b63a038b036b67687b187262cda8fb
Fabienne <fabienne.gerschwiler@gmail.com>
2023-09-15 15:07:06 +0200

    docs: keycloak idp

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -243,6 +243,7 @@ module.exports = {
             "guides/integrate/identity-providers/openldap",
             "guides/integrate/identity-providers/migrate",
             "guides/integrate/identity-providers/okta",
+            "guides/integrate/identity-providers/keycloak",
           ],
         },
         {
