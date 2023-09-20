10942cbdd5bb01f2eb0d647914f5552b1fbcbdce
adlerhurst <silvan.reusser@gmail.com>
2023-09-20 10:49:06 +0200

    ordering

--- cmd/initialise/sql/postgres/11_eventstore_columns.sql
+++ cmd/initialise/sql/postgres/11_eventstore_columns.sql
@@ -4,7 +4,7 @@ BEGIN
     IF NOT EXISTS(SELECT 1 FROM information_schema.columns WHERE table_schema = 'eventstore' AND table_name = 'events' AND column_name = 'position') THEN
         ALTER TABLE eventstore.events ADD COLUMN IF NOT EXISTS "position" DECIMAL;
         ALTER TABLE eventstore.events ALTER COLUMN "position" SET NOT NULL,
-                                      ALTER COLUMN "position" TYPE TIMESTAMPTZ USING EXTRACT(EPOCH FROM clock_timestamp());
+                                      ALTER COLUMN "position" TYPE DECIMAL USING EXTRACT(EPOCH FROM creation_date);
     END IF;
 
     IF NOT EXISTS(SELECT 1 FROM information_schema.columns WHERE table_schema = 'eventstore' AND table_name = 'events' AND column_name = 'in_tx_order') THEN
