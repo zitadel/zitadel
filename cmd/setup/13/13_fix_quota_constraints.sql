eac6d97abeecbb70efe0f08ff9394750cf64a3be
Elio Bischof <eliobischof@gmail.com>
2023-09-21 15:56:20 +0200

    Revert "fix quota projection primary key"
    This reverts commit e72f4d7fa17d03d36493912168490350a320e04f.

--- cmd/setup/13/13_fix_quota_constraints.sql
+++ cmd/setup/13/13_fix_quota_constraints.sql
@@ -2,4 +2,3 @@ ALTER TABLE IF EXISTS projections.quotas ALTER COLUMN from_anchor DROP NOT NULL;
 ALTER TABLE IF EXISTS projections.quotas ALTER COLUMN amount DROP NOT NULL;
 ALTER TABLE IF EXISTS projections.quotas ALTER COLUMN interval DROP NOT NULL;
 ALTER TABLE IF EXISTS projections.quotas ALTER COLUMN limit_usage DROP NOT NULL;
-ALTER TABLE IF EXISTS projections.quotas ALTER PRIMARY KEY USING COLUMNS ( instance_id, unit, id );
