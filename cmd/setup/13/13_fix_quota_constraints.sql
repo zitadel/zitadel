9057ac23491c79bd0fe09d23b4b0b093178916f5
Elio Bischof <eliobischof@gmail.com>
2023-09-21 15:10:39 +0200

    fix initial setup

--- cmd/setup/13/13_fix_quota_constraints.sql
+++ cmd/setup/13/13_fix_quota_constraints.sql
@@ -1,4 +1,4 @@
-ALTER TABLE projections.quotas ALTER COLUMN from_anchor DROP NOT NULL;
-ALTER TABLE projections.quotas ALTER COLUMN amount DROP NOT NULL;
-ALTER TABLE projections.quotas ALTER COLUMN interval DROP NOT NULL;
-ALTER TABLE projections.quotas ALTER COLUMN limit_usage DROP NOT NULL;
+ALTER TABLE IF EXISTS projections.quotas ALTER COLUMN from_anchor DROP NOT NULL;
+ALTER TABLE IF EXISTS projections.quotas ALTER COLUMN amount DROP NOT NULL;
+ALTER TABLE IF EXISTS projections.quotas ALTER COLUMN interval DROP NOT NULL;
+ALTER TABLE IF EXISTS projections.quotas ALTER COLUMN limit_usage DROP NOT NULL;
