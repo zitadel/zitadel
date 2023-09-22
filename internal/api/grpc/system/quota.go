ae1af6bc8cd2294f47f6d6412c4b46192105ade5
Elio Bischof <elio@zitadel.com>
2023-09-22 11:37:16 +0200

    fix: set quotas (#6597)
    * feat: set quotas
    
    * fix: start new period on younger anchor
    
    * cleanup e2e config
    
    * fix set notifications
    
    * lint
    
    * test: fix quota projection tests
    
    * fix add quota tests
    
    * make quota fields nullable
    
    * enable amount 0
    
    * fix initial setup
    
    * create a prerelease
    
    * avoid success comments
    
    * fix quota projection primary key
    
    * Revert "fix quota projection primary key"
    
    This reverts commit e72f4d7fa17d03d36493912168490350a320e04f.
    
    * simplify write model
    
    * fix aggregate id
    
    * avoid push without changes
    
    * test set quota lifecycle
    
    * test set quota mutations
    
    * fix quota unit test
    
    * fix: quotas
    
    * test quota.set event projection
    
    * use SetQuota in integration tests
    
    * fix: release quotas 3
    
    * reset releaserc
    
    * fix comment
    
    * test notification order doesn't matter
    
    * test notification order doesn't matter
    
    * test with unmarshalled events
    
    * test with unmarshalled events

--- internal/api/grpc/system/quota.go
+++ internal/api/grpc/system/quota.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/zitadel/zitadel/internal/api/grpc/object"
 	"github.com/zitadel/zitadel/pkg/grpc/system"
-	system_pb "github.com/zitadel/zitadel/pkg/grpc/system"
 )
 
 func (s *Server) AddQuota(ctx context.Context, req *system.AddQuotaRequest) (*system.AddQuotaResponse, error) {
@@ -16,7 +15,20 @@ func (s *Server) AddQuota(ctx context.Context, req *system.AddQuotaRequest) (*sy
 	if err != nil {
 		return nil, err
 	}
-	return &system_pb.AddQuotaResponse{
+	return &system.AddQuotaResponse{
+		Details: object.AddToDetailsPb(details.Sequence, details.EventDate, details.ResourceOwner),
+	}, nil
+}
+
+func (s *Server) SetQuota(ctx context.Context, req *system.SetQuotaRequest) (*system.SetQuotaResponse, error) {
+	details, err := s.command.SetQuota(
+		ctx,
+		instanceQuotaPbToCommand(req),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &system.SetQuotaResponse{
 		Details: object.AddToDetailsPb(details.Sequence, details.EventDate, details.ResourceOwner),
 	}, nil
 }
@@ -26,7 +38,7 @@ func (s *Server) RemoveQuota(ctx context.Context, req *system.RemoveQuotaRequest
 	if err != nil {
 		return nil, err
 	}
-	return &system_pb.RemoveQuotaResponse{
+	return &system.RemoveQuotaResponse{
 		Details: object.ChangeToDetailsPb(details.Sequence, details.EventDate, details.ResourceOwner),
 	}, nil
 }
