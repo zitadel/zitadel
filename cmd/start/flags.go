d29a86de5323124def253de489ae07dbb6ed3e35
David Schneider <dsbrng25b@gmail.com>
2023-09-25 16:26:34 +0200

    fix(backend): respect start flags in all commands
    Currently flags like --externalDomain do only work in the last
    registered command which currently is start-from-setup.
    This creates the flags globally in the init function in uses them for
    all start commands.

--- cmd/start/flags.go
+++ cmd/start/flags.go
@@ -2,34 +2,33 @@ package start
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 
 	"github.com/zitadel/zitadel/cmd/key"
 	"github.com/zitadel/zitadel/cmd/tls"
 )
 
-var tlsMode *string
+var (
+	tlsMode *string
+
+	portFlag           = "port"
+	externalDomainFlag = "externalDomain"
+	externalPortFlag   = "externalPort"
+
+	startFlagSet = &pflag.FlagSet{}
+)
+
+func init() {
+	startFlagSet.Uint16(portFlag, viper.GetUint16(portFlag), "port to run ZITADEL on")
+	startFlagSet.String(externalDomainFlag, viper.GetString(externalDomainFlag), "domain ZITADEL will be exposed on")
+	startFlagSet.String(externalPortFlag, viper.GetString(externalPortFlag), "port ZITADEL will be exposed on")
+}
 
 func startFlags(cmd *cobra.Command) {
-	bindUint16Flag(cmd, "port", "port to run ZITADEL on")
-	bindStringFlag(cmd, "externalDomain", "domain ZITADEL will be exposed on")
-	bindStringFlag(cmd, "externalPort", "port ZITADEL will be exposed on")
+	cmd.Flags().AddFlagSet(startFlagSet)
+	viper.BindPFlags(startFlagSet)
 
 	tls.AddTLSModeFlag(cmd)
 	key.AddMasterKeyFlag(cmd)
-}
-
-func bindStringFlag(cmd *cobra.Command, name, description string) {
-	cmd.PersistentFlags().String(name, viper.GetString(name), description)
-	viper.BindPFlag(name, cmd.PersistentFlags().Lookup(name))
-}
-
-func bindUint16Flag(cmd *cobra.Command, name, description string) {
-	cmd.PersistentFlags().Uint16(name, uint16(viper.GetUint(name)), description)
-	viper.BindPFlag(name, cmd.PersistentFlags().Lookup(name))
-}
-
-func bindBoolFlag(cmd *cobra.Command, name, description string) {
-	cmd.PersistentFlags().Bool(name, viper.GetBool(name), description)
-	viper.BindPFlag(name, cmd.PersistentFlags().Lookup(name))
 }
