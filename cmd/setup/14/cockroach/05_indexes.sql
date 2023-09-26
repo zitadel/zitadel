9ec7ea44f71727decae8def6d8175532b7f176b5
adlerhurst <silvan.reusser@gmail.com>
2023-09-26 16:06:18 +0200

    fix: indexes

--- cmd/setup/14/cockroach/05_indexes.sql
+++ cmd/setup/14/cockroach/05_indexes.sql
@@ -1,5 +1,4 @@
 DROP INDEX IF EXISTS eventstore.events@write_model;
--- DROP INDEX eventstore.events@es_handler_idx; 
 CREATE INDEX IF NOT EXISTS es_handler_idx ON eventstore.events (aggregate_type, event_type, instance_id);
 CREATE INDEX IF NOT EXISTS es_agg_wm ON eventstore.events (aggregate_type, aggregate_id, event_type, "position");
 CREATE INDEX IF NOT EXISTS es_wm ON eventstore.events (instance_id, resource_owner, aggregate_type, aggregate_id, event_type);
