63e7d6dc56e18c278c5934c6ede9d71326d2b8c0
Elio Bischof <elio@zitadel.com>
2023-09-28 13:34:55 +0200

    Update internal/notification/handlers/commands.go
    Co-authored-by: Tim Möhlmann <tim+github@zitadel.com>

--- internal/notification/handlers/commands.go
+++ internal/notification/handlers/commands.go
@@ -8,17 +8,17 @@ import (
 )
 
 type Commands interface {
-	HumanInitCodeSent(ctx context.Context, orgID, userID string) (err error)
-	HumanEmailVerificationCodeSent(ctx context.Context, orgID, userID string) (err error)
-	PasswordCodeSent(ctx context.Context, orgID, userID string) (err error)
-	HumanOTPSMSCodeSent(ctx context.Context, userID, resourceOwner string) (err error)
-	HumanOTPEmailCodeSent(ctx context.Context, userID, resourceOwner string) (err error)
+	HumanInitCodeSent(ctx context.Context, orgID, userID string) error
+	HumanEmailVerificationCodeSent(ctx context.Context, orgID, userID string) error
+	PasswordCodeSent(ctx context.Context, orgID, userID string) error
+	HumanOTPSMSCodeSent(ctx context.Context, userID, resourceOwner string) error
+	HumanOTPEmailCodeSent(ctx context.Context, userID, resourceOwner string) error
 	OTPSMSSent(ctx context.Context, sessionID, resourceOwner string) error
 	OTPEmailSent(ctx context.Context, sessionID, resourceOwner string) error
-	UserDomainClaimedSent(ctx context.Context, orgID, userID string) (err error)
+	UserDomainClaimedSent(ctx context.Context, orgID, userID string) error
 	HumanPasswordlessInitCodeSent(ctx context.Context, userID, resourceOwner, codeID string) error
-	PasswordChangeSent(ctx context.Context, orgID, userID string) (err error)
-	HumanPhoneVerificationCodeSent(ctx context.Context, orgID, userID string) (err error)
+	PasswordChangeSent(ctx context.Context, orgID, userID string) (er error)
+	HumanPhoneVerificationCodeSent(ctx context.Context, orgID, userID string) error
 	UsageNotificationSent(ctx context.Context, dueEvent *quota.NotificationDueEvent) error
 	MilestonePushed(ctx context.Context, msType milestone.Type, endpoints []string, primaryDomain string) error
 }
