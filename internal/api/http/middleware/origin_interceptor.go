10dba0bd292405885cf9949626c5ab5e9acc8e05
Elio Bischof <eliobischof@gmail.com>
2023-09-28 11:00:02 +0200

    take origin from header

--- internal/api/http/middleware/origin_interceptor.go
+++ internal/api/http/middleware/origin_interceptor.go
@@ -3,17 +3,13 @@ package middleware
 import (
 	"net/http"
 
-	"github.com/zitadel/logging"
-
 	http_util "github.com/zitadel/zitadel/internal/api/http"
 )
 
 func OriginHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.String()
-		origin, err := http_util.GetOriginFromURLString(url)
-		logging.OnError(err).Debugf("can't get origin from url: %v", url)
 		ctx := r.Context()
+		origin := r.Header.Get(http_util.Origin)
 		if origin != "" {
 			ctx = http_util.WithOrigin(ctx, origin)
 		}
