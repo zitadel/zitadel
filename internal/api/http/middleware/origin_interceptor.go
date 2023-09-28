7e0b3f30757762ea85a826840445205c4859db93
Elio Bischof <eliobischof@gmail.com>
2023-09-28 11:15:51 +0200

    take x-forwarded if present

--- internal/api/http/middleware/origin_interceptor.go
+++ internal/api/http/middleware/origin_interceptor.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"strconv"
 
 	http_util "github.com/zitadel/zitadel/internal/api/http"
 )
@@ -9,8 +10,19 @@ import (
 func OriginHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		origin := r.Header.Get(http_util.Origin)
-		if origin != "" {
+		forwardedPort := r.Header.Get("x-forwarded-port")
+		forwardedHost := r.Header.Get("x-forwarded-host")
+		forwardedProto := r.Header.Get("x-forwarded-proto")
+		forwardedProtoBool := forwardedProto == "https"
+		forwardedPortInt, err := strconv.ParseUint(forwardedPort, 10, 16)
+		var origin string
+		if err == nil {
+			origin = http_util.BuildHTTP(forwardedHost, uint16(forwardedPortInt), forwardedProtoBool)
+		}
+		if origin == "" {
+			origin = r.Header.Get(http_util.Origin)
+		}
+		if http_util.IsOrigin(origin) {
 			ctx = http_util.WithOrigin(ctx, origin)
 		}
 		next.ServeHTTP(w, r.WithContext(ctx))
