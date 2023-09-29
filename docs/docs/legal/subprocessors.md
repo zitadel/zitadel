f77a73688dfd04560e3a5c51f9fe7cbb9467a4c4
mffap <mpa@zitadel.com>
2023-09-29 10:55:03 +0200

    subprocessors wip

--- docs/docs/legal/subprocessors.md
+++ docs/docs/legal/subprocessors.md
@@ -4,13 +4,10 @@ sidebar_label: Sub-processors
 custom_edit_url: null
 --- 
 
-import { SubProcessorTable } from "../../src/components/subprocessors";
-
-<SubProcessorTable />
-
 - Update privacy policy
 - Update DPA
 - Update zitadel.com/trust
 
-| Entity name | Relevant ZITADEL Services | Purpose | Country of registration |
-| --- | --- | --- | --- | --- | --- |
+import { SubProcessorTable } from "../../src/components/subprocessors";
+
+<SubProcessorTable />
--- docs/src/components/subprocessors.jsx
+++ docs/src/components/subprocessors.jsx
@@ -2,29 +2,69 @@ import React from "react";
 
 export function SubProcessorTable() {
 
+  const country_list = {
+    us: "USA",
+    eu: "EU",
+  }
   const processors = [
     {
-      entity: "Google",
+      entity: "Google LLC",
+      services: "ZITADEL Cloud, Support Services",
+      purpose: "Cloud infrastructure provider (Google Cloud), business applications and collaboration (Workspace), Data warehouse services, Content delivery network, DDoS and bot prevention",
+      hosting: "Region designated by Customer, United States",
+      country: country_list.us,
+      enduserdata: true
+    },
+    {
+      entity: "Cockroach Labs, Inc.",
       services: "ZITADEL Cloud",
-      purpose: "We use Google as our infrastructure provider and for business applications and collaboration.",
-      country: "United States"
-    }
+      purpose: "Managed database services: Dedicated CockroachDB clusters on Google Cloud",
+      hosting: "Region designated by Customer",
+      country: country_list.us,
+      enduserdata: true
+    },
+    {
+      entity: "Datadog, Inc.",
+      services: "ZITADEL Cloud, Websites",
+      purpose: "",
+      hosting: country_list.eu,
+      country: country_list.us,
+      enduserdata: true
+    },
+    {
+      entity: "Github, Inc.",
+      services: "ZITADEL",
+      purpose: "Source code management, code scanning, dependency management, security advisory, issue management, continuous integration",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: false
+    },
   ]
-  let rows = "";
-
-  for (const processor in processors) {
-    rows = rows + `<tr><td>${processor.entity}</td><td>${processor.services}</td><td>${processor.purpose}</td><td>${processor.county}</td></tr>`
-  }
 
   return (
-    <table>
+    <table className="text-xs">
       <tr>
         <th>Entity name</th>
         <th>Relevant services</th>
         <th>Purpose</th>
+        <th>End-user data</th>
+        <th>Hosting location</th>
         <th>Country of registration</th>
       </tr>
-      {rows}
+      {
+        processors.map((processor, rowID) => {
+          return (
+            <tr>
+              <td key={rowID}>{processor.entity}</td>
+              <td>{processor.services}</td>
+              <td>{processor.purpose}</td>
+              <td>{processor.enduserdata ? 'Yes' : 'No'}</td>
+              <td>{processor.hosting}</td>
+              <td>{processor.country}</td>
+            </tr>
+          )
+        })
+      }
     </table>
   );
 }
