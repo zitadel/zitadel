08b7faf414fb7b24b28fb3c316379d93e5ca1767
mffap <mpa@zitadel.com>
2023-10-02 11:48:17 +0300

    wip

--- docs/src/components/subprocessors.jsx
+++ docs/src/components/subprocessors.jsx
@@ -5,36 +5,125 @@ export function SubProcessorTable() {
   const country_list = {
     us: "USA",
     eu: "EU",
+    ch: "Switzerland",
+    fr: "France"
   }
   const processors = [
     {
       entity: "Google LLC",
-      services: "ZITADEL Cloud, Support Services",
       purpose: "Cloud infrastructure provider (Google Cloud), business applications and collaboration (Workspace), Data warehouse services, Content delivery network, DDoS and bot prevention",
       hosting: "Region designated by Customer, United States",
       country: country_list.us,
-      enduserdata: true
+      enduserdata: "Yes (transit)"
     },
     {
       entity: "Cockroach Labs, Inc.",
-      services: "ZITADEL Cloud",
       purpose: "Managed database services: Dedicated CockroachDB clusters on Google Cloud",
       hosting: "Region designated by Customer",
       country: country_list.us,
-      enduserdata: true
+      enduserdata: "Yes (at rest)"
     },
     {
       entity: "Datadog, Inc.",
-      services: "ZITADEL Cloud, Websites",
-      purpose: "",
+      purpose: "Infrastructure monitoring, log analytics, and alerting",
       hosting: country_list.eu,
       country: country_list.us,
-      enduserdata: true
+      enduserdata: "Yes (logs)"
     },
     {
       entity: "Github, Inc.",
-      services: "ZITADEL",
       purpose: "Source code management, code scanning, dependency management, security advisory, issue management, continuous integration",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: false
+    },
+    {
+      entity: "Stripe Payments Europe, Ltd.",
+      purpose: "Subscription management, payment process",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: false
+    },
+    {
+      entity: "Bexio AG",
+      purpose: "Customer management, payment process",
+      hosting: country_list.ch,
+      country: country_list.ch,
+      enduserdata: false
+    },
+    {
+      entity: "Mailjet SAS",
+      purpose: "Marketing automation",
+      hosting: country_list.eu,
+      country: country_list.fr,
+      enduserdata: false
+    },
+    {
+      entity: "AC PM LLC (Postmark)",
+      purpose: "Transactional mails, if no customer owned SMTP service is configured",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: "Yes (opt-out)"
+    },
+    {
+      entity: "Vercel, Inc.",
+      purpose: "Website hosting",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: false
+    },
+    {
+      entity: "Agolia SAS",
+      purpose: "",
+      hosting: country_list.us,
+      country: undefined, // Not clear for OSS plan, sent a request to algolia
+      enduserdata: false
+    },
+    {
+      entity: "Discord Netherlands BV",
+      purpose: "",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: false
+    },
+    {
+      entity: "Statuspal",
+      purpose: "",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: false
+    },
+    {
+      entity: "Plausible",
+      purpose: "",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: false
+    },
+    {
+      entity: "Twillio",
+      purpose: "",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: false
+    },
+    {
+      entity: "Mohlmann Solutions",
+      purpose: "",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: false
+    },
+    {
+      entity: "Remote Sri Lanka",
+      purpose: "",
+      hosting: country_list.us,
+      country: country_list.us,
+      enduserdata: false
+    },
+    {
+      entity: "Clickhouse",
+      purpose: "",
       hosting: country_list.us,
       country: country_list.us,
       enduserdata: false
@@ -45,20 +134,24 @@ export function SubProcessorTable() {
     <table className="text-xs">
       <tr>
         <th>Entity name</th>
-        <th>Relevant services</th>
         <th>Purpose</th>
         <th>End-user data</th>
         <th>Hosting location</th>
         <th>Country of registration</th>
       </tr>
       {
-        processors.map((processor, rowID) => {
+        processors
+          .sort((a, b) => {
+            if (a.entity < b.entity) return -1
+            if (a.entity > b.entity) return 1
+            else return 0
+          })
+          .map((processor, rowID) => {
           return (
             <tr>
               <td key={rowID}>{processor.entity}</td>
-              <td>{processor.services}</td>
               <td>{processor.purpose}</td>
-              <td>{processor.enduserdata ? 'Yes' : 'No'}</td>
+              <td>{processor.enduserdata ? processor.enduserdata  : 'No'}</td>
               <td>{processor.hosting}</td>
               <td>{processor.country}</td>
             </tr>
