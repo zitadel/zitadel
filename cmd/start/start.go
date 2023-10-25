94cf30c5477bd44255102a26d241de1f8cf8526b
Tim Möhlmann <tim+github@zitadel.com>
2023-10-25 18:44:05 +0300

    feat(oidc): use the new oidc server interface (#6779)
    * feat(oidc): use the new oidc server interface
    
    * rename from provider to server
    
    * pin logging and oidc packages
    
    * use oidc introspection fix branch
    
    * add overloaded methods with tracing
    
    * cleanup unused code
    
    * include latest oidc fixes
    
    ---------
    
    Co-authored-by: Livio Spring <livio.a@gmail.com>

--- cmd/start/start.go
+++ cmd/start/start.go
@@ -417,11 +417,11 @@ func startAPIs(
 	}
 	apis.RegisterHandlerOnPrefix(openapi.HandlerPrefix, openAPIHandler)
 
-	oidcProvider, err := oidc.NewProvider(config.OIDC, login.DefaultLoggedOutPath, config.ExternalSecure, commands, queries, authRepo, keys.OIDC, keys.OIDCKey, eventstore, dbClient, userAgentInterceptor, instanceInterceptor.Handler, limitingAccessInterceptor)
+	oidcServer, err := oidc.NewServer(config.OIDC, login.DefaultLoggedOutPath, config.ExternalSecure, commands, queries, authRepo, keys.OIDC, keys.OIDCKey, eventstore, dbClient, userAgentInterceptor, instanceInterceptor.Handler, limitingAccessInterceptor, config.Log.Slog())
 	if err != nil {
 		return fmt.Errorf("unable to start oidc provider: %w", err)
 	}
-	apis.RegisterHandlerPrefixes(oidcProvider.HttpHandler(), oidcPrefixes...)
+	apis.RegisterHandlerPrefixes(oidcServer, oidcPrefixes...)
 
 	samlProvider, err := saml.NewProvider(config.SAML, config.ExternalSecure, commands, queries, authRepo, keys.OIDC, keys.SAML, eventstore, dbClient, instanceInterceptor.Handler, userAgentInterceptor, limitingAccessInterceptor)
 	if err != nil {
@@ -429,7 +429,7 @@ func startAPIs(
 	}
 	apis.RegisterHandlerOnPrefix(saml.HandlerPrefix, samlProvider.HttpHandler())
 
-	c, err := console.Start(config.Console, config.ExternalSecure, oidcProvider.IssuerFromRequest, middleware.CallDurationHandler, instanceInterceptor.Handler, limitingAccessInterceptor, config.CustomerPortal)
+	c, err := console.Start(config.Console, config.ExternalSecure, oidcServer.IssuerFromRequest, middleware.CallDurationHandler, instanceInterceptor.Handler, limitingAccessInterceptor, config.CustomerPortal)
 	if err != nil {
 		return fmt.Errorf("unable to start console: %w", err)
 	}
@@ -442,11 +442,11 @@ func startAPIs(
 		authRepo,
 		store,
 		console.HandlerPrefix+"/",
-		op.AuthCallbackURL(oidcProvider),
+		oidcServer.AuthCallbackURL(),
 		provider.AuthCallbackURL(samlProvider),
 		config.ExternalSecure,
 		userAgentInterceptor,
-		op.NewIssuerInterceptor(oidcProvider.IssuerFromRequest).Handler,
+		op.NewIssuerInterceptor(oidcServer.IssuerFromRequest).Handler,
 		provider.NewIssuerInterceptor(samlProvider.IssuerFromRequest).Handler,
 		instanceInterceptor.Handler,
 		assetsCache.Handler,
@@ -463,7 +463,7 @@ func startAPIs(
 	apis.HandleFunc(login.EndpointDeviceAuth, login.RedirectDeviceAuthToPrefix)
 
 	// After OIDC provider so that the callback endpoint can be used
-	if err := apis.RegisterService(ctx, oidc_v2.CreateServer(commands, queries, oidcProvider, config.ExternalSecure)); err != nil {
+	if err := apis.RegisterService(ctx, oidc_v2.CreateServer(commands, queries, oidcServer, config.ExternalSecure)); err != nil {
 		return err
 	}
 	// handle grpc at last to be able to handle the root, because grpc and gateway require a lot of different prefixes
