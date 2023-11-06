eeb1ac3bc237214ce6eac391637735ec522a1e75
Petr Mifek <petr.mifek@anapol.cz>
2023-11-06 17:07:40 +0100

    feat(i18n): Add Czech language support

--- console/src/app/utils/language.ts
+++ console/src/app/utils/language.ts
@@ -1,3 +1,3 @@
-export const supportedLanguages = ['de', 'en', 'es', 'fr', 'it', 'ja', 'pl', 'zh', 'bg', 'pt', 'mk'];
-export const supportedLanguagesRegexp: RegExp = /de|en|es|fr|it|ja|pl|zh|bg|pt|mk/;
+export const supportedLanguages = ['de', 'en', 'es', 'fr', 'it', 'ja', 'pl', 'zh', 'bg', 'pt', 'mk', 'cs'];
+export const supportedLanguagesRegexp: RegExp = /de|en|es|fr|it|ja|pl|zh|bg|pt|mk|cs/;
 export const fallbackLanguage: string = 'en';
