49d3ae6238ac33f72a790b558356f3828961180e
Miguel Cabrerizo <30386061+doncicuto@users.noreply.github.com>
2023-11-07 11:43:29 +0100

    feat(console): replace twitter bird with X (#6843)
    feat(console): replace blue bird with X
    
    Co-authored-by: Elio Bischof <elio@zitadel.com>

--- console/src/app/modules/footer/footer.component.ts
+++ console/src/app/modules/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PrivacyPolicy } from 'src/app/proto/generated/zitadel/policy_pb';
 import { GrpcAuthService } from 'src/app/services/grpc-auth.service';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'cnsl-footer',
@@ -9,6 +10,7 @@ import { GrpcAuthService } from 'src/app/services/grpc-auth.service';
 })
 export class FooterComponent implements OnInit {
   public policy?: PrivacyPolicy.AsObject;
+  public faXTwitter = faXTwitter;
   constructor(public authService: GrpcAuthService) {}
 
   ngOnInit(): void {
