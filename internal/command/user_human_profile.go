4e9ea4b17afce0538e54fd81e712660b3c9bd925
Elio Bischof <elio@zitadel.com>
2023-11-29 10:00:42 +0100

    cleanup

--- internal/command/user_human_profile.go
+++ internal/command/user_human_profile.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"context"
+
 	"github.com/zitadel/zitadel/internal/domain"
 	caos_errs "github.com/zitadel/zitadel/internal/errors"
 	"github.com/zitadel/zitadel/internal/telemetry/tracing"
