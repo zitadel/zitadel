46ed7f194a2766c85979dc941793326471f758b0
Elio Bischof <elio@zitadel.com>
2023-12-05 15:35:38 +0100

    cleanup

--- internal/api/grpc/auth/language.go
+++ internal/api/grpc/auth/language.go
@@ -11,15 +11,3 @@ import (
 func (s *Server) GetSupportedLanguages(context.Context, *auth_pb.GetSupportedLanguagesRequest) (*auth_pb.GetSupportedLanguagesResponse, error) {
 	return &auth_pb.GetSupportedLanguagesResponse{Languages: domain.LanguagesToStrings(i18n.SupportedLanguages())}, nil
 }
-
-func (s *Server) GetAllowedLanguages(ctx context.Context, _ *auth_pb.GetAllowedLanguagesRequest) (*auth_pb.GetAllowedLanguagesResponse, error) {
-	restrictions, err := s.query.GetInstanceRestrictions(ctx)
-	if err != nil {
-		return nil, err
-	}
-	allowed := restrictions.AllowedLanguages
-	if len(allowed) == 0 {
-		allowed = i18n.SupportedLanguages()
-	}
-	return &auth_pb.GetAllowedLanguagesResponse{Languages: domain.LanguagesToStrings(allowed)}, nil
-}
--- internal/api/grpc/management/language.go
+++ internal/api/grpc/management/language.go
@@ -10,15 +10,3 @@ import (
 func (s *Server) GetSupportedLanguages(context.Context, *mgmt_pb.GetSupportedLanguagesRequest) (*mgmt_pb.GetSupportedLanguagesResponse, error) {
 	return &mgmt_pb.GetSupportedLanguagesResponse{Languages: domain.LanguagesToStrings(i18n.SupportedLanguages())}, nil
 }
-
-func (s *Server) GetAllowedLanguages(ctx context.Context, _ *mgmt_pb.GetAllowedLanguagesRequest) (*mgmt_pb.GetAllowedLanguagesResponse, error) {
-	restrictions, err := s.query.GetInstanceRestrictions(ctx)
-	if err != nil {
-		return nil, err
-	}
-	allowed := restrictions.AllowedLanguages
-	if len(allowed) == 0 {
-		allowed = i18n.SupportedLanguages()
-	}
-	return &mgmt_pb.GetAllowedLanguagesResponse{Languages: domain.LanguagesToStrings(allowed)}, nil
-}
