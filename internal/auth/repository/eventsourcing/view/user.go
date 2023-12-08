7fd098ac7f2a62c22ec5e432b87af828d5c412f3
Elio Bischof <elio@zitadel.com>
2023-12-08 15:13:01 +0100

    fix import structure

--- internal/auth/repository/eventsourcing/view/user.go
+++ internal/auth/repository/eventsourcing/view/user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/zitadel/logging"
+
 	"github.com/zitadel/zitadel/internal/eventstore"
 	"github.com/zitadel/zitadel/internal/query"
 	usr_model "github.com/zitadel/zitadel/internal/user/model"
