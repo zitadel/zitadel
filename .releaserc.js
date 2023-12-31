2995515e389fd006da6845991446169ab1de3aa1
adlerhurst <silvan.reusser@gmail.com>
2023-12-31 15:34:53 +0100

    chore: release

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
+    { name: "v2.42.x", range: "2.42.x", channel: "2.42.x" },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
