dc88205e42ebe07a2fa07520eaf1ec6295f76608
adlerhurst <silvan.reusser@gmail.com>
2023-12-31 15:43:02 +0100

    chore: so?

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   branches: [
-    { name: "next" },
-    { name: "next-rc", prerelease: "rc" },
+    // { name: "next" },
+    // { name: "next-rc", prerelease: "rc" },
     { name: "v2.42.x", range: "2.42.x", channel: "2.42.x" },
   ],
   plugins: [
