c0cef4983a8c74c2bbb777cb18b7498159c58933
Livio Spring <livio.a@gmail.com>
2024-01-04 16:46:25 +0100

    fix: correctly respect maxFailureCount (#7143)

--- internal/authz/repository/eventsourcing/view/token.go
+++ internal/authz/repository/eventsourcing/view/token.go
@@ -4,11 +4,9 @@ import (
 	"context"
 
 	"github.com/zitadel/zitadel/internal/api/authz"
-	"github.com/zitadel/zitadel/internal/eventstore/v1/models"
 	"github.com/zitadel/zitadel/internal/query"
 	usr_view "github.com/zitadel/zitadel/internal/user/repository/view"
 	usr_view_model "github.com/zitadel/zitadel/internal/user/repository/view/model"
-	"github.com/zitadel/zitadel/internal/zerrors"
 )
 
 const (
@@ -17,26 +15,6 @@ const (
 
 func (v *View) TokenByIDs(tokenID, userID, instanceID string) (*usr_view_model.TokenView, error) {
 	return usr_view.TokenByIDs(v.Db, tokenTable, tokenID, userID, instanceID)
-}
-
-func (v *View) PutToken(token *usr_view_model.TokenView, event *models.Event) error {
-	return usr_view.PutToken(v.Db, tokenTable, token)
-}
-
-func (v *View) DeleteToken(tokenID, instanceID string, event *models.Event) error {
-	err := usr_view.DeleteToken(v.Db, tokenTable, tokenID, instanceID)
-	if err != nil && !zerrors.IsNotFound(err) {
-		return err
-	}
-	return nil
-}
-
-func (v *View) DeleteSessionTokens(agentID, userID, instanceID string, event *models.Event) error {
-	err := usr_view.DeleteSessionTokens(v.Db, tokenTable, agentID, userID, instanceID)
-	if err != nil && !zerrors.IsNotFound(err) {
-		return err
-	}
-	return nil
 }
 
 func (v *View) GetLatestState(ctx context.Context) (_ *query.CurrentState, err error) {
