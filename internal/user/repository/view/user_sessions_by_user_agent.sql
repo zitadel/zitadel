ed8719ada2e8e16e715e085f998a2e1889651fc2
Livio Spring <livio.a@gmail.com>
2024-01-29 12:45:52 +0100

    cleanup

--- internal/user/repository/view/user_sessions_by_user_agent.sql
+++ internal/user/repository/view/user_sessions_by_user_agent.sql
@@ -1,27 +1,27 @@
-SELECT
-    auth.user_sessions.creation_date,
-    auth.user_sessions.change_date,
-    auth.user_sessions.resource_owner,
-    auth.user_sessions.state,
-    auth.user_sessions.user_agent_id,
-    auth.user_sessions.user_id,
-    u.username,
-    l.login_name,
-    h.display_name,
-    h.avatar_key,
-    auth.user_sessions.selected_idp_config_id,
-    auth.user_sessions.password_verification,
-    auth.user_sessions.passwordless_verification,
-    auth.user_sessions.external_login_verification,
-    auth.user_sessions.second_factor_verification,
-    auth.user_sessions.second_factor_verification_type,
-    auth.user_sessions.multi_factor_verification,
-    auth.user_sessions.multi_factor_verification_type,
-    auth.user_sessions.sequence,
-    auth.user_sessions.instance_id
-FROM auth.user_sessions
-    LEFT JOIN projections.users10 u ON auth.user_sessions.user_id = u.id
-    LEFT JOIN projections.users10_humans h ON auth.user_sessions.user_id = h.user_id
-    LEFT JOIN projections.login_names3 l ON auth.user_sessions.user_id = l.user_id
-WHERE (auth.user_sessions.instance_id = $1)
-  AND (auth.user_sessions.user_agent_id = $2)
+SELECT s.creation_date,
+       s.change_date,
+       s.resource_owner,
+       s.state,
+       s.user_agent_id,
+       s.user_id,
+       u.username,
+       l.login_name,
+       h.display_name,
+       h.avatar_key,
+       s.selected_idp_config_id,
+       s.password_verification,
+       s.passwordless_verification,
+       s.external_login_verification,
+       s.second_factor_verification,
+       s.second_factor_verification_type,
+       s.multi_factor_verification,
+       s.multi_factor_verification_type,
+       s.sequence,
+       s.instance_id
+FROM auth.user_sessions s
+         LEFT JOIN projections.users10 u ON s.user_id = u.id AND s.instance_id = u.instance_id
+         LEFT JOIN projections.users10_humans h ON s.user_id = h.user_id AND s.instance_id = h.instance_id
+         LEFT JOIN projections.login_names3 l ON s.user_id = l.user_id AND s.instance_id = l.instance_id AND l.is_primary = true
+WHERE (s.user_agent_id = $1)
+  AND (s.instance_id = $2)
+;
