d297967baad984be4c0967ba0f767e52d1fbb2ae
Elio Bischof <elio@zitadel.com>
2024-02-14 13:47:22 +0100

    cleanup

--- internal/api/grpc/header_test.go
+++ internal/api/grpc/header_test.go
@@ -39,7 +39,7 @@ func TestGetHeader(t *testing.T) {
 				ctx:        metadata.NewIncomingContext(context.Background(), metadata.Pairs("header", "value")),
 				headername: "header",
 			},
-			"fail",
+			"value",
 		},
 	}
 	for _, tt := range tests {
