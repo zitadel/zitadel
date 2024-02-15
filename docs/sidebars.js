4981f3cba7680423c648a39efea8972405754018
Fabienne <fabienne.gerschwiler@gmail.com>
2024-02-15 10:32:21 +0100

    docs: prevent users from accessing ZITADEL console

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -128,6 +128,7 @@ module.exports = {
           label: "Console",
           items: [
             "guides/manage/console/overview",
+            "guides/solution-scenarios/disable-console",
             "guides/manage/console/instance-settings",
             "guides/manage/console/organizations",
             "guides/manage/console/projects",
@@ -416,6 +417,7 @@ module.exports = {
         "guides/solution-scenarios/configurations",
         "guides/solution-scenarios/frontend-calling-backend-API",
         "guides/solution-scenarios/device-authorization",
+        "guides/solution-scenarios/disable-console",
         {
           type: "category",
           label: "Onboarding Customers and Users",
