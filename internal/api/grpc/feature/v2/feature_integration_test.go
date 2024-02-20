e4fd55aaa2d771ab782b53c89619b5fc9a1b203c
Tim Möhlmann <tim+github@zitadel.com>
2024-02-20 11:27:40 +0200

    instance features integration tests

--- internal/api/grpc/feature/v2/feature_integration_test.go
+++ internal/api/grpc/feature/v2/feature_integration_test.go
@@ -11,25 +11,30 @@ import (
 	"github.com/muhlemmer/gu"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
 	"github.com/zitadel/zitadel/internal/integration"
 	feature "github.com/zitadel/zitadel/pkg/grpc/feature/v2beta"
 	object "github.com/zitadel/zitadel/pkg/grpc/object/v2beta"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 var (
-	CTX    context.Context
-	Tester *integration.Tester
-	Client feature.FeatureServiceClient
+	SystemCTX context.Context
+	IamCTX    context.Context
+	OrgCTX    context.Context
+	Tester    *integration.Tester
+	Client    feature.FeatureServiceClient
 )
 
 func TestMain(m *testing.M) {
 	os.Exit(func() int {
 		ctx, _, cancel := integration.Contexts(5 * time.Minute)
-		CTX = ctx
 		defer cancel()
-
 		Tester = integration.NewTester(ctx)
+		SystemCTX = Tester.WithAuthorization(ctx, integration.SystemUser)
+		IamCTX = Tester.WithAuthorization(ctx, integration.IAMOwner)
+		OrgCTX = Tester.WithAuthorization(ctx, integration.OrgOwner)
+
 		defer Tester.Done()
 		Client = Tester.Client.FeatureV2
 
@@ -51,7 +56,7 @@ func TestServer_SetSystemFeatures(t *testing.T) {
 		{
 			name: "permission error",
 			args: args{
-				ctx: Tester.WithAuthorization(CTX, integration.IAMOwner),
+				ctx: IamCTX,
 				req: &feature.SetSystemFeaturesRequest{
 					OidcTriggerIntrospectionProjections: gu.Ptr(true),
 				},
@@ -61,7 +66,7 @@ func TestServer_SetSystemFeatures(t *testing.T) {
 		{
 			name: "success",
 			args: args{
-				ctx: Tester.WithAuthorization(CTX, integration.SystemUser),
+				ctx: SystemCTX,
 				req: &feature.SetSystemFeaturesRequest{
 					OidcTriggerIntrospectionProjections: gu.Ptr(true),
 				},
@@ -78,7 +83,7 @@ func TestServer_SetSystemFeatures(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Cleanup(func() {
 				// make sure we have a clean state after each test
-				_, err := Client.ResetSystemFeatures(Tester.WithAuthorization(CTX, integration.SystemUser), &feature.ResetSystemFeaturesRequest{})
+				_, err := Client.ResetSystemFeatures(SystemCTX, &feature.ResetSystemFeaturesRequest{})
 				require.NoError(t, err)
 			})
 			got, err := Client.SetSystemFeatures(tt.args.ctx, tt.args.req)
@@ -93,7 +98,7 @@ func TestServer_SetSystemFeatures(t *testing.T) {
 }
 
 func TestServer_ResetSystemFeatures(t *testing.T) {
-	_, err := Client.SetSystemFeatures(Tester.WithAuthorization(CTX, integration.SystemUser), &feature.SetSystemFeaturesRequest{
+	_, err := Client.SetSystemFeatures(SystemCTX, &feature.SetSystemFeaturesRequest{
 		LoginDefaultOrg: gu.Ptr(true),
 	})
 	require.NoError(t, err)
@@ -106,12 +111,12 @@ func TestServer_ResetSystemFeatures(t *testing.T) {
 	}{
 		{
 			name:    "permission error",
-			ctx:     Tester.WithAuthorization(CTX, integration.IAMOwner),
+			ctx:     IamCTX,
 			wantErr: true,
 		},
 		{
 			name: "success",
-			ctx:  Tester.WithAuthorization(CTX, integration.SystemUser),
+			ctx:  SystemCTX,
 			want: &feature.ResetSystemFeaturesResponse{
 				Details: &object.Details{
 					ChangeDate:    timestamppb.Now(),
@@ -148,7 +153,7 @@ func TestServer_GetSystemFeatures(t *testing.T) {
 		{
 			name: "permission error",
 			args: args{
-				ctx: Tester.WithAuthorization(CTX, integration.IAMOwner),
+				ctx: IamCTX,
 				req: &feature.GetSystemFeaturesRequest{},
 			},
 			wantErr: true,
@@ -156,7 +161,7 @@ func TestServer_GetSystemFeatures(t *testing.T) {
 		{
 			name: "defaults, no inheritance",
 			args: args{
-				ctx: Tester.WithAuthorization(CTX, integration.SystemUser),
+				ctx: SystemCTX,
 				req: &feature.GetSystemFeaturesRequest{},
 			},
 			want: &feature.GetSystemFeaturesResponse{},
@@ -164,7 +169,7 @@ func TestServer_GetSystemFeatures(t *testing.T) {
 		{
 			name: "defaults, inheritance",
 			args: args{
-				ctx: Tester.WithAuthorization(CTX, integration.SystemUser),
+				ctx: SystemCTX,
 				req: &feature.GetSystemFeaturesRequest{
 					Inheritance: true,
 				},
@@ -187,14 +192,14 @@ func TestServer_GetSystemFeatures(t *testing.T) {
 		{
 			name: "some features, no inheritance",
 			prepare: func(t *testing.T) {
-				_, err := Client.SetSystemFeatures(Tester.WithAuthorization(CTX, integration.SystemUser), &feature.SetSystemFeaturesRequest{
+				_, err := Client.SetSystemFeatures(SystemCTX, &feature.SetSystemFeaturesRequest{
 					LoginDefaultOrg:                     gu.Ptr(true),
 					OidcTriggerIntrospectionProjections: gu.Ptr(false),
 				})
 				require.NoError(t, err)
 			},
 			args: args{
-				ctx: Tester.WithAuthorization(CTX, integration.SystemUser),
+				ctx: SystemCTX,
 				req: &feature.GetSystemFeaturesRequest{},
 			},
 			want: &feature.GetSystemFeaturesResponse{
@@ -211,14 +216,14 @@ func TestServer_GetSystemFeatures(t *testing.T) {
 		{
 			name: "some features, inheritance",
 			prepare: func(t *testing.T) {
-				_, err := Client.SetSystemFeatures(Tester.WithAuthorization(CTX, integration.SystemUser), &feature.SetSystemFeaturesRequest{
+				_, err := Client.SetSystemFeatures(SystemCTX, &feature.SetSystemFeaturesRequest{
 					LoginDefaultOrg:                     gu.Ptr(true),
 					OidcTriggerIntrospectionProjections: gu.Ptr(false),
 				})
 				require.NoError(t, err)
 			},
 			args: args{
-				ctx: Tester.WithAuthorization(CTX, integration.SystemUser),
+				ctx: SystemCTX,
 				req: &feature.GetSystemFeaturesRequest{
 					Inheritance: true,
 				},
@@ -243,13 +248,246 @@ func TestServer_GetSystemFeatures(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Cleanup(func() {
 				// make sure we have a clean state after each test
-				_, err := Client.ResetSystemFeatures(Tester.WithAuthorization(CTX, integration.SystemUser), &feature.ResetSystemFeaturesRequest{})
+				_, err := Client.ResetSystemFeatures(SystemCTX, &feature.ResetSystemFeaturesRequest{})
 				require.NoError(t, err)
 			})
 			if tt.prepare != nil {
 				tt.prepare(t)
 			}
 			got, err := Client.GetSystemFeatures(tt.args.ctx, tt.args.req)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			assertFeatureFlag(t, tt.want.LoginDefaultOrg, got.LoginDefaultOrg)
+			assertFeatureFlag(t, tt.want.OidcTriggerIntrospectionProjections, got.OidcTriggerIntrospectionProjections)
+			assertFeatureFlag(t, tt.want.OidcLegacyIntrospection, got.OidcLegacyIntrospection)
+		})
+	}
+}
+
+func TestServer_SetInstanceFeatures(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *feature.SetInstanceFeaturesRequest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *feature.SetInstanceFeaturesResponse
+		wantErr bool
+	}{
+		{
+			name: "permission error",
+			args: args{
+				ctx: OrgCTX,
+				req: &feature.SetInstanceFeaturesRequest{
+					OidcTriggerIntrospectionProjections: gu.Ptr(true),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "success",
+			args: args{
+				ctx: IamCTX,
+				req: &feature.SetInstanceFeaturesRequest{
+					OidcTriggerIntrospectionProjections: gu.Ptr(true),
+				},
+			},
+			want: &feature.SetInstanceFeaturesResponse{
+				Details: &object.Details{
+					ChangeDate:    timestamppb.Now(),
+					ResourceOwner: Tester.Instance.InstanceID(),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Cleanup(func() {
+				// make sure we have a clean state after each test
+				_, err := Client.ResetInstanceFeatures(IamCTX, &feature.ResetInstanceFeaturesRequest{})
+				require.NoError(t, err)
+			})
+			got, err := Client.SetInstanceFeatures(tt.args.ctx, tt.args.req)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			integration.AssertDetails(t, tt.want, got)
+		})
+	}
+}
+
+func TestServer_ResetInstanceFeatures(t *testing.T) {
+	_, err := Client.SetInstanceFeatures(IamCTX, &feature.SetInstanceFeaturesRequest{
+		LoginDefaultOrg: gu.Ptr(true),
+	})
+	require.NoError(t, err)
+
+	tests := []struct {
+		name    string
+		ctx     context.Context
+		want    *feature.ResetInstanceFeaturesResponse
+		wantErr bool
+	}{
+		{
+			name:    "permission error",
+			ctx:     OrgCTX,
+			wantErr: true,
+		},
+		{
+			name: "success",
+			ctx:  IamCTX,
+			want: &feature.ResetInstanceFeaturesResponse{
+				Details: &object.Details{
+					ChangeDate:    timestamppb.Now(),
+					ResourceOwner: Tester.Instance.InstanceID(),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Client.ResetInstanceFeatures(tt.ctx, &feature.ResetInstanceFeaturesRequest{})
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			integration.AssertDetails(t, tt.want, got)
+		})
+	}
+}
+
+func TestServer_GetInstanceFeatures(t *testing.T) {
+	_, err := Client.SetSystemFeatures(SystemCTX, &feature.SetSystemFeaturesRequest{
+		OidcLegacyIntrospection: gu.Ptr(true),
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		_, err := Client.ResetSystemFeatures(SystemCTX, &feature.ResetSystemFeaturesRequest{})
+		require.NoError(t, err)
+	})
+
+	type args struct {
+		ctx context.Context
+		req *feature.GetInstanceFeaturesRequest
+	}
+	tests := []struct {
+		name    string
+		prepare func(t *testing.T)
+		args    args
+		want    *feature.GetInstanceFeaturesResponse
+		wantErr bool
+	}{
+		{
+			name: "permission error",
+			args: args{
+				ctx: OrgCTX,
+				req: &feature.GetInstanceFeaturesRequest{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "defaults, no inheritance",
+			args: args{
+				ctx: IamCTX,
+				req: &feature.GetInstanceFeaturesRequest{},
+			},
+			want: &feature.GetInstanceFeaturesResponse{},
+		},
+		{
+			name: "defaults, inheritance",
+			args: args{
+				ctx: IamCTX,
+				req: &feature.GetInstanceFeaturesRequest{
+					Inheritance: true,
+				},
+			},
+			want: &feature.GetInstanceFeaturesResponse{
+				LoginDefaultOrg: &feature.FeatureFlag{
+					Enabled: false,
+					Source:  feature.Source_SOURCE_DEFAULT,
+				},
+				OidcTriggerIntrospectionProjections: &feature.FeatureFlag{
+					Enabled: false,
+					Source:  feature.Source_SOURCE_DEFAULT,
+				},
+				OidcLegacyIntrospection: &feature.FeatureFlag{
+					Enabled: true,
+					Source:  feature.Source_SOURCE_SYSTEM,
+				},
+			},
+		},
+		{
+			name: "some features, no inheritance",
+			prepare: func(t *testing.T) {
+				_, err := Client.SetInstanceFeatures(IamCTX, &feature.SetInstanceFeaturesRequest{
+					LoginDefaultOrg:                     gu.Ptr(true),
+					OidcTriggerIntrospectionProjections: gu.Ptr(false),
+				})
+				require.NoError(t, err)
+			},
+			args: args{
+				ctx: IamCTX,
+				req: &feature.GetInstanceFeaturesRequest{},
+			},
+			want: &feature.GetInstanceFeaturesResponse{
+				LoginDefaultOrg: &feature.FeatureFlag{
+					Enabled: true,
+					Source:  feature.Source_SOURCE_INSTANCE,
+				},
+				OidcTriggerIntrospectionProjections: &feature.FeatureFlag{
+					Enabled: false,
+					Source:  feature.Source_SOURCE_INSTANCE,
+				},
+			},
+		},
+		{
+			name: "one feature, inheritance",
+			prepare: func(t *testing.T) {
+				_, err := Client.SetInstanceFeatures(IamCTX, &feature.SetInstanceFeaturesRequest{
+					LoginDefaultOrg: gu.Ptr(true),
+				})
+				require.NoError(t, err)
+			},
+			args: args{
+				ctx: IamCTX,
+				req: &feature.GetInstanceFeaturesRequest{
+					Inheritance: true,
+				},
+			},
+			want: &feature.GetInstanceFeaturesResponse{
+				LoginDefaultOrg: &feature.FeatureFlag{
+					Enabled: true,
+					Source:  feature.Source_SOURCE_INSTANCE,
+				},
+				OidcTriggerIntrospectionProjections: &feature.FeatureFlag{
+					Enabled: false,
+					Source:  feature.Source_SOURCE_DEFAULT,
+				},
+				OidcLegacyIntrospection: &feature.FeatureFlag{
+					Enabled: true,
+					Source:  feature.Source_SOURCE_SYSTEM,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Cleanup(func() {
+				// make sure we have a clean state after each test
+				_, err := Client.ResetInstanceFeatures(IamCTX, &feature.ResetInstanceFeaturesRequest{})
+				require.NoError(t, err)
+			})
+			if tt.prepare != nil {
+				tt.prepare(t)
+			}
+			got, err := Client.GetInstanceFeatures(tt.args.ctx, tt.args.req)
 			if tt.wantErr {
 				assert.Error(t, err)
 				return
