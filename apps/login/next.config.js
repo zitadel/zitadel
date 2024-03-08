324cd893a6bfc18aa1bb344d79b533c8bfb2c553
peintnermax <max@caos.ch>
2024-03-08 11:39:17 +0100

    security headers - csp

--- apps/login/next.config.js
+++ apps/login/next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
+
+const secureHeaders = [
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  // img-src vercel.com needed for deploy button,
+  // script-src va.vercel-scripts.com for analytics/vercel scripts
+  {
+    key: "Content-Security-Policy",
+    value:
+      "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://va.vercel-scripts.com; connect-src 'self'; child-src; style-src 'self' 'unsafe-inline'; font-src 'self'; object-src 'none'; img-src 'self' https://vercel.com;",
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   swcMinify: true,
@@ -20,6 +51,14 @@ const nextConfig = {
         pathname: "/**",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: secureHeaders,
+      },
+    ];
   },
 };
 
