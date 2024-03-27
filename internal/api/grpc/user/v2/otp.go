84644214d7a8c35fc45e191864c37c3261c8ab51
Stefan Benz <46600784+stebenz@users.noreply.github.com>
2024-03-27 19:22:17 +0100

    fix: remove resourceowner read from context in user v2 api (#7641)
    * fix: remove resourceowner read from context in user v2 api
    
    * fix: lint
    
    * fix: remove orgID in addIDPLink
    
    * fix: remove comment as unnecessary
    
    ---------
    
    Co-authored-by: Livio Spring <livio.a@gmail.com>

--- internal/api/grpc/user/v2/otp.go
+++ internal/api/grpc/user/v2/otp.go
@@ -3,13 +3,12 @@ package user
 import (
 	"context"
 
-	"github.com/zitadel/zitadel/internal/api/authz"
 	"github.com/zitadel/zitadel/internal/api/grpc/object/v2"
 	user "github.com/zitadel/zitadel/pkg/grpc/user/v2beta"
 )
 
 func (s *Server) AddOTPSMS(ctx context.Context, req *user.AddOTPSMSRequest) (*user.AddOTPSMSResponse, error) {
-	details, err := s.command.AddHumanOTPSMS(ctx, req.GetUserId(), authz.GetCtxData(ctx).OrgID)
+	details, err := s.command.AddHumanOTPSMS(ctx, req.GetUserId(), "")
 	if err != nil {
 		return nil, err
 	}
@@ -18,7 +17,7 @@ func (s *Server) AddOTPSMS(ctx context.Context, req *user.AddOTPSMSRequest) (*us
 }
 
 func (s *Server) RemoveOTPSMS(ctx context.Context, req *user.RemoveOTPSMSRequest) (*user.RemoveOTPSMSResponse, error) {
-	objectDetails, err := s.command.RemoveHumanOTPSMS(ctx, req.GetUserId(), authz.GetCtxData(ctx).OrgID)
+	objectDetails, err := s.command.RemoveHumanOTPSMS(ctx, req.GetUserId(), "")
 	if err != nil {
 		return nil, err
 	}
@@ -26,7 +25,7 @@ func (s *Server) RemoveOTPSMS(ctx context.Context, req *user.RemoveOTPSMSRequest
 }
 
 func (s *Server) AddOTPEmail(ctx context.Context, req *user.AddOTPEmailRequest) (*user.AddOTPEmailResponse, error) {
-	details, err := s.command.AddHumanOTPEmail(ctx, req.GetUserId(), authz.GetCtxData(ctx).OrgID)
+	details, err := s.command.AddHumanOTPEmail(ctx, req.GetUserId(), "")
 	if err != nil {
 		return nil, err
 	}
@@ -35,7 +34,7 @@ func (s *Server) AddOTPEmail(ctx context.Context, req *user.AddOTPEmailRequest) 
 }
 
 func (s *Server) RemoveOTPEmail(ctx context.Context, req *user.RemoveOTPEmailRequest) (*user.RemoveOTPEmailResponse, error) {
-	objectDetails, err := s.command.RemoveHumanOTPEmail(ctx, req.GetUserId(), authz.GetCtxData(ctx).OrgID)
+	objectDetails, err := s.command.RemoveHumanOTPEmail(ctx, req.GetUserId(), "")
 	if err != nil {
 		return nil, err
 	}
