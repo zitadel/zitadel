61f0bdb7b4f4c889a486cea2e0ebec137bf83e38
peintnermax <max@caos.ch>
2024-04-09 17:08:54 +0200

    iam.restrictions.read

--- console/src/app/modules/settings-list/settings.ts
+++ console/src/app/modules/settings-list/settings.ts
@@ -15,7 +15,7 @@ export const FEATURESETTINGS: SidenavSetting = {
   i18nKey: 'SETTINGS.LIST.FEATURESETTINGS',
   groupI18nKey: 'SETTINGS.GROUPS.GENERAL',
   requiredRoles: {
-    [PolicyComponentServiceType.ADMIN]: ['iam.read'], // iam.restrictions.read
+    [PolicyComponentServiceType.ADMIN]: ['iam.restrictions.read'],
   },
 };
 
