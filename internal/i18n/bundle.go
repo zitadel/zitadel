57d11b4c65031de00e01c7835de527f0a106ba42
adlerhurst <silvan.reusser@gmail.com>
2024-04-16 12:04:06 +0200

    fix(middleware): init translation messages

--- internal/i18n/bundle.go
+++ internal/i18n/bundle.go
@@ -3,58 +3,67 @@ package i18n
 import (
 	"encoding/json"
 	"io"
-	"net/http"
-	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/BurntSushi/toml"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"github.com/zitadel/logging"
 	"golang.org/x/text/language"
 	"sigs.k8s.io/yaml"
 
 	"github.com/zitadel/zitadel/internal/domain"
-	"github.com/zitadel/zitadel/internal/zerrors"
 )
 
 const i18nPath = "/i18n"
 
-func newBundle(dir http.FileSystem, defaultLanguage language.Tag, allowedLanguages []language.Tag) (*i18n.Bundle, error) {
-	bundle := i18n.NewBundle(defaultLanguage)
-	bundle.RegisterUnmarshalFunc("yaml", func(data []byte, v interface{}) error { return yaml.Unmarshal(data, v) })
-	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
-	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
-	i18nDir, err := dir.Open(i18nPath)
-	if err != nil {
-		return nil, zerrors.ThrowNotFound(err, "I18N-MnXRie", "path not found")
-	}
-	defer i18nDir.Close()
-	files, err := i18nDir.Readdir(0)
-	if err != nil {
-		return nil, zerrors.ThrowNotFound(err, "I18N-Gew23", "cannot read dir")
-	}
-	for _, file := range files {
-		fileLang, _ := strings.CutSuffix(file.Name(), filepath.Ext(file.Name()))
-		if err = domain.LanguageIsAllowed(false, allowedLanguages, language.Make(fileLang)); err != nil {
-			continue
-		}
-		if err := addFileFromFileSystemToBundle(dir, bundle, file); err != nil {
-			return nil, zerrors.ThrowNotFoundf(err, "I18N-ZS2AW", "cannot append file %s to Bundle", file.Name())
-		}
-	}
-	return bundle, nil
+var translationMessages = map[Namespace]map[language.Tag]*i18n.MessageFile{
+	ZITADEL:      make(map[language.Tag]*i18n.MessageFile),
+	LOGIN:        make(map[language.Tag]*i18n.MessageFile),
+	NOTIFICATION: make(map[language.Tag]*i18n.MessageFile),
 }
 
-func addFileFromFileSystemToBundle(dir http.FileSystem, bundle *i18n.Bundle, file os.FileInfo) error {
-	f, err := dir.Open("/i18n/" + file.Name())
-	if err != nil {
-		return err
+func init() {
+	unmarshaler := map[string]i18n.UnmarshalFunc{
+		"yaml": func(data []byte, v interface{}) error { return yaml.Unmarshal(data, v) },
+		"json": json.Unmarshal,
+		"toml": toml.Unmarshal,
 	}
-	defer f.Close()
-	content, err := io.ReadAll(f)
-	if err != nil {
-		return err
+	for ns := range translationMessages {
+		dir := LoadFilesystem(ns)
+		i18nDir, err := dir.Open(i18nPath)
+		logging.WithFields("namespace", ns).OnError(err).Panic("unable to open translation files")
+		defer i18nDir.Close()
+		files, err := i18nDir.Readdir(0)
+		logging.WithFields("namespace", ns).OnError(err).Panic("unable to read translation files")
+		for _, file := range files {
+			f, err := dir.Open("/i18n/" + file.Name())
+			logging.WithFields("namespace", ns, "file", file.Name()).OnError(err).Panic("unable to open translation file")
+			defer f.Close()
+
+			content, err := io.ReadAll(f)
+			logging.WithFields("namespace", ns, "file", file.Name()).OnError(err).Panic("unable to read translation file")
+
+			messageFile, err := i18n.ParseMessageFileBytes(content, file.Name(), unmarshaler)
+			logging.WithFields("namespace", ns, "file", file.Name()).OnError(err).Panic("unable to parse translation file")
+
+			fileLang, _ := strings.CutSuffix(file.Name(), filepath.Ext(file.Name()))
+			lang := language.Make(fileLang)
+
+			translationMessages[ns][lang] = messageFile
+		}
 	}
-	_, err = bundle.ParseMessageFileBytes(content, file.Name())
-	return err
+}
+
+func newBundle(ns Namespace, defaultLanguage language.Tag, allowedLanguages []language.Tag) (*i18n.Bundle, error) {
+	bundle := i18n.NewBundle(defaultLanguage)
+
+	for lang, file := range translationMessages[ns] {
+		if err := domain.LanguageIsAllowed(false, allowedLanguages, lang); err != nil {
+			continue
+		}
+		bundle.MustAddMessages(lang, file.Messages...)
+	}
+
+	return bundle, nil
 }
