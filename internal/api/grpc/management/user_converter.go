73fca5c0d2ad7ae7c79e400faebab9b787d03c17
Ari Breitkreuz <ari.breitkreuz@pm.me>
2024-04-17 11:23:35 +0200

    Fix naming error

--- internal/api/grpc/management/user_converter.go
+++ internal/api/grpc/management/user_converter.go
@@ -237,7 +237,7 @@ func AddMachineKeyRequestToCommand(req *mgmt_pb.AddMachineKeyRequest, resourceOw
 		},
 		ExpirationDate: expDate,
 		Type:           authn.KeyTypeToDomain(req.Type),
-		PublicKey:      req.Pubkey,
+		PublicKey:      req.PublicKey,
 	}
 }
 
