66222888f50ca459a502706f022a02e89c380337
peintnermax <max@caos.ch>
2024-04-24 08:37:17 +0200

    rm u2f set cmp

--- apps/login/app/(login)/u2f/set/page.tsx
+++ apps/login/app/(login)/u2f/set/page.tsx
@@ -18,27 +18,10 @@ export default async function Page({
   params: Record<string | number | symbol, string | undefined>;
 }) {
   const { loginName, organization } = searchParams;
-  const { method } = params;
 
   const branding = await getBrandingSettings(server, organization);
 
-  const totpResponse = await loadSession(loginName, organization).then(
-    ({ session, token }) => {
-      if (session && session.factors?.user?.id) {
-        if (method === "time-based") {
-          return registerTOTP(session.factors.user.id, token);
-        } else if (method === "sms") {
-          return addOTPSMS(session.factors.user.id);
-        } else if (method === "email") {
-          return addOTPEmail(session.factors.user.id);
-        } else {
-          throw new Error("Invalid method");
-        }
-      } else {
-        throw new Error("No session found");
-      }
-    }
-  );
+  const totpResponse = await loadSession(loginName, organization).then();
 
   async function loadSession(loginName?: string, organization?: string) {
     const recent = await getMostRecentCookieWithLoginname(
