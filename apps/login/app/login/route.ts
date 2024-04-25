a4f5991ba0fa46cc780ec342d2d6f5924bfcebb9
peintnermax <max@caos.ch>
2024-04-25 08:31:25 +0200

    catch error on callback

--- apps/login/app/login/route.ts
+++ apps/login/app/login/route.ts
@@ -170,11 +170,25 @@ export async function GET(request: NextRequest) {
               sessionId: cookie?.id,
               sessionToken: cookie?.token,
             };
-            const { callbackUrl } = await createCallback(server, {
-              authRequestId,
-              session,
-            });
-            return NextResponse.redirect(callbackUrl);
+            // const { callbackUrl } = await createCallback(server, {
+            //   authRequestId,
+            //   session,
+            // });
+            // return NextResponse.redirect(callbackUrl);
+            try {
+              const { callbackUrl } = await createCallback(server, {
+                authRequestId,
+                session,
+              });
+              if (callbackUrl) {
+                return NextResponse.redirect(callbackUrl);
+              } else {
+                gotoAccounts();
+              }
+            } catch (error) {
+              console.error(error);
+              gotoAccounts();
+            }
           } else {
             return NextResponse.json(
               { error: "No active session found" },
