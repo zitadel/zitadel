cba93d61548f55176ce2a700acbdb65fcb2f34a9
Livio Spring <livio.a@gmail.com>
2024-05-15 11:01:27 +0200

    improve refresh token handling

--- internal/auth/repository/eventsourcing/handler/refresh_token.go
+++ internal/auth/repository/eventsourcing/handler/refresh_token.go
@@ -3,8 +3,6 @@ package handler
 import (
 	"context"
 
-	"github.com/zitadel/logging"
-
 	auth_view "github.com/zitadel/zitadel/internal/auth/repository/eventsourcing/view"
 	"github.com/zitadel/zitadel/internal/eventstore"
 	"github.com/zitadel/zitadel/internal/eventstore/handler/v2"
@@ -98,50 +96,91 @@ func (t *RefreshToken) Reducers() []handler.AggregateReducer {
 }
 
 func (t *RefreshToken) Reduce(event eventstore.Event) (_ *handler.Statement, err error) {
-	return handler.NewStatement(event, func(ex handler.Executer, projectionName string) error {
-		switch event.Type() {
-		case user.HumanRefreshTokenAddedType:
-			token := new(view_model.RefreshTokenView)
-			err := token.AppendEvent(event)
-			if err != nil {
-				return err
-			}
-
-			return t.view.PutRefreshToken(token)
-		case user.HumanRefreshTokenRenewedType:
-			e := new(user.HumanRefreshTokenRenewedEvent)
-			if err := event.Unmarshal(e); err != nil {
-				logging.WithError(err).Error("could not unmarshal event data")
-				return zerrors.ThrowInternal(nil, "MODEL-BHn75", "could not unmarshal data")
-			}
-			token, err := t.view.RefreshTokenByID(e.TokenID, event.Aggregate().InstanceID)
-			if err != nil {
-				return err
-			}
-			err = token.AppendEvent(event)
-			if err != nil {
-				return err
-			}
-			return t.view.PutRefreshToken(token)
-		case user.HumanRefreshTokenRemovedType:
-			e := new(user.HumanRefreshTokenRemovedEvent)
-			if err := event.Unmarshal(e); err != nil {
-				logging.WithError(err).Error("could not unmarshal event data")
-				return zerrors.ThrowInternal(nil, "MODEL-Bz653", "could not unmarshal data")
-			}
-			return t.view.DeleteRefreshToken(e.TokenID, event.Aggregate().InstanceID)
-		case user.UserLockedType,
-			user.UserDeactivatedType,
-			user.UserRemovedType:
-
-			return t.view.DeleteUserRefreshTokens(event.Aggregate().ID, event.Aggregate().InstanceID)
-		case instance.InstanceRemovedEventType:
-
-			return t.view.DeleteInstanceRefreshTokens(event.Aggregate().InstanceID)
-		case org.OrgRemovedEventType:
-			return t.view.DeleteOrgRefreshTokens(event)
-		default:
-			return nil
+	switch event.Type() {
+	case user.HumanRefreshTokenAddedType:
+		e, ok := event.(*user.HumanRefreshTokenAddedEvent)
+		if !ok {
+			return nil, zerrors.ThrowInvalidArgumentf(nil, "MODEL-IoF6j", "reduce.wrong.event.type %s", user.HumanRefreshTokenAddedType)
 		}
-	}), nil
+		return handler.NewCreateStatement(event,
+			[]handler.Column{
+				handler.NewCol("id", e.TokenID),
+				handler.NewCol(userIDCol, e.Aggregate().ID),
+				handler.NewCol(resourceOwnerCol, e.Aggregate().ResourceOwner),
+				handler.NewCol(instanceIDCol, e.Aggregate().InstanceID),
+				handler.NewCol(creationDateCol, event.CreatedAt()),
+				handler.NewCol("amr", e.AuthMethodsReferences),
+				handler.NewCol("auth_time", e.AuthTime),
+				handler.NewCol("audience", e.Audience),
+				handler.NewCol("client_id", e.ClientID),
+				handler.NewCol("expiration", event.CreatedAt().Add(e.Expiration)),
+				handler.NewCol("idle_expiration", event.CreatedAt().Add(e.IdleExpiration)),
+				handler.NewCol("scopes", e.Scopes),
+				handler.NewCol("token", e.TokenID),
+				handler.NewCol(userAgentIDCol, e.UserAgentID),
+				handler.NewCol("actor", view_model.TokenActor{TokenActor: e.Actor}),
+			}), nil
+	case user.HumanRefreshTokenRenewedType:
+		e, ok := event.(*user.HumanRefreshTokenRenewedEvent)
+		if !ok {
+			return nil, zerrors.ThrowInvalidArgumentf(nil, "MODEL-AG43hq", "reduce.wrong.event.type %s", user.HumanRefreshTokenRenewedType)
+		}
+		//e := new(user.HumanRefreshTokenRenewedEvent)
+		//if err := event.Unmarshal(e); err != nil {
+		//	logging.WithError(err).Error("could not unmarshal event data")
+		//	return nil, zerrors.ThrowInternal(nil, "MODEL-BHn75", "could not unmarshal data")
+		//}
+		return handler.NewUpdateStatement(event,
+			[]handler.Column{
+				handler.NewCol("idle_expiration", event.CreatedAt().Add(e.IdleExpiration)),
+				handler.NewCol("token", e.RefreshToken),
+			},
+			[]handler.Condition{
+				handler.NewCond("id", e.TokenID),
+				handler.NewCond(instanceIDCol, e.Aggregate().InstanceID),
+			},
+		), nil
+	case user.HumanRefreshTokenRemovedType:
+		e, ok := event.(*user.HumanRefreshTokenRemovedEvent)
+		if !ok {
+			return nil, zerrors.ThrowInvalidArgumentf(nil, "MODEL-SFF3t", "reduce.wrong.event.type %s", user.HumanRefreshTokenRemovedType)
+		}
+		//e := new(user.HumanRefreshTokenRemovedEvent)
+		//if err := event.Unmarshal(e); err != nil {
+		//	logging.WithError(err).Error("could not unmarshal event data")
+		//	return nil, zerrors.ThrowInternal(nil, "MODEL-Bz653", "could not unmarshal data")
+		//}
+		return handler.NewDeleteStatement(event,
+			[]handler.Condition{
+				handler.NewCond(instanceIDCol, event.Aggregate().InstanceID),
+				handler.NewCond("id", e.TokenID),
+			},
+		), nil
+	case user.UserLockedType,
+		user.UserDeactivatedType,
+		user.UserRemovedType:
+		return handler.NewDeleteStatement(event,
+			[]handler.Condition{
+				handler.NewCond(instanceIDCol, event.Aggregate().InstanceID),
+				handler.NewCond(userIDCol, event.Aggregate().InstanceID),
+			},
+		), nil
+	case instance.InstanceRemovedEventType:
+		return handler.NewDeleteStatement(event,
+			[]handler.Condition{
+				handler.NewCond(instanceIDCol, event.Aggregate().InstanceID),
+			},
+		), nil
+	case org.OrgRemovedEventType:
+		return handler.NewDeleteStatement(event,
+			[]handler.Condition{
+				handler.NewCond(instanceIDCol, event.Aggregate().InstanceID),
+				handler.NewCond(resourceOwnerCol, event.Aggregate().InstanceID),
+			},
+		), nil
+	default:
+		return handler.NewStatement(event, func(ex handler.Executer, projectionName string) error {
+			return nil
+		}), nil
+	}
 }
