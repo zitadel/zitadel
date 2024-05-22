5bccd8e01857c418bf2d16c7c255969d408f03e6
adlerhurst <silvan.reusser@gmail.com>
2024-05-22 17:26:08 +0200

    linting

--- internal/v2/org/domain.go
+++ internal/v2/org/domain.go
@@ -6,8 +6,6 @@ import (
 	"github.com/zitadel/zitadel/internal/zerrors"
 )
 
-var uniqueOrgDomain = "org_domain"
-
 const DomainAddedType = "org." + domain.AddedTypeSuffix
 
 type DomainAddedPayload domain.AddedPayload
