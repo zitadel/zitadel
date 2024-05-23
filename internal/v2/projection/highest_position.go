a736d9df0781470f080e50acef04140a9e2fd9cc
adlerhurst <silvan.reusser@gmail.com>
2024-05-23 11:01:32 +0200

    reduce

--- internal/v2/projection/highest_position.go
+++ internal/v2/projection/highest_position.go
@@ -9,7 +9,7 @@ type HighestPosition eventstore.GlobalPosition
 var _ eventstore.Reducer = (*HighestPosition)(nil)
 
 // Reduce implements eventstore.Reducer.
-func (h *HighestPosition) Reduce(events ...*eventstore.Event[eventstore.StoragePayload]) error {
+func (h *HighestPosition) Reduce(events ...*eventstore.StorageEvent) error {
 	*h = HighestPosition(events[len(events)-1].Position)
 	return nil
 }
