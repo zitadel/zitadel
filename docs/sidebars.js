efb9966aa1a955f4cf4cb217193490d4107429df
Elio Bischof <elio@zitadel.com>
2024-05-24 12:46:03 +0200

    docs: please help

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -763,8 +763,7 @@ module.exports = {
           label: "Assets",
           collapsed: true,
           items: ["apis/assets/assets"],
-        },
-          "apis/known_issues",
+        }
       ],
     },
     {
@@ -833,6 +832,7 @@ module.exports = {
       label: "Rate Limits (Cloud)", // The link label
       href: "/legal/policies/rate-limit-policy", // The internal path
     },
+    "apis/known_docs_issues",
   ],
   selfHosting: [
     {
