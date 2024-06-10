76c4e67b540d1483092fb2751e20c6c96623d14e
adlerhurst <silvan.reusser@gmail.com>
2024-06-10 15:58:14 +0200

    table design

--- cmd/setup/28.sql
+++ cmd/setup/28.sql
@@ -1,10 +1,11 @@
 -- TODO: whats the correct name of this table?
-CREATE TABLE eventstore.fields (
+CREATE TABLE eventstore.lookups (
     instance_id TEXT NOT NULL
+    , resource_owner TEXT NOT NULL
     , aggregate_type TEXT NOT NULL
     , aggregate_id TEXT NOT NULL
     , field_name TEXT NOT NULL
     , "value" JSONB
     , text_value TEXT
-    , CHECK (("value" IS NULL) <> (text_value IS NULL))
+    , CONSTRAINT one_of_values CHECK (("value" IS NULL) <> (text_value IS NULL))
 );
