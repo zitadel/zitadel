132f54f56b665727672ff453370c7af7464fc23e
Tim Möhlmann <tim+github@zitadel.com>
2024-06-15 15:04:06 +0300

    fix(oidc): store requested response_mode

---
+++ internal/api/oidc/auth_request_converter_test.go
@@ -0,0 +1,96 @@
+package oidc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/zitadel/oidc/v3/pkg/oidc"
+
+	"github.com/zitadel/zitadel/internal/domain"
+)
+
+func TestResponseModeToBusiness(t *testing.T) {
+	type args struct {
+		responseMode oidc.ResponseMode
+	}
+	tests := []struct {
+		name string
+		args args
+		want domain.OIDCResponseMode
+	}{
+		{
+			name: "empty",
+			args: args{""},
+			want: domain.OIDCResponseModeUnspecified,
+		},
+		{
+			name: "invalid",
+			args: args{"foo"},
+			want: domain.OIDCResponseModeUnspecified,
+		},
+		{
+			name: "query",
+			args: args{oidc.ResponseModeQuery},
+			want: domain.OIDCResponseModeQuery,
+		},
+		{
+			name: "fragment",
+			args: args{oidc.ResponseModeFragment},
+			want: domain.OIDCResponseModeFragment,
+		},
+		{
+			name: "post_form",
+			args: args{oidc.ResponseModeFormPost},
+			want: domain.OIDCResponseModeFormPost,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ResponseModeToBusiness(tt.args.responseMode)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestResponseModeToOIDC(t *testing.T) {
+	type args struct {
+		responseMode domain.OIDCResponseMode
+	}
+	tests := []struct {
+		name string
+		args args
+		want oidc.ResponseMode
+	}{
+		{
+			name: "unspecified",
+			args: args{domain.OIDCResponseModeUnspecified},
+			want: "",
+		},
+		{
+			name: "invalid",
+			args: args{99},
+			want: "",
+		},
+		{
+			name: "query",
+			args: args{domain.OIDCResponseModeQuery},
+			want: oidc.ResponseModeQuery,
+		},
+		{
+			name: "fragment",
+			args: args{domain.OIDCResponseModeFragment},
+			want: oidc.ResponseModeFragment,
+		},
+		{
+			name: "form_post",
+			args: args{domain.OIDCResponseModeFormPost},
+			want: oidc.ResponseModeFormPost,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ResponseModeToOIDC(tt.args.responseMode)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
