03398d54cf1dc21046b2a52fd92b693853614d6c
Florian Forster <florian@zitadel.com>
2024-06-23 22:11:52 +0200

    test

--- ci/docs/src/index.ts
+++ ci/docs/src/index.ts
@@ -9,12 +9,14 @@ class Docs {
     return dag
       .container()
       .from("node:20")
-      .withWorkdir("/usr/local/app")
-      .withFile("/usr/local/app/package.json", directory.file("package.json"))
-      .withFile("/usr/local/app/yarn.lock", directory.file("yarn.lock"))
+      .withWorkdir("/usr/local/docs")
+      .withFile("/usr/local/docs/package.json", directory.file("docs/package.json"))
+      .withFile("/usr/local/docs/yarn.lock", directory.file("docs/yarn.lock"))
       .withExec(["yarn", "install", "--frozen-lockfile"])
       .withExec(["npm", "cache", "clean", "--force"])
-      .withExec(["mv", "/usr/local/app/node_modules", "/node_modules"])
-      .withDirectory("/usr/local/app", directory)
+      .withExec(["mv", "/usr/local/docs/node_modules", "/node_modules"])
+      .withDirectory("/usr/local/docs", directory, {include: ["docs"]})
+      .withDirectory("/usr/local/proto", directory, {include: ["proto"]})
+      .withExec(["yarn", "build"])
   }
 }
