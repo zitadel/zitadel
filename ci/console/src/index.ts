187f0bfe8b65bfb8b8c65c045bb2a4b159aeb8ad
Florian Forster <florian@zitadel.com>
2024-06-25 12:08:48 +0000

    add generate

--- ci/console/src/index.ts
+++ ci/console/src/index.ts
@@ -5,27 +5,19 @@ import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
 class Console {
 
   @func()
+  build(directory: Directory): Directory {
+    return this.buildEnv(directory)
+      .withDirectory("/src/", this.generate(directory))
+      .withExec(["yarn", "run", "build"])
+      .directory("./dist/console")
+  }
+
+  @func()
   generate(directory: Directory): Directory {
       return this.buildEnv(directory)
       .withExec(["yarn", "generate"])
       .withExec(["ls", "-la", "./src/app/proto/generated"])
       .directory("./src/app/proto/generated")
-  }
-
-  @func()
-  build(directory: Directory): Directory {
-    return dag
-      .container()
-      .from("node:20")
-      .withDirectory("/src/", directory, {include: ["console/**"]})
-      .withDirectory("/src/", directory, {include: ["proto/**"]})
-      .withDirectory("/src/", directory, {include: ["docs/frameworks.json"]})
-      .withWorkdir("/src/console")
-      .withMountedCache("/src/console/node_modules", dag.cacheVolume("console-node-modules"))
-      .withExec(["yarn", "install", "--frozen-lockfile"])
-      .withExec(["yarn", "generate"])
-      .withExec(["yarn", "run", "build"])
-      .directory("./dist/console")
   }
 
   @func()
