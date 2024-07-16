e9523a65306e54392f6c7328c7084a764dc1f61c
peintnermax <max@caos.ch>
2024-07-16 17:16:35 +0200

    cleanup backnavigation

--- apps/login/src/ui/BackButton.tsx
+++ apps/login/src/ui/BackButton.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import Link from "next/link";
 import { Button, ButtonVariants } from "./Button";
 
-type Props = { hasBack?: boolean };
-
-export default function BackButton({ hasBack }: Props) {
-  return hasBack || history?.length > 1 ? (
+export default function BackButton() {
+  return history && history.length > 1 ? (
     <Button
       onClick={() => history.back()}
       type="button"
