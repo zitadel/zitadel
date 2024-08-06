a39b3d713bc8f75f1d4efb8ee8260d10968d488c
adlerhurst <silvan.reusser@gmail.com>
2024-08-06 15:10:23 +0200

    so?

--- e2e/cypress/support/commands.ts
+++ e2e/cypress/support/commands.ts
@@ -15,7 +15,7 @@ interface ShouldNotExistOptions {
 const COMMAND_DELAY = Cypress.env('COMMAND_DELAY') || 0;
 if (COMMAND_DELAY > 0) {
     for (const command of ['visit', 'click', 'trigger', 'type', 'clear', 'reload', 'contains']) {
-        Cypress.Commands.overwrite(command as keyof Cypress.Chainable<any>, (originalFn, ...args) => {
+        Cypress.Commands.overwrite(command as unknown as keyof Cypress.Chainable<any>, (originalFn, ...args) => {
             const origVal = originalFn(...args);
 
             return new Promise((resolve) => {
