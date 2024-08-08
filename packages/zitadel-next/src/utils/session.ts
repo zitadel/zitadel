afa024f1e4b613f5feadc30cb5f6dc0927be2559
peintnermax <max@caos.ch>
2024-08-08 09:06:01 +0200

    move transport out of package

--- packages/zitadel-next/src/utils/session.ts
+++ packages/zitadel-next/src/utils/session.ts
@@ -1,22 +1,14 @@
-import { createSessionServiceClient } from "@zitadel/client/v2";
-import { createServerTransport } from "@zitadel/node";
 import { Session } from "@zitadel/proto/zitadel/session/v2/session_pb";
+import { GetSessionResponse } from "@zitadel/proto/zitadel/session/v2/session_service_pb";
 import { getMostRecentCookieWithLoginname } from "./cookies";
 
-const SESSION_LIFETIME_S = 3000;
-
-const transport = createServerTransport(process.env.ZITADEL_SERVICE_USER_TOKEN!, {
-  baseUrl: process.env.ZITADEL_API_URL!,
-  httpVersion: "2",
-});
-
-const sessionService = createSessionServiceClient(transport);
-
-export async function loadMostRecentSession(loginName?: string, organization?: string): Promise<Session | undefined> {
+export async function loadMostRecentSession(
+  sessionService: any, // TODO: SessionServiceClient,
+  loginName?: string,
+  organization?: string,
+): Promise<Session | undefined> {
   const recent = await getMostRecentCookieWithLoginname(loginName, organization);
-  return getMostRecentSession(recent.id, recent.token);
-}
-
-async function getMostRecentSession(sessionId: string, sessionToken: string) {
-  return sessionService.getSession({ sessionId, sessionToken }, {}).then((resp) => resp.session);
+  return sessionService
+    .getSession({ sessionId: recent.id, sessionToken: recent.token }, {})
+    .then((resp: GetSessionResponse) => resp.session);
 }
