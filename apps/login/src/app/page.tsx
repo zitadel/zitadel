16ea8910d462e37a36d48b393aa16d128b4c1afe
peintnermax <max@caos.ch>
2024-08-12 11:31:18 +0200

    debug env

--- apps/login/src/app/page.tsx
+++ apps/login/src/app/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 
 export default function Page() {
   // automatically redirect to loginname
-  if (process.env.NODE_ENV === "production") {
+  if (!process.env.DEBUG) {
     redirect("/loginname");
   }
 }
