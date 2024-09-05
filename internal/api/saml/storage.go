1b2c9a64c44b65b551f90ed13687f6424ab9d536
Livio Spring <livio.a@gmail.com>
2024-09-05 16:00:28 +0200

    fix: check app state for saml

--- internal/api/saml/storage.go
+++ internal/api/saml/storage.go
@@ -59,9 +59,6 @@ func (p *Storage) GetEntityByID(ctx context.Context, entityID string) (*servicep
 	if err != nil {
 		return nil, err
 	}
-	if app.State != domain.AppStateActive {
-		return nil, zerrors.ThrowPreconditionFailed(nil, "SAML-sdaGg", "app is not active")
-	}
 	return serviceprovider.NewServiceProvider(
 		app.ID,
 		&serviceprovider.Config{
@@ -72,12 +69,9 @@ func (p *Storage) GetEntityByID(ctx context.Context, entityID string) (*servicep
 }
 
 func (p *Storage) GetEntityIDByAppID(ctx context.Context, appID string) (string, error) {
-	app, err := p.query.AppByID(ctx, appID)
+	app, err := p.query.AppByID(ctx, appID, true)
 	if err != nil {
 		return "", err
-	}
-	if app.State != domain.AppStateActive {
-		return "", zerrors.ThrowPreconditionFailed(nil, "SAML-sdaGg", "app is not active")
 	}
 	return app.SAMLConfig.EntityID, nil
 }
