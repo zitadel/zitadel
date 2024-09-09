e1cd7c02aaf94db3aebc6100aeb0590419edf353
Tim Möhlmann <tim+github@zitadel.com>
2024-09-09 16:25:21 +0300

    perf(oidc): remove get user by ID from jwt profile grant

--- internal/api/oidc/token_client_credentials.go
+++ internal/api/oidc/token_client_credentials.go
@@ -26,15 +26,15 @@ func (s *Server) ClientCredentialsExchange(ctx context.Context, r *op.ClientRequ
 	if err != nil {
 		return nil, err
 	}
-	scope, err = s.checkOrgScopes(ctx, client.user, scope)
+	scope, err = s.checkOrgScopes(ctx, client.resourceOwner, scope)
 	if err != nil {
 		return nil, err
 	}
 
 	session, err := s.command.CreateOIDCSession(ctx,
-		client.user.ID,
-		client.user.ResourceOwner,
-		"",
+		client.userID,
+		client.resourceOwner,
+		client.clientID,
 		scope,
 		domain.AddAudScopeToAudience(ctx, nil, r.Data.Scope),
 		[]domain.UserAuthMethodType{domain.UserAuthMethodTypePassword},
