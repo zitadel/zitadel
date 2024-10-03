532aa6783314449c4d518d290e61a75e5f2a143c
Tim Möhlmann <tim+github@zitadel.com>
2024-10-03 12:10:51 +0300

    Update internal/cache/pg/invalidate.sql

--- internal/cache/pg/invalidate.sql
+++ internal/cache/pg/invalidate.sql
@@ -3,7 +3,7 @@ using cache.string_keys k
 where k.cache_name = $1
 	and k.index_id = $2
 	and k.index_key = any($3)
-	and o.cache_name = $1
+	and o.cache_name = k.cache_name
 	and o.id = k.object_id
 ;
 
