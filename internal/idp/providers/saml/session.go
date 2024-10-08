4941f43778777cc11bd8469be6815a2448718ce0
Stuart Douglas <sdouglas@hopper.com>
2024-10-08 14:18:48 +0100

    fix: fix import grouping for lint [NOJIRA]

--- internal/idp/providers/saml/session.go
+++ internal/idp/providers/saml/session.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/crewjam/saml"
 	"github.com/crewjam/saml/samlsp"
-
 	"github.com/zitadel/logging"
+
 	"github.com/zitadel/zitadel/internal/idp"
 	"github.com/zitadel/zitadel/internal/zerrors"
 )
