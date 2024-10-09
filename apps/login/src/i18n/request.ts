faf38c7f6f48c1b35bfe8e14fe84f40d836a6813
peintnermax <max@caos.ch>
2024-10-09 10:40:15 +0200

    implement fallback language

--- apps/login/src/i18n/request.ts
+++ apps/login/src/i18n/request.ts
@@ -1,14 +1,20 @@
+import deepmerge from "deepmerge";
 import { getRequestConfig } from "next-intl/server";
 import { cookies } from "next/headers";
 
 export default getRequestConfig(async () => {
+  const fallback = "en";
   const cookiesList = cookies();
   const locale: string = cookiesList.get("NEXT_LOCALE")?.value ?? "en";
+
+  const userMessages = (await import(`../../locales/${locale}.json`)).default;
+  const fallbackMessages = (await import(`../../locales/${fallback}.json`))
+    .default;
 
   console.log("i18nRequest", locale);
 
   return {
     locale,
-    messages: (await import(`../../locales/${locale}.json`)).default,
+    messages: deepmerge(fallbackMessages, userMessages),
   };
 });
