ef34b7c8b09966ba3ef02af38098579ad2382c87
Livio Spring <livio.a@gmail.com>
2024-10-10 15:32:45 +0200

    fix unit date setup migration

--- cmd/setup/35.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE IF EXISTS projections.apps7_oidc_configs ADD COLUMN IF NOT EXISTS login_version SMALLINT;
-ALTER TABLE IF EXISTS projections.apps7_oidc_configs ADD COLUMN IF NOT EXISTS login_base_uri TEXT;
