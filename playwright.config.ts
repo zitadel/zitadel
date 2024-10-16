be84e9492696ddde6b807f459369bb20617e0969
Elio Bischof <elio@zitadel.com>
2024-10-16 18:22:55 +0200

    build with env

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
    webServer: {
      command: 'pnpm start',
      url: 'http://127.0.0.1:3000',
-     reuseExistingServer: false, //!process.env.CI,
+     reuseExistingServer: !process.env.CI,
      timeout: 5 * 60_000,
    },
 });
