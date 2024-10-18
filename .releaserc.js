4db57d723b58a825e1eac8d56cb27618a21e6470
Stefan Benz <46600784+stebenz@users.noreply.github.com>
2024-10-18 08:33:02 +0200

    chore: maintenance branch

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
+    { name: "v2.63.x", range: "2.63.x", channel: "2.63.x" },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
