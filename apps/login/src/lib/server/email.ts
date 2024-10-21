8a88e939e7c84c1ed3036033bf7ce08903da289a
peintnermax <max@caos.ch>
2024-10-21 17:16:22 +0200

    error handling

--- apps/login/src/lib/server/email.ts
+++ apps/login/src/lib/server/email.ts
@@ -16,11 +16,17 @@ type VerifyUserByEmailCommand = {
 
 export async function verifyUser(command: VerifyUserByEmailCommand) {
   const verifyResponse = command.isInvite
-    ? await verifyInviteCode(command.userId, command.code)
-    : await verifyEmail(command.userId, command.code);
+    ? await verifyInviteCode(command.userId, command.code).catch((error) => {
+        console.log(error.code);
+        return { error: "Could not verify invite" };
+      })
+    : await verifyEmail(command.userId, command.code).catch((error) => {
+        console.log(error.code);
+        return { error: "Could not verify email" };
+      });
 
   if (!verifyResponse) {
-    return { error: "Could not verify user email" };
+    return { error: "Could not verify user" };
   }
 
   const authMethodResponse = await listAuthenticationMethodTypes(
