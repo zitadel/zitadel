35ec0bac30762760c20ad818b062f58970932fd5
Max Peintner <max@caos.ch>
2024-10-24 14:25:33 +0200

    tests

--- apps/login/cypress/integration/verify.cy.ts
+++ apps/login/cypress/integration/verify.cy.ts
@@ -2,21 +2,40 @@ import { stub } from "../support/mock";
 
 describe("/verify", () => {
   it("shows password and passkey method after successful invite verification", () => {
-    stub("zitadel.user.v2.UserService", "VerifyEmail");
+    stub("zitadel.user.v2.UserService", "VerifyInviteCode");
     cy.visit("/verify?userId=123&code=abc&submit=true&invite=true");
     cy.location("pathname", { timeout: 10_000 }).should(
       "eq",
       "/authenticator/set",
     );
   });
-  it("shows an error if validation failed", () => {
-    stub("zitadel.user.v2.UserService", "VerifyEmail", {
+  it("shows an error if invite code validation failed", () => {
+    stub("zitadel.user.v2.UserService", "VerifyInviteCode", {
       code: 3,
       error: "error validating code",
     });
     // TODO: Avoid uncaught exception in application
     cy.once("uncaught:exception", () => false);
     cy.visit("/verify?userId=123&code=abc&submit=true&invite=true");
+    cy.contains("Could not verify user");
+  });
+
+  it("shows password and passkey method after successful invite verification", () => {
+    stub("zitadel.user.v2.UserService", "VerifyEmail");
+    cy.visit("/verify?userId=123&code=abc&submit=true");
+    cy.location("pathname", { timeout: 10_000 }).should(
+      "eq",
+      "/authenticator/set",
+    );
+  });
+  it("shows an error if invite code validation failed", () => {
+    stub("zitadel.user.v2.UserService", "VerifyEmail", {
+      code: 3,
+      error: "error validating code",
+    });
+    // TODO: Avoid uncaught exception in application
+    cy.once("uncaught:exception", () => false);
+    cy.visit("/verify?userId=123&code=abc&submit=true");
     cy.contains("Could not verify user");
   });
 });
