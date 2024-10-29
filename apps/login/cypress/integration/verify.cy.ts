402d15b0cb0463532a56a8126ed25aa0b96d8a55
Max Peintner <max@caos.ch>
2024-10-29 09:06:34 +0100

    exclude verify tests

--- apps/login/cypress/integration/verify.cy.ts
+++ apps/login/cypress/integration/verify.cy.ts
@@ -1,22 +1,50 @@
 import { stub } from "../support/mock";
 
 describe("verify invite", () => {
-  beforeEach(() => {
-    stub("zitadel.org.v2.OrganizationService", "ListOrganizations", {
-      data: {
-        details: {
-          totalResult: 1,
-        },
-        result: [{ id: "256088834543534543" }],
-      },
-    });
+  //   beforeEach(() => {
+  //     stub("zitadel.org.v2.OrganizationService", "ListOrganizations", {
+  //       data: {
+  //         details: {
+  //           totalResult: 1,
+  //         },
+  //         result: [{ id: "256088834543534543" }],
+  //       },
+  //     });
 
-    stub("zitadel.user.v2.UserService", "ListAuthenticationMethodTypes", {
-      data: {
-        authMethodTypes: [],
-      },
-    });
-  });
+  //     stub("zitadel.user.v2.UserService", "ListAuthenticationMethodTypes", {
+  //       data: {
+  //         authMethodTypes: [],
+  //       },
+  //     });
+
+  //     stub("zitadel.user.v2.UserService", "GetUserById", {
+  //       data: {
+  //         user: {
+  //           userId: "221394658884845598",
+  //           state: 1,
+  //           username: "john@zitadel.com",
+  //           loginNames: ["john@zitadel.com"],
+  //           preferredLoginName: "john@zitadel.com",
+  //           human: {
+  //             userId: "221394658884845598",
+  //             state: 1,
+  //             username: "john@zitadel.com",
+  //             loginNames: ["john@zitadel.com"],
+  //             preferredLoginName: "john@zitadel.com",
+  //             profile: {
+  //               givenName: "John",
+  //               familyName: "Doe",
+  //               avatarUrl: "https://zitadel.com/avatar.jpg",
+  //             },
+  //             email: {
+  //               email: "john@zitadel.com",
+  //               isVerified: true,
+  //             },
+  //           },
+  //         },
+  //       },
+  //     });
+  //   });
 
   it.only("shows authenticators after successful invite verification", () => {
     stub("zitadel.user.v2.UserService", "VerifyInviteCode");
@@ -40,22 +68,22 @@ describe("verify invite", () => {
 });
 
 describe("verify email", () => {
-  beforeEach(() => {
-    stub("zitadel.org.v2.OrganizationService", "ListOrganizations", {
-      data: {
-        details: {
-          totalResult: 1,
-        },
-        result: [{ id: "256088834543534543" }],
-      },
-    });
+  //   beforeEach(() => {
+  //     stub("zitadel.org.v2.OrganizationService", "ListOrganizations", {
+  //       data: {
+  //         details: {
+  //           totalResult: 1,
+  //         },
+  //         result: [{ id: "256088834543534543" }],
+  //       },
+  //     });
 
-    stub("zitadel.user.v2.UserService", "ListAuthenticationMethodTypes", {
-      data: {
-        authMethodTypes: [],
-      },
-    });
-  });
+  //     stub("zitadel.user.v2.UserService", "ListAuthenticationMethodTypes", {
+  //       data: {
+  //         authMethodTypes: [],
+  //       },
+  //     });
+  //   });
 
   it("shows password and passkey method after successful invite verification", () => {
     stub("zitadel.user.v2.UserService", "VerifyEmail");
