adaf3e56dfb52fd880922a07024c159a5f446550
Stefan Benz <46600784+stebenz@users.noreply.github.com>
2024-11-19 11:15:27 +0100

    chore: fixes to tests

--- acceptance/tests/username-password-changed.spec.ts
+++ acceptance/tests/username-password-changed.spec.ts
@@ -1,31 +1,31 @@
-import {test as base} from "@playwright/test";
+import { test as base } from "@playwright/test";
 import dotenv from "dotenv";
 import path from "path";
-import {loginWithPassword} from "./login";
-import {startChangePassword} from "./password";
-import {changePasswordScreen, changePasswordScreenExpect} from "./password-screen";
-import {PasswordUser} from "./user";
+import { loginWithPassword } from "./login";
+import { startChangePassword } from "./password";
+import { changePasswordScreen, changePasswordScreenExpect } from "./password-screen";
+import { PasswordUser } from "./user";
 
 // Read from ".env" file.
-dotenv.config({path: path.resolve(__dirname, ".env.local")});
+dotenv.config({ path: path.resolve(__dirname, ".env.local") });
 
 const test = base.extend<{ user: PasswordUser }>({
-    user: async ({page}, use) => {
-        const user = new PasswordUser({
-            email: "password-changed@example.com",
-            firstName: "first",
-            lastName: "last",
-            password: "Password1!",
-            organization: "",
-        });
-        await user.ensure(page);
-        await use(user);
-    },
+  user: async ({ page }, use) => {
+    const user = new PasswordUser({
+      email: "password-changed@example.com",
+      firstName: "first",
+      lastName: "last",
+      password: "Password1!",
+      organization: "",
+    });
+    await user.ensure(page);
+    await use(user);
+  },
 });
 
-test("username and password changed login", async ({user, page}) => {
-    // commented, fix in https://github.com/zitadel/zitadel/pull/8807
-    /*
+test("username and password changed login", async ({ user, page }) => {
+  // commented, fix in https://github.com/zitadel/zitadel/pull/8807
+  /*
     const changedPw = "ChangedPw1!";
     await loginWithPassword(page, user.getUsername(), user.getPassword());
 
@@ -40,11 +40,11 @@ test("username and password changed login", async ({user, page}) => {
      */
 });
 
-test("password not with desired complexity", async ({user, page}) => {
-    const changedPw1 = "change";
-    const changedPw2 = "chang";
-    await loginWithPassword(page, user.getUsername(), user.getPassword());
-    await startChangePassword(page, user.getUsername());
-    await changePasswordScreen(page, changedPw1, changedPw2);
-    await changePasswordScreenExpect(page, changedPw1, changedPw2, false, false, false, false, true, false);
+test("password not with desired complexity", async ({ user, page }) => {
+  const changedPw1 = "change";
+  const changedPw2 = "chang";
+  await loginWithPassword(page, user.getUsername(), user.getPassword());
+  await startChangePassword(page, user.getUsername());
+  await changePasswordScreen(page, changedPw1, changedPw2);
+  await changePasswordScreenExpect(page, changedPw1, changedPw2, false, false, false, false, true, false);
 });
