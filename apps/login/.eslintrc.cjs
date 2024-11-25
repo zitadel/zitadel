b48c25f7cdd986b0c05557dc6503e94007d918fd
Max Peintner <max@caos.ch>
2024-11-25 09:35:36 +0100

    lint

--- apps/login/.eslintrc.cjs
+++ apps/login/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next/babel", "next/core-web-vitals"],
+  extends: ["next/core-web-vitals"],
   ignorePatterns: ["external/**/*.ts"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
