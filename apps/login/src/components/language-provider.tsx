e964c7c0620ce51b310f7b8449e06a20d6cc633b
Max Peintner <max@caos.ch>
2024-11-25 09:19:58 +0100

    rm caching of i18n

--- apps/login/src/components/language-provider.tsx
+++ apps/login/src/components/language-provider.tsx
@@ -1,13 +1,8 @@
-"use cache";
-
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
-import { unstable_cacheLife as cacheLife } from "next/cache";
 import { ReactNode } from "react";
 
 export async function LanguageProvider({ children }: { children: ReactNode }) {
-  cacheLife("hours");
-
   const locale = await getLocale();
   const messages = await getMessages();
   return (
