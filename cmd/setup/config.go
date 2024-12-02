644765a2e531d5375b5d8aa4bea4bf5939a4bb2b
adlerhurst <silvan.reusser@gmail.com>
2024-12-02 17:07:55 +0100

    feat(fields): add instance domain

--- cmd/setup/config.go
+++ cmd/setup/config.go
@@ -127,6 +127,7 @@ type Steps struct {
 	s37Apps7OIDConfigsBackChannelLogoutURI  *Apps7OIDConfigsBackChannelLogoutURI
 	s38BackChannelLogoutNotificationStart   *BackChannelLogoutNotificationStart
 	s39DeleteStaleOrgFields                 *DeleteStaleOrgFields
+	s41FillFieldsForInstanceDomains         *FillFieldsForInstanceDomains
 }
 
 func MustNewSteps(v *viper.Viper) *Steps {
