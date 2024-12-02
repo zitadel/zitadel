c0a93944c30f1ed4f10b265bd0255d7ebe7f999a
Kim JeongHyeon <38876544+KimTibber@users.noreply.github.com>
2024-12-02 22:11:31 +0900

    feat(i18n): add korean language support (#8879)
    Hello everyone,
    
    To support Korean-speaking users who may experience challenges in using
    this excellent tool due to language barriers, I have added Korean
    language support with the help of ChatGPT.
    
    I hope that this contribution allows ZITADEL to be more useful and
    accessible to Korean-speaking users.
    
    Thank you.
    
    ---
    
    안녕하세요 여러분, 언어의 어려움으로 이 훌륭한 도구를 활용하는데 곤란함을 겪는 한국어 사용자들을 위하여 ChatGPT의 도움을
    받아 한국어 지원을 추가하였습니다.
    
    이 기여를 통해 ZITADEL이 한국어 사용자들에게 유용하게 활용되었으면 좋겠습니다.
    
    감사합니다.
    
    Co-authored-by: Max Peintner <max@caos.ch>

--- console/src/app/utils/language.ts
+++ console/src/app/utils/language.ts
@@ -16,6 +16,7 @@ export const supportedLanguages = [
   'nl',
   'sv',
   'hu',
+  'ko',
 ];
-export const supportedLanguagesRegexp: RegExp = /de|en|es|fr|id|it|ja|pl|zh|bg|pt|mk|cs|ru|nl|sv|hu/;
+export const supportedLanguagesRegexp: RegExp = /de|en|es|fr|id|it|ja|pl|zh|bg|pt|mk|cs|ru|nl|sv|hu|ko/;
 export const fallbackLanguage: string = 'en';
