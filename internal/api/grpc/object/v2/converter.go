91d9b0da96e1c281bbde384d3b5beaa8528761e7
conblem <mail@conblem.me>
2024-12-30 11:18:54 +0100

    try different import order

--- internal/api/grpc/object/v2/converter.go
+++ internal/api/grpc/object/v2/converter.go
@@ -2,7 +2,6 @@ package object
 
 import (
 	"context"
-	user_pb "github.com/zitadel/zitadel/pkg/grpc/user/v2"
 
 	"google.golang.org/protobuf/types/known/timestamppb"
 
@@ -10,6 +9,7 @@ import (
 	"github.com/zitadel/zitadel/internal/domain"
 	"github.com/zitadel/zitadel/internal/query"
 	"github.com/zitadel/zitadel/pkg/grpc/object/v2"
+	user_pb "github.com/zitadel/zitadel/pkg/grpc/user/v2"
 )
 
 func DomainToDetailsPb(objectDetail *domain.ObjectDetails) *object.Details {
