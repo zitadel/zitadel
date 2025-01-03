53bbbf1189b0666b676c85410334bb2a8220ccea
Max Peintner <peintnerm@gmail.com>
2025-01-03 13:03:56 +0100

    cleanup eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-zitadel`
+  // This tells ESLint to load the config from the package `@zitadel/eslint-config`
   extends: ["zitadel"],
   settings: {
     next: {
