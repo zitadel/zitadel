25d649e908b59aa92612dc7ee5ade18a3f36bf00
Max Peintner <peintnerm@gmail.com>
2025-01-03 13:26:19 +0100

    eslint extend

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `@zitadel/eslint-config`
-  extends: ["zitadel"],
+  extends: ["@zitadel/eslint-config"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
