ab6309c92f5ac65632b60d19e696fece8c54022b
Tim Möhlmann <tim+github@zitadel.com>
2025-01-06 15:33:05 +0200

    use plpgsql function

--- cmd/setup/40/postgres/02_func.sql
+++ cmd/setup/40/postgres/02_func.sql
@@ -1,69 +1,88 @@
-CREATE OR REPLACE FUNCTION eventstore.commands_to_events(commands eventstore.command[])
-    RETURNS SETOF eventstore.events2
-    STABLE
-    PARALLEL SAFE
-AS $$
-WITH cmds AS (
-    SELECT
-        c.instance_id
-        , c.aggregate_type
-        , c.aggregate_id
-        , c.command_type
-        , c.revision
-        , c.payload
-        , c.creator
-        , c.owner
-        , ROW_NUMBER() OVER () AS in_tx_order
-    FROM
-        UNNEST(commands) AS c
-), aggregates AS (
-	SELECT DISTINCT
-		c.instance_id
-        , c.aggregate_type
-        , c.aggregate_id
-	FROM cmds c
-), max_seq AS (
-    SELECT
-        e.instance_id
-        , e.aggregate_type
-        , e.aggregate_id
-		, e.owner
-        , MAX(e.sequence) sequence
-    FROM
-        eventstore.events2 e
-    JOIN
-        aggregates a
-    ON
-        e.instance_id = a.instance_id
-        AND e.aggregate_type = a.aggregate_type
-        AND e.aggregate_id = a.aggregate_id
-    GROUP BY
-        e.instance_id
-        , e.aggregate_type
-        , e.aggregate_id
-		, e.owner
+CREATE OR REPLACE FUNCTION eventstore.latest_aggregate_state(
+    instance_id TEXT
+    , aggregate_type TEXT
+    , aggregate_id TEXT
+    
+    , sequence OUT BIGINT
+    , owner OUT TEXT
 )
-SELECT
-    cmds.instance_id
-    , cmds.aggregate_type
-    , cmds.aggregate_id
-    , cmds.command_type AS event_type
-    , COALESCE(max_seq.sequence, 0) + ROW_NUMBER() OVER (PARTITION BY cmds.instance_id, cmds.aggregate_type, cmds.aggregate_id ORDER BY cmds.in_tx_order) AS sequence
-    , cmds.revision
-    , NOW() AS created_at
-    , cmds.payload
-    , cmds.creator
-	, COALESCE(max_seq.owner, cmds.owner)
-    , EXTRACT(EPOCH FROM NOW()) AS position
-    , cmds.in_tx_order
-FROM cmds
-LEFT JOIN max_seq 
-    ON cmds.instance_id = max_seq.instance_id
-    AND cmds.aggregate_type = max_seq.aggregate_type
-    AND cmds.aggregate_id = max_seq.aggregate_id
-ORDER BY
-    in_tx_order
-$$ LANGUAGE SQL;
+    LANGUAGE 'plpgsql'
+    STABLE PARALLEL SAFE
+AS $$
+    BEGIN
+        SELECT
+            COALESCE(e.sequence, 0) AS sequence
+            , e.owner
+        INTO
+            sequence
+            , owner
+        FROM
+            eventstore.events2 e
+        WHERE
+            e.instance_id = $1
+            AND e.aggregate_type = $2
+            AND e.aggregate_id = $3
+        ORDER BY 
+            e.sequence DESC
+        LIMIT 1;
+
+        RETURN;
+    END;
+$$;
+
+CREATE OR REPLACE FUNCTION eventstore.commands_to_events(commands eventstore.command[])
+    RETURNS SETOF eventstore.events2 
+    LANGUAGE 'plpgsql'
+    STABLE PARALLEL SAFE
+    ROWS 10
+AS $$
+DECLARE
+    "aggregate" RECORD;
+    current_sequence BIGINT;
+    current_owner TEXT;
+BEGIN
+    FOR "aggregate" IN 
+        SELECT DISTINCT
+            instance_id
+            , aggregate_type
+            , aggregate_id
+        FROM UNNEST(commands)
+    LOOP
+        SELECT 
+            * 
+        INTO
+            current_sequence
+            , current_owner 
+        FROM eventstore.latest_aggregate_state(
+            "aggregate".instance_id
+            , "aggregate".aggregate_type
+            , "aggregate".aggregate_id
+        );
+
+        RETURN QUERY
+        SELECT
+            c.instance_id
+            , c.aggregate_type
+            , c.aggregate_id
+            , c.command_type -- AS event_type
+            , COALESCE(current_sequence, 0) + ROW_NUMBER() OVER () -- AS sequence
+            , c.revision
+            , NOW() -- AS created_at
+            , c.payload
+            , c.creator
+            , COALESCE(current_owner, c.owner) -- AS owner
+            , EXTRACT(EPOCH FROM NOW()) -- AS position
+            , c.ordinality::INT -- AS in_tx_order
+        FROM
+            UNNEST(commands) WITH ORDINALITY AS c
+        WHERE
+            c.instance_id = aggregate.instance_id
+            AND c.aggregate_type = aggregate.aggregate_type
+            AND c.aggregate_id = aggregate.aggregate_id;
+    END LOOP;
+    RETURN;
+END;
+$$;
 
 CREATE OR REPLACE FUNCTION eventstore.push(commands eventstore.command[]) RETURNS SETOF eventstore.events2 VOLATILE AS $$
 INSERT INTO eventstore.events2
