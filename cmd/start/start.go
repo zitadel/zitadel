b58956ba8a2b34c7291eecd70b6584a523db9d4a
Livio Spring <livio.a@gmail.com>
2025-01-06 10:47:46 +0100

    fix(idp): prevent server errors for idps using form post for callbacks (#9097)
    # Which Problems Are Solved
    
    Some IdP callbacks use HTTP form POST to return their data on callbacks.
    For handling CSRF in the login after such calls, a 302 Found to the
    corresponding non form callback (in ZITADEL) is sent. Depending on the
    size of the initial form body, this could lead to ZITADEL terminating
    the connection, resulting in the user not getting a response or an
    intermediate proxy to return them an HTTP 502.
    
    # How the Problems Are Solved
    
    - the form body is parsed and stored into the ZITADEL cache (using the
    configured database by default)
    - the redirect (302 Found) is performed with the request id
    - the callback retrieves the data from the cache instead of the query
    parameters (will fallback to latter to handle open uncached requests)
    
    # Additional Changes
    
    - fixed a typo in the default (cache) configuration: `LastUsage` ->
    `LastUseAge`
    
    # Additional Context
    
    - reported by a customer
    - needs to be backported to current cloud version (2.66.x)
    
    ---------
    
    Co-authored-by: Silvan <27845747+adlerhurst@users.noreply.github.com>
    (cherry picked from commit fa5e590aabda38bd346f1a41484466aebdd8f903)

--- cmd/start/start.go
+++ cmd/start/start.go
@@ -316,6 +316,7 @@ func startZitadel(ctx context.Context, config *Config, masterKey string, server 
 		authZRepo,
 		keys,
 		permissionCheck,
+		cacheConnectors,
 	)
 	if err != nil {
 		return err
@@ -360,6 +361,7 @@ func startAPIs(
 	authZRepo authz_repo.Repository,
 	keys *encryption.EncryptionKeys,
 	permissionCheck domain.PermissionCheck,
+	cacheConnectors connector.Connectors,
 ) (*api.API, error) {
 	repo := struct {
 		authz_repo.Repository
@@ -541,6 +543,7 @@ func startAPIs(
 		keys.User,
 		keys.IDPConfig,
 		keys.CSRFCookieKey,
+		cacheConnectors,
 	)
 	if err != nil {
 		return nil, fmt.Errorf("unable to start login: %w", err)
