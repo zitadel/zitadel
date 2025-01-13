bae9163d79d0ba7df5e7fae03ef3279fbbca66ad
adlerhurst <27845747+adlerhurst@users.noreply.github.com>
2025-01-13 14:23:35 +0100

    cleanup

--- cmd/setup/45/cockroach/03_subscribed_events_table.sql
+++ cmd/setup/45/cockroach/03_subscribed_events_table.sql
@@ -1,11 +1,3 @@
-CREATE TABLE IF NOT EXISTS subscriptions.subscribers (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid()
-    , name TEXT NOT NULL
-    , should_notify BOOLEAN NOT NULL DEFAULT FALSE
-    , last_notified_position NUMERIC
-    , allow_reduce BOOLEAN NOT NULL DEFAULT FALSE
-);
-
 CREATE TABLE IF NOT EXISTS subscriptions.subscribed_events (
     subscriber UUID NOT NULL
     , instance_id TEXT -- if null susbcription is for all instances
