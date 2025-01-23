0a4c214e0ab7d1dcc241928b763b3e00725d995c
Iraq Jaber <iraq+github@zitadel.com>
2025-01-23 13:34:58 +0000

    fixup! fixup! fixup! fix: adding failed_attemps to grpc response when user enters wrong password

--- internal/api/grpc/gerrors/zitadel_errors_test.go
+++ internal/api/grpc/gerrors/zitadel_errors_test.go
@@ -154,7 +154,7 @@ func Test_Extract(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotC, gotMsg, gotID, gotOk := ExtractZITADELError(tt.args.err)
+			_, gotC, gotMsg, gotID, gotOk := ExtractZITADELError(tt.args.err)
 			if gotC != tt.wantC {
 				t.Errorf("extract() gotC = %v, want %v", gotC, tt.wantC)
 			}
