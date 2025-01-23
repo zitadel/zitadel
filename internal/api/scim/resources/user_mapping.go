88d8bee9110948bf4235e875efc20cbaaa924e3a
latonz <lars@riok.ch>
2025-01-23 16:36:52 +0100

    fix: scim use first email or phone if no primary is set

--- internal/api/scim/resources/user_mapping.go
+++ internal/api/scim/resources/user_mapping.go
@@ -136,6 +136,15 @@ func (h *UsersHandler) mapPrimaryEmail(scimUser *ScimUser) *command.Email {
 		}
 	}
 
+	// if no primary email was found, the first email will be used
+	for _, email := range scimUser.Emails {
+		email.Primary = true
+		return &command.Email{
+			Address:  domain.EmailAddress(email.Value),
+			Verified: h.config.EmailVerified,
+		}
+	}
+
 	return nil
 }
 
@@ -145,6 +154,15 @@ func (h *UsersHandler) mapPrimaryPhone(scimUser *ScimUser) *command.Phone {
 			continue
 		}
 
+		return &command.Phone{
+			Number:   domain.PhoneNumber(phone.Value),
+			Verified: h.config.PhoneVerified,
+		}
+	}
+
+	// if no primary phone was found, the first phone will be used
+	for _, phone := range scimUser.PhoneNumbers {
+		phone.Primary = true
 		return &command.Phone{
 			Number:   domain.PhoneNumber(phone.Value),
 			Verified: h.config.PhoneVerified,
