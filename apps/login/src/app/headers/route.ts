cc724889f0e27847bed3652f3a826317012fdf1c
Max Peintner <peintnerm@gmail.com>
2025-01-31 21:07:05 +0100

    headers endpoint

---
+++ apps/login/src/app/headers/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+export const revalidate = false;
+export const fetchCache = "default-no-store";
+
+export async function GET(request: NextRequest) {
+  const headers = request.headers;
+
+  // Convert headers to a plain object
+  const headersObject: Record<string, string> = {};
+  headers.forEach((value, key) => {
+    headersObject[key] = value;
+  });
+
+  return NextResponse.json(headersObject);
+}
