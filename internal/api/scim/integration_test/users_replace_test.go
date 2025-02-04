614823ef423e3468113595d1d24c801756279adf
latonz <lars@riok.ch>
2025-02-04 18:26:38 +0100

    fix: ensure metadata is projected for scim tests

--- internal/api/scim/integration_test/users_replace_test.go
+++ internal/api/scim/integration_test/users_replace_test.go
@@ -317,12 +317,7 @@ func TestReplaceUser_scopedExternalID(t *testing.T) {
 	require.NoError(t, err)
 
 	// set provisioning domain of service user
-	_, err = Instance.Client.Mgmt.SetUserMetadata(CTX, &management.SetUserMetadataRequest{
-		Id:    Instance.Users.Get(integration.UserTypeOrgOwner).ID,
-		Key:   "urn:zitadel:scim:provisioningDomain",
-		Value: []byte("fooBazz"),
-	})
-	require.NoError(t, err)
+	setProvisioningDomain(t, Instance.Users.Get(integration.UserTypeOrgOwner).ID, "fooBazz")
 
 	// replace the user with provisioning domain set
 	_, err = Instance.Client.SCIM.Users.Replace(CTX, Instance.DefaultOrg.Id, createdUser.ID, minimalUserWithExternalIDJson)
@@ -348,9 +343,5 @@ func TestReplaceUser_scopedExternalID(t *testing.T) {
 	_, err = Instance.Client.UserV2.DeleteUser(CTX, &user.DeleteUserRequest{UserId: createdUser.ID})
 	require.NoError(t, err)
 
-	_, err = Instance.Client.Mgmt.RemoveUserMetadata(CTX, &management.RemoveUserMetadataRequest{
-		Id:  Instance.Users.Get(integration.UserTypeOrgOwner).ID,
-		Key: "urn:zitadel:scim:provisioningDomain",
-	})
-	require.NoError(t, err)
+	removeProvisioningDomain(t, Instance.Users.Get(integration.UserTypeOrgOwner).ID)
 }
