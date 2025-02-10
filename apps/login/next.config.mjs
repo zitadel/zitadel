79b2180df5df381b951d55da8d8d4626975fd103
Elio Bischof <elio@zitadel.com>
2025-02-10 15:42:23 +0100

    docker image

--- apps/login/next.config.mjs
+++ apps/login/next.config.mjs
@@ -36,6 +36,7 @@ const secureHeaders = [
 
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  output: 'standalone',
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
     dynamicIO: true,
