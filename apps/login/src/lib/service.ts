9a3d90238ec355395bb05031a26c13923e8a70b7
Max Peintner <peintnerm@gmail.com>
2025-02-10 16:12:44 +0100

    log vars

--- apps/login/src/lib/service.ts
+++ apps/login/src/lib/service.ts
@@ -40,6 +40,11 @@ export async function createServiceForHost<T extends ServiceClass>(
   }
 
   if (!token) {
+    console.log(
+      process.env.AUDIENCE,
+      process.env.SYSTEM_USER_ID,
+      process.env.SYSTEM_USER_PRIVATE_KEY,
+    );
     throw new Error("No token found");
   }
 
