9a1e1f0aae51906452e528d5a95b346e8501daae
Silvan <27845747+adlerhurst@users.noreply.github.com>
2025-02-26 15:03:20 +0100

    docs(benchmarks): v2.70.0 (#9416)
    No benchmarks for v2.70.0 were provided so far.
    
    Benchmarks added
    
    - it's now possible to plot multiple charts, one chart per `metric_name`

--- docs/src/components/benchmark_chart.jsx
+++ docs/src/components/benchmark_chart.jsx
@@ -1,30 +1,25 @@
 import React from "react";
 import { Chart } from "react-google-charts";
 
-export function BenchmarkChart({ testResults = [], height = '500px' }) {
-    if (!Array.isArray(testResults)) {
-        console.error("Invalid testResults data:", testResults);
-        testResults = [];
-    }
+export function BenchmarkChart(testResults=[], height='500px') {
+    const dataPerMetric = new Map();
+    let maxVValue = 0;
 
-    const options = {
-        legend: { position: 'bottom' },
-        focusTarget: 'category',
-        hAxis: { title: 'timestamp' },
-        vAxis: { title: 'latency (ms)' },
-    };
-
-    const data = [
-        [
-            { type: "datetime", label: "timestamp" },
-            { type: "number", label: "p50" },
-            { type: "number", label: "p95" },
-            { type: "number", label: "p99" },
-        ],
-    ];
-
-    testResults.forEach((result) => {
-        data.push([
+    testResults.testResults.forEach((result) => {
+        if (!dataPerMetric.has(result.metric_name)) {
+            dataPerMetric.set(result.metric_name, [
+                [
+                    {type:"datetime", label: "timestamp"},
+                    {type:"number", label: "p50"},
+                    {type:"number", label: "p95"},
+                    {type:"number", label: "p99"},
+                ],
+            ]);
+        }
+        if (result.p99 > maxVValue) {
+            maxVValue = result.p99;
+        }
+        dataPerMetric.get(result.metric_name).push([
             new Date(result.timestamp),
             result.p50,
             result.p95,
@@ -32,14 +27,35 @@ export function BenchmarkChart({ testResults = [], height = '500px' }) {
         ]);
     });
 
-    return (
-        <Chart
-            chartType="LineChart"
-            width="100%"
-            height={height}
-            options={options}
-            data={data}
-            legendToggle
-        />
-    );
+    const options = {
+        legend: { position: 'bottom' },
+        focusTarget: 'category',
+        hAxis: {
+            title: 'timestamp',
+        },
+        vAxis: {
+            title: 'latency (ms)',
+            maxValue: maxVValue,
+        },
+        title: ''
+    };
+    const charts = [];
+    
+    dataPerMetric.forEach((data, metric) => {
+        const opt = Object.create(options);
+        opt.title = metric;
+        charts.push(
+            <Chart
+                chartType="LineChart"
+                width="100%"
+                height={height}
+                options={opt}
+                data={data}
+                legendToggle
+            />
+        );
+    });
+
+
+    return (charts);
 }
