bde038733870cd4160a2c785209cad80d9a6a5f4
adlerhurst <27845747+adlerhurst@users.noreply.github.com>
2025-02-27 09:04:43 +0100

    funcs

--- backend/repository/instance.go
+++ backend/repository/instance.go
@@ -13,17 +13,25 @@ type Instance struct {
 	Name string
 }
 
+type SetUpInstance func(ctx context.Context, instance *Instance) error
+
 type InstanceSetuper interface {
 	SetUp(ctx context.Context, instance *Instance) error
 }
+
+type InstanceByID func(ctx context.Context, id string) (*Instance, error)
 
 type instanceByIDQuerier interface {
 	ByID(ctx context.Context, id string) (*Instance, error)
 }
 
+type InstanceByDomain func(ctx context.Context, domain string) (*Instance, error)
+
 type instanceByDomainQuerier interface {
 	ByDomain(ctx context.Context, domain string) (*Instance, error)
 }
+
+type ListInstances func(ctx context.Context) ([]*Instance, error)
 
 type InstanceLister interface {
 	List(ctx context.Context) ([]*Instance, error)
