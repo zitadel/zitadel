42dfda533af50c9cd7d9f1494c9760cada47743a
Livio Spring <livio.a@gmail.com>
2025-03-03 13:09:40 +0100

    create maintenance branch

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
+    { name: "v2.69.x", range: "2.69.x", channel: "2.69.x" },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
