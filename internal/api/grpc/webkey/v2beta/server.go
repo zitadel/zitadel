5016ce45fe43d2d455ff533aa6d2a96b126d5a76
Livio Spring <livio.a@gmail.com>
2025-03-03 14:22:18 +0100

    fix renaming

--- internal/api/grpc/webkey/v2beta/server.go
+++ internal/api/grpc/webkey/v2beta/server.go
@@ -11,7 +11,7 @@ import (
 )
 
 type Server struct {
-	webkey.UnimplementedZITADELWebKeysServer
+	webkey.UnimplementedWebKeyServiceServer
 	command *command.Commands
 	query   *query.Queries
 }
@@ -27,21 +27,21 @@ func CreateServer(
 }
 
 func (s *Server) RegisterServer(grpcServer *grpc.Server) {
-	webkey.RegisterZITADELWebKeysServer(grpcServer, s)
+	webkey.RegisterWebKeyServiceServer(grpcServer, s)
 }
 
 func (s *Server) AppName() string {
-	return webkey.ZITADELWebKeys_ServiceDesc.ServiceName
+	return webkey.WebKeyService_ServiceDesc.ServiceName
 }
 
 func (s *Server) MethodPrefix() string {
-	return webkey.ZITADELWebKeys_ServiceDesc.ServiceName
+	return webkey.WebKeyService_ServiceDesc.ServiceName
 }
 
 func (s *Server) AuthMethods() authz.MethodMapping {
-	return webkey.ZITADELWebKeys_AuthMethods
+	return webkey.WebKeyService_AuthMethods
 }
 
 func (s *Server) RegisterGateway() server.RegisterGatewayFunc {
-	return webkey.RegisterZITADELWebKeysHandler
+	return webkey.RegisterWebKeyServiceHandler
 }
--- internal/integration/client.go
+++ internal/integration/client.go
@@ -64,7 +64,7 @@ type Client struct {
 	FeatureV2beta  feature_v2beta.FeatureServiceClient
 	FeatureV2      feature.FeatureServiceClient
 	UserSchemaV3   userschema_v3alpha.ZITADELUserSchemasClient
-	WebKeyV2Beta   webkey_v2beta.ZITADELWebKeysClient
+	WebKeyV2Beta   webkey_v2beta.WebKeyServiceClient
 	IDPv2          idp_pb.IdentityProviderServiceClient
 	UserV3Alpha    user_v3alpha.ZITADELUsersClient
 	SAMLv2         saml_pb.SAMLServiceClient
@@ -97,7 +97,7 @@ func newClient(ctx context.Context, target string) (*Client, error) {
 		FeatureV2beta:  feature_v2beta.NewFeatureServiceClient(cc),
 		FeatureV2:      feature.NewFeatureServiceClient(cc),
 		UserSchemaV3:   userschema_v3alpha.NewZITADELUserSchemasClient(cc),
-		WebKeyV2Beta:   webkey_v2beta.NewZITADELWebKeysClient(cc),
+		WebKeyV2Beta:   webkey_v2beta.NewWebKeyServiceClient(cc),
 		IDPv2:          idp_pb.NewIdentityProviderServiceClient(cc),
 		UserV3Alpha:    user_v3alpha.NewZITADELUsersClient(cc),
 		SAMLv2:         saml_pb.NewSAMLServiceClient(cc),
