2bfb336d495534596e887be4f15b489f0e74cfe7
adlerhurst <27845747+adlerhurst@users.noreply.github.com>
2025-03-04 15:13:10 +0100

    maybe this works

--- internal/database/postgres/embedded.go
+++ internal/database/postgres/embedded.go
@@ -1,33 +1,36 @@
 package postgres
 
 import (
-	"os"
-	"sync"
+	"sync/atomic"
 
 	embeddedpostgres "github.com/fergusstrange/embedded-postgres"
 	"github.com/zitadel/logging"
 )
 
 var (
-	port   uint16 = 5432
-	portMu        = sync.Mutex{}
+	runningTests atomic.Int32
+	embedded     *embeddedpostgres.EmbeddedPostgres
 )
 
 func StartEmbedded() (embeddedpostgres.Config, func()) {
-	tempPath, err := os.MkdirTemp("", "db")
-	logging.OnError(err).Fatal("unable to create temp dir")
+	runningCount := runningTests.Add(1)
+	config := embeddedpostgres.DefaultConfig().Version(embeddedpostgres.V16)
 
-	portMu.Lock()
-	logging.WithFields("port", port).Debug("starting embedded postgres")
-	config := embeddedpostgres.DefaultConfig().Version(embeddedpostgres.V16).RuntimePath(tempPath).Port(uint32(port))
-	port++
-	portMu.Unlock()
+	// postgres is already started if runningCount > 1
+	if runningCount > 1 {
+		return config, cleanup
+	}
 
-	psql := embeddedpostgres.NewDatabase(config)
-	err = psql.Start()
+	embedded = embeddedpostgres.NewDatabase(config)
+	err := embedded.Start()
 	logging.OnError(err).Fatal("unable to start db")
 
-	return config, func() {
-		logging.OnError(psql.Stop()).Error("unable to stop db")
+	return config, cleanup
+}
+
+func cleanup() {
+	if runningTests.Add(-1) > 0 {
+		return
 	}
+	logging.OnError(embedded.Stop()).Error("unable to stop db")
 }
