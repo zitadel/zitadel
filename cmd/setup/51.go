2947c0711361f56fec3a4462ec761e40966891f9
Iraq Jaber <IraqJaber@gmail.com>
2025-03-04 14:55:43 +0000

    fixup! fixup! feat(permissions): Addeding system user support for permission check v2

---
+++ cmd/setup/51.go
@@ -0,0 +1,38 @@
+package setup
+
+import (
+	"context"
+	"embed"
+	"fmt"
+
+	"github.com/zitadel/logging"
+
+	"github.com/zitadel/zitadel/internal/database"
+	"github.com/zitadel/zitadel/internal/eventstore"
+)
+
+type InitPermittedOrgsFunction struct {
+	eventstoreClient *database.DB
+}
+
+var (
+	//go:embed 51/*.sql
+	permittedOrgsFunction embed.FS
+)
+var nc (mig *InitPermittedOrgsFunction) Execute(ctx context.Context, _ eventstore.Event) error {
+	statements, err := readStatements(permittedOrgsFunction, "51", "")
+	if err != nil {
+		return err
+	}
+	for _, stmt := range statements {
+		logging.WithFields("file", stmt.file, "migration", mig.String()).Info("execute statement")
+		if _, err := mig.eventstoreClient.ExecContext(ctx, stmt.query); err != nil {
+			return fmt.Errorf("%s %s: %w", mig.String(), stmt.file, err)
+		}
+	}
+	return nil
+}
+
+func (*InitPermittedOrgsFunction) String() string {
+	return "51_init_permitted_orgs_function"
+}
