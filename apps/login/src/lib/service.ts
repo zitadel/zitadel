ef1c2a7425254132278086bbee1e9d36e3d0016f
Max Peintner <peintnerm@gmail.com>
2025-03-05 08:52:29 +0100

    nexturl protocol

--- apps/login/src/lib/service.ts
+++ apps/login/src/lib/service.ts
@@ -113,7 +113,7 @@ export function getServiceUrlFromHeaders(headers: ReadonlyHeaders): {
 export function constructUrl(request: NextRequest, path: string) {
   const forwardedProto = request.headers.get("x-forwarded-proto")
     ? `${request.headers.get("x-forwarded-proto")}:`
-    : request.protocol;
+    : request.nextUrl.protocol;
 
   const forwardedHost =
     request.headers.get("x-zitadel-forward-host") ??
