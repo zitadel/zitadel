2c1939744b3b7d58ff97ee6eb0fee37f342f039c
Thomas Faust <mail@thomas-hesse.eu>
2025-03-16 07:16:53 +0100

    refactor(zitadel-proto): restructure package files and update entry points

--- packages/zitadel-proto/tsup.config.ts
+++ packages/zitadel-proto/tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["index.ts"],
+  entry: ["src/index.ts", "src/v1.ts", "src/v2.ts", "src/v3alpha.ts"],
   dts: true,
   clean: true,
   minify: false,
   splitting: false,
+  treeshake: false,
   sourcemap: true,
   format: ["esm", "cjs"],
   platform: "neutral",
