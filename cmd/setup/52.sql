477ef4a3f797dd8ccacf37b79f60e32ee62b4e3e
Stefan Benz <46600784+stebenz@users.noreply.github.com>
2025-03-18 18:58:59 +0100

    fix: rename idp_templates6_ldap3 to _ldap2 if necessary

--- cmd/setup/52.sql
+++ cmd/setup/52.sql
@@ -1,13 +1,14 @@
-DO $$
+DO
+$$
     BEGIN
         IF NOT EXISTS
-            ( SELECT 1
-              FROM   information_schema.tables
-              WHERE  table_schema = 'projections'
-                AND    table_name = 'idp_templates6_ldap2'
-            )
+            (SELECT 1
+             FROM information_schema.tables
+             WHERE table_schema = 'projections'
+               AND table_name = 'idp_templates6_ldap2')
         THEN
+            ALTER TABLE IF EXISTS projections.idp_templates6_ldap3 RENAME COLUMN rootCA TO root_ca;
             ALTER TABLE IF EXISTS projections.idp_templates6_ldap3 RENAME TO idp_templates6_ldap2;
-        END IF ;
+        END IF;
     END
-$$ ;
+$$;
