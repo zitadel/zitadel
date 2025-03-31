90255ef7d8144a922a7357428cd317b95e41d17a
Livio Spring <livio.a@gmail.com>
2025-03-31 12:45:11 +0200

    fix(oauth): check key expiry on JWT Profile Grant
    # Which Problems Are Solved
    
    ZITADEL allows the use of JSON Web Token (JWT) Profile OAuth 2.0 for Authorization Grants in machine-to-machine (M2M) authentication. Multiple keys can be managed for a single machine account (service user), each with an individual expiry.
    
    A vulnerability existed where expired keys can be used to retrieve tokens. Specifically, ZITADEL fails to properly check the expiration date of the JWT key when used for Authorization Grants. This allows an attacker with an expired key to obtain valid access tokens.
    
    This vulnerability does not affect the use of JWT Profile for OAuth 2.0 Client Authentication on the Token and Introspection endpoints, which correctly reject expired keys.
    
    # How the Problems Are Solved
    
    Added proper validation of the expiry of the stored public key.
    
    # Additional Changes
    
    None
    
    # Additional Context
    
    None
    
    (cherry picked from commit 315503beabd679f2e6aec0c004f0f9d2f5b53ed3)

--- internal/api/oidc/integration_test/token_jwt_profile_test.go
+++ internal/api/oidc/integration_test/token_jwt_profile_test.go
@@ -23,7 +23,9 @@ import (
 func TestServer_JWTProfile(t *testing.T) {
 	t.Parallel()
 
-	user, name, keyData, err := Instance.CreateOIDCJWTProfileClient(CTX)
+	user, name, keyData, err := Instance.CreateOIDCJWTProfileClient(CTX, time.Hour)
+	require.NoError(t, err)
+	_, _, keyDataExpired, err := Instance.CreateOIDCJWTProfileClient(CTX, 10*time.Second)
 	require.NoError(t, err)
 
 	type claims struct {
@@ -106,6 +108,12 @@ func TestServer_JWTProfile(t *testing.T) {
 				resourceOwnerPrimaryDomain: Instance.DefaultOrg.PrimaryDomain,
 			},
 		},
+		{
+			name:    "key expired",
+			keyData: keyDataExpired,
+			scope:   []string{oidc.ScopeOpenID},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -125,6 +133,9 @@ func TestServer_JWTProfile(t *testing.T) {
 				},
 				time.Minute, time.Second,
 			)
+			if tt.wantErr {
+				return
+			}
 
 			provider, err := rp.NewRelyingPartyOIDC(CTX, Instance.OIDCIssuer(), "", "", redirectURI, tt.scope)
 			require.NoError(t, err)
