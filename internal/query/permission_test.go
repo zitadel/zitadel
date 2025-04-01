74e14cacdfe2520964e27c2e639d20f5d65f8a30
Tim Möhlmann <tim+github@zitadel.com>
2025-04-01 12:03:14 +0300

    feat(permission): search orgs uses v2

--- internal/query/permission_test.go
+++ internal/query/permission_test.go
@@ -10,19 +10,15 @@ import (
 
 	"github.com/zitadel/zitadel/internal/api/authz"
 	"github.com/zitadel/zitadel/internal/database"
+	domain_pkg "github.com/zitadel/zitadel/internal/domain"
+	"github.com/zitadel/zitadel/internal/feature"
 )
 
 func TestWherePermittedOrgs(t *testing.T) {
-	const (
-		instanceID = "instanceID"
-		orgID      = "orgID"
-		userID     = "userID"
-		permission = "permission1"
-	)
 	var permissions = []authz.SystemUserPermissions{
 		{
 			MemberType:  authz.MemberTypeOrganization,
-			AggregateID: orgID,
+			AggregateID: "orgID",
 			Permissions: []string{"permission1", "permission2"},
 		},
 		{
@@ -36,50 +32,97 @@ func TestWherePermittedOrgs(t *testing.T) {
 		SystemUserPermissions: permissions,
 	})
 
-	baseQuery := sq.Select("foo", "bar").From("users").Where(sq.Eq{"instance_id": instanceID})
+	type args struct {
+		ctx        context.Context
+		query      sq.SelectBuilder
+		enabled    bool
+		orgIDCol   Column
+		permission string
+		options    []PermittedOrgsOption
+	}
 	tests := []struct {
 		name      string
-		options   []PermittedOrgsOption
+		args      args
 		wantQuery string
 		wantArgs  []any
 	}{
 		{
-			name:      "no options",
+			name: "disabled",
+			args: args{
+				ctx:        ctx,
+				query:      sq.Select("foo", "bar").From("users").Where(sq.Eq{"instance_id": "instanceID"}),
+				enabled:    false,
+				orgIDCol:   UserResourceOwnerCol,
+				permission: "permission1",
+			},
+			wantQuery: "SELECT foo, bar FROM users WHERE instance_id = ?",
+			wantArgs:  []any{"instanceID"},
+		},
+		{
+			name: "no options",
+			args: args{
+				ctx:        ctx,
+				query:      sq.Select("foo", "bar").From("users").Where(sq.Eq{"instance_id": "instanceID"}),
+				enabled:    true,
+				orgIDCol:   UserResourceOwnerCol,
+				permission: "permission1",
+			},
 			wantQuery: "SELECT foo, bar FROM users WHERE instance_id = ? AND (projections.users14.resource_owner = ANY(eventstore.permitted_orgs(?, ?, ?, ?, ?)))",
-			wantArgs:  []any{instanceID, instanceID, userID, database.NewJSONArray(permissions), permission, ""},
+			wantArgs:  []any{"instanceID", "instanceID", "userID", database.NewJSONArray(permissions), "permission1", ""},
 		},
 		{
 			name: "owned rows option",
-			options: []PermittedOrgsOption{
-				OwnedRowsOrgOption(UserIDCol),
+			args: args{
+				ctx:        ctx,
+				query:      sq.Select("foo", "bar").From("users").Where(sq.Eq{"instance_id": "instanceID"}),
+				enabled:    true,
+				orgIDCol:   UserResourceOwnerCol,
+				permission: "permission1",
+				options: []PermittedOrgsOption{
+					OwnedRowsOrgOption(UserIDCol),
+				},
 			},
 			wantQuery: "SELECT foo, bar FROM users WHERE instance_id = ? AND (projections.users14.resource_owner = ANY(eventstore.permitted_orgs(?, ?, ?, ?, ?)) OR projections.users14.id = ?)",
-			wantArgs:  []any{instanceID, instanceID, userID, database.NewJSONArray(permissions), permission, "", userID},
+			wantArgs:  []any{"instanceID", "instanceID", "userID", database.NewJSONArray(permissions), "permission1", "", "userID"},
 		},
 		{
 			name: "override rows option",
-			options: []PermittedOrgsOption{
-				OwnedRowsOrgOption(UserIDCol),
-				OverrideOrgOption(UserStateCol, "bar"),
+			args: args{
+				ctx:        ctx,
+				query:      sq.Select("foo", "bar").From("users").Where(sq.Eq{"instance_id": "instanceID"}),
+				enabled:    true,
+				orgIDCol:   UserResourceOwnerCol,
+				permission: "permission1",
+				options: []PermittedOrgsOption{
+					OwnedRowsOrgOption(UserIDCol),
+					OverrideOrgOption(UserStateCol, "bar"),
+				},
 			},
 			wantQuery: "SELECT foo, bar FROM users WHERE instance_id = ? AND (projections.users14.resource_owner = ANY(eventstore.permitted_orgs(?, ?, ?, ?, ?)) OR projections.users14.id = ? OR projections.users14.state = ?)",
-			wantArgs:  []any{instanceID, instanceID, userID, database.NewJSONArray(permissions), permission, "", userID, "bar"},
+			wantArgs:  []any{"instanceID", "instanceID", "userID", database.NewJSONArray(permissions), "permission1", "", "userID", "bar"},
 		},
 		{
 			name: "single org option",
-			options: []PermittedOrgsOption{
-				SingleOrgOption([]SearchQuery{
-					mustSearchQuery(NewUserDisplayNameSearchQuery("zitadel", TextContains)),
-					mustSearchQuery(NewUserResourceOwnerSearchQuery(orgID, TextEquals)),
-				}),
+			args: args{
+				ctx:        ctx,
+				query:      sq.Select("foo", "bar").From("users").Where(sq.Eq{"instance_id": "instanceID"}),
+				enabled:    true,
+				orgIDCol:   UserResourceOwnerCol,
+				permission: "permission1",
+				options: []PermittedOrgsOption{
+					SingleOrgOption([]SearchQuery{
+						mustSearchQuery(NewUserDisplayNameSearchQuery("zitadel", TextContains)),
+						mustSearchQuery(NewUserResourceOwnerSearchQuery("orgID", TextEquals)),
+					}),
+				},
 			},
 			wantQuery: "SELECT foo, bar FROM users WHERE instance_id = ? AND (projections.users14.resource_owner = ANY(eventstore.permitted_orgs(?, ?, ?, ?, ?)))",
-			wantArgs:  []any{instanceID, instanceID, userID, database.NewJSONArray(permissions), permission, orgID},
+			wantArgs:  []any{"instanceID", "instanceID", "userID", database.NewJSONArray(permissions), "permission1", "orgID"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			query := WherePermittedOrgs(ctx, baseQuery, UserResourceOwnerCol, permission, tt.options...)
+			query := WherePermittedOrgs(tt.args.ctx, tt.args.query, tt.args.enabled, tt.args.orgIDCol, tt.args.permission, tt.args.options...)
 			gotQuery, gotArgs, err := query.ToSql()
 			require.NoError(t, err)
 			assert.Equal(t, tt.wantQuery, gotQuery)
@@ -93,4 +136,65 @@ func mustSearchQuery(q SearchQuery, err error) SearchQuery {
 		panic(err)
 	}
 	return q
+}
+
+func TestPermissionV2(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		cf  domain_pkg.PermissionCheck
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "feature disabled, no permission check",
+			args: args{
+				ctx: context.Background(),
+				cf:  nil,
+			},
+			want: false,
+		},
+		{
+			name: "feature enabled, no permission check",
+			args: args{
+				ctx: authz.WithFeatures(context.Background(), feature.Features{
+					PermissionCheckV2: true,
+				}),
+				cf: nil,
+			},
+			want: false,
+		},
+		{
+			name: "feature enabled, with permission check",
+			args: args{
+				ctx: authz.WithFeatures(context.Background(), feature.Features{
+					PermissionCheckV2: true,
+				}),
+				cf: func(context.Context, string, string, string) error {
+					return nil
+				},
+			},
+			want: true,
+		},
+		{
+			name: "feature disabled, with permission check",
+			args: args{
+				ctx: authz.WithFeatures(context.Background(), feature.Features{
+					PermissionCheckV2: false,
+				}),
+				cf: func(context.Context, string, string, string) error {
+					return nil
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := PermissionV2(tt.args.ctx, tt.args.cf)
+			assert.Equal(t, tt.want, got)
+		})
+	}
 }
