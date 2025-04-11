b2553f489b27043300d544e908120a3664e17e15
Elio Bischof <elio@zitadel.com>
2025-04-11 11:55:04 +0200

    iterable items

--- docs/src/utils/deprecated-items.js
+++ docs/src/utils/deprecated-items.js
@@ -4,7 +4,11 @@ import React from "react";
 // Deprecated items are pushed to the bottom of the list and its labels are given the CSS class zitadel-lifecycle-deprecated.
 // They are styled in docs/src/css/custom.css.
 export default function (props) {
-    const { items, ...rest } = props;
+    const { items = [], ...rest } = props;
+    if (!Array.isArray(items)) {
+        // Do nothing if items is not an array
+        return props;
+    }
     const withDeprecated = [...items].map(({className, label, ...itemRest}) => {
         const zitadelLifecycleDeprecated = className?.indexOf('menu__list-item--deprecated') > -1
         const wrappedLabel = <span className={zitadelLifecycleDeprecated ? "zitadel-lifecycle-deprecated" : undefined}>{label}</span>
