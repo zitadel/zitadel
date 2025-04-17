fec548845966f92f3a718e4bf9aef45ab21bcd59
Tim Möhlmann <tim+github@zitadel.com>
2025-04-17 12:59:35 +0300

    fix setup job

--- cmd/setup/53/03-permitted_projects_func.sql
+++ cmd/setup/53/03-permitted_projects_func.sql
@@ -1,6 +1,6 @@
 -- recreate the view to include the resource_owner
 CREATE OR REPLACE VIEW eventstore.project_members AS
-SELECT instance_id, resource_owner as org_id, aggregate_id as project_id, object_id as user_id, text_value as role
+SELECT instance_id, aggregate_id as project_id, object_id as user_id, text_value as role, resource_owner as org_id
 FROM eventstore.fields
 WHERE aggregate_type = 'project'
 AND object_type = 'project_member_role'
