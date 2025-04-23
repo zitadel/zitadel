56e0df67d59116bff9fe2b3e1000ad83add998d0
Ramon <mail@conblem.me>
2025-04-23 11:21:14 +0200

    feat: Actions V2 improvements in console (#9759)
    # Which Problems Are Solved
    This PR allows one to edit the order of Actions V2 Targets in an
    Execution. Editing of Targets was also added back again.
    
    # How the Problems Are Solved
    One of the changes is the addition of the CorrectlyTypedExecution which
    restricts the Grpc types a bit more to make working with them easier.
    Some fields may be optional in the Grpc Protobuf but in reality are
    always set.
    Typings were generally improved to make them more accurate and safer to
    work with.
    
    # Additional Changes
    Removal of the Actions V2 Feature flag as it will be enabled by default
    anyways.
    
    # Additional Context
    This pr used some advanced Angular Signals logic which is very
    interesting for future PR's.
    - Part of the tasks from #7248
    
    ---------
    
    Co-authored-by: Max Peintner <peintnerm@gmail.com>

--- console/src/app/modules/actions-two/actions-two-actions/actions-two-actions.component.ts
+++ console/src/app/modules/actions-two/actions-two-actions/actions-two-actions.component.ts
@@ -1,19 +1,20 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef } from '@angular/core';
 import { ActionService } from 'src/app/services/action.service';
-import { NewFeatureService } from 'src/app/services/new-feature.service';
-import { defer, firstValueFrom, Observable, of, shareReplay, Subject, TimeoutError } from 'rxjs';
-import { catchError, filter, map, startWith, switchMap, tap, timeout } from 'rxjs/operators';
+import { lastValueFrom, Observable, of, Subject } from 'rxjs';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
 import { ToastService } from 'src/app/services/toast.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ORGANIZATIONS } from '../../settings-list/settings';
-import { ActionTwoAddActionDialogComponent } from '../actions-two-add-action/actions-two-add-action-dialog.component';
+import {
+  ActionTwoAddActionDialogComponent,
+  ActionTwoAddActionDialogData,
+  ActionTwoAddActionDialogResult,
+  CorrectlyTypedExecution,
+  correctlyTypeExecution,
+} from '../actions-two-add-action/actions-two-add-action-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MessageInitShape } from '@bufbuild/protobuf';
-import { Execution, ExecutionSchema } from '@zitadel/proto/zitadel/action/v2beta/execution_pb';
 import { SetExecutionRequestSchema } from '@zitadel/proto/zitadel/action/v2beta/action_service_pb';
 import { Target } from '@zitadel/proto/zitadel/action/v2beta/target_pb';
-import { Value } from 'google-protobuf/google/protobuf/struct_pb';
 
 @Component({
   selector: 'cnsl-actions-two-actions',
@@ -21,123 +22,92 @@ import { Value } from 'google-protobuf/google/protobuf/struct_pb';
   styleUrls: ['./actions-two-actions.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ActionsTwoActionsComponent implements OnInit {
-  protected readonly refresh = new Subject<true>();
-  private readonly actionsEnabled$: Observable<boolean>;
-  protected readonly executions$: Observable<Execution[]>;
+export class ActionsTwoActionsComponent {
+  protected readonly refresh$ = new Subject<true>();
+  protected readonly executions$: Observable<CorrectlyTypedExecution[]>;
   protected readonly targets$: Observable<Target[]>;
 
   constructor(
     private readonly actionService: ActionService,
-    private readonly featureService: NewFeatureService,
     private readonly toast: ToastService,
     private readonly destroyRef: DestroyRef,
-    private readonly router: Router,
-    private readonly route: ActivatedRoute,
     private readonly dialog: MatDialog,
   ) {
-    this.actionsEnabled$ = this.getActionsEnabled$().pipe(shareReplay({ refCount: true, bufferSize: 1 }));
-    this.executions$ = this.getExecutions$(this.actionsEnabled$);
-    this.targets$ = this.getTargets$(this.actionsEnabled$);
+    this.executions$ = this.getExecutions$();
+    this.targets$ = this.getTargets$();
   }
 
-  ngOnInit(): void {
-    // this also preloads
-    this.actionsEnabled$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(async (enabled) => {
-      if (enabled) {
-        return;
-      }
-      await this.router.navigate([], {
-        relativeTo: this.route,
-        queryParams: {
-          id: ORGANIZATIONS.id,
-        },
-        queryParamsHandling: 'merge',
-      });
-    });
-  }
-
-  private getExecutions$(actionsEnabled$: Observable<boolean>) {
-    return this.refresh.pipe(
+  private getExecutions$() {
+    return this.refresh$.pipe(
       startWith(true),
       switchMap(() => {
         return this.actionService.listExecutions({});
       }),
-      map(({ result }) => result),
-      catchError(async (err) => {
-        const actionsEnabled = await firstValueFrom(actionsEnabled$);
-        if (actionsEnabled) {
-          this.toast.showError(err);
-        }
-        return [];
+      map(({ result }) => result.map(correctlyTypeExecution)),
+      catchError((err) => {
+        this.toast.showError(err);
+        return of([]);
       }),
     );
   }
 
-  private getTargets$(actionsEnabled$: Observable<boolean>) {
-    return this.refresh.pipe(
+  private getTargets$() {
+    return this.refresh$.pipe(
       startWith(true),
       switchMap(() => {
         return this.actionService.listTargets({});
       }),
       map(({ result }) => result),
-      catchError(async (err) => {
-        const actionsEnabled = await firstValueFrom(actionsEnabled$);
-        if (actionsEnabled) {
-          this.toast.showError(err);
-        }
-        return [];
-      }),
-    );
-  }
-
-  private getActionsEnabled$() {
-    return defer(() => this.featureService.getInstanceFeatures()).pipe(
-      map(({ actions }) => actions?.enabled ?? false),
-      timeout(1000),
       catchError((err) => {
-        if (!(err instanceof TimeoutError)) {
-          this.toast.showError(err);
-        }
-        return of(false);
+        this.toast.showError(err);
+        return of([]);
       }),
     );
   }
 
-  public openDialog(execution?: Execution): void {
-    const ref = this.dialog.open<ActionTwoAddActionDialogComponent>(ActionTwoAddActionDialogComponent, {
-      width: '400px',
-      data: execution
-        ? {
-            execution: execution,
-          }
-        : {},
-    });
+  public async openDialog(execution?: CorrectlyTypedExecution): Promise<void> {
+    const request$ = this.dialog
+      .open<ActionTwoAddActionDialogComponent, ActionTwoAddActionDialogData, ActionTwoAddActionDialogResult>(
+        ActionTwoAddActionDialogComponent,
+        {
+          width: '400px',
+          data: execution
+            ? {
+                execution,
+              }
+            : {},
+        },
+      )
+      .afterClosed()
+      .pipe(takeUntilDestroyed(this.destroyRef));
 
-    ref.afterClosed().subscribe((request?: MessageInitShape<typeof SetExecutionRequestSchema>) => {
-      if (request) {
-        this.actionService
-          .setExecution(request)
-          .then(() => {
-            setTimeout(() => {
-              this.refresh.next(true);
-            }, 1000);
-          })
-          .catch((error) => {
-            console.error(error);
-            this.toast.showError(error);
-          });
-      }
-    });
+    const request = await lastValueFrom(request$);
+    if (!request) {
+      return;
+    }
+
+    try {
+      await this.actionService.setExecution(request);
+      await new Promise((res) => setTimeout(res, 1000));
+      this.refresh$.next(true);
+    } catch (error) {
+      console.error(error);
+      this.toast.showError(error);
+    }
   }
 
-  public async deleteExecution(execution: Execution) {
+  public async deleteExecution(execution: CorrectlyTypedExecution) {
     const deleteReq: MessageInitShape<typeof SetExecutionRequestSchema> = {
       condition: execution.condition,
       targets: [],
     };
-    await this.actionService.setExecution(deleteReq);
-    await new Promise((res) => setTimeout(res, 1000));
-    this.refresh.next(true);
+    try {
+      await this.actionService.setExecution(deleteReq);
+      await new Promise((res) => setTimeout(res, 1000));
+      this.refresh$.next(true);
+    } catch (error) {
+      console.error(error);
+      this.toast.showError(error);
+    }
   }
 }
--- console/src/app/modules/actions-two/actions-two-targets/actions-two-targets-table/actions-two-targets-table.component.ts
+++ console/src/app/modules/actions-two/actions-two-targets/actions-two-targets-table/actions-two-targets-table.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, EventEmitter, Input, Output } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { filter, startWith } from 'rxjs/operators';
 import { MatTableDataSource } from '@angular/material/table';
 import { Target } from '@zitadel/proto/zitadel/action/v2beta/target_pb';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'cnsl-actions-two-targets-table',
@@ -15,6 +16,9 @@ export class ActionsTwoTargetsTableComponent {
   public readonly refresh = new EventEmitter<void>();
 
   @Output()
+  public readonly selected = new EventEmitter<Target>();
+
+  @Output()
   public readonly delete = new EventEmitter<Target>();
 
   @Input({ required: true })
@@ -22,12 +26,24 @@ export class ActionsTwoTargetsTableComponent {
     this.targets$.next(targets);
   }
 
-  @Output()
-  public readonly selected = new EventEmitter<Target>();
+  protected readonly targets$ = new ReplaySubject<Target[] | null>(1);
+  protected readonly dataSource: MatTableDataSource<Target>;
 
-  private readonly targets$ = new ReplaySubject<Target[] | null>(1);
-  protected readonly dataSource$ = this.targets$.pipe(
-    filter(Boolean),
-    map((keys) => new MatTableDataSource(keys)),
-  );
+  constructor() {
+    this.dataSource = this.getDataSource();
+  }
+
+  private getDataSource() {
+    const targets$ = this.targets$.pipe(filter(Boolean), startWith<Target[]>([]));
+    const targetsSignal = toSignal(targets$, { requireSync: true });
+
+    const dataSource = new MatTableDataSource(targetsSignal());
+    effect(() => {
+      const targets = targetsSignal();
+      if (dataSource.data !== targets) {
+        dataSource.data = targets;
+      }
+    });
+    return dataSource;
+  }
 }
