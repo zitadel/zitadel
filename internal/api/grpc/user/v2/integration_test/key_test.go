c8b3e60c4262b9ce6cba6312817d94464e2987a4
Elio Bischof <elio@zitadel.com>
2025-04-25 14:30:19 +0200

    test ListKeys

--- internal/api/grpc/user/v2/integration_test/key_test.go
+++ internal/api/grpc/user/v2/integration_test/key_test.go
@@ -5,14 +5,19 @@ package user_test
 import (
 	"context"
 	"fmt"
-	"google.golang.org/protobuf/types/known/timestamppb"
+	"slices"
 	"testing"
 	"time"
 
 	"github.com/brianvoe/gofakeit/v6"
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/testing/protocmp"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
+	"github.com/zitadel/zitadel/internal/integration"
+	"github.com/zitadel/zitadel/pkg/grpc/filter/v2"
 	"github.com/zitadel/zitadel/pkg/grpc/user/v2"
 )
 
@@ -316,4 +321,160 @@ func TestServer_RemoveKey_Permission(t *testing.T) {
 			assert.Less(t, creationDate, time.Now(), "creation date is in the future")
 		})
 	}
+}
+
+func TestServer_ListKeys(t *testing.T) {
+	/*	otherInstance := integration.NewInstance(SystemCTX)
+		otherInstanceUserId := otherInstance.CreateUserTypeMachine(SystemCTX).GetId()*/
+	otherOrg := Instance.CreateOrganization(SystemCTX, fmt.Sprintf("ListKeys-%s", gofakeit.AppName()), gofakeit.Email())
+	otherOrgUser, err := Client.CreateUser(SystemCTX, &user.CreateUserRequest{
+		OrganizationId: otherOrg.OrganizationId,
+		UserType: &user.CreateUserRequest_Machine_{
+			Machine: &user.CreateUserRequest_Machine{
+				Name: gofakeit.Name(),
+			},
+		},
+	})
+	require.NoError(t, err)
+	otherOrgUserId := otherOrgUser.GetId()
+	otherUserId := Instance.CreateUserTypeMachine(SystemCTX).GetId()
+	myOrgId := Instance.DefaultOrg.GetId()
+	myUserId := Instance.Users.Get(integration.UserTypeNoPermission).ID
+	expiresInADay := time.Now().Truncate(time.Second).Add(time.Hour * 24)
+	//	otherInstanceDataPoint := setupDataPoint(t, otherInstance.Client.UserV2, otherInstanceUserId, otherInstance.DefaultOrg.GetId(), expiresInADay)
+	otherOrgDataPoint := setupDataPoint(t, otherOrgUserId, otherOrg.OrganizationId, expiresInADay)
+	otherUserDataPoint := setupDataPoint(t, otherUserId, myOrgId, expiresInADay)
+	myDataPoint := setupDataPoint(t, myUserId, myOrgId, expiresInADay)
+	awaitKeys(t, otherOrgDataPoint.GetId(), otherUserDataPoint.GetId(), myDataPoint.GetId())
+	type args struct {
+		ctx context.Context
+		req *user.ListKeysRequest
+	}
+	tests := []struct {
+		name string
+		args args
+		want *user.ListKeysResponse
+	}{
+		{
+			name: "list all, instance",
+			args: args{
+				IamCTX,
+				&user.ListKeysRequest{},
+			},
+			want: &user.ListKeysResponse{
+				Result: []*user.Key{
+					//					otherInstanceDataPoint,
+					myDataPoint,
+					otherUserDataPoint,
+					otherOrgDataPoint,
+				},
+				Pagination: &filter.PaginationResponse{
+					TotalResult:  3,
+					AppliedLimit: 100,
+				},
+			},
+		},
+		{
+			name: "list all, org",
+			args: args{
+				CTX,
+				&user.ListKeysRequest{},
+			},
+			want: &user.ListKeysResponse{
+				Result: []*user.Key{
+					//					otherInstanceDataPoint,
+					myDataPoint,
+					otherUserDataPoint,
+				},
+				Pagination: &filter.PaginationResponse{
+					TotalResult:  2,
+					AppliedLimit: 100,
+				},
+			},
+		},
+		/*
+			{
+				name: "list all, user",
+			},
+			{
+				name: "list by id",
+			},
+			{
+				name: "list by multiple ids",
+			},
+			{
+				name: "list all from other instance",
+			},
+			{
+				name: "list all from other instance and org",
+			},
+			{
+				name: "sort by descending expiration date",
+			},
+			{
+				name: "get page",
+			},*/
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Client.ListKeys(tt.args.ctx, tt.args.req)
+			require.NoError(t, err)
+			assert.Len(t, got.Result, len(tt.want.Result))
+			if diff := cmp.Diff(got, tt.want, protocmp.Transform()); diff != "" {
+				t.Errorf("ListKeys() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func setupDataPoint(t *testing.T, userId, orgId string, expirationDate time.Time) *user.Key {
+	expirationDatePb := timestamppb.New(expirationDate)
+	newKey, err := Client.AddKey(SystemCTX, &user.AddKeyRequest{
+		UserId:         userId,
+		ExpirationDate: expirationDatePb,
+		PublicKey:      nil,
+	})
+	require.NoError(t, err)
+	return &user.Key{
+		CreationDate:   newKey.CreationDate,
+		ChangeDate:     newKey.CreationDate,
+		Id:             newKey.GetKeyId(),
+		UserId:         userId,
+		OrganizationId: orgId,
+		ExpirationDate: expirationDatePb,
+	}
+}
+
+func awaitKeys(t *testing.T, keyIds ...string) {
+	sortingColumn := user.KeyFieldName_KEY_FIELD_NAME_ID
+	slices.Sort(keyIds)
+	var filters []*user.KeysSearchFilter
+	for _, keyId := range keyIds {
+		filters = append(filters, &user.KeysSearchFilter{
+			Filter: &user.KeysSearchFilter_KeyIdFilter{
+				KeyIdFilter: &user.IDFilter{Id: keyId},
+			},
+		})
+	}
+	require.EventuallyWithT(t, func(collect *assert.CollectT) {
+		result, err := Client.ListKeys(SystemCTX, &user.ListKeysRequest{
+			Filters: []*user.KeysSearchFilter{{
+				Filter: &user.KeysSearchFilter_OrFilter{
+					OrFilter: &user.KeysOrFilter{Filters: filters},
+				},
+			}},
+			SortingColumn: &sortingColumn,
+			Pagination: &filter.PaginationRequest{
+				Asc: true,
+			},
+		})
+		require.NoError(t, err)
+		if !assert.Len(collect, result.Result, len(keyIds)) {
+			return
+		}
+		for i := range keyIds {
+			keyId := keyIds[i]
+			require.Equal(collect, keyId, result.Result[i].GetId())
+		}
+	}, 5*time.Second, time.Second, "key not created in time")
 }
