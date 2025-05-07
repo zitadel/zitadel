9df31eb9d6abc6ab4664a086180adf956c8b4174
Max Peintner <peintnerm@gmail.com>
2025-05-07 14:03:18 +0200

    fallback to openid scope

--- apps/login/src/components/consent.tsx
+++ apps/login/src/components/consent.tsx
@@ -43,28 +43,33 @@ export function ConsentScreen({
     }
   }
 
+  const scopes = scope?.filter((s) => !!s);
+
   return (
     <div className="pt-4 w-full flex flex-col items-center space-y-4">
       <ul className="list-disc space-y-2 w-full">
-        {scope
-          ?.filter((s) => !!s)
-          .map((s) => {
-            const translationKey = `device.scope.${s}`;
-            const description = t(translationKey, null);
+        {scopes?.length === 0 && (
+          <span className="w-full text-sm flex flex-row items-center bg-background-light-400 dark:bg-background-dark-400  border border-divider-light py-2 px-4 rounded-md transition-all">
+            {t("device.scope.openid")}
+          </span>
+        )}
+        {scopes?.map((s) => {
+          const translationKey = `device.scope.${s}`;
+          const description = t(translationKey, null);
 
-            // Check if the key itself is returned and provide a fallback
-            const resolvedDescription =
-              description === translationKey ? "" : description;
+          // Check if the key itself is returned and provide a fallback
+          const resolvedDescription =
+            description === translationKey ? "" : description;
 
-            return (
-              <li
-                key={s}
-                className="w-full text-sm flex flex-row items-center bg-background-light-400 dark:bg-background-dark-400  border border-divider-light py-2 px-4 rounded-md transition-all"
-              >
-                <span>{resolvedDescription}</span>
-              </li>
-            );
-          })}
+          return (
+            <li
+              key={s}
+              className="w-full text-sm flex flex-row items-center bg-background-light-400 dark:bg-background-dark-400  border border-divider-light py-2 px-4 rounded-md transition-all"
+            >
+              <span>{resolvedDescription}</span>
+            </li>
+          );
+        })}
       </ul>
 
       <p className="ztdl-p text-xs text-left">
