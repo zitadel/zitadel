66cbc5d18167837d7aa1b655d0a7db27722421f2
adlerhurst <27845747+adlerhurst@users.noreply.github.com>
2025-05-08 19:03:14 +0200

    doc

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,6 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-*/
+// huhu thanks for looking at this.
+// you can find more comments and doc.go files in the v3 package.
+// to get an overview i would start in the api package.
 package main
 
 import "github.com/zitadel/zitadel/backend/cmd"
