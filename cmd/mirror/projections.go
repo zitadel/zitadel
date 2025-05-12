5eafe8aff9e2b7a3b4f43729e05e3bd8ff495a36
Silvan <27845747+adlerhurst@users.noreply.github.com>
2025-05-12 15:58:23 +0200

    fix(mirror): skip notification projections (#9878)
    # Which Problems Are Solved
    
    During the mirror command execution we saw high wait times for
    notification projections.
    
    # How the Problems Are Solved
    
    As the events are skipped anyways because the notifications are sent out
    by the source Zitadel we skip the projections and just set the current
    state.

--- cmd/mirror/projections.go
+++ cmd/mirror/projections.go
@@ -257,7 +257,7 @@ func projections(
 	}()
 
 	for i := 0; i < int(config.Projections.ConcurrentInstances); i++ {
-		go execProjections(ctx, instances, failedInstances, &wg)
+		go execProjections(ctx, es, instances, failedInstances, &wg)
 	}
 
 	existingInstances := queryInstanceIDs(ctx, client)
@@ -273,7 +273,7 @@ func projections(
 	logging.WithFields("took", time.Since(start)).Info("projections executed")
 }
 
-func execProjections(ctx context.Context, instances <-chan string, failedInstances chan<- string, wg *sync.WaitGroup) {
+func execProjections(ctx context.Context, es *eventstore.Eventstore, instances <-chan string, failedInstances chan<- string, wg *sync.WaitGroup) {
 	for instance := range instances {
 		logging.WithFields("instance", instance).Info("starting projections")
 		ctx = internal_authz.WithInstanceID(ctx, instance)
@@ -299,7 +299,7 @@ func execProjections(ctx context.Context, instances <-chan string, failedInstanc
 			continue
 		}
 
-		err = notification.ProjectInstance(ctx)
+		err = notification.SetCurrentState(ctx, es)
 		if err != nil {
 			logging.WithFields("instance", instance).OnError(err).Info("trigger notification failed")
 			failedInstances <- instance
