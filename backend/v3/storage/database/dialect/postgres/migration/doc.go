f02e0f5cc74ffae1af5bf10921e84211b261b49f
adlerhurst <27845747+adlerhurst@users.noreply.github.com>
2025-05-22 11:01:57 +0200

    extend doc

--- backend/v3/storage/database/dialect/postgres/migration/doc.go
+++ backend/v3/storage/database/dialect/postgres/migration/doc.go
@@ -8,4 +8,6 @@
 // The file name MUST be in the format <sequence>_<name>.go.
 // Each migration SHOULD provide an up and down migration.
 // Prefer to write SQL statements instead of funcs if it is reasonable.
+// To keep the folder clean create a folder to store the sql files with the following format: <sequence>_<name>/{up/down}.sql.
+// And use the go embed directive to embed the sql files.
 package migration
