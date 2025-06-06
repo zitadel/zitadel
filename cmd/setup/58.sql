15c2ee392a57897911071a104cfc1d30793df8ed
adlerhurst <27845747+adlerhurst@users.noreply.github.com>
2025-06-06 09:17:10 +0200

    fast en seich passiert

--- cmd/setup/58.sql
+++ cmd/setup/58.sql
@@ -36,4 +36,4 @@ CREATE OR REPLACE VIEW projections.login_names3 AS
             AND u.instance_id = d.instance_id
 ;
 
-CREATE INDEX IF NOT EXISTS login_names3_policies_is_default_owner_idx ON projections.login_names3_policies (instance_id, is_default, resource_owner) INCLUDE (must_be_domain);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS login_names3_policies_is_default_owner_idx ON projections.login_names3_policies (instance_id, is_default, resource_owner) INCLUDE (must_be_domain);
