c41c1684155320e27493d5e28f8778e95cca3dbf
Iraq Jaber <iraq+github@zitadel.com>
2025-06-13 16:10:03 +0200

    chore(linting): changes to make clean-transactional-propsal_lint pass the linter

--- backend/v3/domain/set_email.go
+++ backend/v3/domain/set_email.go
@@ -1,74 +1,74 @@
 package domain
 
-import (
-	"context"
+// import (
+// 	"context"
 
-	"github.com/zitadel/zitadel/backend/v3/storage/eventstore"
-)
+// 	"github.com/zitadel/zitadel/backend/v3/storage/eventstore"
+// )
 
-// SetEmailCommand sets the email address of a user.
-// If allows verification as a sub command.
-// The verification command is executed after the email address is set.
-// The verification command is executed in the same transaction as the email address update.
-type SetEmailCommand struct {
-	UserID       string `json:"userId"`
-	Email        string `json:"email"`
-	verification Commander
-}
+// // SetEmailCommand sets the email address of a user.
+// // If allows verification as a sub command.
+// // The verification command is executed after the email address is set.
+// // The verification command is executed in the same transaction as the email address update.
+// type SetEmailCommand struct {
+// 	UserID       string `json:"userId"`
+// 	Email        string `json:"email"`
+// 	verification Commander
+// }
 
-var (
-	_ Commander      = (*SetEmailCommand)(nil)
-	_ eventer        = (*SetEmailCommand)(nil)
-	_ CreateHumanOpt = (*SetEmailCommand)(nil)
-)
+// var (
+// 	_ Commander      = (*SetEmailCommand)(nil)
+// 	_ eventer        = (*SetEmailCommand)(nil)
+// 	_ CreateHumanOpt = (*SetEmailCommand)(nil)
+// )
 
-type SetEmailOpt interface {
-	applyOnSetEmail(*SetEmailCommand)
-}
+// type SetEmailOpt interface {
+// 	applyOnSetEmail(*SetEmailCommand)
+// }
 
-func NewSetEmailCommand(userID, email string, verificationType SetEmailOpt) *SetEmailCommand {
-	cmd := &SetEmailCommand{
-		UserID: userID,
-		Email:  email,
-	}
-	verificationType.applyOnSetEmail(cmd)
-	return cmd
-}
+// func NewSetEmailCommand(userID, email string, verificationType SetEmailOpt) *SetEmailCommand {
+// 	cmd := &SetEmailCommand{
+// 		UserID: userID,
+// 		Email:  email,
+// 	}
+// 	verificationType.applyOnSetEmail(cmd)
+// 	return cmd
+// }
 
-// String implements [Commander].
-func (cmd *SetEmailCommand) String() string {
-	return "SetEmailCommand"
-}
+// // String implements [Commander].
+// func (cmd *SetEmailCommand) String() string {
+// 	return "SetEmailCommand"
+// }
 
-func (cmd *SetEmailCommand) Execute(ctx context.Context, opts *CommandOpts) error {
-	close, err := opts.EnsureTx(ctx)
-	if err != nil {
-		return err
-	}
-	defer func() { err = close(ctx, err) }()
-	// userStatement(opts.DB).Human().ByID(cmd.UserID).SetEmail(ctx, cmd.Email)
-	repo := userRepo(opts.DB).Human()
-	err = repo.Update(ctx, repo.IDCondition(cmd.UserID), repo.SetEmailAddress(cmd.Email))
-	if err != nil {
-		return err
-	}
+// func (cmd *SetEmailCommand) Execute(ctx context.Context, opts *CommandOpts) error {
+// 	close, err := opts.EnsureTx(ctx)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer func() { err = close(ctx, err) }()
+// 	// userStatement(opts.DB).Human().ByID(cmd.UserID).SetEmail(ctx, cmd.Email)
+// 	repo := userRepo(opts.DB).Human()
+// 	err = repo.Update(ctx, repo.IDCondition(cmd.UserID), repo.SetEmailAddress(cmd.Email))
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return opts.Invoke(ctx, cmd.verification)
-}
+// 	return opts.Invoke(ctx, cmd.verification)
+// }
 
-// Events implements [eventer].
-func (cmd *SetEmailCommand) Events() []*eventstore.Event {
-	return []*eventstore.Event{
-		{
-			AggregateType: "user",
-			AggregateID:   cmd.UserID,
-			Type:          "user.email.set",
-			Payload:       cmd,
-		},
-	}
-}
+// // Events implements [eventer].
+// func (cmd *SetEmailCommand) Events() []*eventstore.Event {
+// 	return []*eventstore.Event{
+// 		{
+// 			AggregateType: "user",
+// 			AggregateID:   cmd.UserID,
+// 			Type:          "user.email.set",
+// 			Payload:       cmd,
+// 		},
+// 	}
+// }
 
-// applyOnCreateHuman implements [CreateHumanOpt].
-func (cmd *SetEmailCommand) applyOnCreateHuman(createUserCmd *CreateUserCommand) {
-	createUserCmd.email = cmd
-}
+// // applyOnCreateHuman implements [CreateHumanOpt].
+// func (cmd *SetEmailCommand) applyOnCreateHuman(createUserCmd *CreateUserCommand) {
+// 	createUserCmd.email = cmd
+// }
--- backend/v3/storage/database/repository/user_human.go
+++ backend/v3/storage/database/repository/user_human.go
@@ -31,7 +31,6 @@ func (u *userHuman) GetEmail(ctx context.Context, condition database.Condition) 
 		&email.Address,
 		&email.VerifiedAt,
 	)
-
 	if err != nil {
 		return nil, err
 	}
@@ -189,18 +188,18 @@ func (h userHuman) PhoneVerifiedAtColumn() database.Column {
 	return database.NewColumn("phone_verified_at")
 }
 
-func (h userHuman) columns() database.Columns {
-	return append(h.user.columns(),
-		h.FirstNameColumn(),
-		h.LastNameColumn(),
-		h.EmailAddressColumn(),
-		h.EmailVerifiedAtColumn(),
-		h.PhoneNumberColumn(),
-		h.PhoneVerifiedAtColumn(),
-	)
-}
+// func (h userHuman) columns() database.Columns {
+// 	return append(h.user.columns(),
+// 		h.FirstNameColumn(),
+// 		h.LastNameColumn(),
+// 		h.EmailAddressColumn(),
+// 		h.EmailVerifiedAtColumn(),
+// 		h.PhoneNumberColumn(),
+// 		h.PhoneVerifiedAtColumn(),
+// 	)
+// }
 
-func (h userHuman) writeReturning(builder *database.StatementBuilder) {
-	builder.WriteString(" RETURNING ")
-	h.columns().Write(builder)
-}
+// func (h userHuman) writeReturning(builder *database.StatementBuilder) {
+// 	builder.WriteString(" RETURNING ")
+// 	h.columns().Write(builder)
+// }
