2af065ad0fef3034c5055e21b8130cd54449b6a6
Elio Bischof <elio@zitadel.com>
2025-06-29 11:23:35 +0200

    pat from file

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,8 +3,9 @@ set -o allexport
 . /.env-file/.env
 set +o allexport
 
-if [ -n "${ZITADEL_SERVICE_USER_TOKEN_PATH}" ] && [ -f "${ZITADEL_SERVICE_USER_TOKEN_PATH}" ]; then
-  export ZITADEL_SERVICE_USER_TOKEN=$(cat "${ZITADEL_SERVICE_USER_TOKEN_PATH}")
+if [ -n "${ZITADEL_SERVICE_USER_TOKEN_FILE}" ] && [ -f "${ZITADEL_SERVICE_USER_TOKEN_FILE}" ]; then
+  echo "ZITADEL_SERVICE_USER_TOKEN_FILE=${ZITADEL_SERVICE_USER_TOKEN_FILE} is set and file exists, setting ZITADEL_SERVICE_USER_TOKEN to the files content"
+  export ZITADEL_SERVICE_USER_TOKEN=$(cat "${ZITADEL_SERVICE_USER_TOKEN_FILE}")
 fi
 
 exec node apps/login/server.js
