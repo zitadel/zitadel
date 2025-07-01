e9e02e62b16cb98387cd7bf90be12c5d66100d76
Stefan Benz <46600784+stebenz@users.noreply.github.com>
2025-07-01 14:04:06 +0200

    fix: review changes

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -366,14 +366,6 @@ module.exports = {
               categoryLinkSource: "auto",
             },
           },
-          authorization_v2beta: {
-            specPath: ".artifacts/openapi/zitadel/authorization/v2beta/authorization_service.swagger.json",
-            outputDir: "docs/apis/resources/authorization_service_v2",
-            sidebarOptions: {
-              groupPathsBy: "tag",
-              categoryLinkSource: "auto",
-            },
-          },
         },
       },
     ],
