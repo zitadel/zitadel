8dfabe7f994c1d1e794152817170b7402e298740
Livio Spring <livio.a@gmail.com>
2025-07-02 11:26:37 +0200

    fix GH pipeline cache and plugin download

--- docs/plugin-download.sh
+++ docs/plugin-download.sh
@@ -3,6 +3,19 @@ echo $(uname -m)
 if [ "$(uname)" = "Darwin" ]; then
   curl -L -o protoc-gen-connect-openapi.tar.gz https://github.com/sudorandom/protoc-gen-connect-openapi/releases/download/v0.18.0/protoc-gen-connect-openapi_0.18.0_darwin_all.tar.gz
 else
-  curl -L -o protoc-gen-connect-openapi.tar.gz https://github.com/sudorandom/protoc-gen-connect-openapi/releases/download/v0.18.0/protoc-gen-connect-openapi_0.18.0_linux_$(uname -m).tar.gz
+  ARCH=$(uname -m)
+  case $ARCH in
+    x86_64)
+      ARCH="amd64"
+      ;;
+    aarch64|arm64)
+      ARCH="arm64"
+      ;;
+    *)
+      echo "Unsupported architecture: $ARCH"
+      exit 1
+      ;;
+  esac
+  curl -L -o protoc-gen-connect-openapi.tar.gz https://github.com/sudorandom/protoc-gen-connect-openapi/releases/download/v0.18.0/protoc-gen-connect-openapi_0.18.0_linux_${ARCH}.tar.gz
 fi
 tar -xvf protoc-gen-connect-openapi.tar.gz
