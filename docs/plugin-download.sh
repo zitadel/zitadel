521295b320e8e8bfdf834d9a3251a6f4feefa1d0
Livio Spring <livio.a@gmail.com>
2025-07-02 10:43:11 +0200

    fix plugin download

--- docs/plugin-download.sh
+++ docs/plugin-download.sh
@@ -1,6 +1,6 @@
 if [ "$(uname)" = "Darwin" ]; then
-  wget -O protoc-gen-connect-openapi.tar.gz https://github.com/sudorandom/protoc-gen-connect-openapi/releases/download/v0.18.0/protoc-gen-connect-openapi_0.18.0_darwin_all.tar.gz
+  curl -L -o protoc-gen-connect-openapi.tar.gz https://github.com/sudorandom/protoc-gen-connect-openapi/releases/download/v0.18.0/protoc-gen-connect-openapi_0.18.0_darwin_all.tar.gz
 else
-  wget -O protoc-gen-connect-openapi.tar.gz https://github.com/sudorandom/protoc-gen-connect-openapi/releases/download/v0.18.0/protoc-gen-connect-openapi_0.18.0_linux_$(uname -m).tar.gz
+  curl -L -o protoc-gen-connect-openapi.tar.gz https://github.com/sudorandom/protoc-gen-connect-openapi/releases/download/v0.18.0/protoc-gen-connect-openapi_0.18.0_linux_$(uname -m).tar.gz
 fi
 tar -xzvf protoc-gen-connect-openapi.tar.gz
