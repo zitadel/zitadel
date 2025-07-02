b1ce14cf6990480e36e29667d0fbcead74c3cadb
Livio Spring <livio.a@gmail.com>
2025-07-02 10:57:19 +0200

    fix extract

--- docs/plugin-download.sh
+++ docs/plugin-download.sh
@@ -3,4 +3,4 @@ if [ "$(uname)" = "Darwin" ]; then
 else
   curl -L -o protoc-gen-connect-openapi.tar.gz https://github.com/sudorandom/protoc-gen-connect-openapi/releases/download/v0.18.0/protoc-gen-connect-openapi_0.18.0_linux_$(uname -m).tar.gz
 fi
-tar -xzvf protoc-gen-connect-openapi.tar.gz
+tar -xvf protoc-gen-connect-openapi.tar.gz
