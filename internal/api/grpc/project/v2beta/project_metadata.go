bc57dfa663fca530a5633bb701d5f5f04f7591e7
Gregory Mironov <devmigregal@mail.ru>
2025-07-11 01:17:02 +0300

    feat: migrate new API to connectRPC

--- internal/api/grpc/project/v2beta/project_metadata.go
+++ internal/api/grpc/project/v2beta/project_metadata.go
@@ -3,51 +3,57 @@ package project
 import (
 	"context"
 
-	"github.com/zitadel/zitadel/internal/api/authz"
+	"connectrpc.com/connect"
 	metadata "github.com/zitadel/zitadel/internal/api/grpc/metadata/v2beta"
 	filter "github.com/zitadel/zitadel/pkg/grpc/filter/v2beta"
 	project_pb "github.com/zitadel/zitadel/pkg/grpc/project/v2beta"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func (s *Server) SetProjectMetadata(ctx context.Context, request *project_pb.SetProjectMetadataRequest) (*project_pb.SetProjectMetadataResponse, error) {
-	result, err := s.command.BulkSetProjectMetadata(ctx, request.GetProjectId(), authz.GetCtxData(ctx).OrgID, BulkSetProjectMetadataToDomain(request)...)
+func (s *Server) SetProjectMetadata(
+	ctx context.Context, request *connect.Request[project_pb.SetProjectMetadataRequest],
+) (*connect.Response[project_pb.SetProjectMetadataResponse], error) {
+	result, err := s.command.BulkSetProjectMetadata(ctx, request.Msg.GetProjectId(), "", BulkSetProjectMetadataToDomain(request.Msg)...)
 	if err != nil {
 		return nil, err
 	}
 
-	return &project_pb.SetProjectMetadataResponse{
+	return connect.NewResponse(&project_pb.SetProjectMetadataResponse{
 		SetDate: timestamppb.New(result.EventDate),
-	}, nil
+	}), nil
 }
 
-func (s *Server) ListProjectMetadata(ctx context.Context, request *project_pb.ListProjectMetadataRequest) (*project_pb.ListProjectMetadataResponse, error) {
-	metadataQueries, err := ListProjectMetadataToDomain(s.systemDefaults, request)
+func (s *Server) ListProjectMetadata(
+	ctx context.Context, request *connect.Request[project_pb.ListProjectMetadataRequest],
+) (*connect.Response[project_pb.ListProjectMetadataResponse], error) {
+	metadataQueries, err := ListProjectMetadataToDomain(s.systemDefaults, request.Msg)
 	if err != nil {
 		return nil, err
 	}
 
-	res, err := s.query.SearchProjectMetadata(ctx, true, request.GetProjectId(), metadataQueries, false)
+	res, err := s.query.SearchProjectMetadata(ctx, true, request.Msg.GetProjectId(), metadataQueries, false)
 	if err != nil {
 		return nil, err
 	}
 
-	return &project_pb.ListProjectMetadataResponse{
+	return connect.NewResponse(&project_pb.ListProjectMetadataResponse{
 		Metadata: metadata.ProjectMetadataListToPb(res.Metadata),
 		Pagination: &filter.PaginationResponse{
 			TotalResult:  res.Count,
-			AppliedLimit: uint64(request.GetPagination().GetLimit()),
+			AppliedLimit: uint64(request.Msg.GetPagination().GetLimit()),
 		},
-	}, nil
+	}), nil
 }
 
-func (s *Server) DeleteProjectMetadata(ctx context.Context, request *project_pb.DeleteProjectMetadataRequest) (*project_pb.DeleteProjectMetadataResponse, error) {
-	result, err := s.command.BulkRemoveProjectMetadata(ctx, request.GetProjectId(), authz.GetCtxData(ctx).OrgID, request.GetKeys()...)
+func (s *Server) DeleteProjectMetadata(
+	ctx context.Context, request *connect.Request[project_pb.DeleteProjectMetadataRequest],
+) (*connect.Response[project_pb.DeleteProjectMetadataResponse], error) {
+	result, err := s.command.BulkRemoveProjectMetadata(ctx, request.Msg.GetProjectId(), "", request.Msg.GetKeys()...)
 	if err != nil {
 		return nil, err
 	}
 
-	return &project_pb.DeleteProjectMetadataResponse{
+	return connect.NewResponse(&project_pb.DeleteProjectMetadataResponse{
 		DeletionDate: timestamppb.New(result.EventDate),
-	}, nil
+	}), nil
 }
