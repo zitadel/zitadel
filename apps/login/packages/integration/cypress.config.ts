e992610c3b801f83c2d25dbfd5e3aa1bb9669b0f
Elio Bischof <elio@zitadel.com>
2025-07-21 17:13:29 +0000

    integration

--- apps/login/packages/integration/cypress.config.ts
+++ apps/login/packages/integration/cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   reporter: "list",
 
   e2e: {
-    baseUrl: process.env.LOGIN_BASE_URL || "http://localhost:3000",
+    baseUrl: process.env.LOGIN_BASE_URL || "http://localhost:3001/ui/v2/login",
     specPattern: "integration/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "support/e2e.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
