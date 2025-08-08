16580df9eb23a70ac25b858055036ac9a403029a
Elio Bischof <elio@zitadel.com>
2025-08-08 14:38:37 +0200

    chore: increase login integration timeout

--- apps/login/cypress.config.ts
+++ apps/login/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   reporter: "list",
   video: true,
+  pageLoadTimeout: 2 * 60_000,
   e2e: {
     baseUrl: process.env.LOGIN_BASE_URL || "http://localhost:3001/ui/v2/login",
     specPattern: "integration/integration/**/*.cy.{js,jsx,ts,tsx}",
