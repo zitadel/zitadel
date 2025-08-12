47662e5ad45a91270cfe4ad67b7363b6eb4ae6ea
adlerhurst <27845747+adlerhurst@users.noreply.github.com>
2025-08-12 10:33:40 +0200

    test

--- internal/api/grpc/feature/v2/converter_test.go
+++ internal/api/grpc/feature/v2/converter_test.go
@@ -107,7 +107,7 @@ func Test_systemFeaturesToPb(t *testing.T) {
 			Source:  feature_pb.Source_SOURCE_SYSTEM,
 		},
 		ImprovedPerformance: &feature_pb.ImprovedPerformanceFeatureFlag{
-			ExecutionPaths: []feature_pb.ImprovedPerformance{feature_pb.ImprovedPerformance_IMPROVED_PERFORMANCE_PROJECT_GRANT},
+			ExecutionPaths: []feature_pb.ImprovedPerformance{feature_pb.ImprovedPerformance_IMPROVED_PERFORMANCE_ORG_DOMAIN_VERIFIED},
 			Source:         feature_pb.Source_SOURCE_SYSTEM,
 		},
 		OidcSingleV1SessionTermination: &feature_pb.FeatureFlag{
