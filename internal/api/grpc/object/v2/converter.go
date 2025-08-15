a722576cf129d20663e96e82e59ca1d7dd37a74f
Marco Ardizzone <marco@zitadel.com>
2025-08-15 17:02:27 +0200

    chore: move converter methods users v2 to separate converter package + add tests

--- internal/api/grpc/object/v2/converter.go
+++ internal/api/grpc/object/v2/converter.go
@@ -39,7 +39,7 @@ func ListQueryToQuery(query *object.ListQuery) (offset, limit uint64, asc bool) 
 	if query == nil {
 		return 0, 0, false
 	}
-	return query.Offset, uint64(query.Limit), query.Asc
+	return query.Offset, uint64(query.GetLimit()), query.GetAsc()
 }
 
 func ResourceOwnerFromReq(ctx context.Context, req *object.RequestContext) string {
