e7493f42d7d126a1db03184b60b4ce690ea3b4a2
Marco Ardizzone <marco@zitadel.com>
2025-08-18 17:41:31 +0200

    lint: remove systemDefaults from user v2beta Server

--- internal/api/grpc/user/v2beta/server.go
+++ internal/api/grpc/user/v2beta/server.go
@@ -10,7 +10,6 @@ import (
 	"github.com/zitadel/zitadel/internal/api/authz"
 	"github.com/zitadel/zitadel/internal/api/grpc/server"
 	"github.com/zitadel/zitadel/internal/command"
-	"github.com/zitadel/zitadel/internal/config/systemdefaults"
 	"github.com/zitadel/zitadel/internal/crypto"
 	"github.com/zitadel/zitadel/internal/domain"
 	"github.com/zitadel/zitadel/internal/query"
@@ -31,8 +30,6 @@ type Server struct {
 	assetAPIPrefix func(context.Context) string
 
 	checkPermission domain.PermissionCheck
-
-	systemDefaults systemdefaults.SystemDefaults
 }
 
 type Config struct{}
