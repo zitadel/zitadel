1c924f0e8368f056d0b2002b5a9d46087a5f9fd2
Silvan <27845747+adlerhurst@users.noreply.github.com>
2025-08-26 05:56:39 +0200

    Update backend/v3/storage/database/dialect/postgres/migration/003_domains_table/down.sql
    Co-authored-by: Tim Möhlmann <tim+github@zitadel.com>

--- backend/v3/storage/database/dialect/postgres/migration/003_domains_table/down.sql
+++ backend/v3/storage/database/dialect/postgres/migration/003_domains_table/down.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS zitadel.instance_domains;
 DROP TABLE IF EXISTS zitadel.org_domains;
+DROP TYPE IF EXISTS zitadel.domain_type;
 DROP TYPE IF EXISTS zitadel.domain_validation_type;
 DROP FUNCTION IF EXISTS zitadel.check_verified_org_domain();
+DROP FUNCTION IF EXISTS zitadel.ensure_single_primary_instance_domain();
