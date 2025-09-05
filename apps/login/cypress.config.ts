c4964a07bf30a6afcb8a299e0003165c0a1f733e
Elio Bischof <elio@zitadel.com>
2025-09-05 16:54:20 +0200

    fix start

--- apps/login/cypress.config.ts
+++ apps/login/cypress.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     baseUrl: process.env.LOGIN_BASE_URL || "http://localhost:3000/ui/v2/login",
     specPattern: "integration/integration/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "integration/support/e2e.{js,jsx,ts,tsx}",
+    env: {
+      CORE_MOCK_STUBS_URL: process.env.CORE_MOCK_STUBS_URL || "http://localhost:22220/v1/stubs"
+    },    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
