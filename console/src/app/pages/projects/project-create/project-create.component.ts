e0e9eea02b28411dd9da64d74eced4d9394fcdef
conblem <mail@conblem.me>
2025-09-05 11:21:27 +0200

    add project owner toggle

--- console/src/app/pages/projects/project-create/project-create.component.ts
+++ console/src/app/pages/projects/project-create/project-create.component.ts
@@ -7,6 +7,7 @@ import { Breadcrumb, BreadcrumbService, BreadcrumbType } from 'src/app/services/
 import { ToastService } from 'src/app/services/toast.service';
 import { NewMgmtService } from 'src/app/services/new-mgmt.service';
 import { UserService } from 'src/app/services/user.service';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'cnsl-project-create',
@@ -23,12 +24,15 @@ export class ProjectCreateComponent {
     ],
   };
 
+  protected readonly form: ReturnType<typeof this.buildForm>;
+
   constructor(
     private readonly router: Router,
     private readonly toast: ToastService,
     private readonly newMgmtService: NewMgmtService,
     private readonly _location: Location,
     private readonly userService: UserService,
+    private readonly fb: FormBuilder,
     breadcrumbService: BreadcrumbService,
   ) {
     const bread: Breadcrumb = {
@@ -36,11 +40,25 @@ export class ProjectCreateComponent {
       routerLink: ['/org'],
     };
     breadcrumbService.setBreadcrumb([bread]);
+
+    this.form = this.buildForm();
+  }
+
+  public buildForm() {
+    return this.fb.group({
+      name: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+      selfAccount: new FormControl(true, { nonNullable: true, validators: [Validators.required] }),
+    });
   }
 
   public saveProject(): void {
+    const { name, selfAccount } = this.form.getRawValue();
+
     this.newMgmtService
-      .addProject(this.project)
+      .addProject({
+        name,
+        admins: selfAccount ? [{ userId: this.userService.userId() }] : undefined,
+      })
       .then((resp) => {
         this.toast.showInfo('PROJECT.TOAST.CREATED', true);
         return this.router.navigate(['projects', resp.id], { queryParams: { new: true } });
