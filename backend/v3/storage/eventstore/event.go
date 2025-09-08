67a9ef697e40ead54f2e1b7bfc3118e64921d16f
adlerhurst <27845747+adlerhurst@users.noreply.github.com>
2025-09-08 10:54:59 +0200

    init

--- backend/v3/storage/eventstore/event.go
+++ backend/v3/storage/eventstore/event.go
@@ -2,8 +2,11 @@ package eventstore
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/zitadel/zitadel/backend/v3/storage/database"
+	legacy_db "github.com/zitadel/zitadel/internal/database"
+	"github.com/zitadel/zitadel/internal/eventstore"
 )
 
 type Event struct {
@@ -22,3 +25,41 @@ func Publish(ctx context.Context, events []*Event, db database.Executor) error {
 	}
 	return nil
 }
+
+func WriteLegacyEvents(ctx context.Context, es eventstore.Pusher, client database.QueryExecutor, commands ...eventstore.Command) error {
+	_, err := es.Push(ctx, LegacyContextQueryExecutorAdapter{client}, commands...)
+	return err
+}
+
+type LegacyContextQueryExecutorAdapter struct{ database.QueryExecutor }
+
+// ExecContext implements database.ContextQueryExecuter.
+func (l LegacyContextQueryExecutorAdapter) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
+	affected, err := l.QueryExecutor.Exec(ctx, query, args...)
+	return &sqlResult{rowsAffected: affected}, err
+}
+
+// QueryContext implements database.ContextQueryExecuter.
+func (l LegacyContextQueryExecutorAdapter) QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
+	rows, err := l.QueryExecutor.Query(ctx, query, args...)
+	return rows, err
+}
+
+var _ legacy_db.ContextQueryExecuter = (*LegacyContextQueryExecutorAdapter)(nil)
+
+type sqlResult struct {
+	rowsAffected int64
+}
+
+// LastInsertId implements [sql.Result].
+// Its never used in Zitadel so it always returns 0, nil.
+func (s *sqlResult) LastInsertId() (int64, error) {
+	return 0, nil
+}
+
+// RowsAffected implements [sql.Result].
+func (s *sqlResult) RowsAffected() (int64, error) {
+	return s.rowsAffected, nil
+}
+
+var _ sql.Result = (*sqlResult)(nil)
