0753ed2d6b648adbdd576859e2166c5337ec0f2f
Livio Spring <livio.a@gmail.com>
2025-09-08 18:30:03 +0200

    feat(service ping): add additional resource counts (#10621)
    # Which Problems Are Solved
    
    Using the service ping, we want to have some additional insights to how
    zitadel is configured. The current resource count report contains
    already some amount of configured policies, such as the login_policy.
    But we do not know if for example MFA is enforced.
    
    # How the Problems Are Solved
    
    - Added the following counts to the report:
      - service users per organization
      - MFA enforcements (though login policy)
      - Notification policies with password change option enabled
      - SCIM provisioned users (using user metadata)
    - Since all of the above are conditional based on at least a column
    inside a projection, a new `migration.CountTriggerConditional` has been
    added, where a condition (column values) and an option to track updates
    on that column should be considered for the count.
    - For this to be possible, the following changes had to be made to the
    existing sql resources:
    - the `resource_name` has been added to unique constraint on the
    `projection.resource_counts` table
    - triggers have been added / changed to individually track `INSERT`,
    `UPDATE`(s) and `DELETE` and be able to handle conditions
    - an optional argument has been added to the
    `projections.count_resource()` function to allow providing the
    information to `UP` or `DOWN` count the resource on an update.
    
    # Additional Changes
    
    None
    
    # Additional Context
    
    - partially solves #10244 (reporting audit log retention limit will be
    handled in #10245 directly)
    - backport to v4.x
    
    (cherry picked from commit 2dbe21fb300907ac430ad3badea1cda140b792a5)

--- cmd/setup/setup.go
+++ cmd/setup/setup.go
@@ -220,6 +220,7 @@ func Setup(ctx context.Context, config *Config, steps *Steps, masterKey string) 
 	steps.s60GenerateSystemID = &GenerateSystemID{eventstore: eventstoreClient}
 	steps.s61IDPTemplate6SAMLSignatureAlgorithm = &IDPTemplate6SAMLSignatureAlgorithm{dbClient: dbClient}
 	steps.s62HTTPProviderAddSigningKey = &HTTPProviderAddSigningKey{dbClient: dbClient}
+	steps.s63AlterResourceCounts = &AlterResourceCounts{dbClient: dbClient}
 
 	err = projection.Create(ctx, dbClient, eventstoreClient, config.Projections, nil, nil, nil)
 	logging.OnError(err).Fatal("unable to start projections")
@@ -270,6 +271,7 @@ func Setup(ctx context.Context, config *Config, steps *Steps, masterKey string) 
 		steps.s60GenerateSystemID,
 		steps.s61IDPTemplate6SAMLSignatureAlgorithm,
 		steps.s62HTTPProviderAddSigningKey,
+		steps.s63AlterResourceCounts,
 	} {
 		setupErr = executeMigration(ctx, eventstoreClient, step, "migration failed")
 		if setupErr != nil {
