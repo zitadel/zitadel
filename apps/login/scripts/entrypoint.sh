0261e1d06caf08f358dc00d456fe5b19621878dd
Elio Bischof <elio@zitadel.com>
2025-09-09 23:05:53 +0200

    independent dev and prod

--- apps/login/scripts/entrypoint.sh
+++ apps/login/scripts/entrypoint.sh
@@ -5,9 +5,13 @@ if [ -f /.env-file/.env ]; then
     set +o allexport
 fi
 
-if [ -n "${ZITADEL_SERVICE_USER_TOKEN_FILE}" ] && [ -f "${ZITADEL_SERVICE_USER_TOKEN_FILE}" ]; then
-    echo "ZITADEL_SERVICE_USER_TOKEN_FILE=${ZITADEL_SERVICE_USER_TOKEN_FILE} is set and file exists, setting ZITADEL_SERVICE_USER_TOKEN to the files content"
+if [ -n "${ZITADEL_SERVICE_USER_TOKEN_FILE}" ]; then
+    echo "ZITADEL_SERVICE_USER_TOKEN_FILE=${ZITADEL_SERVICE_USER_TOKEN_FILE} is set. Awaiting file and reading token."
+    while [ ! -f "${ZITADEL_SERVICE_USER_TOKEN_FILE}" ]; do
+        sleep 2
+    done
+    echo "token file found, reading token"
     export ZITADEL_SERVICE_USER_TOKEN=$(cat "${ZITADEL_SERVICE_USER_TOKEN_FILE}")
 fi
 
-exec node apps/login/server.js
+exec $@
