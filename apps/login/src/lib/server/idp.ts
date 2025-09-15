37d076cf8b50bcd454173bcb07cc111a9b233ec3
Max Peintner <peintnerm@gmail.com>
2025-09-15 11:24:29 +0200

    add mfa check after idp login

--- apps/login/src/lib/server/idp.ts
+++ apps/login/src/lib/server/idp.ts
@@ -3,6 +3,7 @@
 import {
   getLoginSettings,
   getUserByID,
+  listAuthenticationMethodTypes,
   startIdentityProviderFlow,
   startLDAPIdentityProviderFlow,
 } from "@/lib/zitadel";
@@ -10,15 +11,12 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { completeFlowOrGetUrl } from "../client";
 import { getServiceUrlFromHeaders } from "../service-url";
-import { checkEmailVerification } from "../verify-helper";
+import { checkEmailVerification, checkMFAFactors } from "../verify-helper";
 import { createSessionForIdpAndUpdateCookie } from "./cookie";
 
 export type RedirectToIdpState = { error?: string | null } | undefined;
 
-export async function redirectToIdp(
-  prevState: RedirectToIdpState,
-  formData: FormData,
-): Promise<RedirectToIdpState> {
+export async function redirectToIdp(prevState: RedirectToIdpState, formData: FormData): Promise<RedirectToIdpState> {
   const _headers = await headers();
   const { serviceUrl } = getServiceUrlFromHeaders(_headers);
   const host = _headers.get("host");
@@ -102,9 +100,7 @@ type CreateNewSessionCommand = {
   requestId?: string;
 };
 
-export async function createNewSessionFromIdpIntent(
-  command: CreateNewSessionCommand,
-) {
+export async function createNewSessionFromIdpIntent(command: CreateNewSessionCommand) {
   const _headers = await headers();
 
   const { serviceUrl } = getServiceUrlFromHeaders(_headers);
@@ -143,28 +139,41 @@ export async function createNewSessionFromIdpIntent(
     return { error: "Could not create session" };
   }
 
-  const humanUser =
-    userResponse.user.type.case === "human"
-      ? userResponse.user.type.value
-      : undefined;
+  const humanUser = userResponse.user.type.case === "human" ? userResponse.user.type.value : undefined;
 
   // check to see if user was verified
-  const emailVerificationCheck = checkEmailVerification(
-    session,
-    humanUser,
-    command.organization,
-    command.requestId,
-  );
+  const emailVerificationCheck = checkEmailVerification(session, humanUser, command.organization, command.requestId);
 
   if (emailVerificationCheck?.redirect) {
     return emailVerificationCheck;
   }
 
-  // TODO: check if user has MFA methods
-  // const mfaFactorCheck = checkMFAFactors(session, loginSettings, authMethods, organization, requestId);
-  // if (mfaFactorCheck?.redirect) {
-  //   return mfaFactorCheck;
-  // }
+  // check if user has MFA methods
+  let authMethods;
+  if (session.factors?.user?.id) {
+    const response = await listAuthenticationMethodTypes({
+      serviceUrl,
+      userId: session.factors.user.id,
+    });
+    if (response.authMethodTypes && response.authMethodTypes.length) {
+      authMethods = response.authMethodTypes;
+    }
+  }
+
+  if (authMethods) {
+    const mfaFactorCheck = await checkMFAFactors(
+      serviceUrl,
+      session,
+      loginSettings,
+      authMethods,
+      command.organization,
+      command.requestId,
+    );
+
+    if (mfaFactorCheck?.redirect) {
+      return mfaFactorCheck;
+    }
+  }
 
   const nextUrl = await completeFlowOrGetUrl(
     command.requestId && session.id
@@ -193,9 +202,7 @@ type createNewSessionForLDAPCommand = {
   link: boolean;
 };
 
-export async function createNewSessionForLDAP(
-  command: createNewSessionForLDAPCommand,
-) {
+export async function createNewSessionForLDAP(command: createNewSessionForLDAPCommand) {
   const _headers = await headers();
 
   const { serviceUrl } = getServiceUrlFromHeaders(_headers);
@@ -216,11 +223,7 @@ export async function createNewSessionForLDAP(
     password: command.password,
   });
 
-  if (
-    !response ||
-    response.nextStep.case !== "idpIntent" ||
-    !response.nextStep.value
-  ) {
+  if (!response || response.nextStep.case !== "idpIntent" || !response.nextStep.value) {
     return { error: "Could not start LDAP identity provider flow" };
   }
 
