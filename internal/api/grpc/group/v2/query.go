f7f56beb2197594d1d4646fae8e16714c9bf8a61
Gayathri Vijayan <gayathri+github@zitadel.com>
2025-09-19 16:58:53 +0200

    add user groups query-side and projection implementation

--- internal/api/grpc/group/v2/query.go
+++ internal/api/grpc/group/v2/query.go
@@ -6,31 +6,96 @@ import (
 	"connectrpc.com/connect"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	filter "github.com/zitadel/zitadel/internal/api/grpc/filter/v2"
 	"github.com/zitadel/zitadel/internal/query"
-	group "github.com/zitadel/zitadel/pkg/grpc/group/v2"
+	"github.com/zitadel/zitadel/internal/zerrors"
+	group_v2 "github.com/zitadel/zitadel/pkg/grpc/group/v2"
 )
 
 // ListGroups returns a list of groups that match the search criteria
-func (s *Server) ListGroups(ctx context.Context, req *connect.Request[group.ListGroupsRequest]) (*connect.Response[group.ListGroupsResponse], error) {
-	resp, err := s.query.SearchGroups(ctx)
+func (s *Server) ListGroups(ctx context.Context, req *connect.Request[group_v2.ListGroupsRequest]) (*connect.Response[group_v2.ListGroupsResponse], error) {
+	queries, err := s.listGroupsRequestToModel(req.Msg)
+	resp, err := s.query.SearchGroups(ctx, queries)
 	if err != nil {
 		return nil, err
 	}
-	return connect.NewResponse(&group.ListGroupsResponse{
+	return connect.NewResponse(&group_v2.ListGroupsResponse{
 		Groups: groupsToPb(resp.Groups),
 	}), nil
 }
 
-func groupsToPb(groups []*query.Group) []*group.Group {
-	pbGroups := make([]*group.Group, len(groups))
+func (s *Server) listGroupsRequestToModel(req *group_v2.ListGroupsRequest) (*query.GroupSearchQuery, error) {
+	offset, limit, asc, err := filter.PaginationPbToQuery(s.systemDefaults, req.GetPagination())
+	if err != nil {
+		return nil, err
+	}
+	queries, err := groupSearchFiltersToQuery(req.GetFilters())
+	if err != nil {
+		return nil, err
+	}
+	return &query.GroupSearchQuery{
+		SearchRequest: query.SearchRequest{
+			Offset:        offset,
+			Limit:         limit,
+			Asc:           asc,
+			SortingColumn: groupFieldNameToSortingColumn(req.SortingColumn),
+		},
+		Queries: queries,
+	}, nil
+}
+
+func groupSearchFiltersToQuery(filters []*group_v2.GroupsSearchFilter) (_ []query.SearchQuery, err error) {
+	q := make([]query.SearchQuery, len(filters))
+	for i, f := range filters {
+		q[i], err = groupFilterToQuery(f)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return q, nil
+}
+
+func groupFilterToQuery(f *group_v2.GroupsSearchFilter) (query.SearchQuery, error) {
+	switch q := f.Filter.(type) {
+	case *group_v2.GroupsSearchFilter_GroupIds:
+		return query.NewGroupIDsSearchQuery(q.GroupIds.GetIds())
+	case *group_v2.GroupsSearchFilter_NameQuery:
+		return query.NewGroupNameSearchQuery(q.NameQuery.GetName(), filter.TextMethodPbToQuery(q.NameQuery.GetMethod()))
+	case *group_v2.GroupsSearchFilter_OrganizationId:
+		return query.NewGroupOrganizationIdSearchQuery(q.OrganizationId.GetId())
+	default:
+		return nil, zerrors.ThrowInvalidArgument(nil, "GRPC-g3f4g", "List.Query.Invalid")
+	}
+}
+
+func groupFieldNameToSortingColumn(field *group_v2.FieldName) query.Column {
+	if field == nil {
+		return query.GroupColumnCreationDate
+	}
+	switch *field {
+	case group_v2.FieldName_FIELD_NAME_CREATION_DATE, group_v2.FieldName_FIELD_NAME_UNSPECIFIED:
+		return query.GroupColumnCreationDate
+	case group_v2.FieldName_FIELD_NAME_ID:
+		return query.GroupColumnID
+	case group_v2.FieldName_FIELD_NAME_NAME:
+		return query.GroupColumnName
+	case group_v2.FieldName_FIELD_NAME_CHANGE_DATE:
+		return query.GroupColumnChangeDate
+	default:
+		return query.GroupColumnCreationDate
+	}
+}
+
+func groupsToPb(groups []*query.Group) []*group_v2.Group {
+	pbGroups := make([]*group_v2.Group, len(groups))
 	for i, g := range groups {
 		pbGroups[i] = groupToPb(g)
 	}
 	return pbGroups
 }
 
-func groupToPb(g *query.Group) *group.Group {
-	return &group.Group{
+func groupToPb(g *query.Group) *group_v2.Group {
+	return &group_v2.Group{
 		Id:           g.ID,
 		Name:         g.Name,
 		CreationDate: timestamppb.New(g.CreationDate),
--- internal/query/group.go
+++ internal/query/group.go
@@ -2,9 +2,56 @@ package query
 
 import (
 	"context"
+	"database/sql"
 	"time"
 
+	sq "github.com/Masterminds/squirrel"
+
+	"github.com/zitadel/zitadel/internal/domain"
+	"github.com/zitadel/zitadel/internal/query/projection"
+	"github.com/zitadel/zitadel/internal/telemetry/tracing"
 	"github.com/zitadel/zitadel/internal/zerrors"
+)
+
+var (
+	groupsTable = table{
+		name:          projection.GroupProjectionTable,
+		instanceIDCol: projection.GroupColumnInstanceID,
+	}
+
+	GroupColumnID = Column{
+		name:  projection.GroupColumnID,
+		table: groupsTable,
+	}
+	GroupColumnName = Column{
+		name:  projection.GroupColumnName,
+		table: groupsTable,
+	}
+	GroupColumnDescription = Column{
+		name:  projection.GroupColumnDescription,
+		table: groupsTable,
+	}
+	GroupColumnResourceOwner = Column{
+		name: projection.GroupColumnResourceOwner,
+	}
+	GroupColumnCreationDate = Column{
+		name:  projection.GroupColumnCreationDate,
+		table: groupsTable,
+	}
+	GroupColumnChangeDate = Column{
+		name: projection.GroupColumnChangeDate,
+	}
+	GroupColumnInstanceID = Column{
+		name:  projection.GroupColumnInstanceID,
+		table: groupsTable,
+	}
+	GroupColumnSequence = Column{
+		name:  projection.GroupColumnSequence,
+		table: groupsTable,
+	}
+	GroupColumnState = Column{
+		name: projection.GroupColumnState,
+	}
 )
 
 type Groups struct {
@@ -19,9 +66,107 @@ type Group struct {
 	CreationDate  time.Time
 	ChangeDate    time.Time
 	ResourceOwner string
+	InstanceID    string
+	State         domain.GroupState
+	Sequence      uint64
+}
+
+type GroupSearchQuery struct {
+	SearchRequest
+	Queries []SearchQuery
 }
 
 // SearchGroups returns the list of groups that match the search criteria
-func (q *Queries) SearchGroups(ctx context.Context) (*Groups, error) {
-	return nil, zerrors.ThrowUnimplemented(nil, "QUERY-grpfli", "Not implemented")
+func (q *Queries) SearchGroups(ctx context.Context, queries *GroupSearchQuery) (_ *Groups, err error) {
+	ctx, span := tracing.NewSpan(ctx)
+	defer func() { span.EndWithError(err) }()
+
+	groups, err := q.searchGroups(ctx, queries)
+	if err != nil {
+		return nil, err
+	}
+	return groups, nil
+}
+
+func (q *Queries) searchGroups(ctx context.Context, queries *GroupSearchQuery) (groups *Groups, err error) {
+	ctx, span := tracing.NewSpan(ctx)
+	defer func() { span.EndWithError(err) }()
+
+	query, scan := prepareGroupsQuery()
+	stmt, args, err := queries.toQuery(query).ToSql()
+	if err != nil {
+		return nil, zerrors.ThrowInvalidArgument(err, "QUERY-FpBnrv", "Errors.Query.InvalidRequest")
+	}
+
+	err = q.client.QueryContext(ctx, func(rows *sql.Rows) error {
+		groups, err = scan(rows)
+		return err
+	}, stmt, args...)
+	if err != nil {
+		return nil, zerrors.ThrowInternal(err, "QUERY-vnQf5N", "Errors.Internal")
+	}
+	groups.State, err = q.latestState(ctx, groupsTable)
+	return groups, err
+}
+
+func prepareGroupsQuery() (sq.SelectBuilder, func(*sql.Rows) (*Groups, error)) {
+	return sq.Select(
+			GroupColumnID.identifier(),
+			GroupColumnName.identifier(),
+			GroupColumnDescription.identifier(),
+			GroupColumnCreationDate.identifier(),
+			GroupColumnChangeDate.identifier(),
+			GroupColumnResourceOwner.identifier(),
+			GroupColumnInstanceID.identifier(),
+			GroupColumnSequence.identifier(),
+			GroupColumnState.identifier(),
+		).From(groupsTable.identifier()).PlaceholderFormat(sq.Dollar),
+		func(rows *sql.Rows) (*Groups, error) {
+			groups := make([]*Group, 0)
+			var count uint64
+			for rows.Next() {
+				group := new(Group)
+				err := rows.Scan(
+					&group.ID,
+					&group.Name,
+					&group.Description,
+					&group.CreationDate,
+					&group.ChangeDate,
+					&group.ResourceOwner,
+					&group.InstanceID,
+					&group.Sequence,
+					&group.State,
+				)
+				if err != nil {
+					return nil, err
+				}
+				groups = append(groups, group)
+			}
+			if err := rows.Close(); err != nil {
+				return nil, zerrors.ThrowInternal(err, "QUERY-ndNVod", "Errors.Query.CloseRows")
+			}
+
+			return &Groups{
+				Groups: groups,
+				SearchResponse: SearchResponse{
+					Count: count,
+				},
+			}, nil
+		}
+}
+
+func NewGroupNameSearchQuery(value string, comparison TextComparison) (SearchQuery, error) {
+	return NewTextQuery(GroupColumnName, value, comparison)
+}
+
+func NewGroupIDsSearchQuery(ids []string) (SearchQuery, error) {
+	list := make([]interface{}, len(ids))
+	for i, value := range ids {
+		list[i] = value
+	}
+	return NewListQuery(GroupColumnID, list, ListIn)
+}
+
+func NewGroupOrganizationIdSearchQuery(id string) (SearchQuery, error) {
+	return NewTextQuery(GroupColumnResourceOwner, id, TextEquals)
 }
