30ff272c73c8451216b21e7447cd8d3106b98eda
Elio Bischof <elio@zitadel.com>
2025-09-24 00:23:56 +0200

    increase pageLoadTimeout

--- apps/login/cypress.config.ts
+++ apps/login/cypress.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     baseUrl: `http://localhost:3000${process.env.NEXT_PUBLIC_BASE_PATH || ""}`,
     specPattern: "integration/integration/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "integration/support/e2e.{js,jsx,ts,tsx}",
+    pageLoadTimeout: 120_0000,
     env: {
       API_MOCK_STUBS_URL: process.env.API_MOCK_STUBS_URL || "http://localhost:22220/v1/stubs"
     },
