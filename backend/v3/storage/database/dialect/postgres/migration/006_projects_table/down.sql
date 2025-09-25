82eb036be198b1f9ff1a4d68eef19ab8cd308449
Tim Möhlmann <tim+github@zitadel.com>
2025-09-25 10:50:41 +0300

    feat(rt): project migrations

---
+++ backend/v3/storage/database/dialect/postgres/migration/006_projects_table/down.sql
@@ -0,0 +1,5 @@
+DROP TRIGGER trigger_set_updated_at ON zitadel.project_roles;
+DROP TRIGGER trigger_set_updated_at ON zitadel.projects;
+DROP TABLE zitadel.project_roles;
+DROP TABLE zitadel.projects;
+DROP TYPE zitadel.project_state;
---
+++ backend/v3/storage/database/dialect/postgres/migration/006_projects_table/up.sql
@@ -0,0 +1,59 @@
+CREATE TYPE zitadel.project_state AS ENUM (
+    'ACTIVE',
+    'INACTIVE'
+);
+
+CREATE TABLE zitadel.projects(
+    instance_id TEXT NOT NULL
+    , org_id TEXT NOT NULL
+    , id TEXT NOT NULL
+
+    , created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    , updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+
+    , name TEXT NOT NULL CHECK(LENGTH(name) > 0)
+    , state zitadel.project_state NOT NULL
+    -- API: project_role_assertion
+    , should_assert_role BOOLEAN NOT NULL DEFAULT FALSE
+    -- API: authorization_required
+    , is_authorization_required BOOLEAN NOT NULL DEFAULT FALSE
+    -- API: project_access_required
+    , is_project_access_required BOOLEAN NOT NULL DEFAULT FALSE
+    --API: private_labeling_setting
+    , used_labeling_setting_owner SMALLINT
+
+    , PRIMARY KEY (instance_id, org_id, id)
+    , FOREIGN KEY (instance_id, org_id) REFERENCES zitadel.organizations(instance_id, id) ON DELETE CASCADE
+);
+
+CREATE TABLE zitadel.project_roles(
+    instance_id TEXT NOT NULL
+    , org_id TEXT NOT NULL
+    , project_id TEXT NOT NULL
+
+    , created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    , updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+
+    -- API: role_key
+    , key TEXT NOT NULL CHECK(LENGTH(key) > 0)
+    -- API: display_name
+    , display_name TEXT NOT NULL CHECK(LENGTH(display_name) > 0)
+    -- API: group
+    -- group is a reserved keyword in PostgreSQL
+    , role_group TEXT
+
+    , PRIMARY KEY (instance_id, org_id, project_id, key)
+    , FOREIGN KEY (instance_id, org_id, project_id) REFERENCES zitadel.projects(instance_id, org_id, id) ON DELETE CASCADE
+);
+
+CREATE TRIGGER trigger_set_updated_at
+BEFORE UPDATE ON zitadel.projects
+FOR EACH ROW
+WHEN (NEW.updated_at IS NULL)
+EXECUTE FUNCTION zitadel.set_updated_at();
+
+CREATE TRIGGER trigger_set_updated_at
+BEFORE UPDATE ON zitadel.project_roles
+FOR EACH ROW
+WHEN (NEW.updated_at IS NULL)
+EXECUTE FUNCTION zitadel.set_updated_at();
