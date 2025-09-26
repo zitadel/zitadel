0558a467c675264aa1f9a1beec88a7f76112a7a8
Elio Bischof <elio@zitadel.com>
2025-09-26 20:07:16 +0200

    fix workspace root

--- .devcontainer/compose.sh
+++ .devcontainer/compose.sh
@@ -2,5 +2,5 @@
 
 # This script is used to run Docker Compose commands with the appropriate compose project set.
 # This makes sure the commands don't break on different workspace_root folder names.
-export DEVCONTAINER_LOCAL_WORKSPACE_ROOT=${DEVCONTAINER_LOCAL_WORKSPACE_ROOT:-$WORKSPACE_ROOT}
+export DEVCONTAINER_LOCAL_WORKSPACE_ROOT=${DEVCONTAINER_LOCAL_WORKSPACE_ROOT:-$NX_WORKSPACE_ROOT}
 docker compose -p ${DEVCONTAINER_LOCAL_WORKSPACE_ROOT##*/}_devcontainer "$@"
