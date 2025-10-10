6600efe48df976db949a154653467ca0c2a53fb5
Gayathri Vijayan <gayathri+github@zitadel.com>
2025-10-10 17:10:14 +0200

    add functionality to add users to groups

--- internal/domain/group.go
+++ internal/domain/group.go
@@ -11,3 +11,15 @@ const (
 func (g GroupState) Exists() bool {
 	return g != GroupStateRemoved && g != GroupStateUnspecified
 }
+
+type GroupUserState int32
+
+const (
+	GroupUserStateUnspecified GroupUserState = iota
+	GroupUserStateActive
+	GroupUserStateRemoved
+)
+
+func (g GroupUserState) Exists() bool {
+	return g != GroupUserStateRemoved && g != GroupUserStateUnspecified
+}
