552dbb1b7213bb99c0554ba3bc1f0b292027cb46
Livio Spring <livio.a@gmail.com>
2025-10-10 15:00:59 +0200

    test dry run

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
+    { name: "ci-pack", prerelease: "rc" },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
