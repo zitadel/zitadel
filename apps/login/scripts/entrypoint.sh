744db4f4c275b8537c35457bdfc2521c8e5f47ee
Stefan Benz <46600784+stebenz@users.noreply.github.com>
2025-10-15 18:07:03 +0200

    fix: add handling of system user file to entrypoint

--- apps/login/scripts/entrypoint.sh
+++ apps/login/scripts/entrypoint.sh
@@ -6,6 +6,15 @@ if [ -f /.env-file/.env ]; then
     set +o allexport
 fi
 
+if [ -n "${SYSTEM_USER_PRIVATE_KEY_FILE}" ]; then
+    echo "SYSTEM_USER_PRIVATE_KEY_FILE=${SYSTEM_USER_PRIVATE_KEY_FILE} is set. Awaiting file and reading token."
+    while [ ! -f "${SYSTEM_USER_PRIVATE_KEY_FILE}" ]; do
+        sleep 2
+    done
+    echo "private key file found, reading private key"
+    export SYSTEM_USER_PRIVATE_KEY=$(cat "${SYSTEM_USER_PRIVATE_KEY_FILE}")
+fi
+
 if [ -n "${ZITADEL_SERVICE_USER_TOKEN_FILE}" ]; then
     echo "ZITADEL_SERVICE_USER_TOKEN_FILE=${ZITADEL_SERVICE_USER_TOKEN_FILE} is set. Awaiting file and reading token."
     while [ ! -f "${ZITADEL_SERVICE_USER_TOKEN_FILE}" ]; do
