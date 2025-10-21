64e086b3147e355c2c545235b2a257e30415fa7f
Elio Bischof <elio@zitadel.com>
2025-10-21 10:14:43 +0200

    chore: release archives

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
   branches: [
     { name: "next" },
     { name: "next-rc", prerelease: "rc" },
+    { name: "release-archives", prerelease: true },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
