69658edf49060b9bf8512c276a7763ce0122e767
conblem <mail@conblem.me>
2025-10-21 14:58:45 +0200

    improve date time pipe

--- console/src/app/pipes/timestamp-to-date-pipe/timestamp-to-date.pipe.ts
+++ console/src/app/pipes/timestamp-to-date-pipe/timestamp-to-date.pipe.ts
@@ -1,13 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Timestamp as BufTimestamp } from '@bufbuild/protobuf/wkt';
+import { Timestamp as BufTimestamp, TimestampSchema } from '@bufbuild/protobuf/wkt';
 import { Timestamp } from 'src/app/proto/generated/google/protobuf/timestamp_pb';
+import { MessageInitShape } from '@bufbuild/protobuf';
 
 @Pipe({
   name: 'timestampToDate',
   standalone: false,
 })
 export class TimestampToDatePipe implements PipeTransform {
-  transform(date: BufTimestamp | Timestamp.AsObject | undefined): Date | undefined {
+  transform(date: undefined): undefined;
+  transform(date: BufTimestamp | Timestamp.AsObject): Date;
+  transform(date: MessageInitShape<typeof TimestampSchema> | undefined): Date | undefined;
+  transform(date: BufTimestamp | Timestamp.AsObject | undefined): Date | undefined;
+  transform(
+    date: MessageInitShape<typeof TimestampSchema> | BufTimestamp | Timestamp.AsObject | undefined,
+  ): Date | undefined {
     if (date?.seconds !== undefined && date.nanos !== undefined) {
       return new Date(Number(date.seconds) * 1000 + date.nanos / 1000 / 1000);
     }
