877dedbd3b3fd23603e94a20a68c89ff63820c68
Elio Bischof <elio@zitadel.com>
2025-10-24 05:31:09 +0200

    fix: login

--- apps/api/nx-release-publish.sh
+++ apps/api/nx-release-publish.sh
@@ -10,5 +10,5 @@ for os in linux darwin windows; do
 done
 
 gh release upload ${ZITADEL_VERSION} ./.artifacts/pack/zitadel-*-*.tar.gz
-docker buildx build  --file ./apps/api/Dockerfile --push --platform linux/amd64,linux/arm64 -t ghcr.io/eliobischof/zitadel:${ZITADEL_VERSION}-debug --target builder .
-docker buildx build  --file ./apps/api/Dockerfile --push --platform linux/amd64,linux/arm64 -t ghcr.io/eliobischof/zitadel:${ZITADEL_VERSION} .
+docker buildx build  --file ./apps/api/Dockerfile --push --platform linux/amd64,linux/arm64 -t ghcr.io/eliobischof/api:${ZITADEL_VERSION}-debug --target builder .
+docker buildx build  --file ./apps/api/Dockerfile --push --platform linux/amd64,linux/arm64 -t ghcr.io/eliobischof/api:${ZITADEL_VERSION} .
