9ee6a5c5104ce64f88880ed9966c30fe4f0ae19d
Elio Bischof <elio@zitadel.com>
2025-10-24 03:49:30 +0200

    fix: docker

--- apps/api/nx-release-publish.sh
+++ apps/api/nx-release-publish.sh
@@ -10,5 +10,5 @@ for os in linux darwin windows; do
 done
 
 gh release upload ${ZITADEL_VERSION} ./.artifacts/pack/zitadel-*-*.tar.gz
-docker buildx build --push --platform linux/amd64,linux/arm64 -t ghcr.io/zitadel/zitadel:v${ZITADEL_VERSION}-debug --target builder ./apps/api
-docker buildx build --push --platform linux/amd64,linux/arm64 -t ghcr.io/zitadel/zitadel:v${ZITADEL_VERSION} ./apps/api
+docker buildx build  --file ./apps/api/Dockerfile --push --platform linux/amd64,linux/arm64 -t ghcr.io/zitadel/zitadel:v${ZITADEL_VERSION}-debug --target builder .
+docker buildx build  --file ./apps/api/Dockerfile --push --platform linux/amd64,linux/arm64 -t ghcr.io/zitadel/zitadel:v${ZITADEL_VERSION} .
