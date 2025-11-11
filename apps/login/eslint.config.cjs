dbcc602348dbcc39e57466ae2a2fbcf81a78c0f9
Max Peintner <peintnerm@gmail.com>
2025-11-11 13:45:21 +0100

    next lint codemod

--- apps/login/eslint.config.cjs
+++ apps/login/eslint.config.cjs
@@ -1,3 +1,4 @@
+const next = require("eslint-config-next");
 const tsParser = require("@typescript-eslint/parser");
 const typescriptEslint = require("@typescript-eslint/eslint-plugin");
 const js = require("@eslint/js");
@@ -10,45 +11,42 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-module.exports = [
-    ...compat.extends("next", "prettier"),
-    {
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: "latest",
-            sourceType: "module",
+module.exports = [...next, ...compat.extends("prettier"), {
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
 
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-                project: "./tsconfig.json",
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
             },
-        },
-
-        plugins: {
-            "@typescript-eslint": typescriptEslint,
-        },
-
-        rules: {
-            "@next/next/no-html-link-for-pages": "off",
-            "@next/next/no-img-element": "off",
-            "react/no-unescaped-entities": "off",
-            "no-unused-vars": "off",
-
-            "@typescript-eslint/no-unused-vars": ["error", {
-                argsIgnorePattern: "^_",
-                varsIgnorePattern: "^_",
-            }],
-
-            "no-undef": "off",
-
-            "no-restricted-imports": ["error", {
-                paths: [{
-                    name: "next/image",
-                    message: "Use of next/image is forbidden. Use regular <img> elements instead.",
-                }],
-            }],
+            project: "./tsconfig.json",
         },
     },
-];
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    rules: {
+        "@next/next/no-html-link-for-pages": "off",
+        "@next/next/no-img-element": "off",
+        "react/no-unescaped-entities": "off",
+        "no-unused-vars": "off",
+
+        "@typescript-eslint/no-unused-vars": ["error", {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+        }],
+
+        "no-undef": "off",
+
+        "no-restricted-imports": ["error", {
+            paths: [{
+                name: "next/image",
+                message: "Use of next/image is forbidden. Use regular <img> elements instead.",
+            }],
+        }],
+    },
+}];
