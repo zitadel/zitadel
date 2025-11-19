d38ff320958762a72e2f885ee64fc83b0ae986bb
ostempel <github_os@posteo.de>
2025-11-19 14:06:45 +0100

    debug current situation

--- apps/login/constants/csp.js
+++ apps/login/constants/csp.js
@@ -2,5 +2,13 @@ const ZITADEL_DOMAIN = process.env.ZITADEL_API_URL
   ? new URL(process.env.ZITADEL_API_URL).hostname
   : '*.zitadel.cloud';
 
+const ZITADEL_PROTOCOL = process.env.ZITADEL_API_URL
+  ? new URL(process.env.ZITADEL_API_URL).protocol.replace(':', '')
+  : 'https';
+
+console.log('Environment ZITADEL_API_URL:', process.env.ZITADEL_API_URL);
+console.log('Using ZITADEL_DOMAIN for CSP:', ZITADEL_DOMAIN);
+console.log('Using ZITADEL_PROTOCOL for CSP:', ZITADEL_PROTOCOL);
+
 export const DEFAULT_CSP =
   `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self'; child-src; style-src 'self' 'unsafe-inline'; font-src 'self'; object-src 'none'; img-src 'self' https://${ZITADEL_DOMAIN};`;
