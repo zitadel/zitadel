6bb91011745556ce6bfcd4f87d75dafcf422af18
Marco Ardizzone <marco@zitadel.com>
2025-11-24 22:08:46 +0100

    Add system config to `defaults.go`

--- backend/v3/domain/defaults.go
+++ backend/v3/domain/defaults.go
@@ -3,11 +3,13 @@ package domain
 import (
 	"github.com/zitadel/zitadel/backend/v3/storage/database"
 	"github.com/zitadel/zitadel/backend/v3/storage/eventstore"
+	"github.com/zitadel/zitadel/internal/config/systemdefaults"
 )
 
 var (
 	pool             database.Pool
 	legacyEventstore eventstore.LegacyEventstore
+	sysConfig        systemdefaults.SystemDefaults
 )
 
 func SetPool(p database.Pool) {
@@ -16,4 +18,8 @@ func SetPool(p database.Pool) {
 
 func SetLegacyEventstore(es eventstore.LegacyEventstore) {
 	legacyEventstore = es
+}
+
+func SetSystemConfig(cfg systemdefaults.SystemDefaults) {
+	sysConfig = cfg
 }
