4a9678984032180014ff601a46109e071e8117f6
Stefan Benz <46600784+stebenz@users.noreply.github.com>
2025-11-25 11:17:35 +0100

    fix: add missing definitions to relational domain settings

--- backend/v3/storage/database/repository/settings_test.go
+++ backend/v3/storage/database/repository/settings_test.go
@@ -2120,3 +2120,400 @@ func TestDeletePasswordExpirySettings(t *testing.T) {
 		})
 	}
 }
+
+func TestGetLockoutSettings(t *testing.T) {
+	tx, rollback := transactionForRollback(t)
+	defer rollback()
+
+	firstInstanceID := createInstance(t, tx)
+	secondInstanceID := createInstance(t, tx)
+	firstOrgID := createOrganization(t, tx, firstInstanceID)
+	secondOrgID := createOrganization(t, tx, secondInstanceID)
+	repo := repository.LockoutSettingsRepository()
+
+	settings := []*domain.LockoutSettings{
+		{
+			Settings: domain.Settings{
+				InstanceID:     firstInstanceID,
+				OrganizationID: nil,
+			},
+			LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+				MaxPasswordAttempts: gu.Ptr(uint64(3)),
+				MaxOTPAttempts:      gu.Ptr(uint64(3)),
+				ShowLockOutFailures: gu.Ptr(true),
+			},
+		},
+		{
+			Settings: domain.Settings{
+				InstanceID:     secondInstanceID,
+				OrganizationID: nil,
+			},
+			LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+				MaxPasswordAttempts: gu.Ptr(uint64(4)),
+				MaxOTPAttempts:      gu.Ptr(uint64(4)),
+				ShowLockOutFailures: gu.Ptr(true),
+			},
+		},
+		{
+			Settings: domain.Settings{
+				InstanceID:     firstInstanceID,
+				OrganizationID: gu.Ptr(firstOrgID),
+			},
+			LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+				MaxPasswordAttempts: gu.Ptr(uint64(5)),
+				MaxOTPAttempts:      gu.Ptr(uint64(5)),
+				ShowLockOutFailures: gu.Ptr(false),
+			},
+		},
+		{
+			Settings: domain.Settings{
+				InstanceID:     secondInstanceID,
+				OrganizationID: gu.Ptr(secondOrgID),
+			},
+			LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+				MaxPasswordAttempts: gu.Ptr(uint64(6)),
+				MaxOTPAttempts:      gu.Ptr(uint64(6)),
+				ShowLockOutFailures: gu.Ptr(true),
+			},
+		},
+	}
+
+	for _, setting := range settings {
+		err := repo.Set(t.Context(), tx, setting)
+		require.NoError(t, err)
+	}
+
+	tests := []struct {
+		name      string
+		condition database.Condition
+		want      *domain.LockoutSettings
+		wantErr   error
+	}{
+		{
+			name:      "incomplete condition",
+			condition: repo.IDCondition(settings[0].ID),
+			wantErr:   database.NewMissingConditionError(repo.IDColumn()),
+		},
+		{
+			name:      "not found",
+			condition: repo.PrimaryKeyCondition(firstInstanceID, "nix"),
+			wantErr:   database.NewNoRowFoundError(nil),
+		},
+		{
+			name:      "too many",
+			condition: repo.InstanceIDCondition(firstInstanceID),
+			wantErr:   database.NewMultipleRowsFoundError(nil),
+		},
+		{
+			name:      "ok, instance",
+			condition: repo.PrimaryKeyCondition(firstInstanceID, settings[0].ID),
+			want:      settings[0],
+		},
+		{
+			name:      "ok, organization",
+			condition: repo.PrimaryKeyCondition(secondInstanceID, settings[3].ID),
+			want:      settings[3],
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := repo.Get(t.Context(), tx, database.WithCondition(tt.condition))
+			require.ErrorIs(t, err, tt.wantErr)
+			assert.EqualExportedValues(t, tt.want, got)
+		})
+	}
+}
+
+func TestListLockoutSettings(t *testing.T) {
+	tx, rollback := transactionForRollback(t)
+	defer rollback()
+
+	firstInstanceID := createInstance(t, tx)
+	secondInstanceID := createInstance(t, tx)
+	firstOrgID := createOrganization(t, tx, firstInstanceID)
+	secondOrgID := createOrganization(t, tx, secondInstanceID)
+	repo := repository.LockoutSettingsRepository()
+
+	settings := []*domain.LockoutSettings{
+		{
+			Settings: domain.Settings{
+				InstanceID:     firstInstanceID,
+				OrganizationID: nil,
+			},
+			LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+				MaxPasswordAttempts: gu.Ptr(uint64(3)),
+				MaxOTPAttempts:      gu.Ptr(uint64(3)),
+				ShowLockOutFailures: gu.Ptr(true),
+			},
+		},
+		{
+			Settings: domain.Settings{
+				InstanceID:     secondInstanceID,
+				OrganizationID: nil,
+			},
+			LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+				MaxPasswordAttempts: gu.Ptr(uint64(4)),
+				MaxOTPAttempts:      gu.Ptr(uint64(4)),
+				ShowLockOutFailures: gu.Ptr(false),
+			},
+		},
+		{
+			Settings: domain.Settings{
+				InstanceID:     firstInstanceID,
+				OrganizationID: gu.Ptr(firstOrgID),
+			},
+			LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+				MaxPasswordAttempts: gu.Ptr(uint64(5)),
+				MaxOTPAttempts:      gu.Ptr(uint64(5)),
+				ShowLockOutFailures: gu.Ptr(true),
+			},
+		},
+		{
+			Settings: domain.Settings{
+				InstanceID:     secondInstanceID,
+				OrganizationID: gu.Ptr(secondOrgID),
+			},
+			LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+				MaxPasswordAttempts: gu.Ptr(uint64(6)),
+				MaxOTPAttempts:      gu.Ptr(uint64(6)),
+				ShowLockOutFailures: gu.Ptr(true),
+			},
+		},
+	}
+
+	for _, setting := range settings {
+		err := repo.Set(t.Context(), tx, setting)
+		require.NoError(t, err)
+	}
+
+	tests := []struct {
+		name      string
+		condition database.Condition
+		want      []*domain.LockoutSettings
+		wantErr   error
+	}{
+		{
+			name:      "incomplete condition",
+			condition: repo.OrganizationIDCondition(gu.Ptr(firstOrgID)),
+			wantErr:   database.NewMissingConditionError(repo.IDColumn()),
+		},
+		{
+			name:      "no results, ok",
+			condition: repo.PrimaryKeyCondition(firstInstanceID, "nix"),
+			want:      []*domain.LockoutSettings{},
+		},
+		{
+			name:      "all from instance",
+			condition: repo.InstanceIDCondition(firstInstanceID),
+			want:      []*domain.LockoutSettings{settings[2], settings[0]},
+		},
+		{
+			name: "only from instance",
+			condition: database.And(
+				repo.InstanceIDCondition(firstInstanceID),
+				repo.OrganizationIDCondition(nil),
+			),
+			want: []*domain.LockoutSettings{settings[0]},
+		},
+		{
+			name: "all from first org",
+			condition: database.And(
+				repo.InstanceIDCondition(firstInstanceID),
+				repo.OrganizationIDCondition(gu.Ptr(firstOrgID)),
+			),
+			want: []*domain.LockoutSettings{settings[2]},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := repo.List(t.Context(), tx,
+				database.WithCondition(tt.condition),
+				database.WithOrderByAscending(repo.InstanceIDColumn(), repo.OrganizationIDColumn()),
+			)
+			require.ErrorIs(t, err, tt.wantErr)
+			assert.EqualExportedValues(t, tt.want, got)
+		})
+	}
+}
+
+func TestSetLockoutSettings(t *testing.T) {
+	tx, rollback := transactionForRollback(t)
+	defer rollback()
+
+	instanceID := createInstance(t, tx)
+	orgID := createOrganization(t, tx, instanceID)
+	repo := repository.LockoutSettingsRepository()
+
+	existingSettings := &domain.LockoutSettings{
+		Settings: domain.Settings{
+			InstanceID:     instanceID,
+			OrganizationID: gu.Ptr(orgID),
+		},
+		LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+			MaxPasswordAttempts: gu.Ptr(uint64(3)),
+			MaxOTPAttempts:      gu.Ptr(uint64(3)),
+			ShowLockOutFailures: gu.Ptr(true),
+		},
+	}
+
+	err := repo.Set(t.Context(), tx, existingSettings)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name     string
+		settings *domain.LockoutSettings
+		wantErr  error
+	}{
+		{
+			name: "create instance",
+			settings: &domain.LockoutSettings{
+				Settings: domain.Settings{
+					InstanceID:     instanceID,
+					OrganizationID: nil,
+				},
+				LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+					MaxPasswordAttempts: gu.Ptr(uint64(4)),
+					MaxOTPAttempts:      gu.Ptr(uint64(4)),
+					ShowLockOutFailures: gu.Ptr(true),
+				},
+			},
+		},
+		{
+			name: "update organization",
+			settings: &domain.LockoutSettings{
+				Settings: domain.Settings{
+					InstanceID:     instanceID,
+					OrganizationID: gu.Ptr(orgID),
+				},
+				LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+					MaxPasswordAttempts: gu.Ptr(uint64(5)),
+					MaxOTPAttempts:      gu.Ptr(uint64(5)),
+					ShowLockOutFailures: gu.Ptr(true),
+				},
+			},
+		},
+		{
+			name: "non-existing instance",
+			settings: &domain.LockoutSettings{
+				Settings: domain.Settings{
+					InstanceID:     "foo",
+					OrganizationID: nil,
+				},
+				LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+					MaxPasswordAttempts: gu.Ptr(uint64(3)),
+					MaxOTPAttempts:      gu.Ptr(uint64(3)),
+					ShowLockOutFailures: gu.Ptr(true),
+				},
+			},
+			wantErr: new(database.ForeignKeyError),
+		},
+		{
+			name: "non-existing org",
+			settings: &domain.LockoutSettings{
+				Settings: domain.Settings{
+					InstanceID:     instanceID,
+					OrganizationID: gu.Ptr("foo"),
+				},
+				LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+					MaxPasswordAttempts: gu.Ptr(uint64(3)),
+					MaxOTPAttempts:      gu.Ptr(uint64(3)),
+					ShowLockOutFailures: gu.Ptr(true),
+				},
+			},
+			wantErr: new(database.ForeignKeyError),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			savepoint, rollback := savepointForRollback(t, tx)
+			defer rollback()
+			err := repo.Set(t.Context(), savepoint, tt.settings)
+			require.ErrorIs(t, err, tt.wantErr)
+		})
+	}
+}
+
+func TestDeleteLockoutSettings(t *testing.T) {
+	tx, rollback := transactionForRollback(t)
+	defer rollback()
+
+	instanceID := createInstance(t, tx)
+	orgID := createOrganization(t, tx, instanceID)
+	repo := repository.LockoutSettingsRepository()
+
+	existingInstanceSettings := &domain.LockoutSettings{
+		Settings: domain.Settings{
+			InstanceID:     instanceID,
+			OrganizationID: nil,
+		},
+		LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+			MaxPasswordAttempts: gu.Ptr(uint64(3)),
+			MaxOTPAttempts:      gu.Ptr(uint64(3)),
+			ShowLockOutFailures: gu.Ptr(true),
+		},
+	}
+	err := repo.Set(t.Context(), tx, existingInstanceSettings)
+	require.NoError(t, err)
+
+	existingOrganizationSettings := &domain.LockoutSettings{
+		Settings: domain.Settings{
+			InstanceID:     instanceID,
+			OrganizationID: gu.Ptr(orgID),
+		},
+		LockoutSettingsAttributes: domain.LockoutSettingsAttributes{
+			MaxPasswordAttempts: gu.Ptr(uint64(4)),
+			MaxOTPAttempts:      gu.Ptr(uint64(4)),
+			ShowLockOutFailures: gu.Ptr(true),
+		},
+	}
+	err = repo.Set(t.Context(), tx, existingOrganizationSettings)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name             string
+		condition        database.Condition
+		wantRowsAffected int64
+		wantErr          error
+	}{
+		{
+			name:             "incomplete condition",
+			condition:        repo.InstanceIDCondition(instanceID),
+			wantRowsAffected: 0,
+			wantErr:          database.NewMissingConditionError(repo.IDColumn()),
+		},
+		{
+			name:             "not found",
+			condition:        repo.PrimaryKeyCondition(instanceID, "foo"),
+			wantRowsAffected: 0,
+		},
+		{
+			name:             "delete instance",
+			condition:        repo.PrimaryKeyCondition(instanceID, existingInstanceSettings.ID),
+			wantRowsAffected: 1,
+		},
+		{
+			name:             "delete instance twice",
+			condition:        repo.PrimaryKeyCondition(instanceID, existingInstanceSettings.ID),
+			wantRowsAffected: 0,
+		},
+		{
+			name:             "delete organization",
+			condition:        repo.PrimaryKeyCondition(instanceID, existingOrganizationSettings.ID),
+			wantRowsAffected: 1,
+		},
+		{
+			name:             "delete organization twice",
+			condition:        repo.PrimaryKeyCondition(instanceID, existingOrganizationSettings.ID),
+			wantRowsAffected: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rowsAffected, err := repo.Delete(t.Context(), tx, tt.condition)
+			require.ErrorIs(t, err, tt.wantErr)
+			assert.Equal(t, tt.wantRowsAffected, rowsAffected)
+		})
+	}
+}
