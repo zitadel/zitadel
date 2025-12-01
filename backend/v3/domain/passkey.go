3c9e2f495e946232dca139d06bb96bacd7d83d3c
Marco Ardizzone <marco@zitadel.com>
2025-12-01 23:02:37 +0100

    Add webAuthNUser and UserVerificationFromDomain

--- backend/v3/domain/passkey.go
+++ backend/v3/domain/passkey.go
@@ -3,9 +3,11 @@ package domain
 import (
 	"context"
 
+	"github.com/go-webauthn/webauthn/protocol"
 	"github.com/go-webauthn/webauthn/webauthn"
 
 	"github.com/zitadel/zitadel/internal/api/http"
+	old_domain "github.com/zitadel/zitadel/internal/domain"
 )
 
 func PasskeysToCredentials(ctx context.Context, passkeys []*Passkey, rpID string) []webauthn.Credential {
@@ -30,3 +32,50 @@ func PasskeysToCredentials(ctx context.Context, passkeys []*Passkey, rpID string
 
 	return creds
 }
+
+func UserVerificationFromDomain(verification old_domain.UserVerificationRequirement) protocol.UserVerificationRequirement {
+	switch verification {
+	case old_domain.UserVerificationRequirementRequired:
+		return protocol.VerificationRequired
+	case old_domain.UserVerificationRequirementPreferred:
+		return protocol.VerificationPreferred
+	case old_domain.UserVerificationRequirementDiscouraged, old_domain.UserVerificationRequirementUnspecified:
+		fallthrough
+	default:
+		return protocol.VerificationDiscouraged
+	}
+}
+
+type webAuthNUser struct {
+	userID      string
+	username    string
+	displayName string
+	creds       []webauthn.Credential
+}
+
+// WebAuthnCredentials implements [webauthn.User].
+func (w *webAuthNUser) WebAuthnCredentials() []webauthn.Credential {
+	return w.creds
+}
+
+// WebAuthnDisplayName implements [webauthn.User].
+func (w *webAuthNUser) WebAuthnDisplayName() string {
+	return w.displayName
+}
+
+// WebAuthnID implements [webauthn.User].
+func (w *webAuthNUser) WebAuthnID() []byte {
+	return []byte(w.userID)
+}
+
+// WebAuthnIcon implements [webauthn.User].
+func (w *webAuthNUser) WebAuthnIcon() string {
+	return ""
+}
+
+// WebAuthnName implements [webauthn.User].
+func (w *webAuthNUser) WebAuthnName() string {
+	return w.username
+}
+
+var _ webauthn.User = (*webAuthNUser)(nil)
