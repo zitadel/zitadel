85a4208a2b6000ffba2832fa85ca2eac5cacd2f8
PhenixH <PhenixH@users.noreply.github.com>
2025-12-06 13:59:46 +0000

    fix: quick update react and Nextjs for #11140

--- apps/login/next.config.mjs
+++ apps/login/next.config.mjs
@@ -37,7 +37,8 @@ const nextConfig = {
   output: process.env.NEXT_OUTPUT_MODE || undefined,
   reactStrictMode: true,
   experimental: {
-    dynamicIO: true,
+    //dynamicIO: true,
+    useCache: true,
     // Add React 19 compatibility optimizations
     optimizePackageImports: ['@radix-ui/react-tooltip', '@heroicons/react'],
   },
