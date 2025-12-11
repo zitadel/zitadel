b8677c9a9f73cddcc158547259bf7e1a33029bd1
Florian Forster <florian@zitadel.com>
2025-12-11 07:51:41 +0000

    fixes

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  serverExternalPackages: ['shiki'],
 };
 
 export default withMDX(config);
