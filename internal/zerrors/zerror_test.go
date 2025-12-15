4579e0e1a644d1ba80c3d18ce57c990a8f2836c7
Tim Möhlmann <tim+github@zitadel.com>
2025-12-15 14:34:33 +0000

    Reapply "feat: set error caller"
    This reverts commit 24d02603699ed2e8cd968d799874337b451d4c59.

--- internal/zerrors/zerror_test.go
+++ internal/zerrors/zerror_test.go
@@ -1,27 +1,25 @@
-package zerrors_test
+package zerrors
 
 import (
 	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/zitadel/zitadel/internal/zerrors"
 )
 
 func TestErrorMethod(t *testing.T) {
-	err := zerrors.ThrowError(nil, "id", "msg")
+	err := ThrowError(nil, "id", "msg")
 	expected := "ID=id Message=msg"
 	assert.Equal(t, expected, err.Error())
 
-	err = zerrors.ThrowError(err, "subID", "subMsg")
+	err = ThrowError(err, "subID", "subMsg")
 	subExptected := "ID=subID Message=subMsg Parent=(ID=id Message=msg)"
 	assert.Equal(t, subExptected, err.Error())
 }
 
 func TestZitadelError_Is(t *testing.T) {
 	parent := errors.New("parent error")
-	target := zerrors.CreateZitadelError(zerrors.KindAborted, parent, "id", "message")
+	target := CreateZitadelError(KindAborted, parent, "id", "message")
 	tests := []struct {
 		name string // description of this test case
 		err  error
@@ -34,27 +32,27 @@ func TestZitadelError_Is(t *testing.T) {
 		},
 		{
 			name: "different kind",
-			err:  zerrors.CreateZitadelError(zerrors.KindNotFound, parent, "id", "message"),
+			err:  CreateZitadelError(KindNotFound, parent, "id", "message"),
 			want: false,
 		},
 		{
 			name: "different id",
-			err:  zerrors.CreateZitadelError(zerrors.KindAborted, parent, "otherID", "message"),
+			err:  CreateZitadelError(KindAborted, parent, "otherID", "message"),
 			want: false,
 		},
 		{
 			name: "different message",
-			err:  zerrors.CreateZitadelError(zerrors.KindAborted, parent, "id", "other message"),
+			err:  CreateZitadelError(KindAborted, parent, "id", "other message"),
 			want: false,
 		},
 		{
 			name: "different parent",
-			err:  zerrors.CreateZitadelError(zerrors.KindAborted, errors.New("other parent"), "id", "message"),
+			err:  CreateZitadelError(KindAborted, errors.New("other parent"), "id", "message"),
 			want: false,
 		},
 		{
 			name: "same error",
-			err:  zerrors.CreateZitadelError(zerrors.KindAborted, parent, "id", "message"),
+			err:  CreateZitadelError(KindAborted, parent, "id", "message"),
 			want: true,
 		},
 	}
@@ -62,6 +60,32 @@ func TestZitadelError_Is(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := errors.Is(tt.err, target)
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_newCaller(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		skip int
+		want caller
+	}{
+		{
+			name: "skip 0",
+			skip: 0,
+			want: caller{
+				Func: "github.com/zitadel/zitadel/internal/zerrors.Test_newCaller.func1",
+				File: "internal/zerrors/zerror_test.go",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := newCaller(tt.skip)
+			assert.Equal(t, tt.want.Func, got.Func)
+			assert.Contains(t, got.File, tt.want.File)
+			assert.NotZero(t, got.Line)
 		})
 	}
 }
