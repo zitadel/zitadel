be44e209880252e0df962c7be4ea5bd9979937bf
Tim Möhlmann <tim+github@zitadel.com>
2025-12-15 07:13:45 +0000

    fix unit test

--- internal/zerrors/zerror_test.go
+++ internal/zerrors/zerror_test.go
@@ -9,11 +9,11 @@ import (
 
 func TestErrorMethod(t *testing.T) {
 	err := ThrowError(nil, "id", "msg")
-	expected := "Kind=Unknown ID=id Message=msg"
+	expected := "ID=id Message=msg"
 	assert.Equal(t, expected, err.Error())
 
 	err = ThrowError(err, "subID", "subMsg")
-	subExptected := "Kind=Unknown ID=subID Message=subMsg Parent=(Kind=Unknown ID=id Message=msg)"
+	subExptected := "ID=subID Message=subMsg Parent=(ID=id Message=msg)"
 	assert.Equal(t, subExptected, err.Error())
 }
 
