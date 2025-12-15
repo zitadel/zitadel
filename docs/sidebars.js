5a647842c9b28a6429792cad358b091021a15e0f
Livio Spring <livio.a@gmail.com>
2025-12-15 15:21:43 +0100

    document payload verification

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -507,10 +507,6 @@ module.exports = {
             },
             {
               type: "doc",
-              id: "guides/integrate/actions/testing-request-signature",
-            },
-            {
-              type: "doc",
               id: "guides/integrate/actions/testing-response",
             },
             {
@@ -528,6 +524,10 @@ module.exports = {
             {
               type: "doc",
               id: "guides/integrate/actions/testing-event",
+            },
+            {
+              type: "doc",
+              id: "guides/integrate/actions/testing-request-signature",
             },
             {
               type: "doc",
