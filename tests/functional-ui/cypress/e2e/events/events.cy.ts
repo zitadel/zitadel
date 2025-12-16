3060cebf4ba1f5520faa495636bf68c99e6fbb12
conblem <mail@conblem.me>
2025-12-16 11:19:59 +0100

    force at a different place

--- tests/functional-ui/cypress/e2e/events/events.cy.ts
+++ tests/functional-ui/cypress/e2e/events/events.cy.ts
@@ -7,8 +7,8 @@ describe('events', () => {
     const eventTypeEnglish = 'Instance added';
     cy.visit('/instance?id=events');
     cy.get('[data-e2e="event-type-cell"]').should('have.length', 20);
-    cy.get('[data-e2e="open-filter-button"]').click({ force: true });
-    cy.get('[data-e2e="event-type-filter-checkbox"]').click();
+    cy.get('[data-e2e="open-filter-button"]').click();
+    cy.get('[data-e2e="event-type-filter-checkbox"]').click({ force: true });
     cy.get('mat-select[name="eventTypesList"]').click();
     cy.contains('mat-option', eventTypeEnglish).click();
     cy.get('body').type('{esc}');
