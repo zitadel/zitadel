578d950be2de7e9616944cb51d7f7005976cf953
Marco Ardizzone <marco@zitadel.com>
2026-01-05 15:09:18 +0100

    postgres: Create zitadel.identity_provider_intents table

---
+++ backend/v3/storage/database/dialect/postgres/migration/012_identity_provider_intents_table/up.sql
@@ -0,0 +1,41 @@
+CREATE TYPE zitadel.idp_intent_state AS ENUM (
+    'started',
+    'succeeded',
+    'failed',
+    'consumed'
+    );
+
+CREATE TABLE zitadel.identity_provider_intents
+(
+    instance_id             TEXT                        NOT NULL,
+    id                      TEXT                        NOT NULL CHECK ( id <> '' ),
+    state                   zitadel.idp_intent_state    NOT NULL DEFAULT 'started',
+    success_url             TEXT,
+    failure_url             TEXT,
+    created_at              TIMESTAMPTZ                 NOT NULL DEFAULT NOW(),
+    updated_at              TIMESTAMPTZ                 NOT NULL DEFAULT NOW(),
+    idp_id                  TEXT                        NOT NULL,
+    idp_arguments           JSONB,
+    idp_user                BYTEA, 
+    idp_user_id             TEXT,
+    idp_username            TEXT,
+    user_id                 TEXT,
+    idp_access_token        TEXT,
+    idp_entry_attributes    JSONB,
+    request_id              TEXT,
+    assertion               TEXT,
+    succeeded_at            TIMESTAMPTZ,
+    expires_at              TIMESTAMPTZ,
+    max_idp_intent_lifetime INTERVAL,
+
+    PRIMARY KEY (instance_id, id),
+--     FOREIGN KEY (instance_id, user_id) REFERENCES zitadel.users (instance_id, id) ON DELETE CASCADE,
+    FOREIGN KEY (instance_id, idp_id) REFERENCES zitadel.identity_providers (instance_id, id) ON DELETE CASCADE
+);
+
+CREATE TRIGGER trigger_set_updated_at
+    BEFORE UPDATE
+    ON zitadel.identity_provider_intents
+    FOR EACH ROW
+    WHEN (NEW.updated_at IS NULL)
+EXECUTE FUNCTION zitadel.set_updated_at();
