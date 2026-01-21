165f7af7b9759958cae206c15d3c4b01de5a0c4c
Wim Van Laer <wim+github@zitadel.com>
2026-01-21 10:12:51 +0100

    feat: add token source cache

--- internal/notification/channels/smtp/xoauth2_auth.go
+++ internal/notification/channels/smtp/xoauth2_auth.go
@@ -2,7 +2,9 @@ package smtp
 
 import (
 	"context"
+	"crypto/sha256"
 	"net/smtp"
+	"time"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
@@ -10,17 +12,24 @@ import (
 	"github.com/zitadel/zitadel/internal/zerrors"
 )
 
-func XOAuth2Auth(config XOAuth2AuthConfig, host string) smtp.Auth {
-	return &xoauth2Auth{
-		host:   host,
-		config: config,
-	}
+var tokenSourceCache = TokenSourceCache{}
+
+type TokenSourceCache map[string]tokenSourceCacheEntry
+
+type tokenSourceCacheEntry struct {
+	lastUsedAt  time.Time
+	tokenSource oauth2.TokenSource
 }
 
-type xoauth2Auth struct {
-	host        string
-	config      XOAuth2AuthConfig
-	tokenSource oauth2.TokenSource
+func (cache TokenSourceCache) Cleanup() {
+	maxAge := 7 * 24 * time.Hour
+	now := time.Now().UTC()
+	oldestAllowed := now.Add(maxAge)
+	for k, v := range tokenSourceCache {
+		if v.lastUsedAt.Before(oldestAllowed) {
+			delete(tokenSourceCache, k)
+		}
+	}
 }
 
 type XOAuth2AuthConfig struct {
@@ -35,22 +44,52 @@ type OAuth2ClientCredentials struct {
 	ClientSecret string
 }
 
+func (cfg XOAuth2AuthConfig) Hash() string {
+	sha := sha256.New()
+	sha.Write([]byte(cfg.User))
+	sha.Write([]byte(cfg.TokenEndpoint))
+	for _, s := range cfg.Scopes {
+		sha.Write([]byte(s))
+	}
+	if cfg.ClientCredentialsAuth != nil {
+		sha.Write([]byte(cfg.ClientCredentialsAuth.ClientId))
+		sha.Write([]byte(cfg.ClientCredentialsAuth.ClientSecret))
+	}
+	return string(sha.Sum(nil))
+}
+
+type xoauth2Auth struct {
+	host   string
+	config XOAuth2AuthConfig
+}
+
+func XOAuth2Auth(config XOAuth2AuthConfig, host string) smtp.Auth {
+	return &xoauth2Auth{
+		host:   host,
+		config: config,
+	}
+}
 func (a *xoauth2Auth) Start(server *smtp.ServerInfo) (string, []byte, error) {
 	if server.Name != a.host {
 		return "", nil, zerrors.ThrowInternal(nil, "SMTP-eRJLyi", "wrong host name")
 	}
 
-	if a.tokenSource == nil {
+	hash := a.config.Hash()
+	entry, ok := tokenSourceCache[hash]
+	if !ok {
 		config := &clientcredentials.Config{
 			ClientID:     a.config.ClientCredentialsAuth.ClientId,
 			ClientSecret: a.config.ClientCredentialsAuth.ClientSecret,
 			Scopes:       a.config.Scopes,
 			TokenURL:     a.config.TokenEndpoint,
 		}
-		a.tokenSource = config.TokenSource(context.Background())
+		entry.tokenSource = config.TokenSource(context.Background())
 	}
 
-	token, err := a.tokenSource.Token()
+	entry.lastUsedAt = time.Now().UTC()
+	tokenSourceCache[hash] = entry
+
+	token, err := entry.tokenSource.Token()
 	if err != nil {
 		return "", nil, zerrors.ThrowInternal(err, "SMTP-fjHcJu", "Failed to get token to connect with smtp server")
 	}
