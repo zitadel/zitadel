08273f05a128f97f528d280b444c0767b8240de6
Federico Coppede <fcoppede@gmail.com>
2026-01-27 20:32:27 -0300

    docs: refactor inkeep implementation (#11437)

--- apps/docs/app/providers.tsx
+++ apps/docs/app/providers.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { RootProvider } from 'fumadocs-ui/provider/next';
+import dynamic from "next/dynamic";
 import AuthRequestProvider from '@/utils/authrequest';
 import MixpanelProvider from '@/components/mixpanel-provider';
 
+const SearchDialog = dynamic(() => import("@/components/inkeep-search"));
+
 export function Providers({ children }: any) {
   return (
-    <RootProvider search={{ enabled: false }}>
+    <RootProvider search={{ enabled: true, SearchDialog }}>
       <MixpanelProvider>
         <AuthRequestProvider>{children}</AuthRequestProvider>
       </MixpanelProvider>
