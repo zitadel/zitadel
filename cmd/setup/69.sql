c1c9eb63a91f93d001e113bfdfc49216bad53d0d
adlerhurst <27845747+adlerhurst@users.noreply.github.com>
2026-02-02 11:06:10 +0100

    fix for both scenarios

--- cmd/setup/69.sql
+++ cmd/setup/69.sql
@@ -1,11 +1,19 @@
-ALTER TABLE IF EXISTS cache.objects
-    SET LOGGED;
+DO
+$do$
+BEGIN
+   IF EXISTS (SELECT 1 FROM pg_class WHERE oid = 'cache.objects'::REGCLASS::OID AND relpersistence = 'u') THEN
+        ALTER TABLE IF EXISTS cache.objects
+            SET LOGGED;
+   END IF;
+   IF EXISTS (SELECT 1 FROM pg_class WHERE oid = 'cache.string_keys'::REGCLASS::OID AND relpersistence = 'u') THEN
+        ALTER TABLE IF EXISTS cache.string_keys
+            SET LOGGED;
+   END IF;
 
-ALTER TABLE IF EXISTS cache.string_keys
-    SET LOGGED;
+   CREATE UNLOGGED TABLE IF NOT EXISTS cache.objects_default 
+        PARTITION OF cache.objects DEFAULT;
 
-CREATE UNLOGGED TABLE IF NOT EXISTS cache.objects_default 
-    PARTITION OF cache.objects DEFAULT;
-
-CREATE UNLOGGED TABLE IF NOT EXISTS cache.string_keys_default 
-    PARTITION OF cache.string_keys DEFAULT;
+    CREATE UNLOGGED TABLE IF NOT EXISTS cache.string_keys_default 
+        PARTITION OF cache.string_keys DEFAULT;
+END
+$do$
