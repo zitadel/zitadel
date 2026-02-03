e286269775bc923c310f7c4497d4d9bab1716557
Tim Möhlmann <tim+github@zitadel.com>
2026-02-03 11:36:03 +0000

    chore: move instrumentation middleware to protocol packages
    Other changes:
    
    - Removed setting of URI to context in metric middleware. There were ony setters and no getters. (Unused value)
    - Reuse a single statusWriter in the middleware package for middlewares that need to know the response status.

--- internal/api/http/middleware/middleware_test.go
+++ internal/api/http/middleware/middleware_test.go
@@ -1,11 +1,13 @@
 package middleware
 
 import (
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	"golang.org/x/text/language"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/zitadel/zitadel/internal/i18n"
 )
 
@@ -16,4 +18,38 @@ var (
 func TestMain(m *testing.M) {
 	i18n.SupportLanguages(SupportedLanguages...)
 	os.Exit(m.Run())
+}
+
+func Test_statusWriter(t *testing.T) {
+	tests := []struct {
+		name        string
+		writeStatus int
+		wantStatus  int
+	}{
+		{
+			name:       "default status is 200",
+			wantStatus: 200,
+		},
+		{
+			name:        "writes status code and body correctly",
+			writeStatus: 201,
+			wantStatus:  201,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			const body = "Hello, World!"
+			rec := httptest.NewRecorder()
+			sw := newStatusWriter(rec)
+			if tt.writeStatus != 0 {
+				sw.WriteHeader(tt.writeStatus)
+			}
+			sw.Write([]byte(body))
+			assert.Equal(t, tt.wantStatus, sw.Status())
+
+			resp := rec.Result()
+			assert.Equal(t, tt.wantStatus, resp.StatusCode)
+			assert.Equal(t, body, rec.Body.String())
+		})
+	}
 }
--- internal/api/http/middleware/trace_interceptor.go
+++ internal/api/http/middleware/trace_interceptor.go
@@ -3,8 +3,10 @@ package middleware
 import (
 	"net/http"
 
-	"github.com/zitadel/zitadel/backend/v3/instrumentation/tracing"
+	"github.com/zitadel/zitadel/backend/v3/instrumentation"
 	http_utils "github.com/zitadel/zitadel/internal/api/http"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
 )
 
 func DefaultTraceHandler(handler http.Handler) http.Handler {
@@ -13,6 +15,16 @@ func DefaultTraceHandler(handler http.Handler) http.Handler {
 
 func TraceHandler(ignoredPrefix ...string) func(http.Handler) http.Handler {
 	return func(handler http.Handler) http.Handler {
-		return tracing.NewHandler(handler, ignoredPrefix...)
+		return otelhttp.NewHandler(handler,
+			"zitadel",
+			otelhttp.WithFilter(instrumentation.RequestFilter(ignoredPrefix...)),
+			otelhttp.WithPublicEndpoint(),
+			otelhttp.WithSpanNameFormatter(spanNameFormatter),
+			otelhttp.WithMeterProvider(otel.GetMeterProvider()),
+		)
 	}
+}
+
+func spanNameFormatter(_ string, r *http.Request) string {
+	return r.URL.Path
 }
