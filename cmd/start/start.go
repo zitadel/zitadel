ee95d583868466cee8edb7b8b0c2bdbee97474db
Mridang Agarwalla <mridang@zitadel.com>
2026-02-03 23:36:29 +1100

    feat: enable cross-app distributed tracing for v2 APIs (#11453)
    (cherry picked from commit 6363ee0d0af6e0481859a143376ce255a06f6b22)

--- cmd/start/start.go
+++ cmd/start/start.go
@@ -474,6 +474,7 @@ func startAPIs(
 		limitingAccessInterceptor,
 		keys.Target,
 		translator,
+		config.Instrumentation.Trace.TrustRemoteSpans,
 	)
 	if err != nil {
 		return nil, fmt.Errorf("error creating api %w", err)
