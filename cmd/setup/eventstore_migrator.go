2ee51582a9710ffbfcce08959376ee20210a2e1c
Marco Ardizzone <marco@zitadel.com>
2026-02-03 20:02:26 +0100

    db: add missing migrations
      - Add schema creation `projections`
      - Add table creation `current_states`
      - Add table creation `failed_events`

--- cmd/setup/eventstore_migrator.go
+++ cmd/setup/eventstore_migrator.go
@@ -7,9 +7,20 @@ import (
 )
 
 const (
-	downFieldsTable = `DROP TABLE IF EXISTS eventstore.fields;`
+	downFieldsTable        = `DROP TABLE IF EXISTS eventstore.fields;`
+	downFailedEventsTable  = `DROP TABLE IF EXISTS projections.failed_events2;`
+	downCurrentStatesTable = `DROP TABLE IF EXISTS projections.current_states;`
+)
+
+var (
+	//go:embed 15/01_new_failed_events.sql
+	upFailedEventsTable string
+	//go:embed 15/05_current_states.sql
+	upCurrentStatesTable string
 )
 
 func init() {
 	migration.RegisterSQLMigrationNoSequence(addFieldTable, downFieldsTable)
+	migration.RegisterSQLMigrationNoSequence(upFailedEventsTable, downFailedEventsTable)
+	migration.RegisterSQLMigrationNoSequence(upCurrentStatesTable+addOffsetField, downCurrentStatesTable)
 }
