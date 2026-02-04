c664fb9e647c45af4109f5a6254e999beb96c0b0
Florian Forster <florian@zitadel.com>
2026-02-04 13:29:03 -0800

    fix docs issue

--- apps/docs/scripts/generate-proto-docs.mjs
+++ apps/docs/scripts/generate-proto-docs.mjs
@@ -147,7 +147,7 @@ async function run() {
 
         import('child_process').then(({ spawn }) => {
             const args = [
-              '@bufbuild/buf', 'generate',
+              'buf', 'generate',
               bufInput,
               '--template', templatePath,
               '--output', outputPath
@@ -161,10 +161,7 @@ async function run() {
             const child = spawn('npx', args, {
               cwd: taskTempDir, 
               stdio: 'inherit',
-              env: {
-                  ...process.env,
-                  BUF_TOKEN: '851d3e2519b882d9e6da46eadec5e3ccc6a966dae0ce5e78dd285d9f912e35fd'
-              }
+              env: process.env
             });
 
             child.on('close', (code) => {
