96315ebf833759ea428bae3a2cd6d56dc1c5593d
Tim Möhlmann <tim+github@zitadel.com>
2026-02-10 19:37:28 +0000

    fix: linter error

--- internal/api/grpc/server/middleware/request_id_interceptor.go
+++ internal/api/grpc/server/middleware/request_id_interceptor.go
@@ -3,11 +3,13 @@ package middleware
 import (
 	"context"
 
-	"github.com/zitadel/zitadel/backend/v3/instrumentation"
-	"github.com/zitadel/zitadel/internal/api/call"
-	http_util "github.com/zitadel/zitadel/internal/api/http"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
+
+	"github.com/zitadel/zitadel/backend/v3/instrumentation"
+	"github.com/zitadel/zitadel/backend/v3/instrumentation/logging"
+	"github.com/zitadel/zitadel/internal/api/call"
+	http_util "github.com/zitadel/zitadel/internal/api/http"
 )
 
 // RequestIDHandler is a gRPC interceptor that sets a request ID in the context
@@ -16,10 +18,10 @@ import (
 func RequestIDHandler() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		reqCtx, id := instrumentation.NewRequestID(ctx, call.FromContext(ctx))
-		resp, err := handler(reqCtx, req)
-
 		md := metadata.New(map[string]string{http_util.XRequestID: id.String()})
-		grpc.SetHeader(reqCtx, md)
-		return resp, err
+		err := grpc.SetHeader(reqCtx, md)
+		logging.OnError(ctx, err).Warn("cannot set request ID to response header")
+
+		return handler(reqCtx, req)
 	}
 }
