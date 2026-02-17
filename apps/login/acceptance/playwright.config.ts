a479a4d42fc545d2ed271c5a6aba19064b4c4e2e
Robert Lawson <robert@zitadel.com>
2026-02-17 16:44:32 +0100

    remove local config disabling minification

--- apps/login/acceptance/playwright.config.ts
+++ apps/login/acceptance/playwright.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig, devices } from "@playwright/test";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 dotenv.config({ path: path.resolve(__dirname, "../login/.env.test.local") });
 
