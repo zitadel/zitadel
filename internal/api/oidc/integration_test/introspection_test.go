44715588a28795de7257e15aae2e809688e1c156
Wim Van Laer <wim+github@zitadel.com>
2026-02-17 12:36:14 +0100

    test

--- internal/api/oidc/integration_test/introspection_test.go
+++ internal/api/oidc/integration_test/introspection_test.go
@@ -6,16 +6,15 @@ import (
 	"context"
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/zitadel/oidc/v3/pkg/client/rs"
 	"github.com/zitadel/oidc/v3/pkg/oidc"
+	"github.com/zitadel/zitadel/internal/integration"
 	"google.golang.org/grpc/metadata"
 
 	oidc_api "github.com/zitadel/zitadel/internal/api/oidc"
-	"github.com/zitadel/zitadel/internal/integration"
 	"github.com/zitadel/zitadel/pkg/grpc/authn"
 	"github.com/zitadel/zitadel/pkg/grpc/management"
 	oidc_pb "github.com/zitadel/zitadel/pkg/grpc/oidc/v2"
@@ -23,19 +22,56 @@ import (
 
 // This is a regression test for https://github.com/zitadel/zitadel/issues/11177
 func TestIntrospection_RoleAddedAfterLogin(t *testing.T) {
-	createIntrospectionApi := func(projectId string) rs.ResourceServer {
+	createIntrospectionApi := func(tt *testing.T, projectId string) rs.ResourceServer {
 		api, err := Instance.CreateAPIClientJWT(CTX, projectId)
-		require.NoError(t, err)
+		require.NoError(tt, err)
 		keyResp, err := Instance.Client.Mgmt.AddAppKey(CTX, &management.AddAppKeyRequest{
 			ProjectId:      projectId,
 			AppId:          api.GetAppId(),
 			Type:           authn.KeyType_KEY_TYPE_JSON,
 			ExpirationDate: nil,
 		})
-		require.NoError(t, err)
+		require.NoError(tt, err)
 		resourceServer, err := Instance.CreateResourceServerJWTProfile(CTX, keyResp.GetKeyDetails())
-		require.NoError(t, err)
+		require.NoError(tt, err)
 		return resourceServer
+	}
+
+	assignRole := func(tt *testing.T, projectId string, grantId string, role string) {
+		grant, err := Instance.Client.Mgmt.AddUserGrant(CTX, &management.AddUserGrantRequest{
+			UserId:         User.GetUserId(),
+			ProjectId:      projectId,
+			ProjectGrantId: grantId,
+			RoleKeys:       []string{role},
+		})
+		require.NoError(tt, err)
+		g, err := Instance.Client.Mgmt.GetUserGrantByID(CTX, &management.GetUserGrantByIDRequest{
+			UserId:  User.GetUserId(),
+			GrantId: grant.GetUserGrantId(),
+		})
+		require.NoError(tt, err)
+		require.NotNil(tt, g)
+	}
+
+	login := func(tt *testing.T, clientId string, scopes []string) string {
+		authRequestID := createAuthRequest(tt, Instance, clientId, redirectURI, scopes...)
+		sessionID, sessionToken, startTime, changeTime := Instance.CreateVerifiedWebAuthNSession(t, CTXLOGIN, User.GetUserId())
+		linkResp, err := Instance.Client.OIDCv2.CreateCallback(CTXLOGIN, &oidc_pb.CreateCallbackRequest{
+			AuthRequestId: authRequestID,
+			CallbackKind: &oidc_pb.CreateCallbackRequest_Session{
+				Session: &oidc_pb.Session{
+					SessionId:    sessionID,
+					SessionToken: sessionToken,
+				},
+			},
+		})
+		require.NoError(tt, err)
+		code := assertCodeResponse(tt, linkResp.GetCallbackUrl())
+		tokens, err := exchangeTokens(tt, Instance, clientId, code, redirectURI)
+		require.NoError(tt, err)
+		assertTokens(tt, tokens, false)
+		assertIDTokenClaims(tt, tokens.IDTokenClaims, User.GetUserId(), armPasskey, startTime, changeTime, sessionID)
+		return tokens.AccessToken
 	}
 
 	type TestCase struct {
@@ -56,7 +92,7 @@ func TestIntrospection_RoleAddedAfterLogin(t *testing.T) {
 				Name:                  "single project",
 				RoleGrantedProjectId:  projectId,
 				RoleGrantingProjectId: projectId,
-				IntrospectionServer:   createIntrospectionApi(projectId),
+				IntrospectionServer:   createIntrospectionApi(t, projectId),
 				Role:                  role,
 			}
 		}(),
@@ -71,7 +107,7 @@ func TestIntrospection_RoleAddedAfterLogin(t *testing.T) {
 				Name:                  "project2 owns role, project 1 is granted",
 				RoleGrantedProjectId:  project1Id,
 				RoleGrantingProjectId: project2Id,
-				IntrospectionServer:   createIntrospectionApi(project1Id),
+				IntrospectionServer:   createIntrospectionApi(t, project1Id),
 				Role:                  role,
 			}
 		}(),
@@ -95,7 +131,7 @@ func TestIntrospection_RoleAddedAfterLogin(t *testing.T) {
 				Name:                  "project3 (other org) owns role, project 1 is granted",
 				RoleGrantedProjectId:  project1Id,
 				RoleGrantingProjectId: project3Id,
-				IntrospectionServer:   createIntrospectionApi(project1Id),
+				IntrospectionServer:   createIntrospectionApi(t, project1Id),
 				Role:                  role,
 				ProjectGrantId:        projectGrant.GetGrantId(),
 			}
@@ -106,37 +142,19 @@ func TestIntrospection_RoleAddedAfterLogin(t *testing.T) {
 		t.Run(testCase.Name, func(tt *testing.T) {
 			// create app to log in with
 			app, err := Instance.CreateOIDCNativeClient(CTX, redirectURI, logoutRedirectURI, testCase.RoleGrantedProjectId, false)
-			require.NoError(t, err)
+			require.NoError(tt, err)
 
-			// login with user
 			scopes := []string{
 				oidc.ScopeOpenID,
 				oidc.ScopeProfile,
 				"urn:zitadel:iam:org:project:roles",
-				oidc_api.ScopeProjectsRoles,
 				oidc_api.ScopeProjectRolePrefix + testCase.Role,
 				fmt.Sprintf("urn:zitadel:iam:org:project:id:%s:aud", testCase.RoleGrantingProjectId),
 			}
-			authRequestID := createAuthRequest(t, Instance, app.GetClientId(), redirectURI, scopes...)
-			sessionID, sessionToken, startTime, changeTime := Instance.CreateVerifiedWebAuthNSession(t, CTXLOGIN, User.GetUserId())
-			linkResp, err := Instance.Client.OIDCv2.CreateCallback(CTXLOGIN, &oidc_pb.CreateCallbackRequest{
-				AuthRequestId: authRequestID,
-				CallbackKind: &oidc_pb.CreateCallbackRequest_Session{
-					Session: &oidc_pb.Session{
-						SessionId:    sessionID,
-						SessionToken: sessionToken,
-					},
-				},
-			})
-			require.NoError(tt, err)
-			code := assertCodeResponse(t, linkResp.GetCallbackUrl())
-			tokens, err := exchangeTokens(t, Instance, app.GetClientId(), code, redirectURI)
-			require.NoError(tt, err)
-			assertTokens(tt, tokens, false)
-			assertIDTokenClaims(tt, tokens.IDTokenClaims, User.GetUserId(), armPasskey, startTime, changeTime, sessionID)
+			accessToken := login(t, app.GetClientId(), scopes)
 
 			// introspect token and verify role is not present
-			introspectionBefore, err := rs.Introspect[*oidc.IntrospectionResponse](context.Background(), testCase.IntrospectionServer, tokens.AccessToken)
+			introspectionBefore, err := rs.Introspect[*oidc.IntrospectionResponse](context.Background(), testCase.IntrospectionServer, accessToken)
 			require.NoError(tt, err)
 			assert.True(tt, introspectionBefore.Active)
 			projectRolesClaimName := fmt.Sprintf(oidc_api.ClaimProjectRolesFormat, testCase.RoleGrantingProjectId)
@@ -146,23 +164,16 @@ func TestIntrospection_RoleAddedAfterLogin(t *testing.T) {
 				}
 			}
 
-			// assign role to user
-			_, err = Instance.Client.Mgmt.AddUserGrant(CTX, &management.AddUserGrantRequest{
-				UserId:         User.GetUserId(),
-				ProjectId:      testCase.RoleGrantingProjectId,
-				ProjectGrantId: testCase.ProjectGrantId,
-				RoleKeys:       []string{testCase.Role},
-			})
-			require.NoError(t, err)
-			// wait a bit to ensure the grant is processed
-			time.Sleep(1 * time.Second)
+			assignRole(t, testCase.RoleGrantingProjectId, testCase.ProjectGrantId, testCase.Role)
 
 			// introspect token and verify role is present
-			introspectionAfter, err := rs.Introspect[*oidc.IntrospectionResponse](context.Background(), testCase.IntrospectionServer, tokens.AccessToken)
-			require.NoError(t, err)
+			introspectionAfter, err := rs.Introspect[*oidc.IntrospectionResponse](context.Background(), testCase.IntrospectionServer, accessToken)
+			require.NoError(tt, err)
 			require.True(tt, introspectionAfter.Active)
+
 			roles, ok := introspectionAfter.Claims[projectRolesClaimName]
 			require.True(tt, ok, "project roles claim should be present after granting")
+
 			rolesMapAfter, ok := roles.(map[string]interface{})
 			require.True(tt, ok, "project roles claim should be a map")
 
