6003c10a8f5960c14b5a8e4a2f2092a04af558d8
Marco Ardizzone <marco@zitadel.com>
2026-02-18 16:50:45 +0000

    crypto: nil checks on input values

--- internal/crypto/crypto.go
+++ internal/crypto/crypto.go
@@ -4,6 +4,7 @@ import (
 	"database/sql/driver"
 	"encoding/base64"
 	"encoding/json"
+	"slices"
 
 	"github.com/zitadel/zitadel/internal/zerrors"
 )
@@ -41,7 +42,7 @@ func (c *CryptoValue) Value() (driver.Value, error) {
 	return json.Marshal(c)
 }
 
-func (c *CryptoValue) Scan(src interface{}) error {
+func (c *CryptoValue) Scan(src any) error {
 	if b, ok := src.([]byte); ok {
 		return json.Unmarshal(b, c)
 	}
@@ -58,6 +59,9 @@ func Crypt(value []byte, alg EncryptionAlgorithm) (*CryptoValue, error) {
 }
 
 func Encrypt(value []byte, alg EncryptionAlgorithm) (*CryptoValue, error) {
+	if alg == nil {
+		return nil, zerrors.ThrowInvalidArgument(nil, "CRYPT-03ltQd", "input encryption algorithm cannot be nil")
+	}
 	encrypted, err := alg.Encrypt(value)
 	if err != nil {
 		return nil, zerrors.ThrowInternal(err, "CRYPT-qCD0JB", "error encrypting value")
@@ -106,13 +110,17 @@ func DecryptString(value *CryptoValue, alg EncryptionAlgorithm) (string, error) 
 }
 
 func checkEncryptionAlgorithm(value *CryptoValue, alg EncryptionAlgorithm) error {
+	if value == nil {
+		return zerrors.ThrowInvalidArgument(nil, "CRYPT-mNsQwe", "input value cannot be nil")
+	}
+	if alg == nil {
+		return zerrors.ThrowInvalidArgument(nil, "CRYPT-paiRey", "input encryption algorithm cannot be nil")
+	}
 	if value.Algorithm != alg.Algorithm() {
 		return zerrors.ThrowInvalidArgument(nil, "CRYPT-Nx7XlT", "value was encrypted with a different key")
 	}
-	for _, id := range alg.DecryptionKeyIDs() {
-		if id == value.KeyID {
-			return nil
-		}
+	if slices.Contains(alg.DecryptionKeyIDs(), value.KeyID) {
+		return nil
 	}
 	return zerrors.ThrowInvalidArgument(nil, "CRYPT-Kq12vn", "value was encrypted with a different key")
 }
@@ -124,6 +132,9 @@ func CheckToken(alg EncryptionAlgorithm, token string, content string) error {
 	data, err := base64.RawURLEncoding.DecodeString(token)
 	if err != nil {
 		return zerrors.ThrowPermissionDenied(err, "CRYPTO-Swg31", "Errors.Intent.InvalidToken")
+	}
+	if alg == nil {
+		return zerrors.ThrowInvalidArgument(nil, "CRYPT-edCJsp", "input encryption algorithm cannot be nil")
 	}
 	decryptedToken, err := alg.DecryptString(data, alg.EncryptionKeyID())
 	if err != nil {
