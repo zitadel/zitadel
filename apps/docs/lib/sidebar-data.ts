5d8232c8fd00948e8f90d32d3339864a916a1f7a
rgdev <rgdev@users.noreply.github.com>
2026-02-19 12:42:21 +0100

    docs(guide): usage of SATOSA for IDP Federation

--- apps/docs/lib/sidebar-data.ts
+++ apps/docs/lib/sidebar-data.ts
@@ -660,6 +660,7 @@ export const guidesSidebar: readonly SidebarItem[] = [
             items: [
               "guides/integrate/identity-providers/migrate",
               "guides/integrate/identity-providers/additional-information",
+              "guides/integrate/identity-providers/satosa-saml",
             ]
           }
         ],
