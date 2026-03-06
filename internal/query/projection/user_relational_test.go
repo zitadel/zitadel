d9c59c5269ad2d03047ef67b8f361a19f7fbf783
Gayathri Vijayan <gayathri+github@zitadel.com>
2026-03-06 10:15:37 +0100

    update tests

--- internal/query/projection/user_relational_test.go
+++ internal/query/projection/user_relational_test.go
@@ -1,6 +1,7 @@
 package projection
 
 import (
+	"context"
 	"testing"
 	"time"
 
@@ -10,6 +11,7 @@ import (
 
 	"github.com/zitadel/zitadel/backend/v3/domain"
 	"github.com/zitadel/zitadel/backend/v3/storage/database"
+	"github.com/zitadel/zitadel/backend/v3/storage/database/dialect/sql"
 	"github.com/zitadel/zitadel/backend/v3/storage/database/repository"
 	"github.com/zitadel/zitadel/internal/repository/user"
 )
@@ -23,16 +25,19 @@ func TestUserRelationalProjection_Reducers(t *testing.T) {
 
 	ctx := t.Context()
 
+	// create instance
 	instanceReop := repository.InstanceRepository()
 	instanceID := gofakeit.UUID()
+	orgID := gofakeit.UUID()
 	err := instanceReop.Create(ctx, tx, &domain.Instance{
-		ID:   instanceID,
-		Name: "test-instance",
+		ID:           instanceID,
+		Name:         "test-instance",
+		DefaultOrgID: orgID,
 	})
 	require.NoError(t, err)
 
+	// create org
 	orgRepo := repository.OrganizationRepository()
-	orgID := gofakeit.UUID()
 	err = orgRepo.Create(ctx, tx, &domain.Organization{
 		InstanceID: instanceID,
 		ID:         orgID,
@@ -42,18 +47,160 @@ func TestUserRelationalProjection_Reducers(t *testing.T) {
 	require.NoError(t, err)
 
 	userRepo := repository.UserRepository()
-	userID := gofakeit.UUID()
-	err = userRepo.Create(ctx, tx, &domain.User{
+
+	// TODO: add tests for other reducers in user_relational.go
+
+	// recovery code reducers
+	t.Run("reduce user.human.mfa.recoverycode.added event", func(t *testing.T) {
+		// create user
+		existingUserID := gofakeit.UUID()
+		existingUserAgg := createUser(t, ctx, tx, userRepo, instanceID, orgID, existingUserID)
+
+		// set `user.human.mfa.recoverycode.added` event
+		recoveryCodes := []string{"code1", "code2", "code3"}
+		recoveryCodesAddedEvent := user.NewHumanRecoveryCodesAddedEvent(
+			ctx,
+			&existingUserAgg.Aggregate,
+			recoveryCodes,
+			nil)
+
+		// reduce the event
+		eventReduced := callReduce(t, ctx, rawTx, handler, recoveryCodesAddedEvent)
+		require.True(t, eventReduced)
+
+		// assert that the recovery codes are stored
+		gotUser, err := userRepo.Get(ctx, tx, database.WithCondition(
+			database.And(
+				userRepo.IDCondition(existingUserID),
+				userRepo.InstanceIDCondition(instanceID),
+			),
+		))
+		require.NoError(t, err)
+		assert.ElementsMatch(t, recoveryCodes, gotUser.Human.RecoveryCodes.Codes)
+		assert.Zero(t, gotUser.Human.RecoveryCodes.LastSuccessfullyCheckedAt)
+		assert.Zero(t, gotUser.Human.RecoveryCodes.FailedAttempts)
+	})
+
+	t.Run("reduce user.human.mfa.recoverycode.removed event", func(t *testing.T) {
+		// create user
+		existingUserID := gofakeit.UUID()
+		existingUserAgg := createUser(t, ctx, tx, userRepo, instanceID, orgID, existingUserID)
+
+		// add recovery codes to the user
+		_, err = userRepo.Update(ctx, tx,
+			userRepo.PrimaryKeyCondition(instanceID, existingUserID),
+			userRepo.Human().AddRecoveryCodes([]string{"code1", "code2", "code3"}))
+		require.NoError(t, err)
+
+		// set `user.human.mfa.recoverycode.removed` event
+		recoveryCodesRemovedEvent := user.NewHumanRecoveryCodeRemovedEvent(
+			ctx,
+			&existingUserAgg.Aggregate,
+			nil)
+
+		// reduce the event
+		eventReduced := callReduce(t, ctx, rawTx, handler, recoveryCodesRemovedEvent)
+		require.True(t, eventReduced)
+
+		// assert that the recovery codes are removed
+		gotUser, err := userRepo.Get(ctx, tx, database.WithCondition(
+			database.And(
+				userRepo.IDCondition(existingUserID),
+				userRepo.InstanceIDCondition(instanceID),
+			),
+		))
+		require.NoError(t, err)
+		assert.Empty(t, gotUser.Human.RecoveryCodes.Codes)
+		assert.Zero(t, gotUser.Human.RecoveryCodes.LastSuccessfullyCheckedAt)
+		assert.Zero(t, gotUser.Human.RecoveryCodes.FailedAttempts)
+	})
+
+	t.Run("reduce user.human.mfa.recoverycode.check.succeeded event", func(t *testing.T) {
+		// create user
+		existingUserID := gofakeit.UUID()
+		existingUserAgg := createUser(t, ctx, tx, userRepo, instanceID, orgID, existingUserID)
+
+		// add recovery codes to the user
+		_, err = userRepo.Update(ctx, tx,
+			userRepo.PrimaryKeyCondition(instanceID, existingUserID),
+			userRepo.Human().AddRecoveryCodes([]string{"code1", "code2", "code3"}))
+		require.NoError(t, err)
+
+		// set `user.human.mfa.recoverycode.check.succeeded` event
+		recoveryCodeCheckSucceededEvent := user.NewHumanRecoveryCodeCheckSucceededEvent(
+			ctx,
+			&existingUserAgg.Aggregate,
+			"code1",
+			nil)
+
+		// reduce the event
+		eventReduced := callReduce(t, ctx, rawTx, handler, recoveryCodeCheckSucceededEvent)
+		require.True(t, eventReduced)
+
+		// assert that the recovery code is removed and the last_successfully_checked_at timestamp is set
+		gotUser, err := userRepo.Get(ctx, tx, database.WithCondition(
+			database.And(
+				userRepo.IDCondition(existingUserID),
+				userRepo.InstanceIDCondition(instanceID),
+			),
+		))
+		require.NoError(t, err)
+		assert.ElementsMatch(t, []string{"code2", "code3"}, gotUser.Human.RecoveryCodes.Codes)
+		assert.NotZero(t, gotUser.Human.RecoveryCodes.LastSuccessfullyCheckedAt)
+		assert.Zero(t, gotUser.Human.RecoveryCodes.FailedAttempts)
+	})
+
+	t.Run("reduce user.human.mfa.recoverycode.check.failed event", func(t *testing.T) {
+		// create user
+		existingUserID := gofakeit.UUID()
+		existingUserAgg := createUser(t, ctx, tx, userRepo, instanceID, orgID, existingUserID)
+
+		// add recovery codes to the user
+		_, err = userRepo.Update(ctx, tx,
+			userRepo.PrimaryKeyCondition(instanceID, existingUserID),
+			userRepo.Human().AddRecoveryCodes([]string{"code1", "code2", "code3"}))
+		require.NoError(t, err)
+
+		// set `user.human.mfa.recoverycode.check.failed` event
+		recoveryCodeCheckFailedEvent := user.NewHumanRecoveryCodeCheckFailedEvent(
+			ctx,
+			&existingUserAgg.Aggregate,
+			nil,
+		)
+
+		// reduce the event
+		eventReduced := callReduce(t, ctx, rawTx, handler, recoveryCodeCheckFailedEvent)
+		require.True(t, eventReduced)
+
+		// assert that the recovery code failed_attempts is incremented
+		gotUser, err := userRepo.Get(ctx, tx, database.WithCondition(
+			database.And(
+				userRepo.IDCondition(existingUserID),
+				userRepo.InstanceIDCondition(instanceID),
+			),
+		))
+		require.NoError(t, err)
+		assert.ElementsMatch(t, []string{"code1", "code2", "code3"}, gotUser.Human.RecoveryCodes.Codes)
+		assert.Zero(t, gotUser.Human.RecoveryCodes.LastSuccessfullyCheckedAt)
+		assert.Equal(t, uint8(1), gotUser.Human.RecoveryCodes.FailedAttempts)
+	})
+}
+
+func createUser(t *testing.T, ctx context.Context, tx *sql.Transaction, userRepo domain.UserRepository, instanceID, orgID, userID string) *user.Aggregate {
+	userAgg := user.NewAggregate(userID, orgID)
+	userAgg.InstanceID = instanceID
+
+	err := userRepo.Create(ctx, tx, &domain.User{
 		InstanceID:     instanceID,
 		OrganizationID: orgID,
 		ID:             userID,
-		Username:       "testuser@example.com",
+		Username:       gofakeit.Username(),
 		State:          domain.UserStateActive,
 		Human: &domain.HumanUser{
-			FirstName: "Test",
-			LastName:  "User",
+			FirstName: gofakeit.FirstName(),
+			LastName:  gofakeit.LastName(),
 			Email: domain.HumanEmail{
-				Address:    "testuser@example.com",
+				Address:    gofakeit.Email(),
 				VerifiedAt: time.Now(),
 			},
 		},
@@ -66,124 +213,5 @@ func TestUserRelationalProjection_Reducers(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	// TODO: add tests for other reducers in user_relational.go
-
-	// recovery code reducers
-	t.Run("reduce user.human.mfa.recoverycode.added event", func(t *testing.T) {
-		// add recovery codes
-		recoveryCodes := []string{"code1", "code2", "code3"}
-		recoveryCodesAddedEvent := user.NewHumanRecoveryCodesAddedEvent(
-			ctx,
-			&user.NewAggregate(userID, orgID).Aggregate,
-			recoveryCodes,
-			nil)
-
-		// reduce the event
-		res := callReduce(t, ctx, rawTx, handler, recoveryCodesAddedEvent)
-		require.True(t, res)
-
-		// assert that the recovery codes are stored
-		gotUser, err := userRepo.Get(ctx, tx, database.WithCondition(
-			database.And(
-				userRepo.IDCondition(userID),
-				userRepo.InstanceIDCondition(instanceID),
-			),
-		))
-		require.NoError(t, err)
-		assert.ElementsMatch(t, recoveryCodes, gotUser.Human.RecoveryCodes.Codes)
-		assert.Zero(t, gotUser.Human.RecoveryCodes.LastSuccessfullyCheckedAt)
-		assert.Zero(t, gotUser.Human.RecoveryCodes.FailedAttempts)
-	})
-
-	t.Run("reduce user.human.mfa.recoverycode.removed event", func(t *testing.T) {
-		// add recovery codes
-		_, err = userRepo.Update(ctx, tx,
-			userRepo.PrimaryKeyCondition(instanceID, userID),
-			userRepo.Human().AddRecoveryCodes([]string{"code1", "code2", "code3"}))
-		require.NoError(t, err)
-
-		// set remove recovery codes event
-		recoveryCodesRemovedEvent := user.NewHumanRecoveryCodeRemovedEvent(
-			ctx,
-			&user.NewAggregate(userID, orgID).Aggregate,
-			nil)
-
-		// reduce the event
-		res := callReduce(t, ctx, rawTx, handler, recoveryCodesRemovedEvent)
-		require.True(t, res)
-
-		// assert that the recovery codes are removed
-		gotUser, err := userRepo.Get(ctx, tx, database.WithCondition(
-			database.And(
-				userRepo.IDCondition(userID),
-				userRepo.InstanceIDCondition(instanceID),
-			),
-		))
-		require.NoError(t, err)
-		assert.Empty(t, gotUser.Human.RecoveryCodes.Codes)
-		assert.Zero(t, gotUser.Human.RecoveryCodes.LastSuccessfullyCheckedAt)
-		assert.Zero(t, gotUser.Human.RecoveryCodes.FailedAttempts)
-	})
-
-	t.Run("reduce user.human.mfa.recoverycode.check.succeeded event", func(t *testing.T) {
-		// add recovery codes
-		_, err = userRepo.Update(ctx, tx,
-			userRepo.PrimaryKeyCondition(instanceID, userID),
-			userRepo.Human().AddRecoveryCodes([]string{"code1", "code2", "code3"}))
-		require.NoError(t, err)
-
-		// set recovery code check succeeded event
-		recoveryCodeCheckSucceededEvent := user.NewHumanRecoveryCodeCheckSucceededEvent(
-			ctx,
-			&user.NewAggregate(userID, orgID).Aggregate,
-			"code1",
-			nil)
-
-		// reduce the event
-		res := callReduce(t, ctx, rawTx, handler, recoveryCodeCheckSucceededEvent)
-		require.True(t, res)
-
-		// assert that the recovery code is removed and the last_successfully_checked_at timestamp is set
-		gotUser, err := userRepo.Get(ctx, tx, database.WithCondition(
-			database.And(
-				userRepo.IDCondition(userID),
-				userRepo.InstanceIDCondition(instanceID),
-			),
-		))
-		require.NoError(t, err)
-		assert.ElementsMatch(t, []string{"code2", "code3"}, gotUser.Human.RecoveryCodes.Codes)
-		assert.NotZero(t, gotUser.Human.RecoveryCodes.LastSuccessfullyCheckedAt)
-		assert.Zero(t, gotUser.Human.RecoveryCodes.FailedAttempts)
-	})
-
-	t.Run("reduce user.human.mfa.recoverycode.check.failed event", func(t *testing.T) {
-		// add recovery codes
-		_, err = userRepo.Update(ctx, tx,
-			userRepo.PrimaryKeyCondition(instanceID, userID),
-			userRepo.Human().AddRecoveryCodes([]string{"code1", "code2", "code3"}))
-		require.NoError(t, err)
-
-		// set recovery code failed event
-		recoveryCodeCheckFailedEvent := user.NewHumanRecoveryCodeCheckFailedEvent(
-			ctx,
-			&user.NewAggregate(userID, orgID).Aggregate,
-			nil,
-		)
-
-		// reduce the event
-		res := callReduce(t, ctx, rawTx, handler, recoveryCodeCheckFailedEvent)
-		require.True(t, res)
-
-		// assert that the recovery code failed_attempts is incremented
-		gotUser, err := userRepo.Get(ctx, tx, database.WithCondition(
-			database.And(
-				userRepo.IDCondition(userID),
-				userRepo.InstanceIDCondition(instanceID),
-			),
-		))
-		require.NoError(t, err)
-		assert.ElementsMatch(t, []string{"code1", "code2", "code3"}, gotUser.Human.RecoveryCodes.Codes)
-		assert.Zero(t, gotUser.Human.RecoveryCodes.LastSuccessfullyCheckedAt)
-		assert.Equal(t, 1, gotUser.Human.RecoveryCodes.FailedAttempts)
-	})
+	return userAgg
 }
