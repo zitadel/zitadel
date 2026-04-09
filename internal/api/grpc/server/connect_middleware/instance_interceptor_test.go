44f80578839c24d0ff3b5f38858ef9816584c10e
Mridang Agarwalla <mridang@zitadel.com>
2026-04-09 16:34:54 +0700

    test: add byDomain coverage for connect instance interceptor

--- internal/api/grpc/server/connect_middleware/instance_interceptor_test.go
+++ internal/api/grpc/server/connect_middleware/instance_interceptor_test.go
@@ -12,6 +12,7 @@ import (
 	http_util "github.com/zitadel/zitadel/internal/api/http"
 	"github.com/zitadel/zitadel/internal/i18n"
 	"github.com/zitadel/zitadel/internal/zerrors"
+	object_v3 "github.com/zitadel/zitadel/pkg/grpc/object/v3alpha"
 )
 
 func Test_setInstance_errorCodes(t *testing.T) {
@@ -57,7 +58,36 @@ func Test_setInstance_errorCodes(t *testing.T) {
 				t.Errorf("got code %v, want %v", got, tc.wantCode)
 			}
 		})
+
+		t.Run("byDomain/"+tc.name, func(t *testing.T) {
+			req := &mockReq[mockInstanceMsg]{
+				Request: connect.Request[mockInstanceMsg]{
+					Msg: &mockInstanceMsg{instance: object_v3.Instance{
+						Property: &object_v3.Instance_Domain{Domain: "host"},
+					}},
+				},
+			}
+			_, err := setInstance(context.Background(), req, nil, verifier, "", translator)
+			if err == nil {
+				t.Fatal("expected error, got nil")
+			}
+			var connectErr *connect.Error
+			if !errors.As(err, &connectErr) {
+				t.Fatalf("expected *connect.Error, got %T", err)
+			}
+			if got := connectErr.Code(); got != tc.wantCode {
+				t.Errorf("got code %v, want %v", got, tc.wantCode)
+			}
+		})
 	}
+}
+
+type mockInstanceMsg struct {
+	instance object_v3.Instance
+}
+
+func (m *mockInstanceMsg) GetInstance() *object_v3.Instance {
+	return &m.instance
 }
 
 type mockInstanceVerifier struct {
