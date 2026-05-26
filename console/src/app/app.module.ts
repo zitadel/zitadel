669e4adde89b10c0ada7efbac4e37f813a653d3e
TLT <tungluong2809@gmail.com>
2026-05-26 10:35:21 +0700

    feat(i18n): add Vietnamese language support

--- console/src/app/app.module.ts
+++ console/src/app/app.module.ts
@@ -22,6 +22,7 @@ import localeRo from '@angular/common/locales/ro';
 import localeTr from '@angular/common/locales/tr';
 import localeUk from '@angular/common/locales/uk';
 import localeAr from '@angular/common/locales/ar';
+import localeVi from '@angular/common/locales/vi';
 import { NgModule, inject, provideAppInitializer } from '@angular/core';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -125,6 +126,8 @@ registerLocaleData(localeUk);
 i18nIsoCountries.registerLocale(require('i18n-iso-countries/langs/uk.json'));
 registerLocaleData(localeAr);
 i18nIsoCountries.registerLocale(require('i18n-iso-countries/langs/ar.json'));
+registerLocaleData(localeVi);
+i18nIsoCountries.registerLocale(require('i18n-iso-countries/langs/vi.json'));
 
 export class WebpackTranslateLoader implements TranslateLoader {
   getTranslation(lang: string): Observable<any> {
