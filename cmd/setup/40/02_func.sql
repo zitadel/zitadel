e0f161d7c008cbc89dd1d0db431ab3be35348174
adlerhurst <27845747+adlerhurst@users.noreply.github.com>
2026-06-10 08:13:24 +0200

    fix commands_to_events

--- cmd/setup/40/02_func.sql
+++ cmd/setup/40/02_func.sql
@@ -1,34 +1,14 @@
-CREATE OR REPLACE FUNCTION eventstore.latest_aggregate_state(
-    instance_id TEXT
-    , aggregate_type TEXT
-    , aggregate_id TEXT
-    
-    , sequence OUT BIGINT
-    , owner OUT TEXT
-)
-    LANGUAGE 'plpgsql'
-    STABLE PARALLEL SAFE
-AS $$
-    BEGIN
-        SELECT
-            COALESCE(e.sequence, 0) AS sequence
-            , e.owner
-        INTO
-            sequence
-            , owner
-        FROM
-            eventstore.events2 e
-        WHERE
-            e.instance_id = $1
-            AND e.aggregate_type = $2
-            AND e.aggregate_id = $3
-        ORDER BY 
-            e.sequence DESC
-        LIMIT 1;
-
-        RETURN;
-    END;
-$$;
+DO $$ BEGIN
+    CREATE TYPE eventstore.latest_command AS (
+        instance_id TEXT
+        , aggregate_type TEXT
+        , aggregate_id TEXT
+        , owner TEXT
+        , sequence BIGINT
+    );
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
 
 CREATE OR REPLACE FUNCTION eventstore.commands_to_events(commands eventstore.command2[]) 
     RETURNS SETOF eventstore.events2 
@@ -37,32 +17,72 @@ CREATE OR REPLACE FUNCTION eventstore.commands_to_events(commands eventstore.com
     ROWS 10
 AS $$
 DECLARE
-    latest_events eventstore.events2[];
+    _latest_events eventstore.latest_command[];
+    command eventstore.command2;
+    last_owner TEXT;
+    i INTEGER;
 BEGIN
-    SELECT array_agg(e) INTO latest_events
+
+    SELECT array_agg(res) INTO _latest_events
     FROM (
-        SELECT DISTINCT ON (e.instance_id, e.aggregate_type, e.aggregate_id) e
+        SELECT
+            ROW(
+                c.instance_id,
+                c.aggregate_type,
+                c.aggregate_id,
+                COALESCE(e.owner, c.owner),
+                COALESCE(e.sequence, 0)
+            )::eventstore.latest_command AS res
         FROM (
+            SELECT DISTINCT ON (c.instance_id, c.aggregate_type, c.aggregate_id)
+                c.instance_id,
+                c.aggregate_type,
+                c.aggregate_id,
+                c.owner,
+                c.ordinality
+            FROM (
+                SELECT *
+                FROM UNNEST(commands) WITH ORDINALITY AS c
+            ) AS c
+            ORDER BY
+                c.instance_id,
+                c.aggregate_type,
+                c.aggregate_id,
+                c.ordinality
+        ) AS c
+        LEFT JOIN LATERAL (
             SELECT
-                c.instance_id
-                , c.aggregate_type
-                , c.aggregate_id
-            FROM UNNEST(commands) AS c
-            GROUP BY
-                c.instance_id
-                , c.aggregate_type
-                , c.aggregate_id
-        ) AS cmds
-        LEFT JOIN eventstore.events2 AS e
-            ON cmds.instance_id = e.instance_id
-            AND cmds.aggregate_type = e.aggregate_type
-            AND cmds.aggregate_id = e.aggregate_id
-        ORDER BY
-            e.instance_id
-            , e.aggregate_type
-            , e.aggregate_id
-            , e.sequence DESC NULLS LAST
-    );
+                e.owner,
+                e.sequence
+            FROM eventstore.events2 AS e
+            WHERE e.instance_id = c.instance_id
+                AND e.aggregate_type = c.aggregate_type
+                AND e.aggregate_id = c.aggregate_id
+            ORDER BY e.sequence DESC
+            LIMIT 1
+        ) AS e ON TRUE
+    ) AS latest;
+
+    FOR command IN SELECT * FROM UNNEST(commands) LOOP
+        IF NOT command.enforce_owner THEN
+            SELECT owner INTO command.owner
+            FROM UNNEST(_latest_events) AS e
+            WHERE e.instance_id = command.instance_id
+            AND e.aggregate_type = command.aggregate_type
+            AND e.aggregate_id = command.aggregate_id;
+        ELSE
+            FOR i IN 1..array_length(_latest_events, 1) LOOP
+                IF _latest_events[i].instance_id = command.instance_id
+                    AND _latest_events[i].aggregate_type = command.aggregate_type
+                    AND _latest_events[i].aggregate_id = command.aggregate_id
+                THEN
+                    _latest_events[i].owner := command.owner;
+                    EXIT;
+                END IF;
+            END LOOP;
+        END IF;
+
+    END LOOP;
 
 
     RETURN QUERY SELECT
@@ -85,7 +105,7 @@ BEGIN
         SELECT c.*, CAST(ROW_NUMBER() OVER () AS INTEGER) AS in_tx_order
         FROM UNNEST(commands) AS c
     ) AS c
-    LEFT JOIN unnest(latest_events) AS e
+    LEFT JOIN UNNEST(_latest_events) AS e
         ON c.instance_id = e.instance_id
         AND c.aggregate_type = e.aggregate_type
         AND c.aggregate_id = e.aggregate_id
