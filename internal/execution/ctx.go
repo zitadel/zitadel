8693f111fca362e99b307ed1db1e9056a76374bd
Temuri Takalandze <temuri.takalandze@scilife.io>
2026-09-25 17:49:39 +0400

    refactor: remove unused HandlerContext function

--- internal/execution/ctx.go
+++ internal/execution/ctx.go
@@ -9,11 +9,6 @@ import (
 
 const ExecutionUserID = "EXECUTION"
 
-func HandlerContext(parent context.Context, event *eventstore.Aggregate) context.Context {
-	ctx := authz.WithInstanceID(parent, event.InstanceID)
-	return authz.SetCtxData(ctx, authz.CtxData{UserID: ExecutionUserID, OrgID: event.ResourceOwner})
-}
-
 func ContextWithExecuter(ctx context.Context, aggregate *eventstore.Aggregate) context.Context {
 	ctx = authz.WithInstanceID(ctx, aggregate.InstanceID)
 	return authz.SetCtxData(ctx, authz.CtxData{UserID: ExecutionUserID, OrgID: aggregate.ResourceOwner})
